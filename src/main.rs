8594a90a87763b9fce30ccbd09f43cc9f54af8d7
Luke Steensen <luke.steensen@gmail.com>
2018-10-02 17:29:48 -0500

    add logging to stubbed main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate router;
+
+#[macro_use]
+extern crate log;
+extern crate fern;
+
 fn main() {
-    println!("Hello, world!");
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
+    info!("Hello, world!");
 }
