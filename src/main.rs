4ba4eb906f2d5f526b4b05a045d546af7d04accc
Luke Steensen <luke.steensen@gmail.com>
2018-10-08 15:31:48 -0500

    optimize console input and output a bit

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ extern crate router;
 #[macro_use]
 extern crate log;
 extern crate fern;
+extern crate memchr;
 
+use memchr::memchr;
 use router::transport::{Consumer, Coordinator};
-use std::io::BufRead;
+use std::io::{BufRead, Write};
 use std::sync::{atomic::AtomicBool, Arc};
 use std::thread;
 
@@ -23,6 +25,8 @@ fn main() {
     let mut log = coordinator.create_log(&dir).expect("failed to create log");
     let mut consumer = Consumer::new(&dir).expect("failed to build consumer");
 
+    let mut writer = ::std::io::stdout();
+
     let finished = Arc::new(AtomicBool::new(true));
     let finished2 = finished.clone();
     let handle = thread::spawn(move || {
@@ -32,19 +36,31 @@ fn main() {
                 break;
             } else {
                 for record in batch {
-                    println!(
-                        "{}",
-                        std::str::from_utf8(&record).expect("invalid utf8 message")
-                    );
+                    writer.write(&record).unwrap();
                 }
             }
         }
     });
 
-    let stdin = ::std::io::stdin();
-    for line in stdin.lock().lines().filter_map(Result::ok) {
-        log.append(&[line.as_bytes()])
-            .expect("failed to append input");
+    let reader = ::std::io::stdin();
+    let mut buffer = reader.lock();
+    loop {
+        let consumed = match buffer.fill_buf() {
+            Ok(bytes) => {
+                if bytes.len() == 0 { break }
+                if let Some(newline) = memchr(b'\n', bytes) {
+                    let pos = newline + 1;
+                    log.append(&[&bytes[0..pos]])
+                        .expect("failed to append input");
+                    pos
+                } else {
+                    // if we couldn't find a newline, just throw away the buffer
+                    bytes.len()
+                }
+            },
+            _ => break,
+        };
+        buffer.consume(consumed);
     }
     finished.store(false, std::sync::atomic::Ordering::Relaxed);
 
