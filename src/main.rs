a76622e19d2db6bf799c6466bee9b9d9ea71696f
Michael Fairley <michaelfairley@gmail.com>
2018-12-03 13:37:51 -0600

    Switch main over to topology builder (for comcast case)

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,21 @@ extern crate tokio;
 use futures::{Future, Sink, Stream};
 use log::{error, info};
 use prometheus::{opts, register_counter, Encoder, TextEncoder, __register_counter};
-use regex::RegexSet;
 use router::{
     sinks::{self, SinkFactory},
     sources::{self, SourceFactory},
+    topology::TopologyBuilder,
     transforms,
 };
 use std::net::SocketAddr;
-use stream_cancel::Tripwire;
+use stream_cancel::{Trigger, Tripwire};
 use tokio::fs::File;
 
 fn main() {
     router::setup_logger();
 
     let in_addr: SocketAddr = "127.0.0.1:1235".parse().unwrap();
-    let (trigger, tripwire) = Tripwire::new();
+    let (harness_trigger, tripwire) = Tripwire::new();
 
     info!("starting runtime");
     let mut rt = tokio::runtime::Runtime::new().unwrap();
@@ -31,9 +31,12 @@ fn main() {
     // TODO: actually switch between configurations in a reasonable way (separate binaries?)
     if false {
         // ES Writer topology
+
         rt.spawn(es_writer(in_addr, tripwire));
     } else {
         // Comcast topology + input and harness
+        let out_addr: SocketAddr = "127.0.0.1:9999".parse().unwrap();
+        let (server, server_trigger) = comcast(in_addr, out_addr);
 
         // build up a thing that will pipe some sample data at our server
         let input = File::open("sample.log")
@@ -43,29 +46,30 @@ fn main() {
         let sender = sinks::splunk::SplunkSink::build(in_addr)
             .map(|sink| sink.sink_map_err(|e| error!("sender error: {:?}", e)))
             .map_err(|e| error!("error creating sender: {:?}", e));
-        let sender_task = sender.and_then(|sink| input.forward(sink)).map(|_| {
-            info!("done sending test input!");
-            drop(trigger);
-        });
+        let counter = register_counter!("sender_lines", "Lines sent from harness").unwrap();
+        let sender_task = sender
+            .and_then(|sink| input.inspect(move |_| counter.inc()).forward(sink))
+            .map(|_| {
+                info!("done sending test input!");
+                drop(server_trigger);
+                drop(harness_trigger);
+            });
 
         // build up a thing that accept the data our server forwards upstream
-        let out_addr: SocketAddr = "127.0.0.1:9999".parse().unwrap();
         let counter =
             register_counter!("receiver_lines", "Lines received at forwarding destination")
                 .unwrap();
-        let receiver = sources::splunk::SplunkSource::build(out_addr, tripwire.clone()).fold(
-            (),
-            move |(), _line| {
+        let receiver =
+            sources::splunk::SplunkSource::build(out_addr, tripwire).fold((), move |(), _line| {
                 counter.inc();
                 Ok(())
-            },
-        );
+            });
 
         info!("starting receiver");
         rt.spawn(receiver);
 
         info!("starting server");
-        rt.spawn(comcast(in_addr, out_addr, tripwire));
+        rt.spawn(server);
         // wait for the server to come up before trying to send to it
         while let Err(_) = std::net::TcpStream::connect(in_addr) {}
 
@@ -97,26 +101,19 @@ fn es_writer(in_addr: SocketAddr, exit: Tripwire) -> impl Future<Item = (), Erro
 fn comcast(
     in_addr: SocketAddr,
     out_addr: SocketAddr,
-    exit: Tripwire,
-) -> impl Future<Item = (), Error = ()> {
-    let counter = register_counter!("input_lines", "Lines ingested").unwrap();
+) -> (impl Future<Item = (), Error = ()>, Trigger) {
+    let mut topology = TopologyBuilder::new();
 
-    let splunk_in =
-        sources::splunk::SplunkSource::build(in_addr, exit).inspect(move |_| counter.inc());
+    topology.add_source::<sources::splunk::SplunkSource>(in_addr, "in");
+    topology.add_sink::<sinks::splunk::SplunkSink>(out_addr, "out");
+    let sampler_config = transforms::SamplerConfig {
+        rate: 10,
+        pass_list: vec!["(very )?important".to_string()],
+    };
+    topology.add_transform::<transforms::Sampler>(sampler_config, "sampler");
 
-    let counter = register_counter!("output_lines", "Lines forwarded upstream").unwrap();
+    topology.connect("in", "sampler");
+    topology.connect("sampler", "out");
 
-    let exceptions = RegexSet::new(&["(very )?important"]).unwrap();
-    let sampler = transforms::Sampler::new(10, exceptions);
-    let sampled = splunk_in
-        .filter(move |record| sampler.filter(record))
-        .inspect(move |_| counter.inc());
-
-    let splunk_out = sinks::splunk::SplunkSink::build(out_addr)
-        .map(|sink| sink.sink_map_err(|e| error!("tcp sink error: {:?}", e)))
-        .map_err(|e| error!("error creating tcp sink: {:?}", e));
-
-    splunk_out
-        .and_then(|sink| sampled.forward(sink))
-        .map(|_| info!("done!"))
+    topology.build()
 }
