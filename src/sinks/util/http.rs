69352ac2ab84d0997e283c7c778d655650e1749e
Michael Fairley <michaelfairley@gmail.com>
2018-12-17 15:51:40 -0600

    Add basic integration test for HttpSink

--- src/sinks/util/http.rs
+++ src/sinks/util/http.rs
@@ -8,7 +8,6 @@ use log::error;
 use tokio::executor::DefaultExecutor;
 
 
-// TODO: test this against a fake server
 pub struct HttpSink {
     client: Client<HttpsConnector<HttpConnector>, Body>,
     in_flight_request: Option<ResponseFuture>,
@@ -63,5 +62,63 @@ impl Sink for HttpSink {
                 return Ok(Async::Ready(()));
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hyper::{Request, Body, Response, Server, Uri};
+    use super::HttpSink;
+    use futures::{Sink,Future,Stream};
+    use hyper::service::service_fn;
+
+    #[test]
+    fn it_makes_http_requests() {
+        let addr = crate::test_util::next_addr();
+        let uri = format!("http://{}:{}/", addr.ip(), addr.port()).parse::<Uri>().unwrap();
+
+
+        let request = Request::post(uri).body(Body::from("hello")).unwrap();
+        let sink = HttpSink::new();
+
+        let req = sink.send(request);
+
+
+        let (tx, rx) = futures::sync::mpsc::channel(10);
+
+        let new_service = move || {
+            let tx = tx.clone();
+
+            service_fn(move |req: Request<Body>| -> Box<dyn Future<Item = Response<Body>, Error = String> + Send> {
+                let tx = tx.clone();
+
+                Box::new(req.into_body().map_err(|_| "".to_string()).fold::<_, _, Result<_, String>>(vec![], |mut acc, chunk| {
+                    acc.extend_from_slice(&chunk);
+                    Ok(acc)
+                }).and_then(move |v| {
+                    let string = String::from_utf8(v).map_err(|_| "Wasn't UTF-8".to_string());
+                    tx.send(string).map_err(|_| "Send error".to_string())
+                }).and_then(|_| {
+                    futures::future::ok(Response::new(Body::from("")))
+                }))
+            })
+        };
+
+        let server = Server::bind(&addr)
+            .serve(new_service)
+            .map_err(|e| eprintln!("server error: {}", e));
+
+
+        let mut rt = tokio::runtime::Runtime::new().unwrap();
+
+        rt.spawn(server);
+
+        rt.block_on(req).unwrap();
+
+        rt.shutdown_now();
+
+
+        let (body, _rest) = rx.into_future().wait().unwrap();
+        assert_eq!(body.unwrap().unwrap(), "hello");
     }
 }
