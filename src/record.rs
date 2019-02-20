ba8be7dc04ddf1da1b5dd63e6039b27bdb050b40
Michael Fairley <michaelfairley@gmail.com>
2019-02-20 09:50:32 -0600

    Use protobuf for serializating on-disk buffered records

--- src/record.rs
+++ src/record.rs
@@ -1,6 +1,12 @@
 use std::collections::HashMap;
 use string_cache::DefaultAtom as Atom;
 
+pub mod proto {
+    use prost_derive::Message;
+
+    include!(concat!(env!("OUT_DIR"), "/record.proto.rs"));
+}
+
 #[derive(PartialEq, Debug, Clone)]
 pub struct Record {
     pub line: String,
@@ -16,6 +22,55 @@ impl Record {
             timestamp: chrono::Utc::now(),
             custom: HashMap::new(),
             host: None,
+        }
+    }
+}
+
+impl From<proto::Record> for Record {
+    fn from(proto: proto::Record) -> Self {
+        use chrono::offset::TimeZone;
+        let timestamp = proto.timestamp.unwrap();
+        let timestamp = chrono::Utc.timestamp(timestamp.seconds, timestamp.nanos as _);
+
+        let host = if proto.host.is_empty() {
+            None
+        } else {
+            Some(proto.host)
+        };
+
+        let custom = proto
+            .custom
+            .into_iter()
+            .map(|(k, v)| (Atom::from(k), v))
+            .collect::<HashMap<_, _>>();
+
+        Self {
+            line: proto.line,
+            timestamp,
+            custom,
+            host,
+        }
+    }
+}
+
+impl From<Record> for proto::Record {
+    fn from(record: Record) -> Self {
+        let timestamp = ::prost_types::Timestamp {
+            seconds: record.timestamp.timestamp(),
+            nanos: record.timestamp.timestamp_subsec_nanos() as _,
+        };
+
+        let custom = record
+            .custom
+            .into_iter()
+            .map(|(k, v)| (k.to_string(), v))
+            .collect::<HashMap<_, _>>();
+
+        Self {
+            line: record.line,
+            timestamp: Some(timestamp),
+            custom,
+            host: record.host.unwrap_or_else(|| "".to_string()),
         }
     }
 }
