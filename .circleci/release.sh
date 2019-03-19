32e3b673a3f6386425c7cd884feb31f8104a92e8
Lucio Franco <luciofranco14@gmail.com>
2019-03-19 10:38:30 -0400

    Install aws cli and clean up docker builds

---
+++ .circleci/release.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Usage
+#
+# Release with cross via the default system target
+# ./release.sh
+#
+# Release with the specified target
+# TARGET="<my target>" ./release.sh
+#
+# Release with extra cargo flags
+# EXTRA_ARGS="--no-default-features" ./release.sh
+
+set -eou pipefail
+
+APP_NAME=vector
+DIST_DIR="$(pwd)/dist"
+VERSION="$(git describe --abbrev=0 --tags)"
+
+S3_PREFIX="s3://packages.timber.io/vector"
+TAR_NAME="$APP_NAME-$VERSION-$TARGET.tar.gz"
+
+DEFAULT_TARGET="$(rustup target list | grep '(default)' | awk '{print $1}')"
+TARGET=${1:-}
+ARGS=${EXTRA_ARGS:-}
+
+if [ -z "$TARGET" ]; then
+    echo "TARGET is not passed using $DEFAULT_TARGET"
+    TARGET="$DEFAULT_TARGET"
+fi
+
+function build_release() {
+    cross build --target $TARGET --release --frozen $ARGS
+}
+
+function build_tar() {
+    
+
+
+    mkdir -p $DIST_DIR
+    cp "target/$TARGET/release/$APP_NAME" "$DIST_DIR"
+    cd $DIST_DIR
+    tar cvpf $TAR_NAME $APP_NAME
+    rm $APP_NAME
+    cd ..
+}
+
+function upload_s3() {
+    S3_PATH="$S3_PREFIX/$TAR_NAME"
+    aws s3 cp "$DIST_DIR/$TAR_NAME" "$S3_PATH"
+}
+
+build_release
+build_tar
+upload_s3
--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,8 @@
 
 set -eou pipefail
 
-# docker build -t timberio/vector-builder-x86_64-unknown-linux-gnu:latest x86_64-unknown-linux-gnu
+docker build -t timberio/vector-builder-x86_64-unknown-linux-gnu:latest x86_64-unknown-linux-gnu
 docker build -t timberio/vector-builder-x86_64-unknown-freebsd:latest x86_64-unknown-freebsd
+
+docker push timberio/vector-builder-x86_64-unknown-linux-gnu:latest
+docker push timberio/vector-builder-x86_64-unknown-freebsd:latest
