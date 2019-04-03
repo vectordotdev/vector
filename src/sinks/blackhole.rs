e0dab255435036951d9defe66cd6e64dffa00b0e
Lucio Franco <luciofranco14@gmail.com>
2019-04-03 16:49:22 -0400

    Add logging

--- src/sinks/blackhole.rs
+++ src/sinks/blackhole.rs
@@ -3,15 +3,21 @@ use futures::{future, AsyncSink, Future, Poll, Sink, StartSend};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
-pub struct BlackholeSink;
+pub struct BlackholeSink {
+    total_records: usize,
+    total_raw_bytes: usize,
+    config: BlackholeConfig,
+}
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct BlackholeConfig;
+pub struct BlackholeConfig {
+    print_amount: usize,
+}
 
 #[typetag::serde(name = "blackhole")]
 impl crate::topology::config::SinkConfig for BlackholeConfig {
     fn build(&self) -> Result<(super::RouterSink, super::Healthcheck), String> {
-        let sink = Box::new(BlackholeSink);
+        let sink = Box::new(BlackholeSink::new(self.clone()));
         let healthcheck = Box::new(healthcheck());
 
         Ok((sink, healthcheck))
@@ -22,11 +28,31 @@ fn healthcheck() -> impl Future<Item = (), Error = String> {
     future::ok(())
 }
 
+impl BlackholeSink {
+    pub fn new(config: BlackholeConfig) -> Self {
+        BlackholeSink {
+            config,
+            total_records: 0,
+            total_raw_bytes: 0,
+        }
+    }
+}
+
 impl Sink for BlackholeSink {
     type SinkItem = Record;
     type SinkError = ();
 
-    fn start_send(&mut self, _item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+        self.total_records += 1;
+        self.total_raw_bytes += item.raw.len();
+
+        if self.total_records % self.config.print_amount == 0 {
+            info!({
+                records = self.total_records,
+                raw_bytes_collected = self.total_raw_bytes
+            }, "Total records collected");
+        }
+
         Ok(AsyncSink::Ready)
     }
 
