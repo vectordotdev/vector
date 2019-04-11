f1351dbec618160793a5e1a9f4e7b6428c3fc393
Ben Johnson <bjohnson@binarylogic.com>
2019-04-11 10:35:00 -0400

    Build and release latest versions

--- ci/release.sh
+++ ci/release.sh
@@ -16,6 +16,8 @@
 
 set -eou pipefail
 
+. ./semver.sh  
+
 DEFAULT_TARGET="$(rustup target list | grep '(default)' | awk '{print $1}')"
 ARGS=${EXTRA_ARGS:-}
 
@@ -28,6 +30,32 @@ if [ -z "$TARGET" ]; then
     TARGET="$DEFAULT_TARGET"
 fi
 
+BUILDER_COMMAND=${BUILDER:-"cross"}
+
+function build_release() {
+  $BUILDER_COMMAND build --target $TARGET --release $ARGS
+}
+
+function build_tar() {
+  mkdir -p $DIST_DIR
+  cp "target/$TARGET/release/$APP_NAME" "$DIST_DIR"
+  cd $DIST_DIR
+  tar cvpf $TAR_NAME $APP_NAME
+  rm $APP_NAME
+  cd ..
+}
+
+function upload_s3() {
+  S3_URI="s3://packages.timber.io/vector/$S3_PATH$TAR_NAME"
+  aws s3 cp "$DIST_DIR/$TAR_NAME" "$S3_URI"
+}
+
+function build_and_upload() {
+  build_release
+  build_tar
+  upload_s3
+}
+
 # Temporarily allow unset variables in order to construct the BUILDSTAMP based
 # on variables that only _might_ be set
 set +u
@@ -37,41 +65,35 @@ BRANCH=$CIRCLE_BRANCH
 
 if [ -n "$TAG" ]
 then
+  echo "Building release for tag $TAG"
+
+  semverParseInto $TAG MAJOR MINOR PATCH SPECIAL
+
+  # Release this exact version
   S3_PATH="tags/$TAG/"
-  BUILDSTAMP="$TAG-$TARGET"
+  TAR_NAME="$APP_NAME-$TAG-$TARGET.tar.gz"
+  build_and_upload
+
+  # Release the latest minor version
+  S3_PATH="tags/$MAJOR.$MINOR.X/"
+  TAR_NAME="$APP_NAME-$MAJOR.$MINOR.X-$TARGET.tar.gz"
+  build_and_upload
+
+  # Release the latest major version
+  S3_PATH="tags/$MAJOR.X.X/"
+  TAR_NAME="$APP_NAME-$MAJOR.X.X-$TARGET.tar.gz"
+  build_and_upload
 elif [ -n "$BRANCH" ]
 then
   S3_PATH="branches/$BRANCH/"
-  BUILDSTAMP="$BRANCH-$CIRCLE_SHA1-$TARGET"
+  TAR_NAME="$APP_NAME-$BRANCH-$CIRCLE_SHA1-$TARGET.tar.gz"
+  build_and_upload
+
+  S3_PATH="branches/$BRANCH/"
+  TAR_NAME="$APP_NAME-$BRANCH-latest-$TARGET.tar.gz"
+  build_and_upload
 else
   echo "error: neither TAG nor BRANCH was set"
   exit 1
 fi
 set -u
-
-S3_PREFIX="s3://packages.timber.io/vector/$S3_PATH"
-TAR_NAME="$APP_NAME-$BUILDSTAMP.tar.gz"
-
-BUILDER_COMMAND=${BUILDER:-"cross"}
-
-function build_release() {
-    $BUILDER_COMMAND build --target $TARGET --release $ARGS
-}
-
-function build_tar() {
-    mkdir -p $DIST_DIR
-    cp "target/$TARGET/release/$APP_NAME" "$DIST_DIR"
-    cd $DIST_DIR
-    tar cvpf $TAR_NAME $APP_NAME
-    rm $APP_NAME
-    cd ..
-}
-
-function upload_s3() {
-    S3_URI="$S3_PREFIX/$TAR_NAME"
-    aws s3 cp "$DIST_DIR/$TAR_NAME" "$S3_URI"
-}
-
-build_release
-build_tar
-upload_s3
