cd17603e58c9fe02e0356f29c214ef5a77b6e30f
Binary Logic <1663286+binarylogic@users.noreply.github.com>
2019-04-11 11:08:21 -0400

    Circle build updates (#275)
    Update CircleCI builds to release on tags and master more efficiently.

--- ci/release.sh
+++ ci/release.sh
@@ -21,36 +21,64 @@ ARGS=${EXTRA_ARGS:-}
 
 APP_NAME=vector
 DIST_DIR="$(pwd)/dist"
-VERSION="$(git describe --abbrev=0 --tags)"
+BUILDTIME=$(date +%Y-%m-%d.%s)
 
 if [ -z "$TARGET" ]; then
     echo "TARGET is not passed using $DEFAULT_TARGET"
     TARGET="$DEFAULT_TARGET"
 fi
 
-S3_PREFIX="s3://packages.timber.io/vector"
-TAR_NAME="$APP_NAME-$VERSION-$TARGET.tar.gz"
-
 BUILDER_COMMAND=${BUILDER:-"cross"}
 
 function build_release() {
-    $BUILDER_COMMAND build --target $TARGET --release $ARGS
+  $BUILDER_COMMAND build --target $TARGET --release $ARGS
 }
 
 function build_tar() {
-    mkdir -p $DIST_DIR
-    cp "target/$TARGET/release/$APP_NAME" "$DIST_DIR"
-    cd $DIST_DIR
-    tar cvpf $TAR_NAME $APP_NAME
-    rm $APP_NAME
-    cd ..
+  mkdir -p $DIST_DIR
+  cp "target/$TARGET/release/$APP_NAME" "$DIST_DIR"
+  cd $DIST_DIR
+  tar cvpf $TAR_NAME $APP_NAME
+  rm $APP_NAME
+  cd ..
 }
 
 function upload_s3() {
-    S3_PATH="$S3_PREFIX/$TAR_NAME"
-    aws s3 cp "$DIST_DIR/$TAR_NAME" "$S3_PATH"
+  S3_URI="s3://packages.timber.io/vector/$S3_PATH"
+  aws s3 cp "$DIST_DIR/$TAR_NAME" "$S3_URI"
 }
 
-build_release
-build_tar
-upload_s3
+# Temporarily allow unset variables in order to construct the BUILDSTAMP based
+# on variables that only _might_ be set
+set +u
+
+TAG=$CIRCLE_TAG
+BRANCH=$CIRCLE_BRANCH
+COMMIT_SHA=$CIRCLE_SHA1
+
+if [ -n "$TAG" ]
+then
+  echo "Building release for tag $TAG"
+
+  TAR_NAME="$APP_NAME-$TAG-$TARGET.tar.gz"
+  build_release
+  build_tar
+
+  S3_PATH="tags/$TAG/$TAR_NAME"
+  upload_s3
+elif [ -n "$BRANCH" ]
+then
+  TAR_NAME="$APP_NAME-$BRANCH-$COMMIT_SHA-$TARGET.tar.gz"
+  build_release
+  build_tar
+
+  S3_PATH="branches/$BRANCH/$TAR_NAME"
+  upload_s3
+
+  S3_PATH="branches/$BRANCH/$APP_NAME-$BRANCH-latest-$TARGET.tar.gz"
+  upload_s3
+else
+  echo "error: neither TAG nor BRANCH was set"
+  exit 1
+fi
+set -u
