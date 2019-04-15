14340ab4cd03d2a9006e9b8f4ef8096eb8dc32cb
Lucio Franco <luciofranco14@gmail.com>
2019-04-15 11:13:22 -0400

    Remove extra comments

--- src/main.rs
+++ src/main.rs
@@ -250,8 +250,6 @@ fn main() {
     });
 }
 
-// Use of a mod or pub mod is not actually necessary.
-pub mod built_info {
-    // The file has been placed there by the build script.
+mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
