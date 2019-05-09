34009382132893d823def833c7fabd30753f692e
Michael Fairley <michaelfairley@gmail.com>
2019-05-09 11:42:30 -0500

    field filter lua benchmark

--- benches/lua.rs
+++ benches/lua.rs
@@ -3,6 +3,7 @@ use indexmap::IndexMap;
 use vector::{
     transforms::{self, Transform},
     Record,
+    topology::config::TransformConfig,
 };
 
 fn add_fields(c: &mut Criterion) {
@@ -53,4 +54,54 @@ fn add_fields(c: &mut Criterion) {
     );
 }
 
-criterion_group!(lua, add_fields);
+fn field_filter(c: &mut Criterion) {
+    let num_records: usize = 100_000;
+
+    c.bench(
+        "lua_field_filter",
+        Benchmark::new("native", move |b| {
+            b.iter_with_setup(
+                || {
+                    transforms::field_filter::FieldFilterConfig { field: "the_field".to_string(), value: "0".to_string() }.build().unwrap()
+                },
+                |transform| {
+                    let num = (0..num_records)
+                        .map(|i| {
+                            let mut record = Record::new_empty();
+                            record.insert_explicit("the_field".into(), (i % 10).to_string().into());
+                            record
+                        })
+                        .filter_map(|r| transform.transform(r))
+                        .count();
+                    assert_eq!(num, num_records / 10);
+                },
+            )
+        })
+        .with_function("lua", move |b| {
+            b.iter_with_setup(
+                || {
+                    let source = r#"
+                      if record["the_field"] ~= "0" then
+                        record = nil
+                      end
+                    "#;
+                    transforms::lua::Lua::new(&source).unwrap()
+                },
+                |transform| {
+                    let num = (0..num_records)
+                        .map(|i| {
+                            let mut record = Record::new_empty();
+                            record.insert_explicit("the_field".into(), (i % 10).to_string().into());
+                            record
+                        })
+                        .filter_map(|r| transform.transform(r))
+                        .count();
+                    assert_eq!(num, num_records / 10);
+                },
+            )
+        })
+        .sample_size(10),
+    );
+}
+
+criterion_group!(lua, add_fields, field_filter);
