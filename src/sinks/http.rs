63548565b45264c420e0642fbfe49b22c7917c21
Lucio Franco <luciofranco14@gmail.com>
2019-05-20 12:40:48 -0400

    Add batch_size option to http

--- src/sinks/http.rs
+++ src/sinks/http.rs
@@ -28,7 +28,7 @@ pub struct HttpSinkConfig {
     #[serde(flatten)]
     pub basic_auth: Option<BasicAuth>,
     pub headers: Option<IndexMap<String, String>>,
-    pub buffer_size: Option<usize>,
+    pub batch_size: Option<usize>,
     pub batch_timeout: Option<u64>,
     pub compression: Option<Compression>,
     pub encoding: Encoding,
@@ -79,6 +79,7 @@ fn http(config: HttpSinkConfig, acker: Acker) -> Result<super::RouterSink, Strin
         Compression::Gzip => true,
     };
     let batch_timeout = config.batch_timeout.unwrap_or(300);
+    let batch_size = config.batch_size.unwrap_or(2 * 1024 * 1024);
 
     let timeout = config.request_timeout_secs.unwrap_or(10);
     let in_flight_limit = config.request_in_flight_limit.unwrap_or(1);
@@ -136,7 +137,7 @@ fn http(config: HttpSinkConfig, acker: Acker) -> Result<super::RouterSink, Strin
     let sink = BatchServiceSink::new(service, acker)
         .batched_with_min(
             Buffer::new(gzip),
-            2 * 1024 * 1024,
+            batch_size,
             Duration::from_secs(batch_timeout),
         )
         .with(move |event| encode_event(event, &encoding));
