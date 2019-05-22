9177490ac5c1895749255afe8fc181bf1a83df7a
Lucio Franco <luciofranco14@gmail.com>
2019-05-22 11:09:25 -0400

    Add field option to regex parser (#402)

--- benches/bench.rs
+++ benches/bench.rs
@@ -286,6 +286,7 @@ fn benchmark_transforms(c: &mut Criterion) {
                         &["in"],
                         transforms::regex_parser::RegexParserConfig {
                             regex: r"status=(?P<status>\d+)".to_string(),
+                            field: None,
                         },
                     );
                     config.add_transform(
@@ -358,6 +359,7 @@ fn benchmark_complex(c: &mut Criterion) {
                         &["in1", "in2"],
                         transforms::regex_parser::RegexParserConfig {
                             regex: r"status=(?P<status>\d+)".to_string(),
+                            field: None,
                         },
                     );
                     config.add_transform(
--- src/transforms/regex_parser.rs
+++ src/transforms/regex_parser.rs
@@ -3,38 +3,49 @@ use crate::event::{self, Event};
 use regex::bytes::Regex;
 use serde::{Deserialize, Serialize};
 use std::str;
+use string_cache::DefaultAtom as Atom;
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct RegexParserConfig {
     pub regex: String,
+    pub field: Option<String>,
 }
 
 #[typetag::serde(name = "regex_parser")]
 impl crate::topology::config::TransformConfig for RegexParserConfig {
     fn build(&self) -> Result<Box<dyn Transform>, String> {
+        let field = self.field.clone();
+
         Regex::new(&self.regex)
             .map_err(|err| err.to_string())
-            .map::<Box<dyn Transform>, _>(|r| Box::new(RegexParser::new(r)))
+            .map::<Box<dyn Transform>, _>(|r| Box::new(RegexParser::new(r, field)))
     }
 }
 
 pub struct RegexParser {
     regex: Regex,
+    field: Option<Atom>,
 }
 
 impl RegexParser {
-    pub fn new(regex: Regex) -> Self {
-        Self { regex }
+    pub fn new(regex: Regex, field: Option<String>) -> Self {
+        Self {
+            regex,
+            field: field.map(Atom::from),
+        }
     }
 }
 
 impl Transform for RegexParser {
     fn transform(&self, mut event: Event) -> Option<Event> {
-        if let Some(captures) = self
-            .regex
-            .captures(&event.as_log()[&event::MESSAGE].as_bytes().into_owned())
-        {
+        let field = if let Some(field) = &self.field {
+            event.as_log()[&field].as_bytes().into_owned()
+        } else {
+            event.as_log()[&event::MESSAGE].as_bytes().into_owned()
+        };
+
+        if let Some(captures) = self.regex.captures(&field) {
             for name in self.regex.capture_names().filter_map(|c| c) {
                 if let Some(capture) = captures.name(name) {
                     event
@@ -58,8 +69,10 @@ mod tests {
     #[test]
     fn regex_parser_adds_parsed_field_to_event() {
         let event = Event::from("status=1234 time=5678");
-        let parser =
-            RegexParser::new(Regex::new(r"status=(?P<status>\d+) time=(?P<time>\d+)").unwrap());
+        let parser = RegexParser::new(
+            Regex::new(r"status=(?P<status>\d+) time=(?P<time>\d+)").unwrap(),
+            None,
+        );
 
         let event = parser.transform(event).unwrap();
 
@@ -70,7 +83,7 @@ mod tests {
     #[test]
     fn regex_parser_doesnt_do_anything_if_no_match() {
         let event = Event::from("asdf1234");
-        let parser = RegexParser::new(Regex::new(r"status=(?P<status>\d+)").unwrap());
+        let parser = RegexParser::new(Regex::new(r"status=(?P<status>\d+)").unwrap(), None);
 
         let event = parser.transform(event).unwrap();
 
