6a3501fd35e20076b2c886e6bd739601eb0cc197
Ben Johnson <bjohnson@binarylogic.com>
2019-05-31 18:53:53 -0400

    Use git tags for release versioning

--- ci/version.sh
+++ ci/version.sh
@@ -1,2 +1,2 @@
-cat Cargo.toml | grep "version" | head -n 1 | cut -f3 -d' ' | tr -d '"'
-# git describe --abbrev=0 --tags
+# cat Cargo.toml | grep "version" | head -n 1 | cut -f3 -d' ' | tr -d '"'
+git describe --abbrev=0 --tags
