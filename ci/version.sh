548797b8887dc6c1daa82d03f277a08192181a0a
Ben Johnson <bjohnson@binarylogic.com>
2019-06-03 20:18:50 -0400

    Use $CIRCLE_* env vars

--- ci/version.sh
+++ ci/version.sh
@@ -1,10 +1,10 @@
 # cat Cargo.toml | grep "version" | head -n 1 | cut -f3 -d' ' | tr -d '"'
 # git describe --abbrev=0 --tags
 
-if [ -n "$TAG" ]
+if [ -n "$CIRCLE_TAG" ]
 then
   git describe --abbrev=0 --tags
-elif [ -n "$BRANCH" ]
+elif [ -n "$CIRCLE_BRANCH" ]
 then
   git describe --tags
 else
