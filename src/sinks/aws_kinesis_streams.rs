vectordotdev/vector

commit 978d55e1cbd602d00036e935eef77d02b56c17d1

Adjust aws_kinesis_streams sink batch_size to 1mib and request_in_flight_limit to 5

src/sinks/aws_kinesis_streams.rs
@@ -73,9 +73,9 @@ impl KinesisService {
         let batch_timeout = config.batch_timeout.unwrap_or(1);
 
         let timeout = config.request_timeout_secs.unwrap_or(30);
-        let in_flight_limit = config.request_in_flight_limit.unwrap_or(1);
+        let in_flight_limit = config.request_in_flight_limit.unwrap_or(5);
         let rate_limit_duration = config.request_rate_limit_duration_secs.unwrap_or(1);
-        let rate_limit_num = config.request_rate_limit_num.unwrap_or(10);
+        let rate_limit_num = config.request_rate_limit_num.unwrap_or(5);
         let retry_attempts = config.request_retry_attempts.unwrap_or(usize::max_value());
         let retry_backoff_secs = config.request_retry_backoff_secs.unwrap_or(1);
         let encoding = config.encoding.clone();

commit a81c8fd9785b9f7111adf7a02795cca7f2ba56ea

Fix test for aws-kinesis-streams
Fixes kinesis_put_records test where order of output has become undefined. Probably caused by commit: 
https://github.com/timberio/vector/commit/978d55e1cbd602d00036e935eef77d02b56c17d1

src/sinks/aws_kinesis_streams.rs
@@ -290,7 +290,7 @@ mod integration_tests {
 
         let timestamp = chrono::Utc::now().timestamp_millis();
 
-        let (input_lines, events) = random_lines_with_stream(100, 11);
+        let (mut input_lines, events) = random_lines_with_stream(100, 11);
 
         let pump = sink.send_all(events);
         rt.block_on(pump).unwrap();
@@ -302,11 +302,13 @@ mod integration_tests {
             .block_on(fetch_records(STREAM_NAME.into(), timestamp, region))
             .unwrap();
 
-        let output_lines = records
+        let mut output_lines = records
             .into_iter()
             .map(|e| String::from_utf8(e.data).unwrap())
             .collect::<Vec<_>>();
 
+        input_lines.sort();
+        output_lines.sort();
         assert_eq!(output_lines, input_lines)
     }
 