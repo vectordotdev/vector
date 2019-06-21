5af2e0dbb35b608fe746d22dee6e1b3d0e65fbdc
Ben Johnson <bjohnson@binarylogic.com>
2019-06-21 10:57:38 -0400

    Export version in step

--- .circleci/build_release.sh
+++ .circleci/build_release.sh
@@ -16,17 +16,27 @@
 
 set -eou pipefail
 
+# Args
+
 DEFAULT_TARGET="$(rustup target list | grep '(default)' | awk '{print $1}')"
 ARGS=${EXTRA_ARGS:-}
 
-APP_NAME=vector
-BUILDER_COMMAND=${BUILDER:-"cross"}
+# Defaults
 
 if [ -z "$TARGET" ]; then
     echo "TARGET is not passed using $DEFAULT_TARGET"
     TARGET="$DEFAULT_TARGET"
 fi
 
+if [ -z "$VERSION" ]; then
+    echo "VERSION is not passed using version.sh as the default"
+    VERSION=$(./.circleci/version.sh)
+fi
+
+# Variables
+
+APP_NAME=vector
+BUILDER_COMMAND=${BUILDER:-"cross"}
 ROOT_DIR="$(pwd)"
 DIST_DIR="$ROOT_DIR/dist"
 RELEASE_DIR="$DIST_DIR/$APP_NAME-$VERSION"
@@ -34,6 +44,8 @@ BIN_DIR="$RELEASE_DIR/bin"
 CONFIG_DIR="$RELEASE_DIR/config"
 BINARY_PATH="$ROOT_DIR/target/$TARGET/release/$APP_NAME"
 TAR_NAME="$APP_NAME-$VERSION-$TARGET.tar.gz"
+
+# Functions
 
 function build_release() {
   $BUILDER_COMMAND build --target $TARGET --release $ARGS
@@ -51,6 +63,8 @@ function build_tar() {
   tar cvpf $TAR_NAME $DIST_DIR
   rm -rf $RELEASE_DIR
 }
+
+# Execute
 
 build_release
 copy_files
