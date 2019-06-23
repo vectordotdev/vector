ac3c280fc7a04ae4f39473cd42da235f0b65b8da
Ben Johnson <bjohnson@binarylogic.com>
2019-06-22 23:09:42 -0400

    Remove leading v from exported version when releasing

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -1,13 +1,18 @@
 # cat Cargo.toml | grep "version" | head -n 1 | cut -f3 -d' ' | tr -d '"'
 # git describe --abbrev=0 --tags
 
+version=''
+
 if [ -n "$CIRCLE_TAG" ]
 then
-  git describe --abbrev=0 --tags
+  version=git describe --abbrev=0 --tags
 elif [ -n "$CIRCLE_BRANCH" ]
 then
-  git describe --tags
+  version=$(git describe --tags)
 else
   echo "error: neither TAG nor BRANCH was set"
   exit 1
 fi
+
+# Remove the leading v since this is not a valid version
+echo $version | sed 's/^v//g'
