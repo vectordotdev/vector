f6abfc33b59b23cc29c13121dc2f48a57f28a681
Ben Johnson <bjohnson@binarylogic.com>
2019-07-17 16:25:54 -0400

    chore: add timberio/vector-alpine docker image
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>

--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -10,12 +10,15 @@ set -eu
 
 echo "Releasing timberio/vector* Docker images"
 docker build -t timberio/vector:$VERSION distribution/docker
+docker build -t timberio/vector-alpine:$VERSION distribution/docker/alpine
 docker build -t timberio/vector-slim:$VERSION distribution/docker/slim
 docker build -t timberio/vector:latest distribution/docker
 docker build -t timberio/vector-slim:latest distribution/docker/slim
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push timberio/vector:$VERSION
+docker push timberio/vector-alpine:$VERSION
 docker push timberio/vector-slim:$VERSION
 docker push timberio/vector:latest
+docker push timberio/vector-alpine:latest
 docker push timberio/vector-slim:latest
