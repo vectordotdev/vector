57bc070a11ef3141ee5829d043f3720e359da726
Lucio Franco <luciofranco14@gmail.com>
2019-08-01 11:07:09 -0400

    fix: Retry requests on timeouts (#691)
    * fix: Retry requests on timeouts
    
    This change adds the ability to retry requests on `tower_timeout`
    Elapsed errors. This additionall adds three new tests for retries that
    test retryable errors, non retryable errors and timeout
    errors. Additional things added were a new `test_util::trace_init`
    that can be set at the beginging of a test and will allow the user to
    pass a `TEST_LOG` env var to set the tracing level for the tests. This
    is very useful for debuging errors within tests.
    
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- src/sinks/util/retries.rs
+++ src/sinks/util/retries.rs
@@ -2,7 +2,7 @@ use super::Error;
 use futures::{try_ready, Async, Future, Poll};
 use std::time::{Duration, Instant};
 use tokio::timer::Delay;
-use tower::retry::Policy;
+use tower::{retry::Policy, timeout::error::Elapsed};
 
 pub trait RetryLogic: Clone {
     type Error: std::error::Error + Send + Sync + 'static;
@@ -81,6 +81,9 @@ where
                         error!(message = "encountered non-retriable error.", %error);
                         None
                     }
+                } else if let Some(_) = error.downcast_ref::<Elapsed>() {
+                    warn!("request timedout.");
+                    Some(self.build_retry())
                 } else {
                     warn!(message = "unexpected error type.", %error);
                     None
@@ -99,7 +102,108 @@ impl<L: RetryLogic> Future for RetryPolicyFuture<L> {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        try_ready!(self.delay.poll().map_err(|_| ()));
+        try_ready!(self
+            .delay
+            .poll()
+            .map_err(|error| panic!("timer error: {}; this is a bug!", error)));
         Ok(Async::Ready(self.policy.clone()))
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_util::trace_init;
+    use futures::Future;
+    use std::{fmt, time::Duration};
+    use tokio01_test::{assert_err, assert_not_ready, assert_ready, clock};
+    use tower::{retry::Retry, Service};
+    use tower_test::{assert_request_eq, mock};
+
+    #[test]
+    fn service_error_retry() {
+        clock::mock(|clock| {
+            trace_init();
+
+            let policy = FixedRetryPolicy::new(5, Duration::from_secs(1), SvcRetryLogic);
+
+            let (service, mut handle) = mock::pair();
+            let mut svc = Retry::new(policy, service);
+
+            assert_ready!(svc.poll_ready());
+
+            let mut fut = svc.call("hello");
+            assert_request_eq!(handle, "hello").send_error(Error(true));
+            assert_not_ready!(fut.poll());
+
+            clock.advance(Duration::from_secs(2));
+            assert_not_ready!(fut.poll());
+
+            assert_request_eq!(handle, "hello").send_response("world");
+            assert_eq!(fut.wait().unwrap(), "world");
+        });
+    }
+
+    #[test]
+    fn service_error_no_retry() {
+        trace_init();
+
+        let policy = FixedRetryPolicy::new(5, Duration::from_secs(1), SvcRetryLogic);
+
+        let (service, mut handle) = mock::pair();
+        let mut svc = Retry::new(policy, service);
+
+        assert_ready!(svc.poll_ready());
+
+        let mut fut = svc.call("hello");
+        assert_request_eq!(handle, "hello").send_error(Error(false));
+        assert_err!(fut.poll());
+    }
+
+    #[test]
+    fn timeout_error() {
+        clock::mock(|clock| {
+            trace_init();
+
+            let policy = FixedRetryPolicy::new(5, Duration::from_secs(1), SvcRetryLogic);
+
+            let (service, mut handle) = mock::pair();
+            let mut svc = Retry::new(policy, service);
+
+            assert_ready!(svc.poll_ready());
+
+            let mut fut = svc.call("hello");
+            assert_request_eq!(handle, "hello").send_error(tower::timeout::error::Elapsed::new());
+            assert_not_ready!(fut.poll());
+
+            clock.advance(Duration::from_secs(2));
+            assert_not_ready!(fut.poll());
+
+            assert_request_eq!(handle, "hello").send_response("world");
+            assert_eq!(fut.wait().unwrap(), "world");
+        });
+    }
+
+    #[derive(Debug, Clone)]
+    struct SvcRetryLogic;
+
+    impl RetryLogic for SvcRetryLogic {
+        type Error = Error;
+        type Response = &'static str;
+
+        fn is_retriable_error(&self, error: &Self::Error) -> bool {
+            error.0
+        }
+    }
+
+    #[derive(Debug)]
+    struct Error(bool);
+
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "error")
+        }
+    }
+
+    impl std::error::Error for Error {}
 }
