2d00b97281f11ca5674e5141116a88895f0eb271
Ben Johnson <bjohnson@binarylogic.com>
2019-08-02 10:34:27 -0400

    Use nightly not edge
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>

--- scripts/release-s3.sh
+++ scripts/release-s3.sh
@@ -10,15 +10,15 @@ set -eu
 
 CHANNEL=""
 if [[ $VERSION == *"-"* ]]; then
-  CHANNEL="edge"
-  echo "Version ($VERSION) is an edge version, only releasing to edge channels"
+  CHANNEL="nightly"
+  echo "Version ($VERSION) is a nightly version, only releasing to nightly channels"
 else
   CHANNEL="latest"
   echo "Version ($VERSION) is a release version, releasing to latest channels"
 fi
 
 if [ -z "$CHANNEL" ]; then
-  echo 'The CHANNEL env var must be set to "edge" or "latest"'
+  echo 'The CHANNEL env var must be set to "nightly" or "latest"'
   exit 1
 fi
 
@@ -31,18 +31,18 @@ escaped_version=$(echo $VERSION | sed "s/\./\\\./g")
 echo "Uploading all artifacts to s3://packages.timber.io/vector/$VERSION/"
 aws s3 cp "target/artifacts/" "s3://packages.timber.io/vector/$VERSION/" --recursive
 
-# Update the "edge" files
-echo "Uploading all artifacts to s3://packages.timber.io/vector/edge/"
+# Update the "nightly" files
+echo "Uploading all artifacts to s3://packages.timber.io/vector/nightly/"
 td=$(mktemp -d)
 
 cp -a "target/artifacts/." "$td"
-rename -v "s/$escaped_version/edge/" $td/*
-echo "Renamed all builds: via \"s/$escaped_version/edge/\""
+rename -v "s/$escaped_version/nightly/" $td/*
+echo "Renamed all builds: via \"s/$escaped_version/nightly/\""
 ls $td
-aws s3 rm --recursive "s3://packages.timber.io/vector/edge/"
-aws s3 cp "$td" "s3://packages.timber.io/vector/edge/" --recursive
+aws s3 rm --recursive "s3://packages.timber.io/vector/nightly/"
+aws s3 cp "$td" "s3://packages.timber.io/vector/nightly/" --recursive
 rm -rf $td
-echo "Uploaded edge archives"
+echo "Uploaded nightly archives"
 
 if [[ "$CHANNEL" == "latest" ]]; then
   # Update the "latest" files
