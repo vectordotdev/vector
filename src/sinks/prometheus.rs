014d6f63044476c541f9f3f0f9f1092e2446ca05
ktff <krunotf@gmail.com>
2019-08-14 22:16:35 +0200

    feat(prometheus sink): Support sets (#733)
    * feat(prometheus_sink): Support sets (#713)
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * test(prometheus sink): Set test (#733)
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * feat(prometheus sink): Flush sets (#733)
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add check for minimal flush period
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Apply fmt format
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Set default_flush_period to 1min
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>

--- src/sinks/prometheus.rs
+++ src/sinks/prometheus.rs
@@ -4,15 +4,30 @@ use crate::{
     topology::config::{DataType, SinkConfig},
     Event,
 };
-use futures::{future, Async, AsyncSink, Future, Sink};
+use futures::{future, try_ready, Async, AsyncSink, Future, Sink};
 use hyper::{
     header::HeaderValue, service::service_fn, Body, Method, Request, Response, Server, StatusCode,
 };
 use prometheus::{Encoder, Registry, TextEncoder};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    ops::Add,
+    sync::{
+        mpsc::{channel, Sender},
+        Arc,
+    },
+    time::{Duration, Instant},
+};
 use stream_cancel::{Trigger, Tripwire};
+use tokio::timer::Delay;
 use tracing::field;
+
+/// Should be greater than 1ms to avoid accidentaly causing infinite loop.
+/// Limits minimal acceptable flush_period for PrometheusSinkConfig.
+/// 3ms to account for timer and time source inprecisions.
+const MIN_FLUSH_PERIOD_MS: u64 = 3; //ms
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
@@ -21,6 +36,9 @@ pub struct PrometheusSinkConfig {
     pub address: SocketAddr,
     #[serde(default = "default_histogram_buckets")]
     pub buckets: Vec<f64>,
+    /// Should be greater than 1 ms to avoid accidentaly causing infinite loop
+    #[serde(default = "default_flush_period")]
+    pub flush_period: Duration,
 }
 
 pub fn default_histogram_buckets() -> Vec<f64> {
@@ -35,9 +53,22 @@ pub fn default_address() -> SocketAddr {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 9598)
 }
 
+pub fn default_flush_period() -> Duration {
+    Duration::from_secs(60)
+}
+
 #[typetag::serde(name = "prometheus")]
 impl SinkConfig for PrometheusSinkConfig {
     fn build(&self, acker: Acker) -> Result<(super::RouterSink, super::Healthcheck), String> {
+        // Checks
+        if self.flush_period < Duration::from_millis(MIN_FLUSH_PERIOD_MS) {
+            return Err(format!(
+                "Flush period for sets must be greater or equal to {} ms",
+                MIN_FLUSH_PERIOD_MS
+            ));
+        }
+
+        // Build
         let sink = Box::new(PrometheusSink::new(self.clone(), acker));
         let healthcheck = Box::new(future::ok(()));
 
@@ -52,10 +83,12 @@ impl SinkConfig for PrometheusSinkConfig {
 struct PrometheusSink {
     registry: Arc<Registry>,
     server_shutdown_trigger: Option<Trigger>,
+    flush_channel: Option<Sender<prometheus::IntGauge>>,
     config: PrometheusSinkConfig,
     counters: HashMap<String, prometheus::Counter>,
     gauges: HashMap<String, prometheus::Gauge>,
     histograms: HashMap<String, prometheus::Histogram>,
+    sets: HashMap<String, (prometheus::IntGauge, HashSet<String>)>,
     acker: Acker,
 }
 
@@ -95,10 +128,12 @@ impl PrometheusSink {
         Self {
             registry: Arc::new(Registry::new()),
             server_shutdown_trigger: None,
+            flush_channel: None,
             config,
             counters: HashMap::new(),
             gauges: HashMap::new(),
             histograms: HashMap::new(),
+            sets: HashMap::new(),
             acker,
         }
     }
@@ -144,6 +179,33 @@ impl PrometheusSink {
         }
     }
 
+    /// Calls f with entry corresponding to name. Creates entry if needed.
+    fn with_set(
+        &mut self,
+        name: String,
+        f: impl FnOnce(&mut (prometheus::IntGauge, HashSet<String>)),
+    ) {
+        if let Some(set) = self.sets.get_mut(&name) {
+            f(set);
+        } else {
+            let counter = prometheus::IntGauge::new(name.clone(), name.clone()).unwrap();
+            if let Err(e) = self.registry.register(Box::new(counter.clone())) {
+                error!("Error registering Prometheus gauge for set: {}", e);
+            };
+
+            // Send counter to flusher
+            if let Some(ch) = self.flush_channel.as_mut() {
+                if let Err(e) = ch.send(counter.clone()) {
+                    error!("Error sending Prometheus gauge to flusher: {}", e);
+                }
+            }
+
+            let mut set = (counter, HashSet::new());
+            f(&mut set);
+            self.sets.insert(name, set);
+        }
+    }
+
     fn start_server_if_needed(&mut self) {
         if self.server_shutdown_trigger.is_some() {
             return;
@@ -167,11 +229,45 @@ impl PrometheusSink {
 
         let server = Server::bind(&self.config.address)
             .serve(new_service)
-            .with_graceful_shutdown(tripwire)
+            .with_graceful_shutdown(tripwire.clone())
             .map_err(|e| eprintln!("server error: {}", e));
 
         tokio::spawn(server);
         self.server_shutdown_trigger = Some(trigger);
+
+        self.start_flusher(tripwire);
+    }
+
+    /// Flusher will stop when tripwire is done
+    fn start_flusher(&mut self, mut tripwire: Tripwire) {
+        let (send, recv) = channel();
+        self.flush_channel = Some(send);
+
+        let period = self.config.flush_period;
+        let mut timer = Delay::new(Instant::now().add(period));
+
+        let mut counters = Vec::new();
+        let flusher = future::poll_fn(move || {
+            // Check for shutdown
+            while tripwire.poll() == Ok(Async::NotReady) {
+                // Check messages
+                counters.extend(recv.try_iter());
+
+                // Check timer
+                try_ready!(timer.poll().map_err(|_| ()));
+
+                // Reset values
+                for counter in counters.iter() {
+                    counter.set(0);
+                }
+
+                // Reset timer
+                timer.reset(Instant::now().add(period));
+            }
+            Ok(Async::Ready(()))
+        });
+
+        tokio::spawn(flusher);
     }
 }
 
@@ -211,8 +307,37 @@ impl Sink for PrometheusSink {
                     hist.observe(val);
                 }
             }),
-            Metric::Set { .. } => {
-                trace!("Sets are not supported in Prometheus sink");
+            Metric::Set { name, val } => {
+                // Sets are implemented using promethius integer gauges.
+                self.with_set(name, move |&mut (ref mut counter, ref mut set)| {
+                    // Check if counter was reseted
+                    if counter.get() < set.len() as i64 {
+                        // Counter was reseted
+                        set.clear();
+                    }
+                    // Check for uniques of value
+                    if set.insert(val) {
+                        // Val is a new unique value, therefor gauge should be incremented.
+                        counter.add(1);
+                        // There is a possiblity that counter was reseted between counter.get counter.add
+                        // so that needs to be checked
+                        match counter.get() {
+                            // Reseted after counter.add
+                            0 => set.clear(),
+                            // Reseted between first counter.get and counter.add
+                            1 if set.len() > 1 => {
+                                // Outside world could see metric as 1, if they so happen to
+                                // request metrics between counter.add and following counter.set.
+                                // But this glitch is ok since either way flushes are scheduled
+                                // to happen in periods with best effort basis.
+                                counter.set(0);
+                                set.clear();
+                            }
+                            // Everything is fine
+                            _ => (),
+                        }
+                    }
+                });
             }
         }
 
