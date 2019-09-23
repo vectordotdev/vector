389773ed0cf596f5b0055e765394e239c5d057db
Lucio Franco <luciofranco14@gmail.com>
2019-09-23 15:15:46 -0400

    Use different path
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- scripts/package-deb.sh
+++ scripts/package-deb.sh
@@ -20,7 +20,7 @@ td=$(mktemp -d)
 pushd $td
 tar -xvf $absolute_archive_path
 mkdir -p $project_root/target/$TARGET/release
-mv vector-$VERSION/bin/vector $project_root/target/$TARGET/release
+mv vector/bin/vector $project_root/target/$TARGET/release
 popd
 rm -rf $td
 
