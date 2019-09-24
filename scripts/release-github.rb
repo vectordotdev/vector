ae598a38bf446426d288a6384a78eb8931c6b907
Ben Johnson <bjohnson@binarylogic.com>
2019-09-24 12:21:29 -0400

    chore(operations): update release-github to include release notes
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>
    Signed-off-by: albert <63851587@qq.com>

---
+++ scripts/release-github.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+# release-github.sh
+#
+# SUMMARY
+#
+#   Uploads target/artifacts to Github releases
+
+require_relative "setup"
+require_relative "generate/templates"
+require_relative "generate/post_processors/link_definer"
+
+#
+# Constants
+#
+
+VERSION = ENV.fetch("VERSION")
+SHA1 = ENV.fetch("CIRCLE_SHA1")
+
+#
+# Commit
+#
+
+metadata =
+  begin
+    Metadata.load!(META_ROOT, DOCS_ROOT)
+  rescue Exception => e
+    error!(e.message)
+  end
+
+templates = Templates.new(TEMPLATES_DIR, metadata)
+release = metadata.releases.to_h.fetch(:"#{VERSION}")
+
+#
+# Release
+#
+
+flags = ["--assets 'target/artifacts/*'"]
+
+notes = templates.release_notes(release)
+notes = PostProcessors::LinkDefiner.define!(notes.clone, "", metadata.links)
+notes = notes.gsub("'", "'" => "\\'")
+flags << "--notes '#{notes}'"
+
+if release.pre?
+  flags << "--pre"
+end
+
+`grease create-release timberio/vector v#{VERSION} $#{SHA1} #{flags.join(" ")}`
