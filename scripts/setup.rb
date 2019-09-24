ced248773ab9a04d862a22dd4b80dfde5c9e8de3
Ben Johnson <bjohnson@binarylogic.com>
2019-09-24 12:21:29 -0400

    chore(operations): update release-github to include release notes
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>

---
+++ scripts/setup.rb
@@ -0,0 +1,40 @@
+# setup.rb
+#
+# SUMMARY
+#
+#   Common header script that handles boiler plate for Ruby based scripts.
+
+#
+# Setup
+#
+
+# Changes into the scripts directory so that we can load the Bundler
+# dependencies. Unfortunately, Bundler does not provide a way load a Gemfile
+# outside of the cwd.
+Dir.chdir "scripts"
+
+#
+# Requires
+#
+
+require "rubygems"
+require "bundler"
+Bundler.require(:default)
+
+require_relative "util"
+
+#
+# Includes
+#
+
+include Printer
+
+#
+# Constants
+#
+
+ROOT_DIR = Pathname.new("#{Dir.pwd}/..").cleanpath
+
+DOCS_ROOT = File.join(ROOT_DIR, "docs")
+META_ROOT = File.join(ROOT_DIR, ".meta")
+TEMPLATES_DIR = File.join(ROOT_DIR, "scripts", "generate", "templates")
