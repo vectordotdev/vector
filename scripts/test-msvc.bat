2921347d4fe008a4d9b42ebd5ba6d321db81046f
Alexander Rodin <rodin.alexander@gmail.com>
2019-11-20 06:52:51 +0000

    Fix Perl path
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/test-msvc.bat
+++
@@ -1,4 +0,0 @@
-set RUSTFLAGS=-Ctarget-feature=+crt-static
-set PATH=%USERPROFILE%\.cargo\bin;%PATH%
-del rust-toolchain
-cargo test --no-default-features --features "leveldb leveldb/leveldb-sys-3 rdkafka rdkafka/cmake_build"
