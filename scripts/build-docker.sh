a24330b85313beb28a1ba84986546f367e95557e
Alexander Rodin <rodin.alexander@gmail.com>
2019-11-29 12:30:57 +0000

    Build multi-arch Docker images
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -6,13 +6,26 @@
 #
 #   Builds the Vector docker images
 
-set -eu
+set -eux
 
 CHANNEL=$(scripts/util/release-channel.sh)
+VERSION=$(scripts/version.sh)
+DATE=$(date -u +%Y-%m-%d)
 
 #
 # Functions
 #
+
+build() {
+  base=$1
+  version=$2
+
+  docker buildx build \
+    --platform="$PLATFORM" \
+    --tag timberio/vector:$version-$base \
+    target/artifacts \
+    -f distribution/docker/$base/Dockerfile
+}
 
 verify() {
   tag=$1
@@ -31,38 +44,43 @@ verify() {
 }
 
 #
-# Prepare
-#
-
-cp -av target/artifacts/vector-x86_64-unknown-linux-musl.tar.gz distribution/docker/alpine
-cp -av target/artifacts/vector-amd64.deb distribution/docker/debian
-
-#
 # Build
 #
 
 echo "Building timberio/vector:* Docker images"
 
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
+docker buildx rm vector-builder || true
+docker buildx create --use --name vector-builder
+docker buildx install
+
 if [[ "$CHANNEL" == "latest" ]]; then
-  docker build --tag timberio/vector:$VERSION-alpine distribution/docker/alpine
-  docker build --tag timberio/vector:latest-alpine distribution/docker/alpine
-  docker build --tag timberio/vector:$VERSION-debian distribution/docker/debian
-  docker build --tag timberio/vector:latest-debian distribution/docker/debian
+  build alpine latest
+  build alpine $VERSION
+  build debian latest
+  build debian $VERSION
 elif [[ "$CHANNEL" == "nightly" ]]; then
-  docker build --tag timberio/vector:nightly-alpine distribution/docker/alpine
-  docker build --tag timberio/vector:nightly-debian distribution/docker/debian
+  build alpine nightly
+  build alpine nightly-$DATE
+  build debian nightly
+  build debian nightly-$DATE
 fi
 
 #
 # Verify
 #
 
-if [[ "$CHANNEL" == "latest" ]]; then
-  verify timberio/vector:$VERSION-alpine
-  verify timberio/vector:latest-alpine
-  verify timberio/vector:$VERSION-debian
-  verify timberio/vector:latest-debian
-elif [[ "$CHANNEL" == "nightly" ]]; then
-  verify timberio/vector:nightly-alpine
-  verify timberio/vector:nightly-debian
-fi
+# Images built using `buildx` are invisible to `docker run`
+
+# if [[ "$CHANNEL" == "latest" ]]; then
+#   verify timberio/vector:$VERSION-alpine
+#   verify timberio/vector:latest-alpine
+#   verify timberio/vector:$VERSION-debian
+#   verify timberio/vector:latest-debian
+# elif [[ "$CHANNEL" == "nightly" ]]; then
+#   verify timberio/vector:nightly-alpine
+#   verify timberio/vector:nightly-$DATE-alpine
+#   verify timberio/vector:nightly-debian
+#   verify timberio/vector:nightly-$DATE-debian
+# fi
