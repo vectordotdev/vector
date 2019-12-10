5536ca82157b9c0a1d8cc43ca60ac109fef4db17
Alexander Rodin <rodin.alexander@gmail.com>
2019-12-10 17:18:38 +0000

    Fix extraction of Vector from the archive
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/package-msi.sh
+++ scripts/package-msi.sh
@@ -5,7 +5,7 @@ rm -rf target/msi-x64
 cp -R distribution/msi target/msi-x64
 cp target/artifacts/vector-x86_64-pc-windows-msvc.zip target/msi-x64
 pushd target/msi-x64
-powershell Expand-Archive vector-x86_64-pc-windows-msvc.zip
+powershell '$progressPreference = "silentlyContinue"; Expand-Archive vector-x86_64-pc-windows-msvc.zip'
 ./build.sh
 popd
 cp target/msi-x64/vector.msi target/artifacts/vector-x64.msi
