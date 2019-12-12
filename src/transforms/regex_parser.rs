613d90f2da7da92c65204f3a28fb101afac6f8e4
Luke Steensen <luke.steensen@gmail.com>
2019-12-12 14:09:09 -0600

    fix(regex_parser transform): make truncation utf8-aware
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- src/transforms/regex_parser.rs
+++ src/transforms/regex_parser.rs
@@ -177,7 +177,11 @@ impl Transform for RegexParser {
 
 fn truncate_string_at(s: &str, maxlen: usize) -> Cow<str> {
     if s.len() >= maxlen {
-        format!("{}[...]", &s[..maxlen - 5]).into()
+        let mut len = maxlen - "[...]".len();
+        while !s.is_char_boundary(len) {
+            len -= 1;
+        }
+        format!("{}[...]", &s[..len]).into()
     } else {
         s.into()
     }
@@ -331,5 +335,11 @@ mod tests {
         assert_eq!(log[&"check".into()], ValueKind::Boolean(false));
         assert_eq!(log[&"status".into()], ValueKind::Integer(1234));
         assert_eq!(log[&"time".into()], ValueKind::Float(6789.01));
+    }
+
+    #[test]
+    fn regex_parser_truncate_utf8() {
+        let message = "hello 😁 this is test";
+        assert_eq!("hello [...]", super::truncate_string_at(&message, 13));
     }
 }
