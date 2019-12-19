6704f776b361e203bcd42ee92167bccfe33a542b
Lucio Franco <luciofranco14@gmail.com>
2019-12-19 12:26:07 -0500

    chore(networking): Fix https rusoto connections
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- src/sinks/util/rusoto.rs
+++ src/sinks/util/rusoto.rs
@@ -9,7 +9,8 @@ use rusoto_core::HttpClient;
 pub type Client = HttpClient<HttpsConnector<HttpConnector<Resolver>>>;
 
 pub fn client(resolver: Resolver) -> crate::Result<Client> {
-    let http = HttpConnector::new_with_resolver(resolver);
+    let mut http = HttpConnector::new_with_resolver(resolver);
+    http.enforce_http(false);
 
     let ssl = SslConnector::builder(SslMethod::tls())?;
     let https = HttpsConnector::with_connector(http, ssl)?;
