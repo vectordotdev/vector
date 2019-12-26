902776e61980f931ce8f2738fd2c86755c246acd
Alexander Rodin <rodin.alexander@gmail.com>
2019-12-26 18:30:49 +0000

    Generate `cargoSha256` automatically
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/verify-nix.sh
+++ scripts/verify-nix.sh
@@ -3,6 +3,6 @@
 mkdir -p /tmp
 nixpkgs=/tmp/nixpkgs
 git clone --depth=1 https://github.com/nixos/nixpkgs $nixpkgs
-erb < distribution/nix/default.nix > $nixpkgs/pkgs/tools/misc/vector/default.nix
+erb < distribution/nix/default.nix.erb > $nixpkgs/pkgs/tools/misc/vector/default.nix
 
 nix-env -f $nixpkgs -iA vector
