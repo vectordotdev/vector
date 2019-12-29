1685dc980c149ef3f41388b193d2239d000734cf
Alexander Rodin <rodin.alexander@gmail.com>
2019-12-29 19:10:24 +0000

    Use local keyring directories
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/check-blog-signatures.rb
+++ scripts/check-blog-signatures.rb
@@ -17,27 +17,34 @@ require_relative "util/printer"
 # load metadata
 metadata = Metadata.load!(META_ROOT, DOCS_ROOT, PAGES_ROOT)
 
+# the base directory with GPG keyrings
+gpg_base_dir = "#{ROOT_DIR}/target/gpg/github"
+# remove all previously imported GPG keys
+FileUtils::remove_dir gpg_base_dir, true
+
 # check signatures for all blog posts
 metadata.posts.each do |post|
   say("Checking #{post.path}...")
   github_username = post.author_github.rpartition("/").last
 
-  # remove all previously imported GPG keys
-  FileUtils::remove_dir "#{Dir.home}/.gnupg", true
+  # directory with keyring for the given author
+  keyring_dir = "#{gpg_base_dir}/#{github_username}"
+  if not Dir.exists? keyring_dir
+    FileUtils::mkpath keyring_dir
+    # fetch author's GPG public keys added to GitHub
+    uri = URI("https://api.github.com/users/#{github_username}/gpg_keys")
+    gpg_keys = JSON.parse Net::HTTP.get(uri)
 
-  # fetch author's GPG public keys added to GitHub
-  uri = URI("https://api.github.com/users/#{github_username}/gpg_keys")
-  gpg_keys = JSON.parse Net::HTTP.get(uri)
-
-  # import each of the author's keys to GPG keyring
-  gpg_keys.each do |gpg_key|
-    Open3.popen3("gpg", "--import") do |i, o, e, t|
-      i.write gpg_key["raw_key"]
+    # import each of the author's keys to GPG keyring
+    gpg_keys.each do |gpg_key|
+      Open3.popen3("gpg", "--homedir", keyring_dir, "--import") do |i, o, e, t|
+        i.write gpg_key["raw_key"]
+      end
     end
   end
 
   # verify the signature for the post
-  res = system("gpg", "--verify", "#{ROOT_DIR}/#{post.path}.sig")
+  res = system("gpg", "--homedir", keyring_dir, "--verify", "#{ROOT_DIR}/#{post.path}.sig")
   if not res
     error!("Cannot verify GPG signature for #{post.path}")
     exit 1
