7fac5e14bda5af8003018993a8504885c0e1a662
Alexander Rodin <rodin.alexander@gmail.com>
2020-02-05 17:46:55 +0300

    Move `Makefile` to tests, merge `docker-compose` files
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/ci-docker-images/target-graph/target-graph.sh
+++ scripts/ci-docker-images/target-graph/target-graph.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-make -f Makefile.test $1 -Bnd | make2graph | graph-easy --as=boxart
+make -f tests/Makefile $1 -Bnd | make2graph | graph-easy --as=boxart
--- scripts/docker-compose-run.sh
+++ scripts/docker-compose-run.sh
@@ -2,6 +2,6 @@
 set -eu
 
 export USER=$(id -u):$(id -g)
-DC_FILE=docker-compose.test.yml
+DC_FILE=docker-compose.yml
 docker-compose -f $DC_FILE rm -svf $1 2>/dev/null || true
 docker-compose -f $DC_FILE up --build --abort-on-container-exit --exit-code-from $1 $1 | sed $'s/^.*container exit...$/\033[0m\033[1A/'
