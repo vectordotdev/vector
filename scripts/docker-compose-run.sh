e091783f3c0354973410f8c3db343d18550c2ef3
Alexander Rodin <rodin.alexander@gmail.com>
2020-02-05 20:28:24 +0300

    Add more targets
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/docker-compose-run.sh
+++ scripts/docker-compose-run.sh
@@ -3,6 +3,6 @@ set -eu
 
 cd $(dirname $0)/..
 
-export USER=$(id -u):$(id -g)
+export USER=root # $(id -u):$(id -g)
 docker-compose rm -svf $1 2>/dev/null || true
 docker-compose up --build --abort-on-container-exit --exit-code-from $1 $1 | sed $'s/^.*container exit...$/\033[0m\033[1A/'
