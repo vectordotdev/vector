dc1a4396cde40c735ab936fa6306689fde5068d4
Alexander Rodin <rodin.alexander@gmail.com>
2020-02-05 23:23:20 +0300

    WIP
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- scripts/docker-compose-run.sh
+++ scripts/docker-compose-run.sh
@@ -10,6 +10,6 @@ set -euo pipefail
 
 cd $(dirname $0)/..
 
-export USER=root # $(id -u):$(id -g)
+export USER=$(id -u):$(id -g)
 docker-compose rm -svf $1 2>/dev/null || true
 docker-compose up --build --abort-on-container-exit --exit-code-from $1 $1 | sed $'s/^.*container exit...$/\033[0m\033[1A/'
