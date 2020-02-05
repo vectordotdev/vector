042e0219663863f993b9ac39a10f61435e0e72a2
Jakub Bednar <jakub.bednar@gmail.com>
2020-02-05 13:34:39 +0100

    feat(influxdb sink): encode_tags, encode_key
    Signed-off-by: Jakub Bednar <jakub.bednar@gmail.com>

--- src/sinks/influxdb_metrics.rs
+++ src/sinks/influxdb_metrics.rs
@@ -1,4 +1,67 @@
+use crate::{
+    event::metric::{Metric, MetricKind, MetricValue},
+};
 use chrono::{DateTime, Utc};
+use std::collections::HashMap;
+use std::collections::BTreeMap;
+
+fn encode_events(events: Vec<Metric>, namespace: &str) -> Vec<String> {
+    events
+        .into_iter()
+        .filter_map(|event| {
+            let fullname = encode_namespace(namespace, &event.name);
+            let ts = encode_timestamp(event.timestamp);
+            let tags = event.tags.clone();
+            match event.value {
+                MetricValue::Counter { value: _ } => Some(vec![influx_line_protocol(fullname, "counter", tags, ts)]),
+                _ => None
+            }
+        })
+        .flatten()
+        .collect()
+}
+
+fn influx_line_protocol(measurement: String, metric_type: &str, tags: Option<HashMap<String, String>>, timestamp: i64) -> String {
+    let mut line_protocol = vec![encode_key(measurement)];
+
+    // Tags
+    let mut unwrapped_tags = tags.unwrap_or(HashMap::new());
+    unwrapped_tags.insert("metric_type".to_owned(), metric_type.to_owned());
+    line_protocol.push(format!(",{}", encode_tags(unwrapped_tags)));
+
+    // Timestamp
+    line_protocol.push(format!(" {}", timestamp));
+
+    line_protocol.join("")
+}
+
+fn encode_key(key: String) -> String {
+    key.replace("\\", "\\\\")
+        .replace(",", "\\,")
+        .replace(" ", "\\ ")
+        .replace("=", "\\=")
+}
+
+fn encode_tags(tags: HashMap<String, String>) -> String {
+    let ordered: Vec<String> = tags
+        // sort by key
+        .iter().collect::<BTreeMap<_, _>>()
+        // map to key=value
+        .iter().map(|pair| {
+            let key = encode_key(pair.0.to_string());
+            let value = encode_key(pair.1.to_string());
+            if !key.is_empty() && !value.is_empty() {
+                format!("{}={}", key, value)
+            } else {
+                "".to_string()
+            }
+        })
+        // filter empty
+        .filter(|tag_value| !tag_value.is_empty())
+        .collect();
+
+    ordered.join(",")
+}
 
 fn encode_timestamp(timestamp: Option<DateTime<Utc>>) -> i64 {
     if let Some(ts) = timestamp {
@@ -26,6 +89,14 @@ mod tests {
         Utc.ymd(2018, 11, 14).and_hms_nano(8, 9, 10, 11)
     }
 
+    fn tags() -> HashMap<String, String> {
+        vec![
+            ("normal_tag".to_owned(), "value".to_owned()),
+            ("true_tag".to_owned(), "true".to_owned()),
+            ("empty_tag".to_owned(), "".to_owned()),
+        ].into_iter().collect()
+    }
+
     #[test]
     fn test_encode_timestamp() {
         let start = Utc::now().timestamp_nanos();
@@ -37,5 +108,53 @@ mod tests {
     fn test_encode_namespace() {
         assert_eq!(encode_namespace("services", "status"), "services.status");
         assert_eq!(encode_namespace("", "status"), "status")
+    }
+
+    #[test]
+    fn test_encode_key() {
+        assert_eq!(encode_key("measurement_name".to_string()), "measurement_name");
+        assert_eq!(encode_key("measurement name".to_string()), "measurement\\ name");
+        assert_eq!(encode_key("measurement=name".to_string()), "measurement\\=name");
+        assert_eq!(encode_key("measurement,name".to_string()), "measurement\\,name");
+    }
+
+    #[test]
+    fn test_encode_tags() {
+        assert_eq!(encode_tags(tags()), "normal_tag=value,true_tag=true");
+
+        let tags_to_escape = vec![
+            ("tag".to_owned(), "val=ue".to_owned()),
+            ("name escape".to_owned(), "true".to_owned()),
+            ("value_escape".to_owned(), "value escape".to_owned()),
+            ("a_first_place".to_owned(), "10".to_owned()),
+        ].into_iter().collect();
+
+        assert_eq!(encode_tags(tags_to_escape), "a_first_place=10,name\\ escape=true,tag=val\\=ue,value_escape=value\\ escape");
+    }
+
+    #[test]
+    fn encode_counter() {
+        let events = vec![
+            Metric {
+                name: "total".into(),
+                timestamp: Some(ts()),
+                tags: None,
+                kind: MetricKind::Incremental,
+                value: MetricValue::Counter { value: 1.5 },
+            },
+            Metric {
+                name: "check".into(),
+                timestamp: Some(ts()),
+                tags: Some(tags()),
+                kind: MetricKind::Incremental,
+                value: MetricValue::Counter { value: 1.0 },
+            },
+        ];
+
+        let line_protocols = encode_events(events, "ns");
+        assert_eq!(
+            line_protocols,
+            vec!["ns.total,metric_type=counter 1542182950000000011", "ns.check,metric_type=counter,normal_tag=value,true_tag=true 1542182950000000011", ]
+        );
     }
 }
