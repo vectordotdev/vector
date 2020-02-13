37109b80f40b1b454e6bf460671dddd429ccf93b
Ana Hobden <operator@hoverbear.org>
2020-02-13 10:32:56 -0800

    Fix a test
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/topology/config/mod.rs
+++ src/topology/config/mod.rs
@@ -497,9 +497,12 @@ mod test {
         )
         .unwrap();
 
-        assert_eq!("this", config.global.log_schema.host_key.to_string());
-        assert_eq!("that", config.global.log_schema.message_key.to_string());
-        assert_eq!("then", config.global.log_schema.timestamp_key.to_string());
+        assert_eq!("this", config.global.log_schema.host_key().to_string());
+        assert_eq!("that", config.global.log_schema.message_key().to_string());
+        assert_eq!(
+            "then",
+            config.global.log_schema().timestamp_key().to_string()
+        );
     }
 
     #[test]
