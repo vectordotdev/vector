1944ae977dd4143004434589ce4f27cb2714cbaf
gedkins <gedkins@bluecatnetworks.com>
2020-03-02 18:34:25 -0500

    feat(sources): Add http source using Warp (#1650)
    * feat(http source): add http source using Warp
    
    This is based off the logplex source. Code that may be reused by other http source plugins has been separated out into a util/http.rs.
    
    Supports text, json and ndjson body.
    
    Signed-off-by: Giles Edkins <gedkins@bluecatnetworks.com>
    
    * feat(http source): some tidy-ups to http source
    
    Signed-off-by: Giles Edkins <gedkins@bluecatnetworks.com>
    
    * feat(http source): more tidy-ups to http source
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * feat(http source): adding documentation
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * feat(http source): fixing format issues
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * docs: Schema-related fixes for http source
    
    Signed-off-by: Giles Edkins <gedkins@bluecatnetworks.com>
    
    * Update .meta/sources/http.toml
    
    Co-Authored-By: Ana Hobden <operator@hoverbear.org>
    Signed-off-by: Giles Edkins <gedkins@bluecatnetworks.com>
    
    * Run make generate
    
    Signed-off-by: Giles Edkins <gedkins@bluecatnetworks.com>
    
    * feat(sources): removing leak for http source
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * feat(http source): improvement to reading the body
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * feat(http source): switch to using decoding_eof and FullBody
    
    Signed-off-by: Bill Bateman <bbateman@bluecatnetworks.com>
    
    * Fix tests
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Fix tests
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Fix components
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    Co-authored-by: Bill <bbateman@bluecatnetworks.com>
    Co-authored-by: Ana Hobden <operator@hoverbear.org>

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ pipelines. Compared to Logstash and friends, Vector [improves throughput by
 
 #### Reference
 
-* [**Sources**][docs.sources] - [docker][docs.sources.docker], [file][docs.sources.file], [journald][docs.sources.journald], [kafka][docs.sources.kafka], [socket][docs.sources.socket], [syslog][docs.sources.syslog], and [6 more...][docs.sources]
+* [**Sources**][docs.sources] - [docker][docs.sources.docker], [file][docs.sources.file], [http][docs.sources.http], [journald][docs.sources.journald], [kafka][docs.sources.kafka], [socket][docs.sources.socket], and [7 more...][docs.sources]
 * [**Transforms**][docs.transforms] - [json_parser][docs.transforms.json_parser], [log_to_metric][docs.transforms.log_to_metric], [logfmt_parser][docs.transforms.logfmt_parser], [lua][docs.transforms.lua], [regex_parser][docs.transforms.regex_parser], [sampler][docs.transforms.sampler], and [16 more...][docs.transforms]
 * [**Sinks**][docs.sinks] - [aws_cloudwatch_logs][docs.sinks.aws_cloudwatch_logs], [aws_s3][docs.sinks.aws_s3], [clickhouse][docs.sinks.clickhouse], [elasticsearch][docs.sinks.elasticsearch], [gcp_cloud_storage][docs.sinks.gcp_cloud_storage], [gcp_pubsub][docs.sinks.gcp_pubsub], and [21 more...][docs.sinks]
 
@@ -241,10 +241,10 @@ Or use your own [preferred method][docs.installation].
 [docs.sources.docker]: https://vector.dev/docs/reference/sources/docker/
 [docs.sources.file#multiline]: https://vector.dev/docs/reference/sources/file/#multiline
 [docs.sources.file]: https://vector.dev/docs/reference/sources/file/
+[docs.sources.http]: https://vector.dev/docs/reference/sources/http/
 [docs.sources.journald]: https://vector.dev/docs/reference/sources/journald/
 [docs.sources.kafka]: https://vector.dev/docs/reference/sources/kafka/
 [docs.sources.socket]: https://vector.dev/docs/reference/sources/socket/
-[docs.sources.syslog]: https://vector.dev/docs/reference/sources/syslog/
 [docs.sources]: https://vector.dev/docs/reference/sources/
 [docs.topologies]: https://vector.dev/docs/setup/deployment/topologies/
 [docs.transforms.grok_parser]: https://vector.dev/docs/reference/transforms/grok_parser/
---
+++ src/sources/http.rs
@@ -0,0 +1,412 @@
+use crate::{
+    event::{self, Event},
+    sources::util::{ErrorMessage, HttpSource},
+    topology::config::{DataType, GlobalOptions, SourceConfig, SourceDescription},
+};
+use bytes::{Buf, Bytes, BytesMut};
+use chrono::Utc;
+use codec::{self, BytesDelimitedCodec};
+use futures01::sync::mpsc;
+use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+use std::net::SocketAddr;
+use tokio_codec::Decoder;
+use warp::filters::body::FullBody;
+use warp::http::{HeaderMap, HeaderValue, StatusCode};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct SimpleHttpConfig {
+    address: SocketAddr,
+    #[serde(default)]
+    encoding: Encoding,
+    #[serde(default)]
+    headers: Vec<String>,
+}
+
+inventory::submit! {
+    SourceDescription::new_without_default::<SimpleHttpConfig>("http")
+}
+
+#[derive(Clone)]
+struct SimpleHttpSource {
+    encoding: Encoding,
+    headers: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq, Clone, Derivative, Copy)]
+#[serde(rename_all = "snake_case")]
+#[derivative(Default)]
+pub enum Encoding {
+    #[derivative(Default)]
+    Text,
+    Ndjson,
+    Json,
+}
+
+impl HttpSource for SimpleHttpSource {
+    fn build_event(
+        &self,
+        body: FullBody,
+        header_map: HeaderMap,
+    ) -> Result<Vec<Event>, ErrorMessage> {
+        decode_body(body, self.encoding)
+            .map(|events| add_headers(events, &self.headers, header_map))
+    }
+}
+
+#[typetag::serde(name = "http")]
+impl SourceConfig for SimpleHttpConfig {
+    fn build(
+        &self,
+        _: &str,
+        _: &GlobalOptions,
+        out: mpsc::Sender<Event>,
+    ) -> crate::Result<super::Source> {
+        let source = SimpleHttpSource {
+            encoding: self.encoding,
+            headers: self.headers.clone(),
+        };
+        source.run(self.address, "", out)
+    }
+
+    fn output_type(&self) -> DataType {
+        DataType::Log
+    }
+
+    fn source_type(&self) -> &'static str {
+        "http"
+    }
+}
+
+fn add_headers(
+    mut events: Vec<Event>,
+    headers_config: &[String],
+    headers: HeaderMap,
+) -> Vec<Event> {
+    for header_name in headers_config {
+        let value = headers
+            .get(header_name)
+            .map(HeaderValue::as_bytes)
+            .unwrap_or_default();
+        for event in events.iter_mut() {
+            event.as_mut_log().insert(header_name as &str, value);
+        }
+    }
+
+    events
+}
+
+fn body_to_lines(mut body: BytesMut) -> impl Iterator<Item = Result<Bytes, ErrorMessage>> {
+    let mut decoder = BytesDelimitedCodec::new(b'\n');
+    std::iter::from_fn(move || {
+        match decoder.decode_eof(&mut body) {
+            Err(e) => Some(Err(ErrorMessage::new(
+                StatusCode::BAD_REQUEST,
+                format!("Bad request: {}", e),
+            ))),
+            Ok(Some(b)) => Some(Ok(b)),
+            Ok(None) => None, //actually done
+        }
+    })
+    .filter(|s| match s {
+        //filter empty lines
+        Ok(b) => !b.is_empty(),
+        _ => true,
+    })
+}
+
+fn decode_body(buf: FullBody, enc: Encoding) -> Result<Vec<Event>, ErrorMessage> {
+    let body = buf.collect::<BytesMut>();
+
+    match enc {
+        Encoding::Text => body_to_lines(body)
+            .map(|r| Ok(Event::from(r?)))
+            .collect::<Result<_, _>>(),
+        Encoding::Ndjson => body_to_lines(body)
+            .map(|j| {
+                let parsed_json = serde_json::from_slice(&j?)
+                    .map_err(|e| json_error(format!("Error parsing Ndjson: {:?}", e)))?;
+                json_parse_object(parsed_json)
+            })
+            .collect::<Result<_, _>>(),
+        Encoding::Json => {
+            let parsed_json = serde_json::from_slice(&body)
+                .map_err(|e| json_error(format!("Error parsing Json: {:?}", e)))?;
+            json_parse_array_of_object(parsed_json)
+        }
+    }
+}
+
+fn json_parse_object(value: JsonValue) -> Result<Event, ErrorMessage> {
+    let mut event = Event::new_empty_log();
+    let log = event.as_mut_log();
+    log.insert(event::log_schema().timestamp_key().clone(), Utc::now()); // Add timestamp
+    match value {
+        JsonValue::Object(map) => {
+            for (k, v) in map {
+                log.insert(k, v);
+            }
+            Ok(event)
+        }
+        _ => Err(json_error(format!(
+            "Expected Object, got {}",
+            json_value_to_type_string(&value)
+        ))),
+    }
+}
+
+fn json_parse_array_of_object(value: JsonValue) -> Result<Vec<Event>, ErrorMessage> {
+    match value {
+        JsonValue::Array(v) => v
+            .into_iter()
+            .map(json_parse_object)
+            .collect::<Result<_, _>>(),
+        JsonValue::Object(map) => {
+            //treat like an array of one object
+            Ok(vec![json_parse_object(JsonValue::Object(map))?])
+        }
+        _ => Err(json_error(format!(
+            "Expected Array or Object, got {}.",
+            json_value_to_type_string(&value)
+        ))),
+    }
+}
+
+fn json_error(s: String) -> ErrorMessage {
+    ErrorMessage::new(StatusCode::BAD_REQUEST, format!("Bad JSON: {}", s))
+}
+fn json_value_to_type_string(value: &JsonValue) -> &'static str {
+    match value {
+        JsonValue::Object(_) => "Object",
+        JsonValue::Array(_) => "Array",
+        JsonValue::String(_) => "String",
+        JsonValue::Number(_) => "Number",
+        JsonValue::Bool(_) => "Bool",
+        JsonValue::Null => "Null",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Encoding, SimpleHttpConfig};
+    use warp::http::HeaderMap;
+
+    use crate::{
+        event::{self, Event},
+        runtime::Runtime,
+        test_util::{self, collect_n},
+        topology::config::{GlobalOptions, SourceConfig},
+    };
+    use futures01::sync::mpsc;
+    use http::Method;
+    use pretty_assertions::assert_eq;
+    use std::net::SocketAddr;
+    use string_cache::DefaultAtom as Atom;
+
+    fn source(
+        rt: &mut Runtime,
+        encoding: Encoding,
+        headers: Vec<String>,
+    ) -> (mpsc::Receiver<Event>, SocketAddr) {
+        test_util::trace_init();
+        let (sender, recv) = mpsc::channel(100);
+        let address = test_util::next_addr();
+        rt.spawn(
+            SimpleHttpConfig {
+                address,
+                encoding,
+                headers,
+            }
+            .build("default", &GlobalOptions::default(), sender)
+            .unwrap(),
+        );
+        (recv, address)
+    }
+
+    fn send(address: SocketAddr, body: &str) -> u16 {
+        reqwest::Client::new()
+            .request(Method::POST, &format!("http://{}/", address))
+            .body(body.to_owned())
+            .send()
+            .unwrap()
+            .status()
+            .as_u16()
+    }
+
+    fn send_with_headers(address: SocketAddr, body: &str, headers: HeaderMap) -> u16 {
+        reqwest::Client::new()
+            .request(Method::POST, &format!("http://{}/", address))
+            .headers(headers)
+            .body(body.to_owned())
+            .send()
+            .unwrap()
+            .status()
+            .as_u16()
+    }
+
+    #[test]
+    fn http_multiline_text() {
+        let body = "test body\n\ntest body 2";
+
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(&mut rt, Encoding::default(), vec![]);
+
+        assert_eq!(200, send(addr, body));
+
+        let mut events = rt.block_on(collect_n(rx, 2)).unwrap();
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&event::log_schema().message_key()], "test body".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(
+                log[&event::log_schema().message_key()],
+                "test body 2".into()
+            );
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+    }
+
+    #[test]
+    fn http_multiline_text2() {
+        //same as above test but with a newline at the end
+        let body = "test body\n\ntest body 2\n";
+
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(&mut rt, Encoding::default(), vec![]);
+
+        assert_eq!(200, send(addr, body));
+
+        let mut events = rt.block_on(collect_n(rx, 2)).unwrap();
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&event::log_schema().message_key()], "test body".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(
+                log[&event::log_schema().message_key()],
+                "test body 2".into()
+            );
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+    }
+
+    #[test]
+    fn http_json_parsing() {
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(&mut rt, Encoding::Json, vec![]);
+
+        assert_eq!(400, send(addr, "{")); //malformed
+        assert_eq!(400, send(addr, r#"{"key"}"#)); //key without value
+
+        assert_eq!(200, send(addr, "{}")); //can be one object or array of objects
+        assert_eq!(200, send(addr, "[{},{},{}]"));
+
+        let mut events = rt.block_on(collect_n(rx, 2)).unwrap();
+        assert!(events
+            .remove(1)
+            .as_log()
+            .get(&event::log_schema().timestamp_key())
+            .is_some());
+        assert!(events
+            .remove(0)
+            .as_log()
+            .get(&event::log_schema().timestamp_key())
+            .is_some());
+    }
+
+    #[test]
+    fn http_json_values() {
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(&mut rt, Encoding::Json, vec![]);
+
+        assert_eq!(200, send(addr, r#"[{"key":"value"}]"#));
+        assert_eq!(200, send(addr, r#"{"key2":"value2"}"#));
+
+        let mut events = rt.block_on(collect_n(rx, 2)).unwrap();
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&Atom::from("key")], "value".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&Atom::from("key2")], "value2".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+    }
+
+    #[test]
+    fn http_ndjson() {
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(&mut rt, Encoding::Ndjson, vec![]);
+
+        assert_eq!(400, send(addr, r#"[{"key":"value"}]"#)); //one object per line
+
+        assert_eq!(
+            200,
+            send(addr, "{\"key1\":\"value1\"}\n\n{\"key2\":\"value2\"}")
+        );
+
+        let mut events = rt.block_on(collect_n(rx, 2)).unwrap();
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&Atom::from("key1")], "value1".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&Atom::from("key2")], "value2".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+    }
+
+    #[test]
+    fn http_headers() {
+        let mut headers = HeaderMap::new();
+        headers.insert("User-Agent", "test_client".parse().unwrap());
+        headers.insert("Upgrade-Insecure-Requests", "false".parse().unwrap());
+
+        let mut rt = test_util::runtime();
+        let (rx, addr) = source(
+            &mut rt,
+            Encoding::Ndjson,
+            vec![
+                "User-Agent".to_string(),
+                "Upgrade-Insecure-Requests".to_string(),
+                "AbsentHeader".to_string(),
+            ],
+        );
+
+        assert_eq!(
+            200,
+            send_with_headers(addr, "{\"key1\":\"value1\"}", headers)
+        );
+
+        let mut events = rt.block_on(collect_n(rx, 1)).unwrap();
+        {
+            let event = events.remove(0);
+            let log = event.as_log();
+            assert_eq!(log[&Atom::from("key1")], "value1".into());
+            assert_eq!(log[&Atom::from("User-Agent")], "test_client".into());
+            assert_eq!(
+                log[&Atom::from("Upgrade-Insecure-Requests")],
+                "false".into()
+            );
+            assert_eq!(log[&Atom::from("AbsentHeader")], "".into());
+            assert!(log.get(&event::log_schema().timestamp_key()).is_some());
+        }
+    }
+}
