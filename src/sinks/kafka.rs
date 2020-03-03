cd8350df0eb97eaf8139fac81e1d01e7db5b115f
Ana Hobden <operator@hoverbear.org>
2020-03-03 10:06:26 -0800

    Fix kafka encoding default
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/sinks/kafka.rs
+++ src/sinks/kafka.rs
@@ -333,7 +333,7 @@ mod integration_test {
         let config = KafkaSinkConfig {
             bootstrap_servers: server.to_string(),
             topic: topic.clone(),
-            encoding: EncodingConfig::from(Encoding::Text),
+            encoding: EncodingConfig::from(Encoding::Json),
             key_field: None,
             tls,
             socket_timeout_ms: 60000,
