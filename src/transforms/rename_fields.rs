623b1a63be6d218a9d63d10f354480db09c88712
Ana Hobden <operator@hoverbear.org>
2020-03-16 12:05:00 -0700

    Introduce fieldsorvalue
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/transforms/rename_fields.rs
+++ src/transforms/rename_fields.rs
@@ -1,18 +1,17 @@
 use super::Transform;
 use crate::{
     event::Event,
+    serde::Fields,
     topology::config::{DataType, TransformConfig, TransformContext, TransformDescription},
 };
 use indexmap::map::IndexMap;
 use serde::{Deserialize, Serialize};
-use snafu::Snafu;
 use string_cache::DefaultAtom as Atom;
-use toml::value::Value as TomlValue;
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct RenameFieldsConfig {
-    pub fields: IndexMap<String, TomlValue>,
+    pub fields: Fields<String>,
 }
 
 pub struct RenameFields {
@@ -26,7 +25,13 @@ inventory::submit! {
 #[typetag::serde(name = "rename_fields")]
 impl TransformConfig for RenameFieldsConfig {
     fn build(&self, _exec: TransformContext) -> crate::Result<Box<dyn Transform>> {
-        Ok(Box::new(RenameFields::new(self.fields.clone())?))
+        Ok(Box::new(RenameFields::new(
+            self.fields
+                .clone()
+                .all_fields()
+                .map(|(k, v)| (k.into(), v.into()))
+                .collect(),
+        )?))
     }
 
     fn input_type(&self) -> DataType {
@@ -43,49 +48,8 @@ impl TransformConfig for RenameFieldsConfig {
 }
 
 impl RenameFields {
-    pub fn new(fields: IndexMap<String, TomlValue>) -> crate::Result<Self> {
-        Ok(RenameFields {
-            fields: fields
-                .into_iter()
-                .map(|kv| flatten(kv, None))
-                .collect::<crate::Result<_>>()?,
-        })
-    }
-}
-
-#[derive(Debug, Eq, PartialEq, Snafu)]
-enum FlattenError {
-    #[snafu(display(
-        "The key {:?} cannot be flattened. Is it a plain string or a `a.b.c` style map?",
-        key
-    ))]
-    CannotFlatten { key: String },
-}
-
-fn flatten(kv: (String, TomlValue), prequel: Option<String>) -> crate::Result<(Atom, Atom)> {
-    let (k, v) = kv;
-    match v {
-        TomlValue::String(s) => match prequel {
-            Some(prequel) => Ok((format!("{}.{}", prequel, k).into(), s.into())),
-            None => Ok((k.into(), s.into())),
-        },
-        TomlValue::Table(map) => {
-            if map.len() > 1 {
-                Err(Box::new(FlattenError::CannotFlatten { key: k }))
-            } else {
-                let sub_kv = map.into_iter().next().expect("Map of len 1 has no values");
-                let key = match prequel {
-                    Some(prequel) => format!("{}.{}", prequel, k),
-                    None => k,
-                };
-                flatten(sub_kv, Some(key))
-            }
-        }
-        TomlValue::Integer(_)
-        | TomlValue::Float(_)
-        | TomlValue::Boolean(_)
-        | TomlValue::Datetime(_)
-        | TomlValue::Array(_) => Err(Box::new(FlattenError::CannotFlatten { key: k })),
+    pub fn new(fields: IndexMap<Atom, Atom>) -> crate::Result<Self> {
+        Ok(RenameFields { fields })
     }
 }
 
