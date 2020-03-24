3c97d45af2b4a3e4a3719b8a3637eae013d38dcc
MOZGIII <mike-n@narod.ru>
2020-03-25 00:53:06 +0300

    Add TrailingNewlineNormalizer
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- src/transforms/merge.rs
+++ src/transforms/merge.rs
@@ -232,6 +232,39 @@ impl NormalizeLogEvent for PartialEventMarkerFieldNormalizer {
     }
 }
 
+/// A [`NormalizeLogEvent`] implementation that performs pariality detection
+/// based on the presence of the trailing newline at the `probe_field` of the
+/// event.
+///
+/// If the event has `probe_field` field, and it's a string value that DOES NOT
+/// contain a newline (`\n`) at the end, we consider that event partial.
+/// To normalize the event, we remove the trailing newline.
+/// If the event
+/// - has no `probe_field` field, or
+/// - has it, but the values is not a string, or
+/// - it is a string that DOES contain a trailing newline
+/// we consider the event non-partial.
+#[derive(Debug, Clone, PartialEq)]
+pub struct TrailingNewlineNormalizer {
+    pub probe_field: Atom,
+}
+
+impl NormalizeLogEvent for TrailingNewlineNormalizer {
+    fn normalize(&self, mut event: LogEvent) -> MaybePartialLogEvent {
+        // See `TrailingNewlineNormalizer` documentation for logic description.
+
+        if let Some(Value::Bytes(s)) = event.get_mut(&self.probe_field) {
+            if s.ends_with(&[b'\n']) {
+                s.truncate(s.len() - 1);
+            } else {
+                return MaybePartialLogEvent::Partial(event);
+            }
+        }
+
+        MaybePartialLogEvent::NonPartial(event)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::{Merge, MergeConfig};
