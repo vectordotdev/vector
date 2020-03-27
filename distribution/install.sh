de932f7f2a345923476bbaef1cbd44d91ce06870
Alexander Rodin <rodin.alexander@gmail.com>
2020-03-27 22:59:40 +0300

    chore: Support ARM architectures in the install script (#2167)
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- distribution/install.sh
+++ distribution/install.sh
@@ -124,6 +124,12 @@ install_from_archive() {
         x86_64-*linux*)
             _archive_arch="x86_64-unknown-linux-musl"
             ;;
+        armv7-*linux*hf)
+            _archive_arch="armv7-unknown-linux-musleabihf"
+            ;;
+        aarch64-*linux*)
+            _archive_arch="aarch64-unknown-linux-musl"
+            ;;
         *)
             err "unsupported arch: $_arch"
             ;;
