b4b10305d7c129e647c11bcd4395d971c2f1cbd9
Ana Hobden <operator@hoverbear.org>
2020-04-22 15:42:05 -0700

    Format
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/sinks/util/encoding.rs
+++ src/sinks/util/encoding.rs
@@ -29,7 +29,10 @@
 //       `Encoder` that defines some `encode` function which this config then calls internally as
 //       part of it's own (yet to be written) `encode() -> Vec<u8>` function.
 
-use crate::{event::{Value, PathIter}, Event, Result};
+use crate::{
+    event::{PathIter, Value},
+    Event, Result,
+};
 use serde::de::{MapAccess, Visitor};
 use serde::{
     de::{self, DeserializeOwned, Deserializer, IntoDeserializer},
@@ -158,22 +161,20 @@ pub trait EncodingConfiguration<E> {
                         .keys()
                         .filter(|field| {
                             let field_path = PathIter::new(field).collect::<Vec<_>>();
-                            !only_fields
-                                .iter()
-                                .any(|only| {
-                                    if only == field {
-                                        true // fastpath
+                            !only_fields.iter().any(|only| {
+                                if only == field {
+                                    true // fastpath
+                                } else {
+                                    // TODO: This is a hack for #2407, #2410 should fix this fully.
+                                    // This is *slow*.
+                                    let only_path = PathIter::new(only);
+                                    if field_path.starts_with(&only_path.collect::<Vec<_>>()[..]) {
+                                        true
                                     } else {
-                                        // TODO: This is a hack for #2407, #2410 should fix this fully.
-                                        // This is *slow*.
-                                        let only_path = PathIter::new(only);
-                                        if field_path.starts_with(&only_path.collect::<Vec<_>>()[..]) {
-                                            true
-                                        } else {
-                                            false
-                                        }
+                                        false
                                     }
-                                })
+                                }
+                            })
                         })
                         .collect::<VecDeque<_>>();
                     for removal in to_remove {
