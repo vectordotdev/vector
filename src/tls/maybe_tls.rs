8e8c8f6e258ec312ac6a4bc47eab72161080385d
Lucio Franco <luciofranco14@gmail.com>
2020-04-23 17:32:19 -0400

    fix(socket sink): Properly detect shutdown
    This change fixes how we handle when the server side of the TCP
    connection within the socket sink handles _proper_ shutdowns. A proper
    shutdown is when the server side calls `shutdown(2)` with the `SHUT_WR`
    flag. This then produces an EOF on the read side which we can detect to
    start the reconnect process before we attempt to write the next event.
    
    Currently, there is the possibility that an event will get dropped if we
    don't detect this EOF and attempt to write. This has been fixed and we
    now have a test to detect that we don't lose events if the server
    shutsdown properly. There is still a case where if the read returns
    `WouldBlock` and the write fails we will still lose events but this is
    an improper shutdown and should be extremely rare.
    
    Fixes #2406
    
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- src/tls/maybe_tls.rs
+++ src/tls/maybe_tls.rs
@@ -32,6 +32,13 @@ impl<R, T> MaybeTls<R, T> {
             Self::Tls(tls) => Some(&tls),
         }
     }
+
+    pub fn tls_mut(&mut self) -> Option<&mut T> {
+        match self {
+            Self::Raw(_) => None,
+            Self::Tls(tls) => Some(tls),
+        }
+    }
 }
 
 impl<T> From<Option<T>> for MaybeTls<(), T> {
