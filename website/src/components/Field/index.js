3ae92ee0ea4d516121a3c7bd97bc3f581ab9ac02
binarylogic <bjohnson@binarylogic.com>
2020-04-25 18:06:17 -0400

    chore(website): Ensure enums are displayed
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- website/src/components/Field/index.js
+++ website/src/components/Field/index.js
@@ -1,30 +1,30 @@
-import React, {useState} from 'react';
-import classnames from 'classnames';
-import {MDXProvider} from '@mdx-js/react';
-import CodeBlock from '@theme/CodeBlock';
+import React, { useState } from "react";
+import classnames from "classnames";
+import { MDXProvider } from "@mdx-js/react";
+import CodeBlock from "@theme/CodeBlock";
 
 //
 // Misc
 //
 
 function isObject(a) {
-  return (!!a) && (a.constructor === Object);
-};
+  return !!a && a.constructor === Object;
+}
 
 //
 // TOML
 //
 
 function keyToTOML(key) {
-  if ( key.includes(".") ) {
-    return "\"" + key + "\"";
+  if (key.includes(".")) {
+    return '"' + key + '"';
   } else {
     return key;
   }
 }
 
 function valueToTOML(value) {
-  if (typeof(value) == "string" && value.includes("\n")) {
+  if (typeof value == "string" && value.includes("\n")) {
     return `"""
 ${value}
 """`;
@@ -35,10 +35,12 @@ ${value}
 
 function kvToTOML(name, example) {
   if (isObject(example)) {
-    if ('name' in example && 'value' in example) {
+    if ("name" in example && "value" in example) {
       return `${keyToTOML(example.name)} = ${valueToTOML(example.value)}`;
     } else {
-      return `${keyToTOML(Object.keys(example)[0])} = ${valueToTOML(Object.values(example)[0])}`
+      return `${keyToTOML(Object.keys(example)[0])} = ${valueToTOML(
+        Object.values(example)[0]
+      )}`;
     }
   } else if (name) {
     return `${keyToTOML(name)} = ${valueToTOML(example)}`;
@@ -51,12 +53,16 @@ function kvToTOML(name, example) {
 // Enum
 //
 
-function Enum({values}) {
+function Enum({ values }) {
   let elements = [];
 
   if (!Array.isArray(values)) {
     for (var key in values) {
-      elements.push(<code key={key} className="with-info-icon" title={values[key]}>{valueToTOML(key)}</code>);
+      elements.push(
+        <code key={key} className="with-info-icon" title={values[key]}>
+          {valueToTOML(key)}
+        </code>
+      );
       elements.push(" ");
     }
   } else {
@@ -74,32 +80,35 @@ function Enum({values}) {
 // Examples
 //
 
-function Example({name, path, unit, value}) {
-  let unitText = '';
+function Example({ name, path, unit, value }) {
+  let unitText = "";
 
   if (unit) {
     unitText = <> ({unit})</>;
   }
 
-  return <><code>{valueToTOML(value)}</code>{unitText}</>;
+  return (
+    <>
+      <code>{valueToTOML(value)}</code>
+      {unitText}
+    </>
+  );
 }
 
-function Examples({name, path, values}) {
-  let code = '';
+function Examples({ name, path, values }) {
+  let code = "";
 
   values.forEach(function (value) {
     if (path) {
       code += `${path}.`;
     }
 
-    code += (kvToTOML(name, value) + "\n");
+    code += kvToTOML(name, value) + "\n";
   });
 
   return (
     <div>
-      <CodeBlock className="language-toml">
-        {code}
-      </CodeBlock>
+      <CodeBlock className="language-toml">{code}</CodeBlock>
     </div>
   );
 }
@@ -108,7 +117,7 @@ function Examples({name, path, values}) {
 // Groups
 //
 
-function Groups({values}) {
+function Groups({ values }) {
   let elements = [];
 
   for (var index in values) {
@@ -124,20 +133,25 @@ function Groups({values}) {
 // Values
 //
 
-function Value({unit, value}) {
-  let unitText = '';
+function Value({ unit, value }) {
+  let unitText = "";
 
   if (unit) {
     unitText = <> ({unit})</>;
   }
 
-  return <><code>{valueToTOML(value)}</code>{unitText}</>;
+  return (
+    <>
+      <code>{valueToTOML(value)}</code>
+      {unitText}
+    </>
+  );
 }
 
-function Values({values}) {
+function Values({ values }) {
   let elements = [];
 
-  values.forEach(value => elements.push(<Value value={value} />));
+  values.forEach((value) => elements.push(<Value value={value} />));
 
   return elements;
 }
@@ -146,7 +160,7 @@ function Values({values}) {
 // Relevance
 //
 
-function RelevantWhen({value}) {
+function RelevantWhen({ value }) {
   let relKey = Object.keys(value)[0];
   let relValue = Object.values(value)[0];
 
@@ -156,7 +170,10 @@ function RelevantWhen({value}) {
 
   return (
     <span>
-      <code><a href={`#${relKey}`}>{relKey}</a></code> = <code>{valueToTOML(relValue)}</code>
+      <code>
+        <a href={`#${relKey}`}>{relKey}</a>
+      </code>{" "}
+      = <code>{valueToTOML(relValue)}</code>
     </span>
   );
 }
@@ -165,59 +182,138 @@ function RelevantWhen({value}) {
 // Fields
 //
 
-function FieldFooter({defaultValue, enumValues, examples, groups, name, path, relevantWhen, required, unit, warnings}) {
+function FieldFooter({
+  defaultValue,
+  enumValues,
+  examples,
+  groups,
+  name,
+  path,
+  relevantWhen,
+  required,
+  unit,
+  warnings,
+}) {
   const [showExamples, setShowExamples] = useState(false);
 
-  if (defaultValue || enumValues || (examples && examples.length > 0) || (groups && groups.length > 0)) {
-    return (
-      <ul className="info">
-        {warnings && warnings.length > 0 && warnings.map((warning, idx) => (
+  return (
+    <ul className="info">
+      {warnings &&
+        warnings.length > 0 &&
+        warnings.map((warning, idx) => (
           <li key={idx} className="warning">
-            <i className="feather icon-alert-triangle"></i> WARNING: {warning.text}
+            <i className="feather icon-alert-triangle"></i> WARNING:{" "}
+            {warning.text}
           </li>
         ))}
-        {relevantWhen && <li>Only {required ? 'required' : 'relevant'} when: <RelevantWhen value={relevantWhen} /></li>}
-        {defaultValue !== undefined ?
-          (defaultValue !== null ?
-            <li>Default: <Value unit={unit} value={defaultValue} /></li> :
-            <li>No default</li>) :
-          null}
-        {enumValues && enumValues.length > 0 && <li>Enum, must be one of: <Enum values={enumValues} /></li>}
+      {relevantWhen && (
         <li>
-          <div className="show-more" onClick={() => setShowExamples(!showExamples)}>
-            {showExamples ? "Hide examples" : "View examples"}
-          </div>
-          {showExamples && <Examples name={name} path={path} values={examples} />}
+          Only {required ? "required" : "relevant"} when:{" "}
+          <RelevantWhen value={relevantWhen} />
         </li>
-      </ul>
-    );
-  } else {
-    return null;
-  }
+      )}
+      {defaultValue !== undefined ? (
+        defaultValue !== null ? (
+          <li>
+            Default: <Value unit={unit} value={defaultValue} />
+          </li>
+        ) : (
+          <li>No default</li>
+        )
+      ) : null}
+      {enumValues && (
+        <li>
+          Enum, must be one of: <Enum values={enumValues} />
+        </li>
+      )}
+      <li>
+        <div
+          className="show-more"
+          onClick={() => setShowExamples(!showExamples)}
+        >
+          {showExamples ? "Hide examples" : "View examples"}
+        </div>
+        {showExamples && <Examples name={name} path={path} values={examples} />}
+      </li>
+    </ul>
+  );
 }
 
-function Field({children, common, defaultValue, enumValues, examples, groups, name, path, relevantWhen, required, templateable, type, unit, warnings}) {
+function Field({
+  children,
+  common,
+  defaultValue,
+  enumValues,
+  examples,
+  groups,
+  name,
+  path,
+  relevantWhen,
+  required,
+  templateable,
+  type,
+  unit,
+  warnings,
+}) {
   const [collapse, setCollapse] = useState(false);
 
   let filteredChildren = children;
 
   if (collapse) {
-    filteredChildren = filteredChildren.filter(child => child.props.originalType != 'p');
+    filteredChildren = filteredChildren.filter(
+      (child) => child.props.originalType != "p"
+    );
   }
 
   return (
-    <li className={classnames({'field-required': required, 'field-collapsed': collapse})} required={required}>
+    <li
+      className={classnames({
+        "field-required": required,
+        "field-collapsed": collapse,
+      })}
+      required={required}
+    >
       <div className="badges">
-        {templateable && <span className="badge badge--primary with-info-icon" title="This option is dynamic and accepts the Vector template syntax">templateable</span>}
-        {type && <span className="badge badge--secondary">{type}{unit && <> ({unit})</>}</span>}
-        {enumValues && Object.keys(enumValues).length > 0 && <span className="badge badge--secondary with-info-icon" title="This option is an enumation and only allows specific values">enum</span>}
-        {common && <span className="badge badge--primary with-info-icon" title="This is a popular that we recommend for getting started">common</span>}
-        {required ?
-          <span className="badge badge--danger">required{relevantWhen && '*'}</span> :
-          <span className="badge badge--secondary">optional</span>}
+        {templateable && (
+          <span
+            className="badge badge--primary with-info-icon"
+            title="This option is dynamic and accepts the Vector template syntax"
+          >
+            templateable
+          </span>
+        )}
+        {type && (
+          <span className="badge badge--secondary">
+            {type}
+            {unit && <> ({unit})</>}
+          </span>
+        )}
+        {enumValues && Object.keys(enumValues).length > 0 && (
+          <span
+            className="badge badge--secondary with-info-icon"
+            title="This option is an enumation and only allows specific values"
+          >
+            enum
+          </span>
+        )}
+        {common && (
+          <span
+            className="badge badge--primary with-info-icon"
+            title="This is a popular that we recommend for getting started"
+          >
+            common
+          </span>
+        )}
+        {required ? (
+          <span className="badge badge--danger">
+            required{relevantWhen && "*"}
+          </span>
+        ) : (
+          <span className="badge badge--secondary">optional</span>
+        )}
       </div>
       {filteredChildren}
-      {!collapse && type != "table" &&
+      {!collapse && type != "table" && (
         <FieldFooter
           defaultValue={defaultValue}
           enumValues={enumValues}
@@ -228,7 +324,9 @@ function Field({children, common, defaultValue, enumValues, examples, groups, na
           relevantWhen={relevantWhen}
           required={required}
           unit={unit}
-          warnings={warnings} />}
+          warnings={warnings}
+        />
+      )}
     </li>
   );
 }
