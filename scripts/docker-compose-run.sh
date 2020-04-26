7c7a1c63bbd1105776f35d2cc7b955be89aa0db7
binarylogic <bjohnson@binarylogic.com>
2020-04-26 15:08:58 -0400

    chore(operations): Allow the invocation of local script from the tests/Makefile
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- scripts/docker-compose-run.sh
+++ scripts/docker-compose-run.sh
@@ -8,8 +8,7 @@
 
 set -euo pipefail
 
-cd $(dirname $0)/..
-
 export USER=$(id -u):$(id -g)
-docker-compose rm -svf $1 2>/dev/null || true
-docker-compose up --build --abort-on-container-exit --exit-code-from $1 $1 | sed $'s/^.*container exit...$/\033[0m\033[1A/'
+DOCKER=${USE_CONTAINER:-docker}
+${DOCKER}-compose rm -svf $1 2>/dev/null || true
+${DOCKER}-compose up --build --abort-on-container-exit --exit-code-from $1 $1 | sed $'s/^.*container exit...$/\033[0m\033[1A/'
