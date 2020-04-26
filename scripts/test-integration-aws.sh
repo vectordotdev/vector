91d6c34e3310a0c9e95f07533b1b814511bbfa3c
binarylogic <bjohnson@binarylogic.com>
2020-04-26 16:46:53 -0400

    Point aws integration test to a local script
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

---
+++ scripts/test-integration-aws.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# test-integration-aws.sh
+#
+# SUMMARY
+#
+#   Run integration tests for AWS components only.
+
+docker-compose up -d dependencies-aws
+cargo test --no-default-features --features cloudwatch-logs-integration-tests,cloudwatch-metrics-integration-tests,ec2-metadata-integration-tests,firehose-integration-tests,kinesis-integration-tests,s3-integration-tests
