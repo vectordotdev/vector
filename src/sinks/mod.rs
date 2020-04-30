c91298092eb642e51110d8bcaaa06bfe42adc6f9
Adrien Pestel <pestouille@gmail.com>
2020-04-30 21:53:19 +0200

    WIP SignalFx metrics sink
    Signed-off-by: Adrien Pestel <pestouille@gmail.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/sinks/mod.rs
+++ src/sinks/mod.rs
@@ -51,6 +51,8 @@ pub mod prometheus;
 pub mod pulsar;
 #[cfg(feature = "sinks-sematext_logs")]
 pub mod sematext_logs;
+#[cfg(feature = "sinks-signalfx")]
+pub mod signalfx;
 #[cfg(feature = "sinks-socket")]
 pub mod socket;
 #[cfg(feature = "sinks-splunk_hec")]
