746792f79f64316214de9fee8d7ba6dfaf6f6cae
Kruno Tomola Fabro <krunotf@gmail.com>
2020-05-06 10:18:04 +0200

    chore(stdin source): Wire `ShutdownSignal` (#2366)
    * Wire stdin source
    
    Signed-off-by: ktf <krunotf@gmail.com>
    
    * Use mpmc channel with one background thread
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Fix removing sender
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add minor fixes
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>

--- src/sources/stdin.rs
+++ src/sources/stdin.rs
@@ -1,12 +1,27 @@
 use crate::{
     event::{self, Event},
     shutdown::ShutdownSignal,
+    stream::StreamExt,
     topology::config::{DataType, GlobalOptions, SourceConfig, SourceDescription},
 };
 use bytes::Bytes;
-use futures01::{future, sync::mpsc, Future, Sink, Stream};
+use futures::compat::Compat;
+use futures01::{sync::mpsc, Future, Sink, Stream};
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
-use std::{io, thread, time::Duration};
+use snafu::Snafu;
+use std::{io, sync::Mutex, thread};
+use tokio::sync::broadcast::{channel, Sender};
+
+#[derive(Debug, Snafu)]
+enum BuildError {
+    #[snafu(display("CRITICAL_SECTION poisoned"))]
+    CriticalSectionPoisoned,
+}
+
+lazy_static! {
+    static ref CRITICAL_SECTION: Mutex<Option<Sender<Bytes>>> = Mutex::default();
+}
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(deny_unknown_fields, default)]
@@ -39,14 +54,10 @@ impl SourceConfig for StdinConfig {
         &self,
         _name: &str,
         _globals: &GlobalOptions,
-        _shutdown: ShutdownSignal,
+        shutdown: ShutdownSignal,
         out: mpsc::Sender<Event>,
     ) -> crate::Result<super::Source> {
-        Ok(stdin_source(
-            io::BufReader::new(io::stdin()),
-            self.clone(),
-            out,
-        ))
+        stdin_source(io::BufReader::new(io::stdin()), self.clone(), shutdown, out)
     }
 
     fn output_type(&self) -> DataType {
@@ -58,49 +69,108 @@ impl SourceConfig for StdinConfig {
     }
 }
 
-pub fn stdin_source<R>(stdin: R, config: StdinConfig, out: mpsc::Sender<Event>) -> super::Source
+pub fn stdin_source<R>(
+    stdin: R,
+    config: StdinConfig,
+    shutdown: ShutdownSignal,
+    out: mpsc::Sender<Event>,
+) -> crate::Result<super::Source>
 where
     R: Send + io::BufRead + 'static,
 {
-    Box::new(future::lazy(move || {
-        info!("Capturing STDIN");
+    // The idea is to have one dedicated future for reading stdin running in the background,
+    // and the sources would recieve the lines thorugh a multi consumer channel.
+    //
+    // Implemented solution relies on having a copy of optional sender behind a global mutex,
+    // and have stdin sources and background thread synchronize on it.
+    //
+    // When source is built it must:
+    // 1. enter critical section by locking mutex.
+    // 2. if sender isn't present start background thread and set sender.
+    // 3. gain receiver by calling subscribe on the sender.
+    // 4. release lock.
+    //
+    // When source has finished it should just drop it's receiver.
+    //
+    // Background thread should be started with copy of the sender.
+    //
+    // Once background thread wants to stop it must:
+    // 1. enter critical section by locking mutex.
+    // 2. if there are receivers it must abort the stop.
+    // 3. remove sender.
+    // 4. release lock.
+    //
+    // Although it's possible to implement this in a lock free, maybe even wait free manner,
+    // this should be easier to reason about and performance shouldn't suffer since this procedure
+    // is cold compared to the rest of the source.
 
-        let host_key = config
-            .host_key
-            .clone()
-            .unwrap_or(event::log_schema().host_key().to_string());
-        let hostname = hostname::get_hostname();
-        let (mut tx, rx) = futures01::sync::mpsc::channel(1024);
+    let host_key = config
+        .host_key
+        .clone()
+        .unwrap_or(event::log_schema().host_key().to_string());
+    let hostname = hostname::get_hostname();
 
-        thread::spawn(move || {
-            for line in stdin.lines() {
-                match line {
-                    Err(e) => {
-                        error!(message = "Unable to read from source.", error = %e);
-                        break;
-                    }
-                    Ok(string_data) => {
-                        let msg = Bytes::from(string_data);
-                        while let Err(e) = tx.try_send(msg.clone()) {
-                            if e.is_full() {
-                                thread::sleep(Duration::from_millis(10));
-                                continue;
-                            }
-                            error!(message = "Unable to send event.", error = %e);
+    let mut guard = CRITICAL_SECTION
+        .lock()
+        .map_err(|_| BuildError::CriticalSectionPoisoned)?;
+    let receiver = match guard.as_ref() {
+        Some(sender) => sender.subscribe(),
+        None => {
+            let (sender, receiver) = channel(1024);
+            *guard = Some(sender.clone());
+
+            // Start the background thread
+            thread::spawn(move || {
+                info!("Capturing STDIN.");
+
+                for line in stdin.lines() {
+                    match line {
+                        Err(e) => {
+                            error!(message = "Unable to read from source.", error = %e);
                             break;
+                        }
+                        Ok(line) => {
+                            if sender.send(Bytes::from(line)).is_err() {
+                                // There are no active receivers.
+                                // Try to stop.
+                                let mut guard =
+                                    CRITICAL_SECTION.lock().expect("CRITICAL_SECTION poisoned");
+
+                                if sender.receiver_count() == 0 {
+                                    guard.take();
+                                    return;
+                                }
+
+                                // A new receiver has shown up.
+
+                                // It's fine not to resend the line since it came from
+                                // before this new receiver has shown up.
+                            }
                         }
                     }
                 }
-            }
-        });
 
-        rx.map(move |line| create_event(line, &host_key, &hostname))
+                CRITICAL_SECTION
+                    .lock()
+                    .expect("CRITICAL_SECTION poisoned")
+                    .take();
+            });
+
+            receiver
+        }
+    };
+    std::mem::drop(guard);
+
+    Ok(Box::new(
+        Compat::new(receiver)
+            .take_until(shutdown)
+            .map(move |line| create_event(line, &host_key, &hostname))
             .map_err(|e| error!("error reading line: {:?}", e))
             .forward(
                 out.sink_map_err(|e| error!(message = "Unable to send event to out.", error = %e)),
             )
-            .map(|_| info!("finished sending"))
-    }))
+            .map(|_| info!("finished sending")),
+    ))
 }
 
 fn create_event(line: Bytes, host_key: &str, hostname: &Option<String>) -> Event {
@@ -146,12 +216,13 @@ mod tests {
 
     #[test]
     fn stdin_decodes_line() {
+        crate::test_util::trace_init();
         let (tx, mut rx) = mpsc::channel(10);
         let config = StdinConfig::default();
         let buf = Cursor::new(String::from("hello world\nhello world again"));
 
         let mut rt = Runtime::new().unwrap();
-        let source = stdin_source(buf, config, tx);
+        let source = stdin_source(buf, config, ShutdownSignal::noop(), tx).unwrap();
 
         rt.block_on(source).unwrap();
 
