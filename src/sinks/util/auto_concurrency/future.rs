b95d25faa4339a3aaf2e82f10c9f0429917f8e10
Bruce Guenter <bruce@timber.io>
2020-05-07 15:50:26 -0600

    Link the semaphore controller and start time into the response future
    Signed-off-by: Bruce Guenter <bruce@timber.io>

--- src/sinks/util/auto_concurrency/future.rs
+++ src/sinks/util/auto_concurrency/future.rs
@@ -1,15 +1,18 @@
 //! Future types
 //!
+use super::controller::Controller;
 use futures::ready;
 use pin_project::pin_project;
+use std::time::Instant;
 use std::{
     future::Future,
     pin::Pin,
+    sync::Arc,
     task::{Context, Poll},
 };
 use tokio::sync::OwnedSemaphorePermit;
 
-/// Future for the `ConcurrencyLimit` service.
+/// Future for the `AutoConcurrencyLimit` service.
 #[pin_project]
 #[derive(Debug)]
 pub struct ResponseFuture<T> {
@@ -17,11 +20,22 @@ pub struct ResponseFuture<T> {
     inner: T,
     // Keep this around so that it is dropped when the future completes
     _permit: OwnedSemaphorePermit,
+    controller: Arc<Controller>,
+    start: Instant,
 }
 
 impl<T> ResponseFuture<T> {
-    pub(super) fn new(inner: T, _permit: OwnedSemaphorePermit) -> ResponseFuture<T> {
-        ResponseFuture { inner, _permit }
+    pub(super) fn new(
+        inner: T,
+        _permit: OwnedSemaphorePermit,
+        controller: Arc<Controller>,
+    ) -> ResponseFuture<T> {
+        ResponseFuture {
+            inner,
+            _permit,
+            controller,
+            start: Instant::now(),
+        }
     }
 }
 
