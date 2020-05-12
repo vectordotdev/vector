4e764f2f446d80269afe6f70e966ecae649608c3
MOZGIII <mike-n@narod.ru>
2020-05-12 03:02:32 +0300

    Add support for pulling images in docker-compose-run.sh
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- scripts/docker-compose-run.sh
+++ scripts/docker-compose-run.sh
@@ -11,10 +11,34 @@ SERVICE="$1"
 
 DOCKER="${USE_CONTAINER:-"docker"}"
 COMPOSE="${COMPOSE:-"${DOCKER}-compose"}"
+DOCKER_IMAGES_STRATEGY="${DOCKER_IMAGES_STRATEGY:-"build"}"
+
+ARGS=()
+case "$DOCKER_IMAGES_STRATEGY" in
+  build)
+    ARGS+=(--build)
+    ;;
+  pull)
+    ARGS+=(--no-build)
+    $COMPOSE pull "$SERVICE"
+    ;;
+  default)
+    # No custom args
+    ;;
+  *)
+    echo "Error: invalid DOCKER_IMAGES_STRATEGY: $DOCKER_IMAGES_STRATEGY" >&2
+    exit 1
+    ;;
+esac
 
 USER="$(id -u):$(id -g)"
 export USER
 
 $COMPOSE rm -svf "$SERVICE" 2>/dev/null || true
-$COMPOSE up --build --abort-on-container-exit --exit-code-from "$SERVICE" "$SERVICE" \
+
+$COMPOSE up \
+  "${ARGS[@]}" \
+  --abort-on-container-exit \
+   --exit-code-from "$SERVICE" \
+   "$SERVICE" \
   | sed $'s/^.*container exit...$/\033[0m\033[1A/'
