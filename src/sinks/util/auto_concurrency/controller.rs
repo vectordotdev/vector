ec0c3149115657d2c543540b5fe14a1f5efe7f19
Bruce Guenter <bruce@timber.io>
2020-05-14 15:32:04 -0600

    Wrap the shrinkable semaphore in its own module
    Signed-off-by: Bruce Guenter <bruce@timber.io>

--- src/sinks/util/auto_concurrency/controller.rs
+++ src/sinks/util/auto_concurrency/controller.rs
@@ -1,11 +1,8 @@
-use futures::ready;
+use super::semaphore::ShrinkableSemaphore;
 use std::future::Future;
-use std::mem::{drop, replace};
-use std::pin::Pin;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
-use tokio::sync::{OwnedSemaphorePermit, Semaphore};
+use tokio::sync::OwnedSemaphorePermit;
 
 const EWMA_ALPHA: f64 = 0.5;
 const THRESHOLD_RATIO: f64 = 0.01;
@@ -14,7 +11,7 @@ const THRESHOLD_RATIO: f64 = 0.01;
 /// semaphore size and other associated data.
 #[derive(Clone, Debug)]
 pub(super) struct Controller {
-    semaphore: Arc<Semaphore>,
+    semaphore: Arc<ShrinkableSemaphore>,
     max: usize,
     inner: Arc<Mutex<Inner>>,
 }
@@ -22,7 +19,6 @@ pub(super) struct Controller {
 #[derive(Debug)]
 struct Inner {
     current: usize,
-    to_forget: usize,
     measured_rtt: EWMA,
     next_update: Instant,
 }
@@ -30,11 +26,10 @@ struct Inner {
 impl Controller {
     pub(super) fn new(max: usize, current: usize) -> Self {
         Self {
-            semaphore: Arc::new(Semaphore::new(current)),
+            semaphore: Arc::new(ShrinkableSemaphore::new(current)),
             max,
             inner: Arc::new(Mutex::new(Inner {
                 current,
-                to_forget: 0,
                 measured_rtt: Default::default(),
                 next_update: Instant::now(),
             })),
@@ -42,11 +37,7 @@ impl Controller {
     }
 
     pub(super) fn acquire(&self) -> impl Future<Output = OwnedSemaphorePermit> + Send + 'static {
-        MaybeForgetFuture {
-            semaphore: self.semaphore.clone(),
-            inner: self.inner.clone(),
-            future: Box::pin(Arc::clone(&self.semaphore).acquire_owned()),
-        }
+        self.semaphore.clone().acquire()
     }
 
     pub(super) fn adjust_to_response(&self, start: Instant) {
@@ -61,22 +52,13 @@ impl Controller {
             let threshold = avg * THRESHOLD_RATIO;
             if inner.current > 1 && rtt >= avg + threshold {
                 // Decrease (multiplicative) the current concurrency
-                let new_current = inner.current / 2;
-                // Have to forget some permits from the semaphore but there
-                // may not be enough available. If so, increase the count we
-                // need to forget later and finish.
-                for _ in new_current..inner.current {
-                    match self.semaphore.try_acquire() {
-                        Ok(permit) => permit.forget(),
-                        Err(_) => inner.to_forget += 1,
-                    }
-                }
-                inner.current = new_current;
+                let to_forget = inner.current / 2;
+                self.semaphore.forget_permits(to_forget);
+                inner.current -= to_forget;
             } else if inner.current < self.max && rtt <= avg {
                 // Increase (additive) the current concurrency
                 self.semaphore.add_permits(1);
                 inner.current += 1;
-                inner.to_forget = inner.to_forget.saturating_sub(1);
             }
 
             let new_avg = inner.measured_rtt.update(rtt);
@@ -84,31 +66,6 @@ impl Controller {
         } else {
             inner.measured_rtt.update(rtt);
         }
-    }
-}
-
-/// A future that accounts for the possibility of needing to forget some
-/// number of permits before outputting a valid one.
-struct MaybeForgetFuture {
-    semaphore: Arc<Semaphore>,
-    inner: Arc<Mutex<Inner>>,
-    future: Pin<Box<dyn Future<Output = OwnedSemaphorePermit> + Send + 'static>>,
-}
-
-impl Future for MaybeForgetFuture {
-    type Output = OwnedSemaphorePermit;
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let inner = self.inner.clone();
-        let mut inner = inner.lock().expect("Controller mutex is poisoned");
-        while inner.to_forget > 0 {
-            let permit = ready!(self.future.as_mut().poll(cx));
-            permit.forget();
-            inner.to_forget -= 1;
-            let future = Arc::clone(&self.semaphore).acquire_owned();
-            replace(&mut self.future, Box::pin(future));
-        }
-        drop(inner);
-        self.future.as_mut().poll(cx)
     }
 }
 
