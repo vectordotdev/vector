2000438e41761393ec6b41ba0d8057e5841f3a9a
MOZGIII <mike-n@narod.ru>
2020-05-20 07:28:28 +0300

    chore: A script to install git hooks
    Signed-off-by: MOZGIII <mike-n@narod.ru>

---
+++ scripts/install-git-hooks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+MODE="${1:-"all"}"
+
+mkdir -p "$(git rev-parse --git-dir)/hooks"
+
+if [[ "$MODE" == "all" || "$MODE" == "signoff" ]]; then
+  cp scripts/signoff-git-hook.sh "$(git rev-parse --git-dir)/hooks/commit-msg"
+fi
