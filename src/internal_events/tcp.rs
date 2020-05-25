d921c8f9e62305d1d3d4abceabecb8288c95e224
Ana Hobden <operator@hoverbear.org>
2020-05-25 06:33:13 -0700

    Warn on tcp error
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/internal_events/tcp.rs
+++ src/internal_events/tcp.rs
@@ -80,7 +80,7 @@ pub struct TcpConnectionError {
 
 impl InternalEvent for TcpConnectionError {
     fn emit_logs(&self) {
-        info!(message = "connection error.", error = %self.error, rate_limit_secs = 10);
+        warn!(message = "connection error.", error = %self.error, rate_limit_secs = 10);
     }
 
     fn emit_metrics(&self) {
