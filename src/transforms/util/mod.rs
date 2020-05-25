9636f3f1565c9a98e20421d83cfff6c4090392d9
MOZGIII <mike-n@narod.ru>
2020-05-26 02:45:27 +0300

    Add a to do comment to make transform utils part of core
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- src/transforms/util/mod.rs
+++ src/transforms/util/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(any(feature = "transforms-lua"))]
 pub mod runtime_transform;
 
+// TODO: make these part of the vector core - enabled unconditionally.
 #[cfg(any(feature = "sources-kubernetes-logs"))]
 pub mod chain;
 #[cfg(any(feature = "sources-kubernetes-logs"))]
