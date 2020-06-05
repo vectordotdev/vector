d681431f47e05fb3449817f1795c431d8e6dc11c
Ana Hobden <operator@hoverbear.org>
2020-06-05 15:37:08 -0700

    Add comments on deps and env vars
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- scripts/environment/definition.nix
+++ scripts/environment/definition.nix
@@ -2,37 +2,50 @@ scope@{ pkgs ? import <nixpkgs> {} }:
 
 {
   environmentVariables =  {
+    # We must set some protoc related env vars for the prost crate.
     PROTOC = "${pkgs.protobuf}/bin/protoc";
     PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+    # On Linux builds, we need some level of localization.
     LOCALE_ARCHIVE= if pkgs.stdenv.isLinux then
     "${pkgs.glibcLocales}/lib/locale/locale-archive"
     else
     "";
     LC_ALL = "en_US.UTF-8";
+    # Without setting a tzdata folder, some tests will fail.
+    TZDIR = "${pkgs.tzdata}/share/zoneinfo";
+    # Crates expect information about OpenSSL in these vars.
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+    # Git looks to this env var for SSL certificates.
     GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+    # Curl looks to this env var for SSL certificates.
     CURL_CA_BUNDLE = "${pkgs.cacert}/etc/ca-bundle.crt";
+    # Encourage Cargo to be pretty.
     CARGO_TERM_COLOR = "always";
+    # Enable backtraces in the environment.
+    RUST_BACKTRACE = "full";
+    # Vector gets very angry if you don't set these and use the AWS components.
     AWS_ACCESS_KEY_ID = "dummy";
     AWS_SECRET_ACCESS_KEY = "dummy";
-    RUST_BACKTRACE = "full";
-    TZDIR = "${pkgs.tzdata}/share/zoneinfo";
   };
 
   packages = with pkgs; [
-    git
+    # Core CLI tools
     dnsutils
     curl
     bash
     nix
     direnv
     binutils
+    remarshal
+    libiconv
+    tzdata
+    jq
     stdenv
     bashInteractive
-    docker
-    docker-compose
+    # Build Env
+    git
     cacert
     cmake
     rustup
@@ -40,26 +53,29 @@ scope@{ pkgs ? import <nixpkgs> {} }:
     openssl
     protobuf
     rdkafka
-    ruby_2_7
-    shellcheck
-    docker
-    yarn
-    nodejs
     openssl
+    ruby_2_7
+    nodejs
     perl
-    remarshal
+    yarn
     snappy
     gnumake
     autoconf
-    jq
-    libiconv
-    tzdata
+    shellcheck
+    # Container tools
+    docker
+    docker-compose
+    podman
   ] ++ (if stdenv.isDarwin then [
     darwin.cf-private
     darwin.apple_sdk.frameworks.CoreServices
     darwin.apple_sdk.frameworks.Security
     darwin.apple_sdk.frameworks.SecurityFoundation
   ] else [
-    systemd gcc (glibcLocales.override { locales = ["en_US.UTF-8"]; })
+    # Build
+    gcc
+    (glibcLocales.override { locales = ["en_US.UTF-8"]; })
+    # Testing
+    systemd
   ]);
 }
