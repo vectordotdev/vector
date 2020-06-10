e6e2326c89a4fc1d771e3cd2036c26862e8a3ab1
Ana Hobden <operator@hoverbear.org>
2020-06-10 09:55:54 -0700

    Give topo time to startup in buffer tests
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- tests/buffering.rs
+++ tests/buffering.rs
@@ -62,6 +62,7 @@ fn test_buffering() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let (input_events, input_events_stream) =
         test_util::random_events_with_stream(line_length, num_events);
@@ -69,7 +70,6 @@ fn test_buffering() {
         .sink_map_err(|err| panic!(err))
         .send_all(input_events_stream);
     let _ = rt.block_on(send).unwrap();
-    std::thread::sleep(std::time::Duration::from_secs(2));
 
     // There was a race caused by a channel in the mock source, and this
     // check is here to ensure it's really gone.
@@ -98,6 +98,7 @@ fn test_buffering() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let (input_events2, input_events_stream) =
         test_util::random_events_with_stream(line_length, num_events);
@@ -161,7 +162,7 @@ fn test_max_size() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(1)); // Give topo a moment to start up.
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let send = in_tx
         .sink_map_err(|err| panic!(err))
@@ -197,6 +198,7 @@ fn test_max_size() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let output_events = test_util::receive_events(out_rx);
 
@@ -242,6 +244,7 @@ fn test_max_size_resume() {
     let mut rt = runtime::Runtime::new().unwrap();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     // Send all of the input events _before_ the output sink is ready.
     // This causes the writers to stop writing to the on-disk buffer, and once
@@ -298,6 +301,7 @@ fn test_reclaim_disk_space() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let (input_events, input_events_stream) =
         test_util::random_events_with_stream(line_length, num_events);
@@ -336,6 +340,7 @@ fn test_reclaim_disk_space() {
     let mut rt = test_util::runtime();
 
     let (topology, _crash) = topology::start(config, &mut rt, false).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Give topo a moment to start up.
 
     let (input_events2, input_events_stream) =
         test_util::random_events_with_stream(line_length, num_events);
