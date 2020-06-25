3f2e1d0927285d579c267e34dfad1cb287083df1
MOZGIII <mike-n@narod.ru>
2020-06-25 16:47:46 +0300

    Fix clippy
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- lib/kubernetes-test-framework/src/util.rs
+++ lib/kubernetes-test-framework/src/util.rs
@@ -3,7 +3,7 @@ use crate::Result;
 pub async fn run_command(mut command: tokio::process::Command) -> Result<()> {
     let exit_status = command.spawn()?.await?;
     if !exit_status.success() {
-        Err(format!("exec failed: {:?}", command))?;
+        return Err(format!("exec failed: {:?}", command).into());
     }
     Ok(())
 }
@@ -12,7 +12,7 @@ pub fn run_command_blocking(mut command: std::process::Command) -> Result<()> {
     let mut child = command.spawn()?;
     let exit_status = child.wait()?;
     if !exit_status.success() {
-        Err(format!("exec failed: {:?}", command))?;
+        return Err(format!("exec failed: {:?}", command).into());
     }
     Ok(())
 }
