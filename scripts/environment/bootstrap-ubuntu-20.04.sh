e1bfece962a13cbb60f051f32c01ee0826c30944
Ana Hobden <operator@hoverbear.org>
2020-07-08 11:05:03 -0700

    Finnick some sudos
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- scripts/environment/bootstrap-ubuntu-20.04.sh
+++ scripts/environment/bootstrap-ubuntu-20.04.sh
@@ -24,8 +24,16 @@ dpkg-reconfigure locales
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Yarn
-
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   xenial \
+   stable"
+
+# Install those new things
 apt update --yes
-apt install --yes yarn
+apt install --yes yarn docker-ce docker-ce-cli containerd.io
