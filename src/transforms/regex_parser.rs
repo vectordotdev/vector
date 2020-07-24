e862e5aa0e68c83c958c463e54cbe41f6ea9d2cc
Jesse Szwedko <jesse@szwedko.me>
2020-07-24 19:13:38 +0000

    Refactor parallel vectors into a type
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/transforms/regex_parser.rs
+++ src/transforms/regex_parser.rs
@@ -56,14 +56,78 @@ impl TransformConfig for RegexParserConfig {
 
 pub struct RegexParser {
     regexset: RegexSet,
-    patterns: Vec<Regex>,
+    patterns: Vec<CompiledRegex>, // indexes correspend to RegexSet
     field: Atom,
     drop_field: bool,
     drop_failed: bool,
     target_field: Option<Atom>,
     overwrite_target: bool,
-    capture_names: Vec<Vec<(usize, Atom, Conversion)>>,
-    capture_locs: Vec<CaptureLocations>,
+}
+
+struct CompiledRegex {
+    regex: Regex,
+    capture_names: Vec<(usize, Atom, Conversion)>,
+    capture_locs: CaptureLocations,
+}
+
+impl CompiledRegex {
+    fn new(regex: Regex, types: &HashMap<Atom, Conversion>) -> CompiledRegex {
+        // Calculate the location (index into the capture locations) of
+        // each named capture, and the required type coercion.
+        let capture_names: Vec<(usize, Atom, Conversion)> = regex
+            .capture_names()
+            .enumerate()
+            .filter_map(|(idx, cn)| {
+                cn.map(|cn| {
+                    let cn: Atom = cn.into();
+                    let conv = types.get(&cn).unwrap_or(&Conversion::Bytes);
+                    (idx, cn, conv.clone())
+                })
+            })
+            .collect();
+        let capture_locs = regex.capture_locations();
+        CompiledRegex {
+            regex,
+            capture_names,
+            capture_locs,
+        }
+    }
+
+    /// Returns a list of captures (name, value) or None if the regex does not
+    /// match
+    fn captures<'a>(&'a mut self, value: &[u8]) -> Option<Vec<(&'a Atom, Value)>> {
+        match self.regex.captures_read(&mut self.capture_locs, value) {
+            Some(_) => {
+                let values = self
+                    .capture_names
+                    .iter()
+                    .filter_map(
+                        |(idx, name, conversion)| match self.capture_locs.get(*idx) {
+                            Some((start, end)) => {
+                                let capture: Value = value[start..end].into();
+
+                                match conversion.convert(capture) {
+                                    Ok(value) => Some((name, value)),
+                                    Err(error) => {
+                                        debug!(
+                                            message = "Could not convert types.",
+                                            name = &name[..],
+                                            %error,
+                                            rate_limit_secs = 30
+                                        );
+                                        None
+                                    }
+                                }
+                            }
+                            None => None,
+                        },
+                    )
+                    .collect();
+                Some(values)
+            }
+            None => None,
+        }
+    }
 }
 
 impl RegexParser {
@@ -143,35 +207,20 @@ impl RegexParser {
         overwrite_target: bool,
         types: HashMap<Atom, Conversion>,
     ) -> Self {
-        // Build a buffer of the regex capture locations to avoid
+        // Build a buffer of the regex capture locations and names to avoid
         // repeated allocations.
-        let capture_locs = patterns
-            .iter()
-            .map(|regex| regex.capture_locations())
-            .collect();
-
-        // Calculate the location (index into the capture locations) of
-        // each named capture, and the required type coercion.
-        let capture_names: Vec<Vec<(usize, Atom, Conversion)>> = patterns
-            .iter()
-            .map(|regex| {
-                let capture_names: Vec<(usize, Atom, Conversion)> = regex
-                    .capture_names()
-                    .enumerate()
-                    .filter_map(|(idx, cn)| {
-                        cn.map(|cn| {
-                            let cn: Atom = cn.into();
-                            let conv = types.get(&cn).unwrap_or(&Conversion::Bytes);
-                            (idx, cn, conv.clone())
-                        })
-                    })
-                    .collect();
-                capture_names
-            })
+        let patterns: Vec<CompiledRegex> = patterns
+            .into_iter()
+            .map(|regex| CompiledRegex::new(regex, &types))
             .collect();
 
         // Pre-calculate if the source field name should be dropped.
-        drop_field = drop_field && !capture_names.iter().flatten().any(|(_, f, _)| *f == field);
+        drop_field = drop_field
+            && !patterns
+                .iter()
+                .map(|p| &p.capture_names)
+                .flatten()
+                .any(|(_, f, _)| *f == field);
 
         Self {
             regexset,
@@ -181,8 +230,6 @@ impl RegexParser {
             drop_failed,
             target_field,
             overwrite_target,
-            capture_names,
-            capture_locs,
         }
     }
 }
@@ -207,18 +254,15 @@ impl Transform for RegexParser {
                 }
             };
 
-            let mut capture_locs = match self.capture_locs.get_mut(id) {
-                Some(capture_locs) => capture_locs,
+            let pattern = match self.patterns.get_mut(id) {
+                Some(pattern) => pattern,
                 None => {
                     error!(message = "Cannot find capture locations for pattern", %id, rate_limit_secs = 30);
                     return None;
                 }
             };
 
-            if self.patterns[id]
-                .captures_read(&mut capture_locs, &value)
-                .is_some()
-            {
+            if let Some(captures) = pattern.captures(&value) {
                 // Handle optional overwriting of the target field
                 if let Some(target_field) = &self.target_field {
                     if log.contains(target_field) {
@@ -231,27 +275,12 @@ impl Transform for RegexParser {
                     }
                 }
 
-                for (idx, name, conversion) in &self.capture_names[id] {
-                    if let Some((start, end)) = capture_locs.get(*idx) {
-                        let capture: Value = value[start..end].into();
-                        match conversion.convert(capture) {
-                            Ok(value) => {
-                                let name = match &self.target_field {
-                                    Some(target) => Atom::from(format!("{}.{}", target, name)),
-                                    None => name.clone(),
-                                };
-                                log.insert(name, value);
-                            }
-                            Err(error) => {
-                                debug!(
-                                    message = "Could not convert types.",
-                                    name = &name[..],
-                                    %error,
-                                    rate_limit_secs = 30
-                                );
-                            }
-                        }
-                    }
+                for (name, value) in captures {
+                    let name = match &self.target_field {
+                        Some(target) => Atom::from(format!("{}.{}", target, name)),
+                        None => name.clone(),
+                    };
+                    log.insert(name, value);
                 }
                 if self.drop_field {
                     log.remove(&self.field);
