50c6ea73ab5b0807d858ecf1b69c8d61dd123ee0
ktf <krunotf@gmail.com>
2020-07-26 21:23:43 +0200

    Adjust features
    Signed-off-by: ktf <krunotf@gmail.com>

--- src/sources/util/mod.rs
+++ src/sources/util/mod.rs
@@ -1,11 +1,13 @@
 #[cfg(feature = "sources-http")]
 mod http;
+#[cfg(feature = "sources-tls")]
 mod tcp;
 #[cfg(unix)]
 mod unix;
 
 #[cfg(feature = "sources-http")]
 pub use self::http::{ErrorMessage, HttpSource};
+#[cfg(feature = "sources-tls")]
 pub use tcp::{SocketListenAddr, TcpSource};
 #[cfg(unix)]
 pub use unix::build_unix_source;
