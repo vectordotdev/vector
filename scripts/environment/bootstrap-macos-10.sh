4c5a57f64a7a78e4449e073497bc1882c647a4ac
Jesse Szwedko <jesse@szwedko.me>
2020-07-28 19:06:47 -0400

    chore: Install Ruby on OSX CI runner (#3189)
    It comes preinstalled with 2.6 which is below our Gemfile requirement in
    scripts/.
    
    https://github.com/actions/virtual-environments/blob/3d7f73608f6c438ef990a591dee574e90de9dce1/images/macos/macos-10.15-Readme.md
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

---
+++ scripts/environment/bootstrap-macos-10.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+set -e -o verbose
+
+brew install ruby
+
+echo "export PATH=\"/usr/local/opt/ruby/bin:\$PATH\"" >> "$HOME/.bash_profile"
+
+if [ -n "${CI-}" ] ; then
+  echo "::add-path::/usr/local/opt/ruby/bin"
+fi
