9745e9a3560570ba17afd5148e37e329affec9a2
Bruce Guenter <bruce@untroubled.org>
2020-07-28 16:14:56 -0600

    enhancement(journald source): Add received and invalid line events (#3241)
    Signed-off-by: Bruce Guenter <bruce@timber.io>

---
+++ src/internal_events/journald.rs
@@ -0,0 +1,47 @@
+use super::InternalEvent;
+use metrics::counter;
+
+#[derive(Debug)]
+pub(crate) struct JournaldEventReceived {
+    pub byte_size: usize,
+}
+
+impl InternalEvent for JournaldEventReceived {
+    fn emit_logs(&self) {
+        trace!(message = "received line.", byte_size = %self.byte_size);
+    }
+
+    fn emit_metrics(&self) {
+        counter!("event_processed", 1,
+                 "component_kind" => "source",
+                 "component_name" => "journald",
+        );
+        counter!("bytes_processed", self.byte_size as u64,
+                 "component_kind" => "source",
+                 "component_name" => "journald",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct JournaldInvalidRecord {
+    pub error: serde_json::Error,
+    pub text: String,
+}
+
+impl InternalEvent for JournaldInvalidRecord {
+    fn emit_logs(&self) {
+        error!(message = "Invalid record from journald, discarding.", error = %self.error, text = %self.text);
+    }
+
+    fn emit_metrics(&self) {
+        counter!("invalid_record", 1,
+                 "component_kind" => "source",
+                 "component_name" => "journald",
+        );
+        counter!("invalid_record_bytes", self.text.len() as u64,
+                 "component_kind" => "source",
+                 "component_name" => "journald",
+        );
+    }
+}
