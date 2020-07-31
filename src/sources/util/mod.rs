1ddb3a856c55725f8600b39fbb33cf0dd500c1b1
Kirill Fomichev <fanatid@ya.ru>
2020-07-31 23:08:21 +0300

    chore: extra cfg checks for unix source
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- src/sources/util/mod.rs
+++ src/sources/util/mod.rs
@@ -2,12 +2,20 @@
 mod http;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 mod tcp;
-#[cfg(unix)]
+#[cfg(all(
+    feature = "sources-socket",
+    feature = "sources-syslog",
+    feature = "unix"
+))]
 mod unix;
 
 #[cfg(all(feature = "sources-tls", feature = "warp"))]
 pub use self::http::{ErrorMessage, HttpSource};
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 pub use tcp::{SocketListenAddr, TcpSource};
-#[cfg(unix)]
+#[cfg(all(
+    feature = "sources-socket",
+    feature = "sources-syslog",
+    feature = "unix"
+))]
 pub use unix::build_unix_source;
