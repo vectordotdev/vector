f776b6bd2dca7c7ae4b5862edae1e617c88d8c73
Kruno Tomola Fabro <krunotf@gmail.com>
2020-08-01 18:27:40 +0200

    Connected check
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>

--- src/test_util.rs
+++ src/test_util.rs
@@ -360,12 +360,19 @@ where
 pub struct CountReceiver<T> {
     count: Arc<AtomicUsize>,
     trigger: oneshot::Sender<()>,
+    connected: Option<oneshot::Receiver<()>>,
     handle: JoinHandle<Vec<T>>,
 }
 
 impl<T: Send + 'static> CountReceiver<T> {
     pub fn count(&self) -> usize {
         self.count.load(Ordering::Relaxed)
+    }
+
+    pub async fn connected(&mut self) {
+        if let Some(tripwire) = self.connected.take() {
+            tripwire.await.unwrap();
+        }
     }
 
     pub async fn wait(self) -> Vec<T> {
@@ -375,25 +382,33 @@ impl<T: Send + 'static> CountReceiver<T> {
 
     fn new<F, Fut>(make_fut: F) -> CountReceiver<T>
     where
-        F: FnOnce(Arc<AtomicUsize>, oneshot::Receiver<()>) -> Fut,
+        F: FnOnce(Arc<AtomicUsize>, oneshot::Receiver<()>, oneshot::Sender<()>) -> Fut,
         Fut: std::future::Future<Output = Vec<T>> + Send + 'static,
     {
         let count = Arc::new(AtomicUsize::new(0));
         let (trigger, tripwire) = oneshot::channel();
+        let (trigger_connected, connected) = oneshot::channel();
 
         CountReceiver {
             count: Arc::clone(&count),
             trigger,
-            handle: tokio::spawn(make_fut(count, tripwire)),
+            connected: Some(connected),
+            handle: tokio::spawn(make_fut(count, tripwire, trigger_connected)),
         }
     }
 }
 
 impl CountReceiver<String> {
     pub fn receive_lines(addr: SocketAddr) -> CountReceiver<String> {
-        CountReceiver::new(|count, tripwire| async move {
+        CountReceiver::new(|count, tripwire, connected| async move {
             let mut listener = TcpListener::bind(addr).await.unwrap();
-            CountReceiver::receive_lines_stream(listener.incoming(), count, tripwire).await
+            CountReceiver::receive_lines_stream(
+                listener.incoming(),
+                count,
+                tripwire,
+                Some(connected),
+            )
+            .await
         })
     }
 
@@ -402,9 +417,15 @@ impl CountReceiver<String> {
     where
         P: AsRef<Path> + Send + 'static,
     {
-        CountReceiver::new(|count, tripwire| async move {
+        CountReceiver::new(|count, tripwire, connected| async move {
             let mut listener = tokio::net::UnixListener::bind(path).unwrap();
-            CountReceiver::receive_lines_stream(listener.incoming(), count, tripwire).await
+            CountReceiver::receive_lines_stream(
+                listener.incoming(),
+                count,
+                tripwire,
+                Some(connected),
+            )
+            .await
         })
     }
 
@@ -412,15 +433,21 @@ impl CountReceiver<String> {
         stream: S,
         count: Arc<AtomicUsize>,
         tripwire: oneshot::Receiver<()>,
+        mut connected: Option<oneshot::Sender<()>>,
     ) -> Vec<String>
     where
         S: Stream<Item = IoResult<T>>,
         T: AsyncWrite + AsyncRead,
     {
+        tokio::time::delay_for(std::time::Duration::from_secs(5)).await;
+
         stream
             .take_until(tripwire)
             .map_ok(|socket| FramedRead::new(socket, LinesCodec::new()))
-            .map(|x| x.unwrap())
+            .map(|x| {
+                connected.take().map(|trigger| trigger.send(()));
+                x.unwrap()
+            })
             .flatten()
             .map(|x| x.unwrap())
             .inspect(move |_| {
@@ -437,7 +464,8 @@ impl CountReceiver<Event> {
         S: Stream01<Item = Event> + Send + 'static,
         <S as Stream01>::Error: std::fmt::Debug,
     {
-        CountReceiver::new(|count, tripwire| async move {
+        CountReceiver::new(|count, tripwire, connected| async move {
+            connected.send(()).unwrap();
             stream
                 .compat()
                 .take_until(tripwire)
