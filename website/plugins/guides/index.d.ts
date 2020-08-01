7e62841a9ac94294a9aeb93eb13d0abc9907f7e1
binarylogic <bjohnson@binarylogic.com>
2020-08-01 12:13:30 -0400

    chore: Remove website
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- website/plugins/guides/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PluginOptions, GuideContent } from './types';
-import { LoadContext, Plugin } from '@docusaurus/types';
-export default function pluginContentGuide(context: LoadContext, opts: Partial<PluginOptions>): Plugin<GuideContent | null>;
-//# sourceMappingURL=index.d.ts.map
--- website/plugins/highlights/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PluginOptions, HighlightContent } from './types';
-import { LoadContext, Plugin } from '@docusaurus/types';
-export default function pluginContentHighlight(context: LoadContext, opts: Partial<PluginOptions>): Plugin<HighlightContent | null>;
-//# sourceMappingURL=index.d.ts.map
