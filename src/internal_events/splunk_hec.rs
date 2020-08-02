c3598da414ec9fe2b951533acdf55afed7352e1e
Kruno Tomola Fabro <krunotf@gmail.com>
2020-08-02 22:23:14 +0200

    Add feature flag
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>

--- src/internal_events/splunk_hec.rs
+++ src/internal_events/splunk_hec.rs
@@ -1,5 +1,4 @@
 use super::InternalEvent;
-use crate::sources::splunk_hec::ApiError;
 use metrics::counter;
 use serde_json::Error;
 
@@ -46,70 +45,76 @@ impl InternalEvent for SplunkEventEncodeError {
     }
 }
 
-#[derive(Debug)]
-pub struct SplunkHECEventReceived;
+#[cfg(feature = "sources-splunk_hec")]
+mod source {
+    use super::*;
+    use crate::sources::splunk_hec::ApiError;
 
-impl InternalEvent for SplunkHECEventReceived {
-    fn emit_logs(&self) {
-        trace!(message = "received one event.");
+    #[derive(Debug)]
+    pub struct SplunkHECEventReceived;
+
+    impl InternalEvent for SplunkHECEventReceived {
+        fn emit_logs(&self) {
+            trace!(message = "received one event.");
+        }
+
+        fn emit_metrics(&self) {
+            counter!(
+                "events_processed", 1,
+                "component_kind" => "source",
+                "component_type" => "splunk_hec",
+            );
+        }
     }
 
-    fn emit_metrics(&self) {
-        counter!(
-            "events_processed", 1,
-            "component_kind" => "source",
-            "component_type" => "splunk_hec",
-        );
-    }
-}
-
-#[derive(Debug)]
-pub struct SplunkHECRequestReceived<'a> {
-    pub path: &'a str,
-}
-
-impl<'a> InternalEvent for SplunkHECRequestReceived<'a> {
-    fn emit_logs(&self) {
-        debug!(message = "received one request.", %self.path, rate_limit_secs = 10);
+    #[derive(Debug)]
+    pub struct SplunkHECRequestReceived<'a> {
+        pub path: &'a str,
     }
 
-    fn emit_metrics(&self) {
-        counter!(
-            "request_received", 1,
-            "component_kind" => "source",
-            "component_type" => "splunk_hec",
-        );
-    }
-}
+    impl<'a> InternalEvent for SplunkHECRequestReceived<'a> {
+        fn emit_logs(&self) {
+            debug!(message = "received one request.", %self.path, rate_limit_secs = 10);
+        }
 
-#[derive(Debug)]
-pub struct SplunkHECRequestBodyInvalid {
-    pub error: std::io::Error,
-}
-
-impl InternalEvent for SplunkHECRequestBodyInvalid {
-    fn emit_logs(&self) {
-        error!(message = "invalid request body.",%self.error, rate_limit_secs = 10);
+        fn emit_metrics(&self) {
+            counter!(
+                "request_received", 1,
+                "component_kind" => "source",
+                "component_type" => "splunk_hec",
+            );
+        }
     }
 
-    fn emit_metrics(&self) {}
-}
-
-#[derive(Debug)]
-pub struct SplunkHECRequestError {
-    pub error: ApiError,
-}
-
-impl InternalEvent for SplunkHECRequestError {
-    fn emit_logs(&self) {
-        error!(message = "error processing request.",%self.error, rate_limit_secs = 10);
+    #[derive(Debug)]
+    pub struct SplunkHECRequestBodyInvalid {
+        pub error: std::io::Error,
     }
 
-    fn emit_metrics(&self) {
-        counter!(
-            "request_errors", 1,
-            "component_kind" => "source",
-            "component_type" => "splunk_hec",
-        );
+    impl InternalEvent for SplunkHECRequestBodyInvalid {
+        fn emit_logs(&self) {
+            error!(message = "invalid request body.",%self.error, rate_limit_secs = 10);
+        }
+
+        fn emit_metrics(&self) {}
+    }
+
+    #[derive(Debug)]
+    pub struct SplunkHECRequestError {
+        pub error: ApiError,
+    }
+
+    impl InternalEvent for SplunkHECRequestError {
+        fn emit_logs(&self) {
+            error!(message = "error processing request.",%self.error, rate_limit_secs = 10);
+        }
+
+        fn emit_metrics(&self) {
+            counter!(
+                "request_errors", 1,
+                "component_kind" => "source",
+                "component_type" => "splunk_hec",
+            );
+        }
     }
 }
