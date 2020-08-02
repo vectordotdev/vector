29e9e73e28e0b700e996cdb96a13c87fc1f08447
Nick <nhagianis@gmail.com>
2020-08-02 16:36:28 -0400

    ref(kv_parser): rename file to be more explicit
    Signed-off-by: Nick <nhagianis@gmail.com>

--- src/transforms/mod.rs
+++ src/transforms/mod.rs
@@ -30,7 +30,7 @@ pub mod grok_parser;
 #[cfg(feature = "transforms-json_parser")]
 pub mod json_parser;
 #[cfg(feature = "transforms-kv_parser")]
-pub mod kv_parser;
+pub mod key_value_parser;
 #[cfg(feature = "transforms-log_to_metric")]
 pub mod log_to_metric;
 #[cfg(feature = "transforms-logfmt_parser")]
