1a1865d269d08f5cea28633b303cbab701836e76
Kirill Fomichev <fanatid@ya.ru>
2020-08-04 13:01:36 +0300

    commit cfg logic for unix sources socket
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- src/sources/util/mod.rs
+++ src/sources/util/mod.rs
@@ -2,18 +2,12 @@
 mod http;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 mod tcp;
-#[cfg(all(
-    any(feature = "sources-socket", feature = "sources-syslog"),
-    feature = "unix"
-))]
+#[cfg(all(unix, any(feature = "sources-socket", feature = "sources-syslog")))]
 mod unix;
 
 #[cfg(all(feature = "sources-tls", feature = "warp"))]
 pub use self::http::{ErrorMessage, HttpSource};
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 pub use tcp::{SocketListenAddr, TcpSource};
-#[cfg(all(
-    any(feature = "sources-socket", feature = "sources-syslog"),
-    feature = "unix"
-))]
+#[cfg(all(unix, any(feature = "sources-socket", feature = "sources-syslog")))]
 pub use unix::build_unix_source;
