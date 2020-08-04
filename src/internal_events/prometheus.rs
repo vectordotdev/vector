9fc065e31821735a8dda0d578f17bc9401070787
Kruno Tomola Fabro <krunotf@gmail.com>
2020-08-04 11:07:48 +0200

    enhancement(prometheus source): Update instrumentation (#3317)
    * Add instrumentation
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Rename to request_duration_nanoseconds
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/internal_events/prometheus.rs
+++ src/internal_events/prometheus.rs
@@ -1,14 +1,50 @@
 use super::InternalEvent;
 use crate::sources::prometheus::parser::ParserError;
-use metrics::counter;
+use metrics::{counter, timing};
+use std::time::Instant;
 
 #[derive(Debug)]
-pub struct PrometheusRequestCompleted;
+pub struct PrometheusEventReceived {
+    pub byte_size: usize,
+    pub count: usize,
+}
+
+impl InternalEvent for PrometheusEventReceived {
+    fn emit_logs(&self) {
+        debug!(message = "scraped events.", ?self.count);
+    }
+
+    fn emit_metrics(&self) {
+        counter!(
+            "events_processed", self.count as u64,
+            "component_kind" => "source",
+            "component_type" => "prometheus",
+        );
+        counter!(
+            "bytes_processed", self.byte_size as u64,
+            "component_kind" => "source",
+            "component_type" => "prometheus",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct PrometheusRequestCompleted {
+    pub start: Instant,
+    pub end: Instant,
+}
 
 impl InternalEvent for PrometheusRequestCompleted {
+    fn emit_logs(&self) {
+        debug!(message = "request completed.");
+    }
+
     fn emit_metrics(&self) {
-        // TODO: make this a timer
         counter!("requests_completed", 1,
+            "component_kind" => "source",
+            "component_type" => "prometheus",
+        );
+        timing!("request_duration_nanoseconds", self.start,self.end,
             "component_kind" => "source",
             "component_type" => "prometheus",
         );
@@ -22,7 +58,7 @@ pub struct PrometheusParseError {
 
 impl InternalEvent for PrometheusParseError {
     fn emit_logs(&self) {
-        error!(message = "parsing error", error = ?self.error);
+        error!(message = "parsing error.", error = ?self.error);
     }
 
     fn emit_metrics(&self) {
@@ -40,7 +76,7 @@ pub struct PrometheusHttpError {
 
 impl InternalEvent for PrometheusHttpError {
     fn emit_logs(&self) {
-        error!(message = "http request processing error", error = %self.error);
+        error!(message = "http request processing error.", error = %self.error);
     }
 
     fn emit_metrics(&self) {
