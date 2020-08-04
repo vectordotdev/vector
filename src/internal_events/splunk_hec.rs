e767b63840b5b2ef3b045d9715fa30987b5bf7a7
Jesse Szwedko <jesse@szwedko.me>
2020-08-04 17:21:01 -0400

    feat(splunk_hec sink): Make sourcetype templatable (#3351)
    * feat(splunk_hec sink): Make sourcetype templatable
    
    Allows for setting the sourcetype dynamically.
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/internal_events/splunk_hec.rs
+++ src/internal_events/splunk_hec.rs
@@ -1,6 +1,7 @@
 use super::InternalEvent;
 use metrics::counter;
 use serde_json::Error;
+use string_cache::DefaultAtom as Atom;
 
 #[derive(Debug)]
 pub struct SplunkEventSent {
@@ -39,6 +40,29 @@ impl InternalEvent for SplunkEventEncodeError {
     fn emit_metrics(&self) {
         counter!(
             "encode_errors", 1,
+            "component_kind" => "sink",
+            "component_type" => "splunk_hec",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct SplunkSourceTypeMissingKeys {
+    pub keys: Vec<Atom>,
+}
+
+impl InternalEvent for SplunkSourceTypeMissingKeys {
+    fn emit_logs(&self) {
+        warn!(
+            message = "failed to render template for sourcetype, leaving empty",
+            missing_keys = ?self.keys,
+            rate_limit_secs = 30,
+        )
+    }
+
+    fn emit_metrics(&self) {
+        counter!(
+            "sourcetype_missing_keys", 1,
             "component_kind" => "sink",
             "component_type" => "splunk_hec",
         );
