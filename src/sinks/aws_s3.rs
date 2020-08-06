vectordotdev/vector

commit 8605d64b41ca3174ef6540cb7bff9bb1ef355042

Change interpretation of in_flight_limit to a fixed limit
If `request.in_flight_limit` is set, it specifies a fixed constant
limit, effectively bypassing the automatic concurrency mechanisms. If
unset, the limit is allowed to float up to a maximum of MAX_CONCURRENCY,
currently set to 200.

Note that this explicitly does not apply to the aws_s3 and
gcp_cloud_storage sinks, which use the old fixed concurrency limiter,
nor aws_cloudwatch_logs, which uses the old sink service.

Signed-off-by: Bruce Guenter <bruce@timber.io>

.meta/_partials/fields/_request_options.toml.erb
@@ -10,10 +10,20 @@ description = "Configures the sink request behavior."
 [<%= namespace %>.request.children.in_flight_limit]
 type = "uint"
 common = true
+<%- if defined?(in_flight_limit) %>
 default = <%= in_flight_limit %>
+<% else %>
+examples = [5]
+<% end %>
 groups = <%= groups.to_toml %>
 unit = "requests"
-description = "The maximum number of in-flight requests allowed at any given time."
+description = """\
+The maximum number of in-flight requests allowed at any given time. \
+<%- if !defined?(in_flight_limit) %>
+If this is not set, this limit will vary continuously based on the \
+timing and contents of responses received from the remote service.\
+<% end %>
+"""
 
 [<%= namespace %>.request.children.rate_limit_duration_secs]
 type = "uint"

.meta/sinks/http.toml.erb
@@ -32,7 +32,6 @@ write_to_description = "a generic [HTTP][urls.http] endpoint"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.http.options",
   common: true,
-  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 1000,
   retry_initial_backoff_secs: 1,

.meta/sinks/humio_logs.toml.erb
@@ -39,7 +39,6 @@ write_to_description = "[Humio][urls.humio] via the [HEC API][urls.humio_hec]"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.humio_logs.options",
   common: false,
-  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 10,
   retry_initial_backoff_secs: 1,

.meta/sinks/new_relic_logs.toml.erb
@@ -37,7 +37,6 @@ write_to_description = "[New Relic's log service][urls.new_relic] via their [log
   "_partials/fields/_request_options.toml",
   namespace: "sinks.new_relic_logs.options",
   common: false,
-  in_flight_limit: 5,
   rate_limit_duration_secs: 1,
   rate_limit_num: 100,
   retry_initial_backoff_secs: 1,

.meta/sinks/splunk_hec.toml.erb
@@ -34,7 +34,6 @@ write_to_description = "a [Splunk's HTTP Event Collector][urls.splunk_hec]"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.splunk_hec.options",
   common: false,
-  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 10,
   retry_initial_backoff_secs: 1,

src/sinks/http.rs
@@ -71,7 +71,6 @@ fn default_config(e: Encoding) -> HttpSinkConfig {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
-        in_flight_limit: Some(10),
         timeout_secs: Some(30),
         rate_limit_num: Some(10),
         ..Default::default()

src/sinks/new_relic_logs.rs
@@ -118,7 +118,6 @@ impl NewRelicLogsConfig {
         let request = TowerRequestConfig {
             // The default throughput ceiling defaults are relatively
             // conservative so we crank them up for New Relic.
-            in_flight_limit: Some(self.request.in_flight_limit.unwrap_or(100)),
             rate_limit_num: Some(self.request.rate_limit_num.unwrap_or(100)),
             ..self.request
         };
@@ -183,7 +182,7 @@ mod tests {
             http_config.batch.max_bytes,
             Some(bytesize::mib(5u64) as usize)
         );
-        assert_eq!(http_config.request.in_flight_limit, Some(100));
+        assert_eq!(http_config.request.in_flight_limit, None);
         assert_eq!(http_config.request.rate_limit_num, Some(100));
         assert_eq!(
             http_config.headers.unwrap()["X-License-Key"],

src/sinks/splunk_hec.rs
@@ -53,7 +53,6 @@ pub struct HecSinkConfig {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
-        in_flight_limit: Some(10),
         rate_limit_num: Some(10),
         ..Default::default()
     };

commit 671d3dd9e22a2868c33a104beab1f37946493dd0

Make the automatic limit opt-in by default
This restores the previous defaults for the fixed concurrency limits,
and enables the automatic limiter if `request.in_flight_limit = "auto"`.

This fixes the self tests that were accidentally broken by commit
8605d64b41ca3174ef6540cb7bff9bb1ef355042 changing the behavior of
specifying an in_flight_limit number in the settings.

Signed-off-by: Bruce Guenter <bruce@timber.io>

.meta/_partials/fields/_request_options.toml.erb
@@ -1,5 +1,6 @@
 <%- groups ||= [] -%>
 <%- retry_attempts ||= 18446744073709551615 %>
+<%- in_flight_limit ||= 5 %>
 [<%= namespace %>.request]
 type = "table"
 category = "Request"
@@ -10,19 +11,13 @@ description = "Configures the sink request behavior."
 [<%= namespace %>.request.children.in_flight_limit]
 type = "uint"
 common = true
-<%- if defined?(in_flight_limit) %>
 default = <%= in_flight_limit %>
-<% else %>
-examples = [5]
-<% end %>
 groups = <%= groups.to_toml %>
 unit = "requests"
 description = """\
 The maximum number of in-flight requests allowed at any given time. \
-<%- if !defined?(in_flight_limit) %>
-If this is not set, this limit will vary continuously based on the \
-timing and contents of responses received from the remote service.\
-<% end %>
+If this is set to `"auto"`, the effective limit will vary continuously based \
+on the timing and contents of responses received from the remote service.\
 """
 
 [<%= namespace %>.request.children.rate_limit_duration_secs]

.meta/sinks/http.toml.erb
@@ -32,6 +32,7 @@ write_to_description = "a generic [HTTP][urls.http] endpoint"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.http.options",
   common: true,
+  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 1000,
   retry_initial_backoff_secs: 1,

.meta/sinks/humio_logs.toml.erb
@@ -39,6 +39,7 @@ write_to_description = "[Humio][urls.humio] via the [HEC API][urls.humio_hec]"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.humio_logs.options",
   common: false,
+  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 10,
   retry_initial_backoff_secs: 1,

.meta/sinks/new_relic_logs.toml.erb
@@ -37,6 +37,7 @@ write_to_description = "[New Relic's log service][urls.new_relic] via their [log
   "_partials/fields/_request_options.toml",
   namespace: "sinks.new_relic_logs.options",
   common: false,
+  in_flight_limit: 100,
   rate_limit_duration_secs: 1,
   rate_limit_num: 100,
   retry_initial_backoff_secs: 1,

.meta/sinks/splunk_hec.toml.erb
@@ -34,6 +34,7 @@ write_to_description = "a [Splunk's HTTP Event Collector][urls.splunk_hec]"
   "_partials/fields/_request_options.toml",
   namespace: "sinks.splunk_hec.options",
   common: false,
+  in_flight_limit: 10,
   rate_limit_duration_secs: 1,
   rate_limit_num: 10,
   retry_initial_backoff_secs: 1,

src/sinks/aws_s3.rs
@@ -7,7 +7,7 @@ use crate::{
         encoding::{EncodingConfigWithDefault, EncodingConfiguration},
         retries2::RetryLogic,
         rusoto,
-        service2::{ServiceBuilderExt, TowerCompat, TowerRequestConfig},
+        service2::{InFlightLimit, ServiceBuilderExt, TowerCompat, TowerRequestConfig},
         sink::Response,
         BatchConfig, BatchSettings, Buffer, Compression, PartitionBatchSink, PartitionBuffer,
         PartitionInnerBuffer,
@@ -119,7 +119,7 @@ enum S3StorageClass {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
-        in_flight_limit: Some(50),
+        in_flight_limit: InFlightLimit::Fixed(50),
         rate_limit_num: Some(250),
         ..Default::default()
     };

src/sinks/gcp/cloud_storage.rs
@@ -7,7 +7,7 @@ use crate::{
             encoding::{EncodingConfig, EncodingConfiguration},
             http::{HttpClient, HttpClientFuture},
             retries2::{RetryAction, RetryLogic},
-            service2::{ServiceBuilderExt, TowerCompat, TowerRequestConfig},
+            service2::{InFlightLimit, ServiceBuilderExt, TowerCompat, TowerRequestConfig},
             BatchConfig, BatchSettings, Buffer, Compression, PartitionBatchSink, PartitionBuffer,
             PartitionInnerBuffer,
         },
@@ -123,7 +123,7 @@ enum GcsStorageClass {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
-        in_flight_limit: Some(25),
+        in_flight_limit: InFlightLimit::Fixed(25),
         rate_limit_num: Some(25),
         ..Default::default()
     };

src/sinks/http.rs
@@ -3,7 +3,7 @@ use crate::{
     sinks::util::{
         encoding::{EncodingConfig, EncodingConfiguration},
         http::{Auth, BatchedHttpSink, HttpClient, HttpSink},
-        service2::TowerRequestConfig,
+        service2::{InFlightLimit, TowerRequestConfig},
         BatchConfig, BatchSettings, Buffer, Compression, UriSerde,
     },
     tls::{TlsOptions, TlsSettings},
@@ -71,6 +71,7 @@ fn default_config(e: Encoding) -> HttpSinkConfig {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
+        in_flight_limit: InFlightLimit::Fixed(10),
         timeout_secs: Some(30),
         rate_limit_num: Some(10),
         ..Default::default()

src/sinks/new_relic_logs.rs
@@ -2,7 +2,7 @@ use crate::{
     sinks::http::{HttpMethod, HttpSinkConfig},
     sinks::util::{
         encoding::{EncodingConfigWithDefault, EncodingConfiguration},
-        service2::TowerRequestConfig,
+        service2::{InFlightLimit, TowerRequestConfig},
         BatchConfig, Compression,
     },
     topology::config::{DataType, SinkConfig, SinkContext, SinkDescription},
@@ -118,6 +118,7 @@ impl NewRelicLogsConfig {
         let request = TowerRequestConfig {
             // The default throughput ceiling defaults are relatively
             // conservative so we crank them up for New Relic.
+            in_flight_limit: (self.request.in_flight_limit).if_none(InFlightLimit::Fixed(100)),
             rate_limit_num: Some(self.request.rate_limit_num.unwrap_or(100)),
             ..self.request
         };
@@ -144,7 +145,7 @@ mod tests {
     use super::*;
     use crate::{
         event::Event,
-        sinks::util::test::build_test_server,
+        sinks::util::{service2::InFlightLimit, test::build_test_server},
         test_util::{next_addr, runtime, shutdown_on_idle},
         topology::config::SinkConfig,
     };
@@ -182,7 +183,10 @@ mod tests {
             http_config.batch.max_bytes,
             Some(bytesize::mib(5u64) as usize)
         );
-        assert_eq!(http_config.request.in_flight_limit, None);
+        assert_eq!(
+            http_config.request.in_flight_limit,
+            InFlightLimit::Fixed(100)
+        );
         assert_eq!(http_config.request.rate_limit_num, Some(100));
         assert_eq!(
             http_config.headers.unwrap()["X-License-Key"],
@@ -198,7 +202,7 @@ mod tests {
         nr_config.insert_key = Some("foo".to_owned());
         nr_config.region = Some(NewRelicLogsRegion::Eu);
         nr_config.batch.max_size = Some(bytesize::mib(8u64) as usize);
-        nr_config.request.in_flight_limit = Some(12);
+        nr_config.request.in_flight_limit = InFlightLimit::Fixed(12);
         nr_config.request.rate_limit_num = Some(24);
 
         let http_config = nr_config.create_config().unwrap();
@@ -213,7 +217,10 @@ mod tests {
             http_config.batch.max_bytes,
             Some(bytesize::mib(8u64) as usize)
         );
-        assert_eq!(http_config.request.in_flight_limit, Some(12));
+        assert_eq!(
+            http_config.request.in_flight_limit,
+            InFlightLimit::Fixed(12)
+        );
         assert_eq!(http_config.request.rate_limit_num, Some(24));
         assert_eq!(
             http_config.headers.unwrap()["X-Insert-Key"],
@@ -250,7 +257,10 @@ mod tests {
             http_config.batch.max_bytes,
             Some(bytesize::mib(8u64) as usize)
         );
-        assert_eq!(http_config.request.in_flight_limit, Some(12));
+        assert_eq!(
+            http_config.request.in_flight_limit,
+            InFlightLimit::Fixed(12)
+        );
         assert_eq!(http_config.request.rate_limit_num, Some(24));
         assert_eq!(
             http_config.headers.unwrap()["X-Insert-Key"],

src/sinks/splunk_hec.rs
@@ -4,7 +4,7 @@ use crate::{
     sinks::util::{
         encoding::{EncodingConfigWithDefault, EncodingConfiguration},
         http::{BatchedHttpSink, HttpClient, HttpSink},
-        service2::TowerRequestConfig,
+        service2::{InFlightLimit, TowerRequestConfig},
         BatchConfig, BatchSettings, Buffer, Compression,
     },
     tls::{TlsOptions, TlsSettings},
@@ -53,6 +53,7 @@ pub struct HecSinkConfig {
 
 lazy_static! {
     static ref REQUEST_DEFAULTS: TowerRequestConfig = TowerRequestConfig {
+        in_flight_limit: InFlightLimit::Fixed(10),
         rate_limit_num: Some(10),
         ..Default::default()
     };

src/sinks/util/auto_concurrency/service.rs
@@ -160,7 +160,7 @@ mod tests {
 
     impl TestService {
         fn start() -> Self {
-            let layer = AutoConcurrencyLimitLayer::new(Some(10), TestRetryLogic);
+            let layer = AutoConcurrencyLimitLayer::new(None, TestRetryLogic);
             let (service, handle) = mock::spawn_layer(layer);
             let controller = service.get_ref().controller.clone();
             let inner = Arc::clone(&controller.inner);

src/sinks/util/auto_concurrency/tests.rs
@@ -1,12 +1,17 @@
 #![cfg(all(test, feature = "sources-generator"))]
 
 use super::controller::ControllerStatistics;
+use super::MAX_CONCURRENCY;
 use crate::{
     assert_within,
     event::{metric::MetricValue, Event},
     metrics::{self, capture_metrics, get_controller},
     sinks::{
-        util::{retries2::RetryLogic, service2::TowerRequestConfig, BatchSettings, VecBuffer},
+        util::{
+            retries2::RetryLogic,
+            service2::{InFlightLimit, TowerRequestConfig},
+            BatchSettings, VecBuffer,
+        },
         Healthcheck, RouterSink,
     },
     sources::generator::GeneratorConfig,
@@ -218,7 +223,7 @@ fn run_test(lines: usize, interval: Option<f64>, params: TestParams) -> TestData
 
     let test_config = TestConfig {
         request: TowerRequestConfig {
-            in_flight_limit: Some(10),
+            in_flight_limit: InFlightLimit::Auto,
             rate_limit_num: Some(9999),
             timeout_secs: Some(1),
             ..Default::default()
@@ -302,11 +307,17 @@ fn constant_link() {
     // With a constant response time link and enough responses, the
     // limiter will ramp up to the maximum concurrency,
     let in_flight = results.stats.in_flight.stats().unwrap();
-    assert_eq!(in_flight.max, 10, "{:#?}", results);
+    assert_within!(in_flight.max, 10, MAX_CONCURRENCY, "{:#?}", results);
     // and will spend most of its time in the top half of the
     // concurrency range.
-    assert_eq!(in_flight.mode, 10, "{:#?}", results);
-    assert_within!(in_flight.mean, 6.0, 10.0, "{:#?}", results);
+    assert_within!(in_flight.mode, 9, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(
+        in_flight.mean,
+        6.0,
+        MAX_CONCURRENCY as f64,
+        "{:#?}",
+        results
+    );
 
     let observed_rtt = results.cstats.observed_rtt.stats().unwrap();
     assert_within!(observed_rtt.min, 0.099, 0.110, "{:#?}", results);
@@ -317,13 +328,25 @@ fn constant_link() {
     assert_within!(averaged_rtt.max, 0.099, 0.115, "{:#?}", results);
     assert_within!(averaged_rtt.mean, 0.099, 0.110, "{:#?}", results);
     let concurrency_limit = results.cstats.concurrency_limit.stats().unwrap();
-    assert_within!(concurrency_limit.max, 9, 10, "{:#?}", results);
-    assert_within!(concurrency_limit.mode, 9, 10, "{:#?}", results);
-    assert_within!(concurrency_limit.mean, 5.0, 10.0, "{:#?}", results);
+    assert_within!(concurrency_limit.max, 9, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(concurrency_limit.mode, 9, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(
+        concurrency_limit.mean,
+        5.0,
+        MAX_CONCURRENCY as f64,
+        "{:#?}",
+        results
+    );
     let c_in_flight = results.cstats.in_flight.stats().unwrap();
-    assert_within!(c_in_flight.max, 9, 10, "{:#?}", results);
-    assert_within!(c_in_flight.mode, 9, 10, "{:#?}", results);
-    assert_within!(c_in_flight.mean, 6.5, 10.0, "{:#?}", results);
+    assert_within!(c_in_flight.max, 9, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(c_in_flight.mode, 9, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(
+        c_in_flight.mean,
+        6.5,
+        MAX_CONCURRENCY as f64,
+        "{:#?}",
+        results
+    );
 }
 
 #[test]
@@ -531,9 +554,9 @@ fn medium_send() {
     assert_within!(averaged_rtt.min, 0.099, 0.110, "{:#?}", results);
     assert_within!(averaged_rtt.mean, 0.099, 0.500, "{:#?}", results);
     let concurrency_limit = results.cstats.concurrency_limit.stats().unwrap();
-    assert_within!(concurrency_limit.max, 4, 10, "{:#?}", results);
+    assert_within!(concurrency_limit.max, 4, MAX_CONCURRENCY, "{:#?}", results);
     let c_in_flight = results.cstats.in_flight.stats().unwrap();
-    assert_within!(c_in_flight.max, 4, 10, "{:#?}", results);
+    assert_within!(c_in_flight.max, 4, MAX_CONCURRENCY, "{:#?}", results);
     assert_within!(c_in_flight.mode, 4, 5, "{:#?}", results);
     assert_within!(c_in_flight.mean, 4.0, 5.0, "{:#?}", results);
 }
@@ -553,7 +576,7 @@ fn jittery_link_small() {
     // Jitter can cause concurrency management to vary widely, though it
     // will typically reach the maximum of 10 in flight.
     let in_flight = results.stats.in_flight.stats().unwrap();
-    assert_within!(in_flight.max, 6, 10, "{:#?}", results);
+    assert_within!(in_flight.max, 6, MAX_CONCURRENCY, "{:#?}", results);
     assert_within!(in_flight.mean, 4.0, 9.0, "{:#?}", results);
 
     let observed_rtt = results.cstats.observed_rtt.stats().unwrap();
@@ -561,9 +584,15 @@ fn jittery_link_small() {
     let averaged_rtt = results.cstats.averaged_rtt.stats().unwrap();
     assert_within!(averaged_rtt.mean, 0.099, 0.130, "{:#?}", results);
     let concurrency_limit = results.cstats.concurrency_limit.stats().unwrap();
-    assert_within!(concurrency_limit.max, 6, 10, "{:#?}", results);
-    assert_within!(concurrency_limit.mean, 4.0, 10.0, "{:#?}", results);
+    assert_within!(concurrency_limit.max, 6, MAX_CONCURRENCY, "{:#?}", results);
+    assert_within!(
+        concurrency_limit.mean,
+        4.0,
+        MAX_CONCURRENCY as f64,
+        "{:#?}",
+        results
+    );
     let c_in_flight = results.cstats.in_flight.stats().unwrap();
-    assert_within!(c_in_flight.max, 6, 10, "{:#?}", results);
+    assert_within!(c_in_flight.max, 6, MAX_CONCURRENCY, "{:#?}", results);
     assert_within!(c_in_flight.mean, 4.0, 9.0, "{:#?}", results);
 }

src/sinks/util/service2.rs
@@ -4,7 +4,11 @@ use super::sink::Response;
 use super::{Batch, BatchSink};
 use crate::buffers::Acker;
 use futures::TryFutureExt;
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{self, Unexpected, Visitor},
+    Deserialize, Deserializer, Serialize,
+};
+use std::fmt;
 use std::sync::Arc;
 use std::task::Poll;
 use std::time::Duration;
@@ -55,10 +59,79 @@ impl<L> ServiceBuilderExt<L> for ServiceBuilder<L> {
     }
 }
 
+#[derive(Clone, Copy, Debug, Derivative, Eq, PartialEq, Serialize)]
+#[derivative(Default)]
+pub enum InFlightLimit {
+    #[derivative(Default)]
+    None,
+    Auto,
+    Fixed(usize),
+}
+
+impl InFlightLimit {
+    pub fn if_none(self, other: Self) -> Self {
+        match self {
+            Self::None => other,
+            _ => self,
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for InFlightLimit {
+    // Deserialize either a positive integer or the string "auto"
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct UsizeOrAuto;
+
+        impl<'de> Visitor<'de> for UsizeOrAuto {
+            type Value = InFlightLimit;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(r#"positive integer or "auto""#)
+            }
+
+            fn visit_str<E: de::Error>(self, value: &str) -> Result<InFlightLimit, E> {
+                if value == "auto" {
+                    Ok(InFlightLimit::Auto)
+                } else {
+                    Err(de::Error::unknown_variant(value, &["auto"]))
+                }
+            }
+
+            fn visit_i64<E: de::Error>(self, value: i64) -> Result<InFlightLimit, E> {
+                if value > 0 {
+                    Ok(InFlightLimit::Fixed(value as usize))
+                } else {
+                    Err(de::Error::invalid_value(
+                        Unexpected::Signed(value),
+                        &"positive integer",
+                    ))
+                }
+            }
+
+            fn visit_u64<E: de::Error>(self, value: u64) -> Result<InFlightLimit, E> {
+                if value > 0 {
+                    Ok(InFlightLimit::Fixed(value as usize))
+                } else {
+                    Err(de::Error::invalid_value(
+                        Unexpected::Unsigned(value),
+                        &"positive integer",
+                    ))
+                }
+            }
+        }
+
+        deserializer.deserialize_any(UsizeOrAuto)
+    }
+}
+
 /// Tower Request based configuration
 #[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
 pub struct TowerRequestConfig {
-    pub in_flight_limit: Option<usize>,        // None == auto
+    #[serde(default)]
+    pub in_flight_limit: InFlightLimit, // 5
     pub timeout_secs: Option<u64>,             // 60
     pub rate_limit_duration_secs: Option<u64>, // 1
     pub rate_limit_num: Option<u64>,           // 5
@@ -70,7 +143,11 @@ pub struct TowerRequestConfig {
 impl TowerRequestConfig {
     pub fn unwrap_with(&self, defaults: &TowerRequestConfig) -> TowerRequestSettings {
         TowerRequestSettings {
-            in_flight_limit: self.in_flight_limit.or(defaults.in_flight_limit),
+            in_flight_limit: match self.in_flight_limit.if_none(defaults.in_flight_limit) {
+                InFlightLimit::None => Some(5),
+                InFlightLimit::Auto => None,
+                InFlightLimit::Fixed(limit) => Some(limit),
+            },
             timeout: Duration::from_secs(self.timeout_secs.or(defaults.timeout_secs).unwrap_or(60)),
             rate_limit_duration: Duration::from_secs(
                 self.rate_limit_duration_secs
@@ -285,5 +362,28 @@ mod compat {
         fn call(&mut self, req: Request) -> Self::Future {
             Compat::new(Box::pin(self.inner.call(req)))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn in_flight_limit_works() {
+        let cfg = toml::from_str::<TowerRequestConfig>("").expect("Empty config failed");
+        assert_eq!(cfg.in_flight_limit, InFlightLimit::None);
+        let cfg = toml::from_str::<TowerRequestConfig>("in_flight_limit = 10")
+            .expect("Fixed in_flight_limit failed");
+        assert_eq!(cfg.in_flight_limit, InFlightLimit::Fixed(10));
+        let cfg = toml::from_str::<TowerRequestConfig>(r#"in_flight_limit = "auto""#)
+            .expect("Auto in_flight_limit failed");
+        assert_eq!(cfg.in_flight_limit, InFlightLimit::Auto);
+        toml::from_str::<TowerRequestConfig>(r#"in_flight_limit = "broken""#)
+            .expect_err("Invalid in_flight_limit didn't fail");
+        toml::from_str::<TowerRequestConfig>(r#"in_flight_limit = 0"#)
+            .expect_err("Invalid in_flight_limit didn't fail on zero");
+        toml::from_str::<TowerRequestConfig>(r#"in_flight_limit = -9"#)
+            .expect_err("Invalid in_flight_limit didn't fail on negative number");
     }
 }