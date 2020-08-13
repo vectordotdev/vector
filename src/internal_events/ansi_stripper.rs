8bf5ea353b1cffcccadbf2810ea3c3a39993086d
Kruno Tomola Fabro <krunotf@gmail.com>
2020-08-13 14:57:54 +0200

    enhancement(ansi_stripper transform): Add instrumentation (#3421)
    * Add ANSIStripperEventProcessed
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add ANSIStripperFieldMissing
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add ANSIStripperFieldInvalid
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add ANSIStripperFailed
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Cleanup
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

---
+++ src/internal_events/ansi_stripper.rs
@@ -0,0 +1,85 @@
+use super::InternalEvent;
+use metrics::counter;
+use string_cache::DefaultAtom as Atom;
+
+#[derive(Debug)]
+pub struct ANSIStripperEventProcessed;
+
+impl InternalEvent for ANSIStripperEventProcessed {
+    fn emit_metrics(&self) {
+        counter!("events_processed", 1,
+            "component_kind" => "transform",
+            "component_type" => "ansi_stripper",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct ANSIStripperFieldMissing<'a> {
+    pub field: &'a Atom,
+}
+
+impl InternalEvent for ANSIStripperFieldMissing<'_> {
+    fn emit_logs(&self) {
+        debug!(
+            message = "field does not exist.",
+            field = %self.field,
+            rate_limit_secs = 10
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!("processing_errors", 1,
+            "component_kind" => "transform",
+            "component_type" => "ansi_stripper",
+            "error_type" => "field_missing",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct ANSIStripperFieldInvalid<'a> {
+    pub field: &'a Atom,
+}
+
+impl InternalEvent for ANSIStripperFieldInvalid<'_> {
+    fn emit_logs(&self) {
+        debug!(
+            message = "field value must be a string.",
+            field = %self.field,
+            rate_limit_secs = 10
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!("processing_errors", 1,
+            "component_kind" => "transform",
+            "component_type" => "ansi_stripper",
+            "error_type" => "value_invalid",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct ANSIStripperFailed<'a> {
+    pub field: &'a Atom,
+    pub error: std::io::Error,
+}
+
+impl InternalEvent for ANSIStripperFailed<'_> {
+    fn emit_logs(&self) {
+        debug!(
+            message = "could not strip ANSI escape sequences.",
+            field = %self.field,
+            error = %self.error,
+            rate_limit_secs = 10,
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!("processing_errors", 1,
+            "component_kind" => "transform",
+            "component_type" => "ansi_stripper",
+        );
+    }
+}
--- src/internal_events/mod.rs
+++ src/internal_events/mod.rs
@@ -1,4 +1,5 @@
 mod add_fields;
+mod ansi_stripper;
 mod auto_concurrency;
 mod aws_kinesis_streams;
 mod blackhole;
@@ -35,6 +36,7 @@ mod wasm;
 pub mod kubernetes;
 
 pub use self::add_fields::*;
+pub use self::ansi_stripper::*;
 pub use self::auto_concurrency::*;
 pub use self::aws_kinesis_streams::*;
 pub use self::blackhole::*;
