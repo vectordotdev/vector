5f6ce7cfa5fea6e8a3078e63d2e04425b8c38c64
Kruno Tomola Fabro <krunotf@gmail.com>
2020-08-19 22:25:56 +0200

    enhancement: Add events for starting, stopping, and reloading (#3476)
    * Add process events
    
    Signed-off-by: ktf <krunotf@gmail.com>
    
    * Use past tense
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * One last metric capture
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * One more move
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Remove unused use
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>

--- src/internal_events/mod.rs
+++ src/internal_events/mod.rs
@@ -17,6 +17,7 @@ mod kubernetes_logs;
 mod logplex;
 #[cfg(feature = "transforms-lua")]
 mod lua;
+mod process;
 #[cfg(feature = "sources-prometheus")]
 mod prometheus;
 mod regex;
@@ -57,6 +58,7 @@ pub use self::kubernetes_logs::*;
 pub use self::logplex::*;
 #[cfg(feature = "transforms-lua")]
 pub use self::lua::*;
+pub use self::process::*;
 #[cfg(feature = "sources-prometheus")]
 pub use self::prometheus::*;
 pub use self::regex::*;
