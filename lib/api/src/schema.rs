5fe26f6aa08c3e8b244780021f942b7050e6ef9f
Lee Benson <lee@leebenson.com>
2020-08-22 17:19:12 +0100

    heartbeat subscription

--- lib/api/src/schema.rs
+++ lib/api/src/schema.rs
@@ -1,4 +1,7 @@
-use juniper::{EmptyMutation, EmptySubscription, RootNode};
+use chrono::{DateTime, Utc};
+use juniper::{EmptyMutation, FieldError, RootNode};
+use std::pin::Pin;
+use tokio::{stream::Stream, time::Duration};
 
 #[derive(Clone)]
 pub struct Context {}
@@ -19,8 +22,45 @@ impl Query {
     }
 }
 
-pub type Schema = RootNode<'static, Query, EmptyMutation<Context>, EmptySubscription<Context>>;
+pub struct Subscription;
+
+#[derive(Clone)]
+struct Heartbeat {
+    utc: DateTime<Utc>,
+}
+
+impl Heartbeat {
+    fn new() -> Self {
+        Heartbeat { utc: Utc::now() }
+    }
+}
+
+#[juniper::graphql_object(Context = Context)]
+impl Heartbeat {
+    fn utc(&self) -> DateTime<Utc> {
+        self.utc
+    }
+}
+
+type HeartbeatStream = Pin<Box<dyn Stream<Item = Result<Heartbeat, FieldError>> + Send>>;
+
+#[juniper::graphql_subscription(Context = Context)]
+impl Subscription {
+    async fn heartbeat(interval: Option<i32>) -> HeartbeatStream {
+        let interval = match interval {
+            Some(i) if (100..=60_000).contains(&i) => i as u64,
+            _ => 1_000,
+        };
+
+        let stream =
+            tokio::time::interval(Duration::from_millis(interval)).map(|_| Ok(Heartbeat::new()));
+
+        Box::pin(stream)
+    }
+}
+
+pub type Schema = RootNode<'static, Query, EmptyMutation<Context>, Subscription>;
 
 pub fn schema() -> Schema {
-    Schema::new(Query, EmptyMutation::new(), EmptySubscription::new())
+    Schema::new(Query, EmptyMutation::new(), Subscription)
 }
