f8eb6543040ead2e751b039fd2c178d58846c671
MOZGIII <mike-n@narod.ru>
2020-08-25 20:07:17 +0300

    chore(file source): Multiline config extract (#3466)
    * Extract the the multiline config to allow sharing among the sources
    
    Signed-off-by: MOZGIII <mike-n@narod.ru>
    
    * Add multiline_config to CODEOWNERS
    
    Signed-off-by: MOZGIII <mike-n@narod.ru>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/sources/util/mod.rs
+++ src/sources/util/mod.rs
@@ -1,5 +1,6 @@
 #[cfg(all(feature = "sources-tls", feature = "warp"))]
 mod http;
+pub mod multiline_config;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 mod tcp;
 #[cfg(all(unix, any(feature = "sources-socket", feature = "sources-syslog")))]
@@ -7,6 +8,7 @@ mod unix;
 
 #[cfg(all(feature = "sources-tls", feature = "warp"))]
 pub use self::http::{ErrorMessage, HttpSource};
+pub use multiline_config::MultilineConfig;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 pub use tcp::{SocketListenAddr, TcpSource};
 #[cfg(all(unix, any(feature = "sources-socket", feature = "sources-syslog")))]
