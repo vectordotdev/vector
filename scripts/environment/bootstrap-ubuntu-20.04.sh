7d9de03911193e4518f060c6980a644271a3e20c
James Turnbull <james@lovedthanlost.net>
2020-08-25 15:41:59 -0400

    Test move to container execution for speed
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- scripts/environment/bootstrap-ubuntu-20.04.sh
+++ scripts/environment/bootstrap-ubuntu-20.04.sh
@@ -52,6 +52,9 @@ add-apt-repository \
    xenial \
    stable"
 
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # Install those new things
 apt update --yes
 apt install --yes yarn docker-ce docker-ce-cli containerd.io
