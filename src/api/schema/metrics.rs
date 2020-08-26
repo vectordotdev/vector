025fe2ff54e92e18311f366ef602bb5854d91658
Lee Benson <lee@leebenson.com>
2020-08-26 14:50:04 +0100

    tidy up schema

---
+++ src/api/schema/metrics.rs
@@ -0,0 +1,79 @@
+use crate::event::{Event, Metric, MetricValue};
+use crate::metrics::{capture_metrics, get_controller};
+use async_graphql::{Interface, Object};
+use async_stream::stream;
+use chrono::{DateTime, Utc};
+use tokio::stream::{Stream, StreamExt};
+use tokio::time::Duration;
+
+pub struct EventsProcessed(Metric);
+
+#[Object]
+impl EventsProcessed {
+    /// Metric timestamp
+    async fn timestamp(&self) -> Option<DateTime<Utc>> {
+        self.0.timestamp
+    }
+
+    /// Number of events processed
+    async fn events_processed(&self) -> f64 {
+        match self.0.value {
+            MetricValue::Counter { value } => value,
+            _ => 0.00,
+        }
+    }
+}
+
+impl Into<EventsProcessed> for Metric {
+    fn into(self) -> EventsProcessed {
+        EventsProcessed(self)
+    }
+}
+
+pub struct BytesProcessed(Metric);
+
+#[Object]
+impl BytesProcessed {
+    /// Metric timestamp
+    async fn timestamp(&self) -> Option<DateTime<Utc>> {
+        self.0.timestamp
+    }
+
+    /// Number of bytes processed
+    async fn bytes_processed(&self) -> f64 {
+        match self.0.value {
+            MetricValue::Counter { value } => value,
+            _ => 0.00,
+        }
+    }
+}
+
+#[Interface(field(name = "timestamp", type = "Option<DateTime<Utc>>"))]
+pub enum MetricType {
+    EventsProcessed(EventsProcessed),
+    BytesProcessed(BytesProcessed),
+}
+
+/// Returns a new metrics stream, at `interval` milliseconds
+pub fn metrics_stream(interval: i32) -> impl Stream<Item = MetricType> {
+    get_metrics(interval).filter_map(|m| match m.name.as_str() {
+        "events_processed" => Some(MetricType::EventsProcessed(m.into())),
+        _ => None,
+    })
+}
+
+/// Returns a stream of `Metric`s, collected at the provided millisecond interval
+fn get_metrics(interval: i32) -> impl Stream<Item = Metric> {
+    let controller = get_controller().unwrap();
+    let mut interval = tokio::time::interval(Duration::from_millis(interval as u64));
+
+    stream! {
+        while let _ = interval.tick().await {
+            for ev in capture_metrics(&controller) {
+                if let Event::Metric(m) = ev {
+                    yield m
+                }
+            }
+        }
+    }
+}
