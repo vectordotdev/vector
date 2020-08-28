56ba5d81982b4d8ba3e8f764dd48c54270df3dec
Ana Hobden <operator@hoverbear.org>
2020-08-28 10:22:36 -0700

    chore(concat transform): Add events to concat transform (#3509)
    * chore(concat transform): Add events to concat transform
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Features
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

---
+++ src/internal_events/concat.rs
@@ -0,0 +1,59 @@
+use super::InternalEvent;
+use metrics::counter;
+
+#[derive(Debug)]
+pub struct ConcatEventProcessed;
+
+impl InternalEvent for ConcatEventProcessed {
+    fn emit_metrics(&self) {
+        counter!("events_processed", 1,
+            "component_kind" => "transform",
+            "component_type" => "add_fields",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct ConcatSubstringError<'a> {
+    pub source: &'a str,
+    pub condition: &'a str,
+    pub start: usize,
+    pub end: usize,
+    pub length: usize,
+}
+
+impl<'a> InternalEvent for ConcatSubstringError<'a> {
+    fn emit_logs(&self) {
+        error!(
+            message = "Substring error.",
+            self.condition,
+            self.source,
+            self.start,
+            self.end,
+            self.length,
+            rate_limit_secs = 30
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!("processing_errors", 1,
+            "component_kind" => "transform",
+            "component_type" => "concat",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct ConcatSubstringSourceMissing<'a> {
+    pub source: &'a str,
+}
+
+impl<'a> InternalEvent for ConcatSubstringSourceMissing<'a> {
+    fn emit_logs(&self) {
+        debug!(
+            message = "Substring source missing.",
+            self.source,
+            rate_limit_secs = 30
+        );
+    }
+}
--- src/internal_events/mod.rs
+++ src/internal_events/mod.rs
@@ -6,6 +6,8 @@ mod ansi_stripper;
 mod auto_concurrency;
 mod aws_kinesis_streams;
 mod blackhole;
+#[cfg(feature = "transforms-concat")]
+mod concat;
 #[cfg(feature = "sources-docker")]
 mod docker;
 mod elasticsearch;
@@ -61,6 +63,8 @@ pub use self::ansi_stripper::*;
 pub use self::auto_concurrency::*;
 pub use self::aws_kinesis_streams::*;
 pub use self::blackhole::*;
+#[cfg(feature = "transforms-concat")]
+pub use self::concat::*;
 #[cfg(feature = "sources-docker")]
 pub use self::docker::*;
 pub use self::elasticsearch::*;
