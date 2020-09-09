77365a3915ebf1b06951e60a05cc48c8329d955b
Lee Benson <lee@leebenson.com>
2020-09-09 19:23:13 +0100

    portpicker (#3772)
    Signed-off-by: Lee Benson <lee@leebenson.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/test_util/mod.rs
+++ src/test_util/mod.rs
@@ -111,12 +111,11 @@ high: `{:?}`
 
 }
 
-static NEXT_PORT: AtomicUsize = AtomicUsize::new(1234);
-
 pub fn next_addr() -> SocketAddr {
+    use portpicker::pick_unused_port;
     use std::net::{IpAddr, Ipv4Addr};
 
-    let port = NEXT_PORT.fetch_add(1, Ordering::AcqRel) as u16;
+    let port = pick_unused_port().unwrap();
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port)
 }
 
