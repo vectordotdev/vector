348c776f62b31a6310eefe1eef8c5ecee2d073a6
Ana Hobden <operator@hoverbear.org>
2020-09-14 14:56:44 -0700

    chore: Remove cpan
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- scripts/package-deb.sh
+++ scripts/package-deb.sh
@@ -74,8 +74,9 @@ cargo deb --target "$TARGET" --deb-version "$PACKAGE_VERSION" --no-build
 
 # Rename the resulting .deb file to use - instead of _ since this
 # is consistent with our package naming scheme.
-# shellcheck disable=SC2016
-rename -v 's/vector_([^_]*)_(.*)\.deb/vector-$2\.deb/' "target/$TARGET/debian"/*.deb
+for file in /target/"${TARGET}"/debian/*.deb; do
+  mv "${file}" "${file//_/-}}";
+done
 
 #
 # Move the deb into the artifacts dir
