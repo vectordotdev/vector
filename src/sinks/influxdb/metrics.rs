42b2cbe99512e05f5dd4bdb9be5834cd242a0b02
Kruno Tomola Fabro <krunotf@gmail.com>
2020-09-15 18:50:03 +0200

    enhancement(influxdb_metrics sink): Support `summary` statistic (#3846)
    * Update influxdb_metrics sink
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    
    * Add test
    
    Signed-off-by: Kruno Tomola Fabro <krunotf@gmail.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/sinks/influxdb/metrics.rs
+++ src/sinks/influxdb/metrics.rs
@@ -1,6 +1,6 @@
 use crate::{
     config::{DataType, SinkConfig, SinkContext, SinkDescription},
-    event::metric::{Metric, MetricValue},
+    event::metric::{Metric, MetricValue, StatisticKind},
     sinks::{
         influxdb::{
             encode_namespace, encode_timestamp, healthcheck, influx_line_protocol,
@@ -269,9 +269,13 @@ fn encode_events(
             MetricValue::Distribution {
                 values,
                 sample_rates,
-                statistic: _,
+                statistic,
             } => {
-                let fields = encode_distribution(&values, &sample_rates);
+                let quantiles = match statistic {
+                    StatisticKind::Histogram => &[0.95] as &[_],
+                    StatisticKind::Summary => &[0.5, 0.75, 0.9, 0.95, 0.99] as &[_],
+                };
+                let fields = encode_distribution(&values, &sample_rates, quantiles);
 
                 influx_line_protocol(
                     protocol_version,
@@ -291,7 +295,11 @@ fn encode_events(
     output
 }
 
-fn encode_distribution(values: &[f64], counts: &[u32]) -> Option<HashMap<String, Field>> {
+fn encode_distribution(
+    values: &[f64],
+    counts: &[u32],
+    quantiles: &[f64],
+) -> Option<HashMap<String, Field>> {
     if values.len() != counts.len() {
         return None;
     }
@@ -317,9 +325,13 @@ fn encode_distribution(values: &[f64], counts: &[u32]) -> Option<HashMap<String,
                 ("avg".to_owned(), Field::Float(val)),
                 ("sum".to_owned(), Field::Float(val)),
                 ("count".to_owned(), Field::Float(1.0)),
-                ("quantile_0.95".to_owned(), Field::Float(val)),
             ]
             .into_iter()
+            .chain(
+                quantiles
+                    .iter()
+                    .map(|p| (format!("quantile_{:.2}", p), Field::Float(val))),
+            )
             .collect(),
         );
     }
@@ -330,8 +342,11 @@ fn encode_distribution(values: &[f64], counts: &[u32]) -> Option<HashMap<String,
     let min = samples.first().unwrap();
     let max = samples.last().unwrap();
 
-    let p50 = samples[(0.50 * length - 1.0).round() as usize];
-    let p95 = samples[(0.95 * length - 1.0).round() as usize];
+    let median = samples[(0.50 * length - 1.0).round() as usize];
+    let quantiles = quantiles.iter().map(|p| {
+        let sample = samples[(p * length - 1.0).round() as usize];
+        (format!("quantile_{:.2}", p), Field::Float(sample))
+    });
 
     let sum = samples.iter().sum();
     let avg = sum / length;
@@ -339,13 +354,13 @@ fn encode_distribution(values: &[f64], counts: &[u32]) -> Option<HashMap<String,
     let fields: HashMap<String, Field> = vec![
         ("min".to_owned(), Field::Float(*min)),
         ("max".to_owned(), Field::Float(*max)),
-        ("median".to_owned(), Field::Float(p50)),
+        ("median".to_owned(), Field::Float(median)),
         ("avg".to_owned(), Field::Float(avg)),
         ("sum".to_owned(), Field::Float(sum)),
         ("count".to_owned(), Field::Float(length)),
-        ("quantile_0.95".to_owned(), Field::Float(p95)),
     ]
     .into_iter()
+    .chain(quantiles)
     .collect();
 
     Some(fields)
@@ -736,6 +751,50 @@ mod tests {
 
         let line_protocols = encode_events(ProtocolVersion::V2, events, Some("ns"));
         assert_eq!(line_protocols.len(), 0);
+    }
+
+    #[test]
+    fn test_encode_distribution_summary() {
+        let events = vec![Metric {
+            name: "requests".into(),
+            timestamp: Some(ts()),
+            tags: Some(tags()),
+            kind: MetricKind::Incremental,
+            value: MetricValue::Distribution {
+                values: vec![1.0, 2.0, 3.0],
+                sample_rates: vec![3, 3, 2],
+                statistic: StatisticKind::Summary,
+            },
+        }];
+
+        let line_protocols = encode_events(ProtocolVersion::V2, events, Some("ns"));
+        let line_protocols: Vec<&str> = line_protocols.split('\n').collect();
+        assert_eq!(line_protocols.len(), 1);
+
+        let line_protocol = split_line_protocol(line_protocols[0]);
+        assert_eq!("ns.requests", line_protocol.0);
+        assert_eq!(
+            "metric_type=distribution,normal_tag=value,true_tag=true",
+            line_protocol.1
+        );
+        assert_fields(
+            line_protocol.2.to_string(),
+            [
+                "avg=1.875",
+                "count=8",
+                "max=3",
+                "median=2",
+                "min=1",
+                "sum=15",
+                "quantile_0.50=2",
+                "quantile_0.75=2",
+                "quantile_0.90=3",
+                "quantile_0.95=3",
+                "quantile_0.99=3",
+            ]
+            .to_vec(),
+        );
+        assert_eq!("1542182950000000011", line_protocol.3);
     }
 }
 
