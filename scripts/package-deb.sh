cb1c7f611919c946b367671e7430396df564e43e
Ana Hobden <operator@hoverbear.org>
2020-09-15 07:56:09 -0700

    Correct name
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- scripts/package-deb.sh
+++ scripts/package-deb.sh
@@ -76,8 +76,8 @@ cargo deb --target "$TARGET" --deb-version "$PACKAGE_VERSION" --no-build
 # is consistent with our package naming scheme.
 for file in target/"${TARGET}"/debian/*.deb; do
   base=$(basename "${file}")
-  renamed=${base//_/-}
-  mv "${file}" target/"${TARGET}"/debian/"${renamed}";
+  arch=${base#vector_${PACKAGE_VERSION}_}
+  mv "${file}" target/"${TARGET}"/debian/vector-"${arch}";
 done
 
 #
