1951ff03c11f798f817669b957a5450d964ca610
Duy Do <juchiast@gmail.com>
2020-09-22 15:37:30 +0700

    aws_cloudwatch_metrics
    Signed-off-by: Duy Do <juchiast@gmail.com>

--- src/sinks/aws_cloudwatch_metrics.rs
+++ src/sinks/aws_cloudwatch_metrics.rs
@@ -57,7 +57,7 @@ inventory::submit! {
 
 impl GenerateConfig for CloudWatchMetricsSinkConfig {
     fn generate_config() -> toml::Value {
-        toml::Value::Table(Default::default())
+        toml::Value::try_from(&Self::default()).unwrap()
     }
 }
 
@@ -273,6 +273,11 @@ mod tests {
     use chrono::offset::TimeZone;
     use pretty_assertions::assert_eq;
     use rusoto_cloudwatch::PutMetricDataInput;
+
+    #[test]
+    fn generate_config() {
+        crate::test_util::test_generate_config::<CloudWatchMetricsSinkConfig>();
+    }
 
     fn config() -> CloudWatchMetricsSinkConfig {
         CloudWatchMetricsSinkConfig {
