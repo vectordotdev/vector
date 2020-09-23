8fdfe7fa37c5f7935746ad2ebe065f706497c6f4
Ana Hobden <operator@hoverbear.org>
2020-09-23 10:04:17 -0700

    add some benches
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

---
+++ benches/lookup.rs
@@ -0,0 +1,103 @@
+use criterion::{black_box, criterion_group, criterion_main, Benchmark, BenchmarkId, Criterion};
+use serde_json::Value;
+use std::{collections::HashMap, fs, io::Read, path::Path};
+use vector::{
+    event::Lookup,
+};
+use tracing::trace;
+use std::convert::TryFrom;
+use indexmap::map::IndexMap;
+
+const FIXTURE_ROOT: &str = "tests/data/fixtures/lookup";
+
+fn parse_artifact(path: impl AsRef<Path>) -> std::io::Result<String> {
+    let mut test_file = match fs::File::open(path) {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut buf = Vec::new();
+    test_file.read_to_end(&mut buf)?;
+    let string = String::from_utf8(buf).unwrap();
+    Ok(string)
+}
+
+// This test iterates over the `tests/data/fixtures/lookup` folder and ensures the lookup parsed,
+// then turned into a string again is the same.
+fn lookup_to_string(c: &mut Criterion) {
+    vector::test_util::trace_init();
+    let mut fixtures = IndexMap::new();
+
+    std::fs::read_dir(FIXTURE_ROOT)
+        .unwrap()
+        .for_each(|fixture_file| match fixture_file {
+            Ok(fixture_file) => {
+                let path = fixture_file.path();
+                tracing::trace!(?path, "Opening.");
+                let buf = parse_artifact(&path).unwrap();
+                fixtures.insert(path, buf);
+            }
+            _ => panic!("This test should never read Err'ing test fixtures."),
+        });
+
+    let mut group_from_elem = c.benchmark_group("from_elem");
+    for (_path, fixture) in fixtures.iter() {
+        group_from_elem.bench_with_input(
+            BenchmarkId::from_parameter(&fixture),
+            &fixture.clone(),
+            move |b, ref param| {
+                let input = param.clone();
+                b.iter_with_setup(
+                    || input.clone(),
+                    |input| {
+                        let lookup = Lookup::try_from(input).unwrap();
+                        black_box(lookup)
+                    },
+                )
+            }
+        );
+    }
+    group_from_elem.finish();
+
+    let mut group_to_string = c.benchmark_group("to_string");
+    for (_path, fixture) in fixtures.iter() {
+        group_to_string.bench_with_input(
+            BenchmarkId::from_parameter(&fixture),
+            &fixture.clone(),
+            move |b, ref param| {
+                let input = param.clone();
+                b.iter_with_setup(
+                    || Lookup::try_from(input.clone()).unwrap(),
+                    |input| {
+                        let string = input.to_string();
+                        black_box(string)
+                    },
+                )
+            }
+        );
+    }
+    group_to_string.finish();
+
+    let mut group_serialize = c.benchmark_group("serialize");
+    for (_path, fixture) in fixtures.iter() {
+        group_serialize.bench_with_input(
+            BenchmarkId::from_parameter(&fixture),
+            &fixture.clone(),
+            move |b, ref param| {
+                let input = param.clone();
+                b.iter_with_setup(
+                    || Lookup::try_from(input.clone()).unwrap(),
+                    |input| {
+                        let string = serde_json::to_string(&input);
+                        black_box(string)
+                    },
+                )
+            }
+        );
+    }
+    group_serialize.finish();
+}
+
+
+criterion_group!(lookup, lookup_to_string);
+criterion_main!(lookup);
