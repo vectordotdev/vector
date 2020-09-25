76d51db9c519b71343e6ba2002c6c1a8b796c781
Lee Benson <lee@leebenson.com>
2020-09-25 08:31:03 +0100

    enhancement(observability): GraphQL client (#3778)
    * wip
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * API v2
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * -println
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * happy clippy
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * copyable playground url
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * loop
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * ohNo
    
    * fix conflicts
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * fix config tests
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * stop breaking things
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * separate metrics
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * 3x metrics
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * health and playground tests
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * graphql client; health test
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * start server
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * required-features
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * Update Cargo.lock
    
    * server shutdown
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * fix API server scope
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * drop(server)
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * fix events/mod
    
    * minor cleanup
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * lint
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * wip
    
    * heartbeat
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * retry_until
    
    * check-internal-events
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * working client
    
    * check-fmt
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * heartbeat assertions
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * gen newline
    
    * drop moot helper
    
    * close -> stop
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * uptime metrics
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * events processed test
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * parallel subscriptions
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * minor use
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * extra commentary
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * fix tests
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,7 @@
+pub mod client;
 mod handler;
-mod schema;
+pub mod schema;
 mod server;
 
+pub use client::subscription::make_subscription_client;
 pub use server::Server;
---
+++ src/api/schema/gen.rs
@@ -0,0 +1,110 @@
+use std::fs;
+use vector::api::schema::build_schema;
+
+static INTROSPECTION_QUERY: &str = r#"
+query IntrospectionQuery {
+  __schema {
+    queryType {
+      name
+    }
+    mutationType {
+      name
+    }
+    subscriptionType {
+      name
+    }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+fragment InputValue on __InputValue {
+  name
+  description
+  type {
+    ...TypeRef
+  }
+  defaultValue
+}
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"#;
+
+#[tokio::main]
+async fn main() {
+    let schema = build_schema().finish();
+    let res = schema.execute(INTROSPECTION_QUERY).await;
+    let json = serde_json::to_string_pretty(&async_graphql::http::GQLResponse(res)).unwrap();
+
+    fs::write("graphql/schema.json", format!("{}\n", json)).expect("Couldn't save schema file");
+}
