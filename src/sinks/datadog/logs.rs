b5cac1a321c7ccfbfbf054dc38c4ae7fcba25802
Stephen Wakely <fungus.humungus@gmail.com>
2020-09-27 14:35:46 +0200

    Datadog logs to use http
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/datadog/logs.rs
+++ src/sinks/datadog/logs.rs
@@ -3,20 +3,20 @@ use crate::{
     event::Event,
     sinks::{
         util::{
-            self,
+            encode_event,
             encoding::{EncodingConfig, EncodingConfiguration},
             http::{BatchedHttpSink, HttpClient, HttpSink},
             BatchConfig, BatchSettings, BoxedRawValue, Encoding, JsonArrayBuffer,
-            TowerRequestConfig,
+            TowerRequestConfig, VecBuffer,
         },
         Healthcheck, VectorSink,
     },
-    tls::TlsConfig,
 };
 use bytes::Bytes;
 use futures::FutureExt;
 use futures01::Sink;
 use http::{Request, StatusCode};
+use hyper::body::Body;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use string_cache::DefaultAtom as Atom;
@@ -27,13 +27,22 @@ pub struct DatadogLogsConfig {
     endpoint: Option<String>,
     api_key: String,
     encoding: EncodingConfig<Encoding>,
-    tls: Option<TlsConfig>,
 
     #[serde(default)]
     batch: BatchConfig,
 
     #[serde(default)]
     request: TowerRequestConfig,
+}
+
+#[derive(Clone)]
+pub struct DatadogLogsJsonService {
+    config: DatadogLogsConfig,
+}
+
+#[derive(Clone)]
+pub struct DatadogLogsTextService {
+    config: DatadogLogsConfig,
 }
 
 inventory::submit! {
@@ -43,7 +52,7 @@ inventory::submit! {
 impl GenerateConfig for DatadogLogsConfig {}
 
 impl DatadogLogsConfig {
-    fn get_endpoint<'a>(&'a self) -> &'a str {
+    fn get_endpoint(&self) -> &str {
         match &self.endpoint {
             Some(ref endpoint) => &endpoint,
             None => "https://http-intake.logs.datadoghq.eu/v1/input",
@@ -56,26 +65,58 @@ impl DatadogLogsConfig {
 impl SinkConfig for DatadogLogsConfig {
     async fn build(&self, cx: SinkContext) -> crate::Result<(VectorSink, Healthcheck)> {
         let request_settings = self.request.unwrap_with(&TowerRequestConfig::default());
-        let batch_settings = BatchSettings::default()
-            .bytes(bytesize::kib(100u64))
-            .timeout(1)
-            .parse_config(self.batch)?;
 
         let client = HttpClient::new(cx.resolver(), None)?;
 
-        let sink = BatchedHttpSink::new(
-            self.clone(),
-            JsonArrayBuffer::new(batch_settings.size),
-            request_settings,
-            batch_settings.timeout,
-            client.clone(),
-            cx.acker(),
-        )
-        .sink_map_err(|e| error!("Fatal datadog_logs sink error: {}", e));
+        // Create a different sink depending on which encoding we have chosen.
+        // Json and text have different batching strategies and so each needs to be
+        // handled differently.
+        match self.encoding.codec {
+            Encoding::Json => {
+                let batch_settings = BatchSettings::default()
+                    .bytes(bytesize::kib(100u64))
+                    .timeout(1)
+                    .parse_config(self.batch)?;
 
-        let healthcheck = healthcheck(self.clone(), client).boxed();
+                let service = DatadogLogsJsonService {
+                    config: self.clone(),
+                };
+                let healthcheck = healthcheck(service.clone(), client.clone()).boxed();
+                let sink = BatchedHttpSink::new(
+                    service,
+                    JsonArrayBuffer::new(batch_settings.size),
+                    request_settings,
+                    batch_settings.timeout,
+                    client,
+                    cx.acker(),
+                )
+                .sink_map_err(|e| error!("Fatal datadog_logs json sink error: {}", e));
 
-        Ok((VectorSink::Futures01Sink(Box::new(sink)), healthcheck))
+                Ok((VectorSink::Futures01Sink(Box::new(sink)), healthcheck))
+            }
+            Encoding::Text => {
+                let batch_settings = BatchSettings::default()
+                    .bytes(bytesize::kib(100u64))
+                    .timeout(1)
+                    .parse_config(self.batch)?;
+
+                let service = DatadogLogsTextService {
+                    config: self.clone(),
+                };
+                let healthcheck = healthcheck(service.clone(), client.clone()).boxed();
+                let sink = BatchedHttpSink::new(
+                    service,
+                    VecBuffer::new(batch_settings.size),
+                    request_settings,
+                    batch_settings.timeout,
+                    client,
+                    cx.acker(),
+                )
+                .sink_map_err(|e| error!("Fatal datadog_logs text sink error: {}", e));
+
+                Ok((VectorSink::Futures01Sink(Box::new(sink)), healthcheck))
+            }
+        }
     }
 
     fn input_type(&self) -> DataType {
@@ -88,7 +129,7 @@ impl SinkConfig for DatadogLogsConfig {
 }
 
 #[async_trait::async_trait]
-impl HttpSink for DatadogLogsConfig {
+impl HttpSink for DatadogLogsJsonService {
     type Input = serde_json::Value;
     type Output = Vec<BoxedRawValue>;
 
@@ -107,28 +148,52 @@ impl HttpSink for DatadogLogsConfig {
             log.insert("host", host);
         }
 
-        self.encoding.apply_rules(&mut event);
+        self.config.encoding.apply_rules(&mut event);
 
         Some(json!(event.into_log()))
     }
 
     async fn build_request(&self, events: Self::Output) -> crate::Result<http::Request<Vec<u8>>> {
-        let uri = self.get_endpoint();
+        let uri = self.config.get_endpoint();
 
         let request = Request::post(uri)
             .header("Content-Type", "application/json")
-            .header("DD-API-KEY", self.api_key.clone());
+            .header("DD-API-KEY", self.config.api_key.clone());
 
         let buf = serde_json::to_vec(&events)?;
         request.body(buf).map_err(Into::into)
     }
 }
 
-async fn healthcheck(config: DatadogLogsConfig, mut client: HttpClient) -> crate::Result<()> {
-    let req = config
-        .build_request(Vec::new())
-        .await?
-        .map(hyper::Body::from);
+#[async_trait::async_trait]
+impl HttpSink for DatadogLogsTextService {
+    type Input = Bytes;
+    type Output = Vec<Bytes>;
+
+    fn encode_event(&self, event: Event) -> Option<Self::Input> {
+        encode_event(event, &self.config.encoding)
+    }
+
+    async fn build_request(&self, events: Self::Output) -> crate::Result<http::Request<Vec<u8>>> {
+        let uri = self.config.get_endpoint();
+        let body: Vec<u8> = events.iter().flat_map(|b| b.into_iter()).cloned().collect();
+
+        let request = Request::post(uri)
+            .header("Content-Type", "text/plain")
+            .header("Content-Length", body.len())
+            .header("DD-API-KEY", self.config.api_key.clone());
+
+        request.body(body).map_err(Into::into)
+    }
+}
+
+/// The healthcheck is performed by sending an empty request to Datadog and checking
+/// the return.
+async fn healthcheck<T, O>(config: T, mut client: HttpClient) -> crate::Result<()>
+where
+    T: HttpSink<Output = Vec<O>>,
+{
+    let req = config.build_request(Vec::new()).await?.map(Body::from);
 
     let res = client.send(req).await?;
 
@@ -159,5 +224,102 @@ async fn healthcheck(config: DatadogLogsConfig, mut client: HttpClient) -> crate
             status, body
         )
         .into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        config::SinkConfig,
+        sinks::util::test::{build_test_server, load_sink},
+        test_util::{next_addr, random_lines_with_stream},
+    };
+    use futures::StreamExt;
+
+    #[tokio::test]
+    async fn smoke_text() {
+        let (mut config, cx) = load_sink::<DatadogLogsConfig>(
+            r#"
+            api_key = "atoken"
+            encoding = "text"
+            batch.max_events = 1
+            "#,
+        )
+        .unwrap();
+
+        let _ = config.build(cx.clone()).unwrap();
+
+        let addr = next_addr();
+        // Swap out the endpoint so we can force send it
+        // to our local server
+        let endpoint = format!("http://{}", addr);
+        config.endpoint = Some(endpoint.clone());
+
+        let (sink, _) = config.build(cx).unwrap();
+
+        let (rx, _trigger, server) = build_test_server(addr);
+        tokio::spawn(server);
+
+        let (expected, events) = random_lines_with_stream(100, 10);
+
+        let _ = sink.run(events).await.unwrap();
+
+        let output = rx.take(expected.len()).collect::<Vec<_>>().await;
+
+        for (i, val) in output.iter().enumerate() {
+            assert_eq!(val.1, format!("{}\n", expected[i]));
+        }
+    }
+
+    #[tokio::test]
+    async fn smoke_json() {
+        let (mut config, cx) = load_sink::<DatadogLogsConfig>(
+            r#"
+            api_key = "atoken"
+            encoding = "json"
+            batch.max_events = 1
+            "#,
+        )
+        .unwrap();
+
+        let _ = config.build(cx.clone()).unwrap();
+
+        let addr = next_addr();
+        // Swap out the endpoint so we can force send it
+        // to our local server
+        let endpoint = format!("http://{}", addr);
+        config.endpoint = Some(endpoint.clone());
+
+        let (sink, _) = config.build(cx).unwrap();
+
+        let (rx, _trigger, server) = build_test_server(addr);
+        tokio::spawn(server);
+
+        let (expected, events) = random_lines_with_stream(100, 10);
+
+        let _ = sink.run(events).await.unwrap();
+
+        let output = rx.take(expected.len()).collect::<Vec<_>>().await;
+
+        for (i, val) in output.iter().enumerate() {
+            let mut json = serde_json::Deserializer::from_slice(&val.1[..])
+                .into_iter::<serde_json::Value>()
+                .map(|v| v.expect("decoding json"));
+
+            let json = json.next().unwrap();
+
+            // The json we send to Datadog is an array of events.
+            // As we have set batch.max_events to 1, each entry will be
+            // an array containing a single record.
+            let message = json
+                .get(0)
+                .unwrap()
+                .get("message")
+                .unwrap()
+                .as_str()
+                .unwrap();
+            assert_eq!(message, expected[i]);
+        }
     }
 }
