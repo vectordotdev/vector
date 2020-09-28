3e4953ff5ca04d6abed87e7fb4109610a3528e41
James Turnbull <james@lovedthanlost.net>
2020-09-28 19:18:03 -0400

    Fixed markdown scripts
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -12,7 +12,13 @@ bundle update --bundler
 bundle install
 cd ..
 
-npm -g install markdownlint-cli
+
+if ! [ -x "$(command -v sudo)" ]; then
+  npm -g install markdownlint-cli
+else
+  sudo npm -g install markdownlint-cli
+fi
+
 
 pip3 install jsonschema==3.2.0
 pip3 install remarshal==0.11.2
