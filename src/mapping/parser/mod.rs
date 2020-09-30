d853839871caf77f4c7aba08fb45ddda4e71f7eb
Jean Mertz <git@jeanmertz.com>
2020-09-30 12:25:32 +0200

    feat(remap transform): add `sha2` function (#4188)
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/mapping/parser/mod.rs
+++ src/mapping/parser/mod.rs
@@ -507,7 +507,7 @@ mod tests {
     use super::*;
     use crate::mapping::query::function::{
         ContainsFn, DowncaseFn, FormatTimestampFn, Md5Fn, NowFn, ParseJsonFn, ParseTimestampFn,
-        Sha1Fn, SliceFn, StripAnsiEscapeCodesFn, StripWhitespaceFn, ToBooleanFn, ToFloatFn,
+        Sha1Fn, Sha2Fn, SliceFn, StripAnsiEscapeCodesFn, StripWhitespaceFn, ToBooleanFn, ToFloatFn,
         ToIntegerFn, ToStringFn, ToTimestampFn, TokenizeFn, TruncateFn, UpcaseFn, UuidV4Fn,
     };
 
@@ -1098,6 +1098,16 @@ mod tests {
                 Mapping::new(vec![Box::new(Assignment::new(
                     "foo".to_string(),
                     Box::new(Sha1Fn::new(Box::new(QueryPath::from("foo")))),
+                ))]),
+            ),
+            (
+                r#".foo = sha2(.foo, variant = "SHA-224")"#,
+                Mapping::new(vec![Box::new(Assignment::new(
+                    "foo".to_string(),
+                    Box::new(Sha2Fn::new(
+                        Box::new(QueryPath::from("foo")),
+                        Some("SHA-224"),
+                    )),
                 ))]),
             ),
             (
