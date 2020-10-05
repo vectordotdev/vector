cfc13278dcf280852aa99109d4c0f9059b4388b2
MOZGIII <mike-n@narod.ru>
2020-10-05 17:54:56 +0300

    Annotate configs for shellcheck exclusion at distribution/helm/scripting-config.sh
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- distribution/helm/scripting-config.sh
+++ distribution/helm/scripting-config.sh
@@ -3,6 +3,7 @@
 # This file holds the shared configuration for various helm automation scripts.
 
 # The order in which the dependency updates are issued.
+# shellcheck disable=SC2034
 DEPENDENCY_UPDATE_ORDER=(
   # Lowest level.
   vector-shared
@@ -17,6 +18,7 @@ DEPENDENCY_UPDATE_ORDER=(
 # The list of charts to release to our chart repo.
 # We only need to release the application charts, library charts don't need to
 # be released.
+# shellcheck disable=SC2034
 CHARTS_TO_PUBLISH=(
   vector-agent
   vector
