be46322220439076f5898cee0e6ae0a94376e5a1
MOZGIII <mike-n@narod.ru>
2020-10-06 01:30:50 +0300

    Manually copy over the chart contents instead of archiving it
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- scripts/helm-dependencies.sh
+++ scripts/helm-dependencies.sh
@@ -7,17 +7,58 @@ set -euo pipefail
 #
 #   Update Helm chart dependencies in the proper order to propagate
 #   the changes.
+#
+#   This script implements our custom compatible dependency update mechanism,
+#   rather than relying on the official `helm dependency update`.
+#   We'd be happy to, however, the official mechanism doesn't produce
+#   reproducible results.
+#   See https://github.com/helm/helm/issues/8850
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 # Read the shared scripting config.
 source "distribution/helm/scripting-config.sh"
 
+list-chart-dependencies() {
+  local CHART="$1"
+  helm dependency list "$CHART" | tail -n +2 | sed '/^$/d' | awk '{ gsub("file://", "", $3); print $1, $3 }'
+}
+
 update() {
   for CHART in "${DEPENDENCY_UPDATE_ORDER[@]}"; do
-    set -x
-    helm dependency update --skip-refresh "distribution/helm/$CHART" "$@"
-    { set +x; } &> /dev/null
+    echo "=> $CHART"
+
+    CHART_PATH="distribution/helm/$CHART"
+
+    DEPENDENCIES_STRING="$(list-chart-dependencies "$CHART_PATH")"
+    if [[ -z "$DEPENDENCIES_STRING" ]]; then
+      echo "No dependencies"
+      continue;
+    fi
+
+    CHART_VENDORED_DEPENDENCIES_PATH="$CHART_PATH/charts"
+    rm -rf "$CHART_VENDORED_DEPENDENCIES_PATH"
+    mkdir -p "$CHART_VENDORED_DEPENDENCIES_PATH"
+
+    DEPENDENCIES=( "$DEPENDENCIES_STRING" )
+    for DEPENDENCY_PAIR in "${DEPENDENCIES[@]}"; do
+      read -ra KV <<<"$DEPENDENCY_PAIR"
+      DEPENDENCY_NAME="${KV[0]}"
+      DEPENDENCY_PATH="${KV[1]}"
+
+      COPY_FROM="$CHART_PATH/$DEPENDENCY_PATH"
+      COPY_TO="$CHART_VENDORED_DEPENDENCIES_PATH/$DEPENDENCY_NAME"
+
+      echo "Copying \"$DEPENDENCY_NAME\" from \"$COPY_FROM\" to \"$COPY_TO\"..."
+      cp -r -T "$COPY_FROM" "$COPY_TO"
+    done
+  done
+}
+
+list() {
+  for CHART in "${DEPENDENCY_UPDATE_ORDER[@]}"; do
+    echo "=> $CHART"
+    list-chart-dependencies "distribution/helm/$CHART"
   done
 }
 
@@ -28,13 +69,14 @@ Usage: $0 MODE
 Modes:
   update - update Helm chart dependencies and vendor them to the respective
            charts/ dir of each chart.
+  list   - list the dependencies for each Helm chart.
 EOF
   exit 1
 }
 
 MODE="${1:-}"
 case "$MODE" in
-  update)
+  update|list)
     "$MODE"
     ;;
   *)
