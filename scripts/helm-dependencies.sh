3681026975ba15ffc8548dbe6eab023798c82e5f
MOZGIII <mike-n@narod.ru>
2020-10-06 01:54:57 +0300

    Add validate command
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- scripts/helm-dependencies.sh
+++ scripts/helm-dependencies.sh
@@ -62,21 +62,51 @@ list() {
   done
 }
 
+validate() {
+  for CHART in "${DEPENDENCY_UPDATE_ORDER[@]}"; do
+    echo "=> $CHART"
+
+    CHART_PATH="distribution/helm/$CHART"
+
+    DEPENDENCIES_STRING="$(list-chart-dependencies "$CHART_PATH")"
+    if [[ -z "$DEPENDENCIES_STRING" ]]; then
+      echo "No dependencies"
+      continue;
+    fi
+
+    CHART_VENDORED_DEPENDENCIES_PATH="$CHART_PATH/charts"
+    DEPENDENCIES=( "$DEPENDENCIES_STRING" )
+    for DEPENDENCY_PAIR in "${DEPENDENCIES[@]}"; do
+      read -ra KV <<<"$DEPENDENCY_PAIR"
+      DEPENDENCY_NAME="${KV[0]}"
+      DEPENDENCY_PATH="${KV[1]}"
+
+      VENDORED_PATH="$CHART_PATH/$DEPENDENCY_PATH"
+      UPSTREAM_PATH="$CHART_VENDORED_DEPENDENCIES_PATH/$DEPENDENCY_NAME"
+
+      echo "Validating \"$DEPENDENCY_NAME\" at \"$VENDORED_PATH\" against \"$UPSTREAM_PATH\"..."
+      diff -qr "$VENDORED_PATH" "$UPSTREAM_PATH"
+    done
+  done
+}
+
 usage() {
   cat >&2 <<-EOF
 Usage: $0 MODE
 
 Modes:
-  update - update Helm chart dependencies and vendor them to the respective
-           charts/ dir of each chart.
-  list   - list the dependencies for each Helm chart.
+  update   - update Helm chart dependencies and vendor them to the respective
+             charts/ dir of each chart.
+  list     - list the dependencies for each Helm chart.
+  validate - check that vendored Helm chart dependencies are up-to-date with
+             with their upstream counterparts.
 EOF
   exit 1
 }
 
 MODE="${1:-}"
 case "$MODE" in
-  update|list)
+  update|list|validate)
     "$MODE"
     ;;
   *)
