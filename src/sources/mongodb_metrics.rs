23ab072a8677f9d7bf6f3ded38e372149fe37dd7
Kirill Fomichev <fanatid@ya.ru>
2020-10-07 21:07:21 +0300

    add interval with stub metric
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- src/sources/mongodb_metrics.rs
+++ src/sources/mongodb_metrics.rs
@@ -1,12 +1,18 @@
 use crate::{
     config::{self, GlobalOptions, SourceConfig, SourceDescription},
+    event::metric::{Metric, MetricKind, MetricValue},
     shutdown::ShutdownSignal,
-    Pipeline,
+    Event, Pipeline,
 };
 use futures::{
-    FutureExt, TryFutureExt,
+    compat::{Future01CompatExt, Sink01CompatExt},
+    FutureExt, Stream, StreamExt, TryFutureExt,
 };
+use futures01::Sink;
+use mongodb::Client;
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::time::{interval, Duration};
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 struct MongoDBMetricsConfig {
@@ -15,6 +21,12 @@ struct MongoDBMetricsConfig {
     scrape_interval_secs: u64,
     #[serde(default = "default_namespace")]
     namespace: String,
+}
+
+#[derive(Debug)]
+struct MongoDBMetrics {
+    pub client: Client,
+    pub namespace: String,
 }
 
 pub fn default_scrape_interval_secs() -> u64 {
@@ -29,7 +41,6 @@ inventory::submit! {
     SourceDescription::new_without_default::<MongoDBMetricsConfig>("mongodb_metrics")
 }
 
-
 #[async_trait::async_trait]
 #[typetag::serde(name = "mongodb_metrics")]
 impl SourceConfig for MongoDBMetricsConfig {
@@ -40,7 +51,25 @@ impl SourceConfig for MongoDBMetricsConfig {
         shutdown: ShutdownSignal,
         out: Pipeline,
     ) -> crate::Result<super::Source> {
-        Ok(Box::new(async move { Ok(()) }.boxed().compat()))
+        let mongodb = MongoDBMetrics::new(&self.endpoint, &self.namespace).await?;
+
+        let out = out
+            .sink_map_err(|e| error!("error sending metric: {:?}", e))
+            .sink_compat();
+
+        let task = interval(Duration::from_secs(self.scrape_interval_secs))
+            .take_until(shutdown.compat())
+            .filter_map(move |_| {
+                let mongodb = Arc::clone(&mongodb);
+                async { Some(mongodb.collect().await) }
+            })
+            .flatten()
+            .map(Event::Metric)
+            .map(Ok)
+            .forward(out)
+            .inspect(|_| info!("finished sending"));
+
+        Ok(Box::new(task.boxed().compat()))
     }
 
     fn output_type(&self) -> crate::config::DataType {
@@ -49,5 +78,31 @@ impl SourceConfig for MongoDBMetricsConfig {
 
     fn source_type(&self) -> &'static str {
         "mongodb_metrics"
+    }
+}
+
+impl MongoDBMetrics {
+    async fn new(endpoint: &str, namespace: &str) -> crate::Result<Arc<MongoDBMetrics>> {
+        Ok(Arc::new(Self {
+            client: Client::with_uri_str(endpoint).await?,
+            namespace: namespace.to_owned(),
+        }))
+    }
+
+    fn encode_namespace(&self, name: &str) -> String {
+        match self.namespace.as_str() {
+            "" => name.to_string(),
+            _ => format!("{}_{}", self.namespace, name),
+        }
+    }
+
+    async fn collect(self: Arc<Self>) -> impl Stream<Item = Metric> {
+        futures::stream::once(futures::future::ready(Metric {
+            name: self.encode_namespace("up"),
+            timestamp: None,
+            tags: None,
+            kind: MetricKind::Absolute,
+            value: MetricValue::Gauge { value: 1.0 },
+        }))
     }
 }
