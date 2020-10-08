30ec3999be2b94288544cc69be81014e98f7eed3
drunkirishcoder <daniel.jin@gmail.com>
2020-10-08 12:17:32 -0400

    fix tests
    Signed-off-by: drunkirishcoder <daniel.jin@gmail.com>

--- src/sinks/console.rs
+++ src/sinks/console.rs
@@ -202,7 +202,7 @@ mod test {
             },
         });
         assert_eq!(
-            r#"{"name":"users","timestamp":null,"tags":null,"kind":"incremental","set":{"values":["bob"]}}"#,
+            r#"{"name":"users","kind":"incremental","set":{"values":["bob"]}}"#,
             encode_event(event, &EncodingConfig::from(Encoding::Json)).unwrap()
         );
     }
@@ -221,7 +221,7 @@ mod test {
             },
         });
         assert_eq!(
-            r#"{"name":"glork","timestamp":null,"tags":null,"kind":"incremental","distribution":{"values":[10.0],"sample_rates":[1],"statistic":"histogram"}}"#,
+            r#"{"name":"glork","kind":"incremental","distribution":{"values":[10.0],"sample_rates":[1],"statistic":"histogram"}}"#,
             encode_event(event, &EncodingConfig::from(Encoding::Json)).unwrap()
         );
     }
