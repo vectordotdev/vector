3833aca800b8c71115e7639d2ffa48a8c48cc00b
Kruno Tomola Fabro <krunotf@gmail.com>
2020-10-09 00:39:45 +0200

    fix(config): Merge `api::Options` (#4342)
    * Merge api::Options
    
    Signed-off-by: ktf <krunotf@gmail.com>
    
    * & playground
    
    Signed-off-by: ktf <krunotf@gmail.com>
    
    * Add test
    
    Signed-off-by: ktf <krunotf@gmail.com>
    
    * Remove clone
    
    Signed-off-by: ktf <krunotf@gmail.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/config/api.rs
+++ src/config/api.rs
@@ -1,8 +1,7 @@
-use super::ConfigBuilder;
 use serde::{Deserialize, Serialize};
 use std::net::{Ipv4Addr, SocketAddr};
 
-#[derive(Default, Debug, Deserialize, Serialize, PartialEq, Copy, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Copy, Clone)]
 #[serde(default)]
 pub struct Options {
     #[serde(default = "default_enabled")]
@@ -13,6 +12,16 @@ pub struct Options {
 
     #[serde(default = "default_playground")]
     pub playground: bool,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            enabled: default_enabled(),
+            playground: default_playground(),
+            bind: default_bind(),
+        }
+    }
 }
 
 fn default_enabled() -> bool {
@@ -27,7 +36,88 @@ fn default_playground() -> bool {
     true
 }
 
-/// Updates the configuration to take into account API changes
-pub fn update_config(old_config: &mut ConfigBuilder, new_config: &ConfigBuilder) {
-    old_config.api = new_config.api; // copy
+impl Options {
+    pub fn merge(&mut self, other: Self) -> Result<(), String> {
+        // Merge options
+
+        // Try to merge bind
+        let bind = match (self.bind, other.bind) {
+            (None, b) => b,
+            (Some(a), None) => Some(a),
+            (Some(a), Some(b)) if a == b => Some(a),
+            // Prefer non default bind
+            (Some(a), Some(b)) => match (Some(a) == default_bind(), Some(b) == default_bind()) {
+                (false, false) => {
+                    return Err(format!("Conflicting `api` bindings: {}, {} .", a, b))
+                }
+                (false, true) => Some(a),
+                (true, _) => Some(b),
+            },
+        };
+
+        let options = Options {
+            bind,
+            enabled: self.enabled | other.enabled,
+            playground: self.playground & other.playground,
+        };
+
+        *self = options;
+        Ok(())
+    }
+}
+
+#[test]
+fn bool_merge() {
+    let mut a = Options {
+        enabled: true,
+        bind: None,
+        playground: false,
+    };
+
+    a.merge(Options::default()).unwrap();
+
+    assert_eq!(
+        a,
+        Options {
+            enabled: true,
+            bind: default_bind(),
+            playground: false,
+        }
+    );
+}
+
+#[test]
+fn bind_merge() {
+    let address = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 9000);
+    let mut a = Options {
+        enabled: true,
+        bind: Some(address),
+        playground: true,
+    };
+
+    a.merge(Options::default()).unwrap();
+
+    assert_eq!(
+        a,
+        Options {
+            enabled: true,
+            bind: Some(address),
+            playground: true,
+        }
+    );
+}
+
+#[test]
+fn bind_conflict() {
+    let mut a = Options {
+        bind: Some(SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 9000)),
+        ..Options::default()
+    };
+
+    let b = Options {
+        bind: Some(SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 9001)),
+        ..Options::default()
+    };
+
+    assert!(a.merge(b).is_err());
 }
