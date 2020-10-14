fdbdacacbff994b4e6aa7051dcc9979503d0e493
Jesse Szwedko <jesse@szwedko.me>
2020-10-14 09:39:32 -0400

    fix!(prometheus source): Do not automatically append /metrics (#4276)
    This is a breaking change to just the configured endpoints as-is for the
    `prometheus` source. Previously, `/metrics` was appended, but this
    prohibited its use with prometheus exporters that do not use this path
    (for example, a prometheus `federate` endpoint).
    
    I added a couple of warning messages to help guide people that run into
    issues when upgrading `vector` and depending on the current automatic
    suffixing behavior of the `prometheus` source. If they fail to update,
    I think they will commonly hit two types of errors:
    
    * They will get a 404
    * They will get a 200, but not prometheus output. For example, the
      `node_exporter` serves an HTML page on `/`.
    
    Reference: #3801 discussion
    Fixes #3800
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>
    
    Co-authored-by: binarylogic <bjohnson@binarylogic.com>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

--- src/sources/prometheus/mod.rs
+++ src/sources/prometheus/mod.rs
@@ -49,11 +49,11 @@ impl SourceConfig for PrometheusConfig {
         shutdown: ShutdownSignal,
         out: Pipeline,
     ) -> crate::Result<super::Source> {
-        let mut urls = Vec::new();
-        for host in self.endpoints.iter() {
-            let base_uri = host.parse::<http::Uri>().context(super::UriParseError)?;
-            urls.push(format!("{}metrics", base_uri));
-        }
+        let urls = self
+            .endpoints
+            .iter()
+            .map(|s| s.parse::<http::Uri>().context(super::UriParseError))
+            .collect::<Result<Vec<http::Uri>, super::BuildError>>()?;
         Ok(prometheus(urls, self.scrape_interval_secs, shutdown, out))
     }
 
@@ -67,7 +67,7 @@ impl SourceConfig for PrometheusConfig {
 }
 
 fn prometheus(
-    urls: Vec<String>,
+    urls: Vec<http::Uri>,
     interval: u64,
     shutdown: ShutdownSignal,
     out: Pipeline,
@@ -116,6 +116,13 @@ fn prometheus(
                                     Some(stream::iter(metrics).map(Event::Metric).map(Ok))
                                 }
                                 Err(error) => {
+                                    if url.path() == "/" {
+                                        // https://github.com/timberio/vector/pull/3801#issuecomment-700723178
+                                        warn!(
+                                            message = "No path is set on the endpoint and we got a parse error, did you mean to use /metrics? This behavior changed in version 0.11.",
+                                            endpoint = %url
+                                        );
+                            }
                                     emit!(PrometheusParseError {
                                         error,
                                         url: url.clone(),
@@ -126,6 +133,13 @@ fn prometheus(
                             }
                         }
                         Ok((header, _)) => {
+                            if header.status == hyper::StatusCode::NOT_FOUND && url.path() == "/" {
+                                // https://github.com/timberio/vector/pull/3801#issuecomment-700723178
+                                warn!(
+                                    message = "No path is set on the endpoint and we got a 404, did you mean to use /metrics? This behavior changed in version 0.11.",
+                                    endpoint = %url
+                                );
+                            }
                             emit!(PrometheusErrorResponse {
                                 code: header.status,
                                 url: url.clone(),
