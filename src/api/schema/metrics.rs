d62a1f52bf7fbe30316cbb140c6361290cada549
Lee Benson <lee@leebenson.com>
2020-10-14 09:53:14 +0100

    -mutex
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/api/schema/metrics.rs
+++ src/api/schema/metrics.rs
@@ -4,14 +4,13 @@ use async_graphql::{validators::IntRange, Interface, Object, Subscription};
 use async_stream::stream;
 use chrono::{DateTime, Utc};
 use lazy_static::lazy_static;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use tokio::stream::{Stream, StreamExt};
 use tokio::time::Duration;
 
 lazy_static! {
-    static ref GLOBAL_CONTROLLER: Arc<Mutex<&'static Controller>> = Arc::new(Mutex::new(
-        get_controller().expect("Metrics system not initialized. Please report.")
-    ));
+    static ref GLOBAL_CONTROLLER: Arc<&'static Controller> =
+        Arc::new(get_controller().expect("Metrics system not initialized. Please report."));
 }
 
 pub struct Uptime(Metric);
@@ -167,18 +166,15 @@ fn get_metrics(interval: i32) -> impl Stream<Item = Metric> {
 pub fn topology_events_processed(topology_name: String) -> Option<EventsProcessed> {
     let key = String::from("component_name");
 
-    capture_metrics(
-        *GLOBAL_CONTROLLER
-            .lock()
-            .expect("Couldn't acquire metrics lock. Please report."),
-    )
-    .find(|ev| match ev {
-        Event::Metric(m)
-            if m.name.as_str().eq("events_processed") && m.tag_matches(&key, &topology_name) =>
-        {
-            true
-        }
-        _ => false,
-    })
-    .map(|ev| EventsProcessed(ev.into_metric()))
+    capture_metrics(&GLOBAL_CONTROLLER)
+        .find(|ev| match ev {
+            Event::Metric(m)
+                if m.name.as_str().eq("events_processed")
+                    && m.tag_matches(&key, &topology_name) =>
+            {
+                true
+            }
+            _ => false,
+        })
+        .map(|ev| EventsProcessed(ev.into_metric()))
 }
