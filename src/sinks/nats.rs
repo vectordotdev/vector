4a2f81374952387b13e9940b4f64e966f0c016b6
Eric Lee <erhlee.bird@gmail.com>
2020-10-14 17:11:54 -0400

    nats fix to latest conventions; add integration test
    Signed-off-by: Eric Lee <erhlee.bird@gmail.com>

--- src/sinks/nats.rs
+++ src/sinks/nats.rs
@@ -1,18 +1,14 @@
 use crate::{
-    config::{DataType, SinkConfig, SinkContext, SinkDescription},
+    config::{DataType, GenerateConfig, SinkConfig, SinkContext, SinkDescription},
     emit,
-    event::{self, Event},
+    event::Event,
     internal_events::NatsEventSent,
     sinks::util::StreamSink,
 };
 use async_trait::async_trait;
-use futures::pin_mut;
-use futures::stream::{Stream, StreamExt};
-use futures01::future;
+use futures::{future, stream::BoxStream, FutureExt, StreamExt};
 use nats;
 use serde::{Deserialize, Serialize};
-
-use super::streaming_sink::{self, StreamingSink};
 
 /**
  * Code dealing with the SinkConfig struct.
@@ -22,25 +18,27 @@ use super::streaming_sink::{self, StreamingSink};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct NatsSinkConfig {
-    server: String,
+    url: String,
     subject: String,
 }
 
 inventory::submit! {
-    SinkDescription::new_without_default::<NatsSinkConfig>("nats")
+    SinkDescription::new::<NatsSinkConfig>("nats")
 }
 
+impl GenerateConfig for NatsSinkConfig {}
+
+#[async_trait::async_trait]
 #[typetag::serde(name = "nats")]
 impl SinkConfig for NatsSinkConfig {
-    fn build(&self, cx: SinkContext) -> crate::Result<(super::RouterSink, super::Healthcheck)> {
+    async fn build(
+        &self,
+        _cx: SinkContext,
+    ) -> crate::Result<(super::VectorSink, super::Healthcheck)> {
         let sink = NatsSink::new(self.clone());
-        let sink = streaming_sink::compat::adapt_to_topology(sink);
-        let sink = StreamSink::new(sink, cx.acker());
-        let sink = Box::new(sink);
+        let healthcheck = future::ok(()).boxed();
 
-        let healthcheck = Box::new(future::ok(()));
-
-        Ok((sink, healthcheck))
+        Ok((super::VectorSink::Stream(Box::new(sink)), healthcheck))
     }
 
     fn input_type(&self) -> DataType {
@@ -65,7 +63,7 @@ pub struct NatsSink {
 impl NatsSink {
     fn new(config: NatsSinkConfig) -> Self {
         let options = nats::Options::new();
-        let nc = options.connect(&config.server).unwrap();
+        let nc = options.connect(&config.url).unwrap();
 
         Self {
             nc: nc,
@@ -75,34 +73,85 @@ impl NatsSink {
 }
 
 #[async_trait]
-impl StreamingSink for NatsSink {
-    async fn run(
-        &mut self,
-        input: impl Stream<Item = Event> + Send + Sync + 'static,
-    ) -> crate::Result<()> {
-        pin_mut!(input);
+impl StreamSink for NatsSink {
+    async fn run(&mut self, mut input: BoxStream<'_, Event>) -> Result<(), ()> {
         while let Some(event) = input.next().await {
             match event {
                 Event::Log(log) => {
                     let body = log
-                        .get(&event::log_schema().message_key())
+                        .get(crate::config::log_schema().message_key())
                         .map(|v| v.to_string_lossy())
                         .unwrap_or_else(|| "".into());
 
                     let message_len = body.len();
 
-                    self.nc.publish(&self.subject, body)?;
-
-                    emit!(NatsEventSent {
-                        byte_size: message_len,
-                    });
-
-                },
-                Event::Metric(_metric) => {
+                    match self.nc.publish(&self.subject, body) {
+                        Ok(_) => {
+                            emit!(NatsEventSent {
+                                byte_size: message_len,
+                            });
+                        }
+                        _ => {
+                            // DEV: Code path for handling unsuccessful
+                            //      publications. Future work to buffer unsent
+                            //      messages might go here.
+                        }
+                    }
                 }
+                Event::Metric(_metric) => {}
             }
         }
 
         Ok(())
+    }
+}
+
+#[cfg(feature = "nats-integration-tests")]
+#[cfg(test)]
+mod integration_tests {
+    use super::*;
+    use crate::test_util::{random_lines_with_stream, random_string, trace_init};
+    use std::{sync::{Arc, Mutex}, time::Duration};
+
+    #[tokio::test]
+    async fn nats_happy() {
+        // Publish `N` messages to NATS.
+        //
+        // Observe with a second subscriber that at least some of the messages
+        // were received.
+        //
+        // NATS operates with at_most_once delivery semantics
+        // - https://docs.nats.io/faq#does-nats-guarantee-message-delivery
+        //
+        // All messages should be accountable in the local integration test
+        // case, but, to prevent flakiness initially, validate the basic
+        // acceptable outcome.
+
+        trace_init();
+
+        let subject = format!("test-{}", random_string(10));
+
+        let cnf = NatsSinkConfig {
+            url: "nats://127.0.0.1:4222".to_owned(),
+            subject: subject.clone(),
+        };
+
+        // Establish the consumer subscription.
+        let consumer = NatsSink::new(cnf.clone());
+        let num_recv_events: Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
+        let sub = consumer.nc.subscribe(&subject).unwrap();
+
+        // Publish events.
+        let mut sink = NatsSink::new(cnf.clone());
+        let num_events = 1_000;
+        let (_input_lines, events) = random_lines_with_stream(100, num_events);
+
+        let _ = sink.run(Box::pin(events)).await.unwrap();
+
+        // Observe that there are delivered events.
+        for _msg in sub.timeout_iter(Duration::from_secs(3)) {
+            *num_recv_events.lock().unwrap() += 1;
+        }
+        assert!(*num_recv_events.lock().unwrap() > 0);
     }
 }
