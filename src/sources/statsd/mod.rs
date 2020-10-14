10de89fc8ec6ddbdf4a9ea778cb14987b425ea30
Duy Do <juchiast@gmail.com>
2020-10-14 19:54:27 +0700

    test
    Signed-off-by: Duy Do <juchiast@gmail.com>

--- src/sources/statsd/mod.rs
+++ src/sources/statsd/mod.rs
@@ -175,7 +175,7 @@ impl TcpSource for StatsdTcpSource {
 #[cfg(feature = "sinks-prometheus")]
 #[cfg(test)]
 mod test {
-    use super::{StatsdConfig, UdpConfig};
+    use super::{StatsdConfig, TcpConfig, UdpConfig};
     use crate::{
         config,
         sinks::prometheus::PrometheusSinkConfig,
@@ -183,6 +183,7 @@ mod test {
     };
     use futures::{compat::Future01CompatExt, TryStreamExt};
     use futures01::Stream;
+    use tokio::sync::mpsc;
     use tokio::time::{delay_for, Duration};
 
     #[test]
@@ -201,12 +202,62 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_statsd() {
+    async fn test_statsd_udp() {
         let in_addr = next_addr();
+        let config = StatsdConfig::Udp(UdpConfig { address: in_addr });
+        let sender = {
+            let (sender, mut receiver) = mpsc::channel(200);
+            let addr = in_addr.clone();
+            tokio::spawn(async move {
+                let bind_addr = next_addr();
+                let mut socket = tokio::net::UdpSocket::bind(bind_addr).await.unwrap();
+                socket.connect(addr).await.unwrap();
+                while let Some(bytes) = receiver.recv().await {
+                    socket.send(bytes).await.unwrap();
+                }
+            });
+            sender
+        };
+        test_statsd(config, sender).await;
+    }
+
+    #[tokio::test]
+    async fn test_statsd_tcp() {
+        let in_addr = next_addr();
+        let config = StatsdConfig::Tcp(TcpConfig {
+            address: in_addr.into(),
+            tls: None,
+            shutdown_timeout_secs: 30,
+        });
+        let sender = {
+            let (sender, mut receiver) = mpsc::channel(200);
+            let addr = in_addr.clone();
+            tokio::spawn(async move {
+                while let Some(bytes) = receiver.recv().await {
+                    use tokio::io::AsyncWriteExt;
+                    tokio::net::TcpStream::connect(addr)
+                        .await
+                        .unwrap()
+                        .write_all(bytes)
+                        .await
+                        .unwrap();
+                }
+            });
+            sender
+        };
+        test_statsd(config, sender).await;
+    }
+
+    async fn test_statsd(
+        statsd_config: StatsdConfig,
+        // could use unbounded channel,
+        // but we want to reserve the order messages.
+        mut sender: mpsc::Sender<&'static [u8]>,
+    ) {
         let out_addr = next_addr();
 
         let mut config = config::Config::builder();
-        config.add_source("in", StatsdConfig::Udp(UdpConfig { address: in_addr }));
+        config.add_source("in", statsd_config);
         config.add_sink(
             "out",
             &["in"],
@@ -221,16 +272,13 @@ mod test {
 
         let (topology, _crash) = start_topology(config.build().unwrap(), false).await;
 
-        let bind_addr = next_addr();
-        let socket = std::net::UdpSocket::bind(&bind_addr).unwrap();
+        // Give some time for the topology to start
+        delay_for(Duration::from_millis(100)).await;
 
         for _ in 0..100 {
-            socket
-                .send_to(
-                    b"foo:1|c|#a,b:b\nbar:42|g\nfoo:1|c|#a,b:c\nglork:3|h|@0.1\nmilliglork:3000|ms|@0.1\nset:0|s\nset:1|s\n",
-                    &in_addr,
-                )
-                .unwrap();
+            sender.send(
+                b"foo:1|c|#a,b:b\nbar:42|g\nfoo:1|c|#a,b:c\nglork:3|h|@0.1\nmilliglork:3000|ms|@0.1\nset:0|s\nset:1|s\n"
+            ).await.unwrap();
             // Space things out slightly to try to avoid dropped packets
             delay_for(Duration::from_millis(10)).await;
         }
@@ -317,9 +365,7 @@ mod test {
 
             // Re-check that set is also reset------------
 
-            socket.send_to(b"set:0|s\nset:1|s\n", &in_addr).unwrap();
-            // Space things out slightly to try to avoid dropped packets
-            delay_for(Duration::from_millis(10)).await;
+            sender.send(b"set:0|s\nset:1|s\n").await.unwrap();
             // Give packets some time to flow through
             delay_for(Duration::from_millis(100)).await;
 
