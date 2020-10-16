97d1b4704ec3afec3a63d5480f4aecd4d12f794c
MOZGIII <mike-n@narod.ru>
2020-10-17 00:45:13 +0300

    Blackhole metrics registry
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- src/metrics.rs
+++ src/metrics.rs
@@ -43,38 +43,17 @@ struct VectorRecorder {
 
 impl Recorder for VectorRecorder {
     fn register_counter(&self, key: Key, _description: Option<&'static str>) {
-        let ckey = CompositeKey::new(MetricKind::Counter, key);
-        self.registry.op(ckey, |_| {}, Handle::counter)
     }
     fn register_gauge(&self, key: Key, _description: Option<&'static str>) {
-        let ckey = CompositeKey::new(MetricKind::Gauge, key);
-        self.registry.op(ckey, |_| {}, Handle::gauge)
     }
     fn register_histogram(&self, key: Key, _description: Option<&'static str>) {
-        let ckey = CompositeKey::new(MetricKind::Histogram, key);
-        self.registry.op(ckey, |_| {}, Handle::histogram)
     }
 
     fn increment_counter(&self, key: Key, value: u64) {
-        let ckey = CompositeKey::new(MetricKind::Counter, key);
-        self.registry.op(
-            ckey,
-            |handle| handle.increment_counter(value),
-            Handle::counter,
-        )
     }
     fn update_gauge(&self, key: Key, value: f64) {
-        let ckey = CompositeKey::new(MetricKind::Gauge, key);
-        self.registry
-            .op(ckey, |handle| handle.update_gauge(value), Handle::gauge)
     }
     fn record_histogram(&self, key: Key, value: u64) {
-        let ckey = CompositeKey::new(MetricKind::Histogram, key);
-        self.registry.op(
-            ckey,
-            |handle| handle.record_histogram(value),
-            Handle::histogram,
-        )
     }
 }
 
