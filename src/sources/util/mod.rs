bf5742231dad11faf494d0105ccb663862a93bed
Duy Do <juchiast@gmail.com>
2020-10-19 10:04:17 +0700

    feature sources-utils-unix
    Signed-off-by: Duy Do <juchiast@gmail.com>

--- src/sources/util/mod.rs
+++ src/sources/util/mod.rs
@@ -3,14 +3,7 @@ mod http;
 pub mod multiline_config;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 mod tcp;
-#[cfg(all(
-    unix,
-    any(
-        feature = "sources-socket",
-        feature = "sources-syslog",
-        feature = "sources-statsd"
-    )
-))]
+#[cfg(all(unix, feature = "sources-utils-unix",))]
 mod unix;
 
 #[cfg(all(feature = "sources-tls", feature = "warp"))]
@@ -18,12 +11,5 @@ pub use self::http::{ErrorMessage, HttpSource, HttpSourceAuthConfig};
 pub use multiline_config::MultilineConfig;
 #[cfg(all(feature = "sources-tls", feature = "listenfd"))]
 pub use tcp::{SocketListenAddr, TcpSource};
-#[cfg(all(
-    unix,
-    any(
-        feature = "sources-socket",
-        feature = "sources-syslog",
-        feature = "sources-statsd"
-    )
-))]
+#[cfg(all(unix, feature = "sources-utils-unix",))]
 pub use unix::build_unix_source;
