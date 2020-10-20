a45fed5f0a8109de1a8c34f576aba34664843646
Bruce Guenter <bruce@timber.io>
2020-10-20 13:39:47 -0600

    Import remote protocol from Prometheus
    Signed-off-by: Bruce Guenter <bruce@timber.io>

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
 fn main() {
     println!("cargo:rerun-if-changed=proto/event.proto");
+    println!("cargo:rerun-if-changed=proto/prometheus-remote.proto");
+    println!("cargo:rerun-if-changed=proto/prometheus-types.proto");
+    println!("cargo:rerun-if-changed=proto/gogoproto/gogo.proto");
     let mut prost_build = prost_build::Config::new();
     prost_build.btree_map(&["."]);
     prost_build
-        .compile_protos(&["proto/event.proto"], &["proto/"])
+        .compile_protos(
+            &["proto/event.proto", "proto/prometheus-remote.proto"],
+            &["proto/"],
+        )
         .unwrap();
     built::write_built_file().expect("Failed to acquire build-time information");
 }
