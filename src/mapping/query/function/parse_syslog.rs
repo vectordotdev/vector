e20549b3c43abb5ad5b091cbe885160362d21aaa
Stephen Wakely <fungus.humungus@gmail.com>
2020-10-20 13:40:49 +0100

    Added more tests
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/mapping/query/function/parse_syslog.rs
+++ src/mapping/query/function/parse_syslog.rs
@@ -121,42 +121,133 @@ mod tests {
 
     #[test]
     fn parses() {
-        let cases = vec![(
-            Event::from(""),
-            Ok(Value::from({
-                let mut map = BTreeMap::new();
-                map.insert("severity".to_string(), Value::from("notice"));
-                map.insert("facility".to_string(), Value::from("user"));
-                map.insert(
-                    "timestamp".to_string(),
-                    Value::from(chrono::Utc.ymd(2020, 3, 13).and_hms_milli(20, 45, 38, 119)),
-                );
-                map.insert("hostname".to_string(), Value::from("dynamicwireless.name"));
-                map.insert("appname".to_string(), Value::from("non"));
-                map.insert("procid".to_string(), Value::from(2426));
-                map.insert("msgid".to_string(), Value::from("ID931"));
-                map.insert("exampleSDID@32473.iut".to_string(), Value::from("3"));
-                map.insert(
-                    "exampleSDID@32473.eventSource".to_string(),
-                    Value::from("Application"),
-                );
-                map.insert("exampleSDID@32473.eventID".to_string(), Value::from("1011"));
-                map.insert(
+        let cases = vec![
+            (
+                Event::from(""),
+                Ok(Value::from({
+                    let mut map = BTreeMap::new();
+                    map.insert("severity".to_string(), Value::from("notice"));
+                    map.insert("facility".to_string(), Value::from("user"));
+                    map.insert(
+                        "timestamp".to_string(),
+                        Value::from(chrono::Utc.ymd(2020, 3, 13).and_hms_milli(20, 45, 38, 119)),
+                    );
+                    map.insert("hostname".to_string(), Value::from("dynamicwireless.name"));
+                    map.insert("appname".to_string(), Value::from("non"));
+                    map.insert("procid".to_string(), Value::from(2426));
+                    map.insert("msgid".to_string(), Value::from("ID931"));
+                    map.insert("exampleSDID@32473.iut".to_string(), Value::from("3"));
+                    map.insert(
+                        "exampleSDID@32473.eventSource".to_string(),
+                        Value::from("Application"),
+                    );
+                    map.insert("exampleSDID@32473.eventID".to_string(), Value::from("1011"));
+                    map.insert(
                     "message".to_string(),
                     Value::from(
                         "Try to override the THX port, maybe it will reboot the neural interface!",
                     ),
                 );
-                map
-            })),
-            ParseSyslogFn::new(Box::new(Literal::from(Value::from(
-                r#"<13>1 2020-03-13T20:45:38.119Z dynamicwireless.name non 2426 ID931 [exampleSDID@32473 iut="3" eventSource= "Application" eventID="1011"] Try to override the THX port, maybe it will reboot the neural interface!"#,
-            )))),
-        )];
+                    map
+                })),
+                ParseSyslogFn::new(Box::new(Literal::from(Value::from(
+                    r#"<13>1 2020-03-13T20:45:38.119Z dynamicwireless.name non 2426 ID931 [exampleSDID@32473 iut="3" eventSource= "Application" eventID="1011"] Try to override the THX port, maybe it will reboot the neural interface!"#,
+                )))),
+            ),
+            (
+                Event::from(""),
+                Ok(Value::from({
+                    let mut map = BTreeMap::new();
+                    map.insert(
+                        "message".to_string(),
+                        Value::from("not much of a syslog message"),
+                    );
+                    map
+                })),
+                ParseSyslogFn::new(Box::new(Literal::from(Value::from(
+                    r#"not much of a syslog message"#,
+                )))),
+            ),
+            (
+                Event::from(""),
+                Ok(Value::from({
+                    // Syslog message which doesn't include the hostname or the current year.
+                    let mut map = BTreeMap::new();
+                    map.insert(
+                        "message".to_string(),
+                        Value::from("Proxy sticky-servers started."),
+                    );
+                    map.insert("facility".to_string(), Value::from("local0"));
+                    map.insert("severity".to_string(), Value::from("notice"));
+                    map.insert(
+                        "timestamp".to_string(),
+                        Value::from(
+                            chrono::Utc
+                                .ymd(Utc::now().year(), 1, 13)
+                                .and_hms_milli(16, 33, 35, 0),
+                        ),
+                    );
+                    map.insert("appname".to_string(), Value::from("haproxy"));
+                    map.insert("procid".to_string(), Value::from(73411));
+                    map
+                })),
+                ParseSyslogFn::new(Box::new(Literal::from(Value::from(
+                    r#"<133>Jan 13 16:33:35 haproxy[73411]: Proxy sticky-servers started."#,
+                )))),
+            ),
+        ];
 
         for (input_event, exp, query) in cases {
             assert_eq!(query.execute(&input_event), exp);
         }
+    }
+
+    #[test]
+    fn handles_empty_sd_element() {
+        fn there_is_map_called_empty(value: Value) -> Result<bool> {
+            match value {
+                Value::Map(map) => {
+                    Ok(map.iter().find(|(key, _)| (&key[..]).starts_with("empty")) == None)
+                }
+                _ => Err("Result was not a map".to_string()),
+            }
+        }
+
+        let msg = format!(
+            r#"<13>1 2019-02-13T19:48:34+00:00 74794bfb6795 root 8449 - {} qwerty"#,
+            r#"[empty]"#
+        );
+
+        let query = ParseSyslogFn::new(Box::new(Literal::from(Value::from(msg))));
+        let value = query.execute(&Event::from("")).unwrap();
+        assert!(there_is_map_called_empty(value).unwrap());
+
+        let msg = format!(
+            r#"<13>1 2019-02-13T19:48:34+00:00 74794bfb6795 root 8449 - {} qwerty"#,
+            r#"[non_empty x="1"][empty]"#
+        );
+
+        let query = ParseSyslogFn::new(Box::new(Literal::from(Value::from(msg))));
+        let value = query.execute(&Event::from("")).unwrap();
+        assert!(there_is_map_called_empty(value).unwrap());
+
+        let msg = format!(
+            r#"<13>1 2019-02-13T19:48:34+00:00 74794bfb6795 root 8449 - {} qwerty"#,
+            r#"[empty][non_empty x="1"]"#
+        );
+
+        let query = ParseSyslogFn::new(Box::new(Literal::from(Value::from(msg))));
+        let value = query.execute(&Event::from("")).unwrap();
+        assert!(there_is_map_called_empty(value).unwrap());
+
+        let msg = format!(
+            r#"<13>1 2019-02-13T19:48:34+00:00 74794bfb6795 root 8449 - {} qwerty"#,
+            r#"[empty not_really="testing the test"]"#
+        );
+
+        let query = ParseSyslogFn::new(Box::new(Literal::from(Value::from(msg))));
+        let value = query.execute(&Event::from("")).unwrap();
+        assert!(!there_is_map_called_empty(value).unwrap());
     }
 
     #[test]
