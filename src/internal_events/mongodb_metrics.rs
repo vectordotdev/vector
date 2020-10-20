1ac7e39e2cb51b8d4bd96c99a57345c32fafc037
Kirill Fomichev <fanatid@ya.ru>
2020-10-20 09:22:15 +0300

    feat(mongodb_metrics source): Initial implementation (#4500)
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>
    Signed-off-by: Brian Menges <brian.menges@anaplan.com>

---
+++ src/internal_events/mongodb_metrics.rs
@@ -0,0 +1,57 @@
+use super::InternalEvent;
+use metrics::{counter, histogram};
+use mongodb::{bson, error::Error as MongoError};
+use std::time::Instant;
+
+#[derive(Debug)]
+pub struct MongoDBMetricsCollectCompleted {
+    pub start: Instant,
+    pub end: Instant,
+}
+
+impl InternalEvent for MongoDBMetricsCollectCompleted {
+    fn emit_logs(&self) {
+        debug!(message = "Collect completed.");
+    }
+
+    fn emit_metrics(&self) {
+        counter!("collect_completed", 1);
+        histogram!("collect_duration_nanoseconds", self.end - self.start);
+    }
+}
+
+pub struct MongoDBMetricsRequestError<'a> {
+    pub error: MongoError,
+    pub endpoint: &'a str,
+}
+
+impl<'a> InternalEvent for MongoDBMetricsRequestError<'a> {
+    fn emit_logs(&self) {
+        error!(message = "MongoDB request error.", endpoint = %self.endpoint, error = %self.error)
+    }
+
+    fn emit_metrics(&self) {
+        counter!("request_error", 1,
+            "component_kind" => "source",
+            "component_type" => "mongodb_metrics",
+        );
+    }
+}
+
+pub struct MongoDBMetricsBsonParseError<'a> {
+    pub error: bson::de::Error,
+    pub endpoint: &'a str,
+}
+
+impl<'a> InternalEvent for MongoDBMetricsBsonParseError<'a> {
+    fn emit_logs(&self) {
+        error!(message = "BSON document parse error.", endpoint = %self.endpoint, error = ?self.error)
+    }
+
+    fn emit_metrics(&self) {
+        counter!("bson_parse_error", 1,
+            "component_kind" => "source",
+            "component_type" => "mongodb_metrics",
+        )
+    }
+}
