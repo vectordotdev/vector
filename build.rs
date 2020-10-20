642cda2bc1085de597575e22fdac4d791eb9263e
Bruce Guenter <bruce@timber.io>
2020-10-20 13:45:01 -0600

    Remove dependency on gogoproto by inserting the one definition
    Signed-off-by: Bruce Guenter <bruce@timber.io>

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() {
     println!("cargo:rerun-if-changed=proto/event.proto");
     println!("cargo:rerun-if-changed=proto/prometheus-remote.proto");
     println!("cargo:rerun-if-changed=proto/prometheus-types.proto");
-    println!("cargo:rerun-if-changed=proto/gogoproto/gogo.proto");
     let mut prost_build = prost_build::Config::new();
     prost_build.btree_map(&["."]);
     prost_build
