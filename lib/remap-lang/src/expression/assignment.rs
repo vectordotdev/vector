79d146eb849539335c3f06b8df073d71767052f6
Jean Mertz <git@jeanmertz.com>
2020-10-26 14:00:29 +0100

    feat(remap transform): add "remap-lang" crate (#4695)

---
+++ lib/remap-lang/src/expression/assignment.rs
@@ -0,0 +1,44 @@
+use super::Error as E;
+use crate::{Expr, Expression, Object, Result, State, Value};
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("unable to insert value in path: {0}")]
+    PathInsertion(String),
+}
+
+#[derive(Debug)]
+pub(crate) enum Target {
+    Path(String),
+}
+
+#[derive(Debug)]
+pub(crate) struct Assignment {
+    target: Target,
+    value: Box<Expr>,
+}
+
+impl Assignment {
+    pub fn new(target: Target, value: Box<Expr>) -> Self {
+        Self { target, value }
+    }
+}
+
+impl Expression for Assignment {
+    fn execute(&self, state: &mut State, object: &mut dyn Object) -> Result<Option<Value>> {
+        let value = self.value.execute(state, object)?;
+
+        match value {
+            None => Ok(None),
+            Some(value) => {
+                match &self.target {
+                    Target::Path(path) => object
+                        .insert(&path, value.clone())
+                        .map_err(|e| E::Assignment(Error::PathInsertion(e)))?,
+                }
+
+                Ok(Some(value))
+            }
+        }
+    }
+}
