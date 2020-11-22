000c2d17fc5faa95f06df62b9d6194a28fe1a5b2
Kruno Tomola Fabro <krunotf@gmail.com>
2020-11-22 19:22:14 +0100

    chore: Remove unused `use` (#5172)
    Signed-off-by: ktf <krunotf@gmail.com>

--- tests/tcp.rs
+++ tests/tcp.rs
@@ -7,7 +7,6 @@
 use approx::assert_relative_eq;
 use futures::compat::Future01CompatExt;
 use vector::{
-    conditions::CheckFieldsConfig,
     config, sinks, sources,
     test_util::{
         next_addr, random_lines, send_lines, start_topology, trace_init, wait_for_tcp,
