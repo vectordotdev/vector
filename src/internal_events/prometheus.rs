d90e8cd521281f9c6686a6c6e125d176650e9e7c
Kirill Fomichev <fanatid@ya.ru>
2020-12-02 18:22:31 +0300

    fix(prometheus sink): change logs level on request
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- src/internal_events/prometheus.rs
+++ src/internal_events/prometheus.rs
@@ -167,7 +167,12 @@ pub struct PrometheusServerRequestComplete {
 
 impl InternalEvent for PrometheusServerRequestComplete {
     fn emit_logs(&self) {
-        error!(message = "Request to prometheus server complete.", status_code = %self.status_code);
+        let message = "Request to prometheus server complete.";
+        if self.status_code.is_success() {
+            debug!(message, status_code = %self.status_code);
+        } else {
+            error!(message, status_code = %self.status_code);
+        }
     }
 
     fn emit_metrics(&self) {
