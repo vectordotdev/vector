0d565f59e36423e9511977061d2f7b9d68ec4d2c
binarylogic <bjohnson@binarylogic.com>
2020-12-01 21:12:32 -0500

    chore: Fix release-github.sh script
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- scripts/release-github.rb
+++ scripts/release-github.rb
@@ -6,8 +6,6 @@
 #
 #   Uploads target/artifacts to Github releases
 
-require_relative "setup"
-
 #
 # Constants
 #
@@ -16,17 +14,8 @@ VERSION = ENV.fetch("VECTOR_VERSION")
 SHA1 = ENV.fetch("SHA1")
 
 #
-# Setup
-#
-
-metadata = Metadata.load!(META_ROOT, DOCS_ROOT, GUIDES_ROOT, PAGES_ROOT)
-release = metadata.releases.to_h.fetch(:"#{VERSION}")
-
-#
 # Release
 #
-
-Printer.title("Releasing artifacts to Github")
 
 flags = [
   "--assets '#{ROOT_DIR}/target/artifacts/*'",
@@ -34,18 +23,5 @@ flags = [
   "--name v#{VERSION}"
 ]
 
-if release.pre?
-  flags << "--pre"
-end
-
 command = "grease --debug create-release timberio/vector v#{VERSION} #{SHA1} #{flags.join(" ")}"
-
-Printer.say(
-  <<~EOF
-  Running command:
-
-    #{command}
-  EOF
-)
-
 puts `#{command}`
