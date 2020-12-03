2c86087200df7ad7b883f1570a17d9225585159f
Luc Perkins <luc@timber.io>
2020-12-03 14:47:43 -0800

    Refactor config test
    Signed-off-by: Luc Perkins <luc@timber.io>
    
    fix
    
    Signed-off-by: Luc Perkins <luc@timber.io>

--- src/sources/docker_logs.rs
+++ src/sources/docker_logs.rs
@@ -1299,37 +1299,40 @@ mod integration_tests {
             .await
     }
 
-    fn test_config(config: DockerLogsConfig, expect_ok: bool) {
-        let (sender, _) = Pipeline::new_test();
-        let shutdown = ShutdownSignal::noop();
-        let source = DockerLogsSource::new(config, sender, shutdown);
-        if expect_ok {
-            assert!(source.is_ok());
-        } else {
-            assert!(source.is_err());
-        }
-    }
-
     #[test]
-    fn config_allows_inclusion_or_exclusion() {
+    fn config_allows_inclusion_or_exclusion_not_both() {
+        let config_ok = |config: DockerLogsConfig| {
+            let (sender, _) = Pipeline::new_test();
+            let shutdown = ShutdownSignal::noop();
+            let source = DockerLogsSource::new(config, sender, shutdown);
+            assert!(source.is_ok());
+        };
+
+        let config_err = |config: DockerLogsConfig| {
+            let (sender, _) = Pipeline::new_test();
+            let shutdown = ShutdownSignal::noop();
+            let source = DockerLogsSource::new(config, sender, shutdown);
+            assert!(source.is_err());
+        };
+
         let good_config_1 = DockerLogsConfig {
             include_containers: Some(vec!["container1".to_owned()]),
             ..DockerLogsConfig::default()
         };
-        test_config(good_config_1, true);
+        config_ok(good_config_1);
 
         let good_config_2 = DockerLogsConfig {
             exclude_containers: Some(vec!["container1".to_owned()]),
             ..DockerLogsConfig::default()
         };
-        test_config(good_config_2, true);
+        config_ok(good_config_2);
 
         let errant_config = DockerLogsConfig {
             exclude_containers: Some(vec!["container1".to_owned()]),
             include_containers: Some(vec!["container1".to_owned()]),
             ..DockerLogsConfig::default()
         };
-        test_config(errant_config, false);
+        config_err(errant_config);
     }
 
     #[tokio::test]
