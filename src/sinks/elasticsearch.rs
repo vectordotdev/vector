24c9443075802c9ec2062e0963032a52c0d27546
Samuel Gabel <samuel.gabel@idealo.de>
2020-12-03 15:42:29 +0100

    When enabling compression with AWS ES, it seems the Content-Encoding header wasn't set
    Signed-off-by: Samuel Gabel <samuel.gabel@idealo.de>

--- src/sinks/elasticsearch.rs
+++ src/sinks/elasticsearch.rs
@@ -212,6 +212,10 @@ impl HttpSink for ElasticSearchCommon {
 
             request.add_header("Content-Type", "application/x-ndjson");
 
+            if let Some(ce) = self.compression.content_encoding() {
+                request.add_header("Content-Encoding", ce);
+            }
+
             if let Some(headers) = &self.config.headers {
                 for (header, value) in headers {
                     request.add_header(header, value);
@@ -709,7 +713,23 @@ mod integration_tests {
             },
             false,
         )
-        .await;
+            .await;
+    }
+
+    #[tokio::test]
+    async fn insert_events_on_aws_with_compression() {
+        trace_init();
+
+        run_insert_tests(
+            ElasticSearchConfig {
+                auth: Some(ElasticSearchAuth::Aws { assume_role: None }),
+                endpoint: "http://localhost:4571".into(),
+                compression: Compression::gzip_default(),
+                ..config()
+            },
+            false,
+        )
+            .await;
     }
 
     #[tokio::test]
