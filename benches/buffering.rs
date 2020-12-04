05ea7bc0bfd6119ac9428f7d58b6b31f22e0f125
Jesse Szwedko <jesse@szwedko.me>
2020-12-04 17:32:40 -0500

    chore(ci): Disable buffers/low-limit-on-disk benchmark (#5390)
    This seems to hang in CI.
    
    Opened https://github.com/timberio/vector/issues/5389 to look into it.
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- benches/buffering.rs
+++ benches/buffering.rs
@@ -108,49 +108,53 @@ fn benchmark_buffers(c: &mut Criterion) {
         );
     });
 
-    group.bench_function("low-limit-on-disk", |b| {
-        b.iter_batched(
-            || {
-                let data_dir = tempdir().unwrap();
+    // TODO(jesse): reenable
+    // This benchmark hangs in CI sometimes
+    // https://github.com/timberio/vector/issues/5389
+    //
+    //group.bench_function("low-limit-on-disk", |b| {
+    //b.iter_batched(
+    //|| {
+    //let data_dir = tempdir().unwrap();
 
-                let mut config = config::Config::builder();
-                config.add_source(
-                    "in",
-                    sources::socket::SocketConfig::make_basic_tcp_config(in_addr),
-                );
-                config.add_sink(
-                    "out",
-                    &["in"],
-                    sinks::socket::SocketSinkConfig::make_basic_tcp_config(out_addr.to_string()),
-                );
-                config.sinks["out"].buffer = BufferConfig::Disk {
-                    max_size: 10_000,
-                    when_full: Default::default(),
-                };
-                config.global.data_dir = Some(data_dir.path().to_path_buf());
-                let mut rt = runtime();
-                let (output_lines, topology) = rt.block_on(async move {
-                    let output_lines = CountReceiver::receive_lines(out_addr);
-                    let (topology, _crash) = start_topology(config.build().unwrap(), false).await;
-                    wait_for_tcp(in_addr).await;
-                    (output_lines, topology)
-                });
-                (rt, topology, output_lines)
-            },
-            |(mut rt, topology, output_lines)| {
-                rt.block_on(async move {
-                    let lines = random_lines(line_size).take(num_lines);
-                    send_lines(in_addr, lines).await.unwrap();
-                    topology.stop().compat().await.unwrap();
+    //let mut config = config::Config::builder();
+    //config.add_source(
+    //"in",
+    //sources::socket::SocketConfig::make_basic_tcp_config(in_addr),
+    //);
+    //config.add_sink(
+    //"out",
+    //&["in"],
+    //sinks::socket::SocketSinkConfig::make_basic_tcp_config(out_addr.to_string()),
+    //);
+    //config.sinks["out"].buffer = BufferConfig::Disk {
+    //max_size: 10_000,
+    //when_full: Default::default(),
+    //};
+    //config.global.data_dir = Some(data_dir.path().to_path_buf());
+    //let mut rt = runtime();
+    //let (output_lines, topology) = rt.block_on(async move {
+    //let output_lines = CountReceiver::receive_lines(out_addr);
+    //let (topology, _crash) = start_topology(config.build().unwrap(), false).await;
+    //wait_for_tcp(in_addr).await;
+    //(output_lines, topology)
+    //});
+    //(rt, topology, output_lines)
+    //},
+    //|(mut rt, topology, output_lines)| {
+    //rt.block_on(async move {
+    //let lines = random_lines(line_size).take(num_lines);
+    //send_lines(in_addr, lines).await.unwrap();
+    //topology.stop().compat().await.unwrap();
 
-                    // TODO: shutdown after flush
-                    // assert_eq!(num_lines, output_lines.await.len());
-                    output_lines.await
-                });
-            },
-            BatchSize::PerIteration,
-        );
-    });
+    //// TODO: shutdown after flush
+    //// assert_eq!(num_lines, output_lines.await.len());
+    //output_lines.await
+    //});
+    //},
+    //BatchSize::PerIteration,
+    //);
+    //});
 }
 
 criterion_group!(benches, benchmark_buffers);
