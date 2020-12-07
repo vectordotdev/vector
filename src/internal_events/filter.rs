175e42a54f2814dfea789a3cdd7b5f55433e4a8c
Lee Benson <lee@leebenson.com>
2020-12-07 11:03:10 +0000

    filter events
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/internal_events/filter.rs
+++ src/internal_events/filter.rs
@@ -1,37 +1,33 @@
 use super::InternalEvent;
+use metrics::counter;
+use serde_json::Error;
 
 #[derive(Debug)]
 pub struct FilterEventProcessed;
 
 impl InternalEvent for FilterEventProcessed {
     fn emit_metrics(&self) {
-        counter!("events_processed", 1,
-            "component_kind" => "transform",
-            "component_type" => "filter",
-        );
+        counter!("events_processed", 1);
     }
 }
 
 #[derive(Debug)]
-pub struct FilterEventDiscard;
+pub struct FilterEventDiscarded;
 
-impl InternalEvent for FilterEventProcessed {
+impl InternalEvent for FilterEventDiscarded {
     fn emit_metrics(&self) {
-        counter!("events_discarded", 1,
-            "component_kind" => "transform",
-            "component_type" => "filter",
-        );
+        counter!("events_discarded", 1);
     }
 }
 
 #[derive(Debug)]
-pub struct FilterEventCheckError {
-    pub error: dyn std::error::Error,
+pub struct FilterEventError {
+    pub error: Error,
 }
 
-impl InternalEvent for FilterEventCheckError {
+impl InternalEvent for FilterEventError {
     fn emit_logs(&self) {
-        error!(message = "Error in lua script; discarding event.", error = ?self.error, rate_limit_secs = 30);
+        error!(message = "Error in filter; discarding event.", error = ?self.error, rate_limit_secs = 30);
     }
 
     fn emit_metrics(&self) {
