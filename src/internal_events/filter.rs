417e7e806ceb8609458dfcc14db777a9295c5ab4
Lee Benson <lee@leebenson.com>
2020-12-07 11:07:29 +0000

    -FilterEventError
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/internal_events/filter.rs
+++ src/internal_events/filter.rs
@@ -1,6 +1,5 @@
 use super::InternalEvent;
 use metrics::counter;
-use serde_json::Error;
 
 #[derive(Debug)]
 pub struct FilterEventProcessed;
@@ -17,20 +16,5 @@ pub struct FilterEventDiscarded;
 impl InternalEvent for FilterEventDiscarded {
     fn emit_metrics(&self) {
         counter!("events_discarded", 1);
-    }
-}
-
-#[derive(Debug)]
-pub struct FilterEventError {
-    pub error: Error,
-}
-
-impl InternalEvent for FilterEventError {
-    fn emit_logs(&self) {
-        error!(message = "Error in filter; discarding event.", error = ?self.error, rate_limit_secs = 30);
-    }
-
-    fn emit_metrics(&self) {
-        counter!("processing_errors_total", 1);
     }
 }
