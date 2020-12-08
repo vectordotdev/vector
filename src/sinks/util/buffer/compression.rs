0b52537645cc7206207d1ab823a831ab47f85080
Kirill Fomichev <fanatid@ya.ru>
2020-12-08 09:04:37 +0300

    remove extra macro call
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- src/sinks/util/buffer/compression.rs
+++ src/sinks/util/buffer/compression.rs
@@ -108,7 +108,7 @@ impl<'de> de::Deserialize<'de> for Compression {
                                     Some(value) if value <= 9 => value as usize,
                                     Some(_) | None => {
                                         return Err(de::Error::invalid_value(
-                                            de::Unexpected::Other(&format!("{}", level)),
+                                            de::Unexpected::Other(&level.to_string()),
                                             &"0, 1, 2, 3, 4, 5, 6, 7, 8 or 9",
                                         ))
                                     }
@@ -127,7 +127,7 @@ impl<'de> de::Deserialize<'de> for Compression {
                                 },
                                 value => {
                                     return Err(de::Error::invalid_type(
-                                        de::Unexpected::Other(&format!("{}", value)),
+                                        de::Unexpected::Other(&value.to_string()),
                                         &"integer or string",
                                     ));
                                 }
