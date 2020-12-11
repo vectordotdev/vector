1d530928c5f018a6749c1188b5cba258d345572f
Luc Perkins <luc@timber.io>
2020-12-11 10:58:28 -0800

    Add to_severity function for Remap
    Signed-off-by: Luc Perkins <luc@timber.io>

---
+++ lib/remap-functions/src/to_severity.rs
@@ -0,0 +1,123 @@
+use remap::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct ToSeverity;
+
+impl Function for ToSeverity {
+    fn identifier(&self) -> &'static str {
+        "to_severity"
+    }
+
+    fn parameters(&self) -> &'static [Parameter] {
+        &[
+            Parameter {
+                keyword: "level",
+                accepts: |v| matches!(v, Value::Bytes(_)),
+                required: true,
+            }
+        ]
+    }
+
+    fn compile(&self, mut arguments: ArgumentList) -> Result<Box<dyn Expression>> {
+        let level = arguments.required("level")?.boxed();
+
+        Ok(Box::new(ToSeverityFn { level }))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ToSeverityFn {
+    level: Box<dyn Expression>,
+}
+
+impl Expression for ToSeverityFn {
+    fn execute(&self, state: &mut state::Program, object: &mut dyn Object) -> Result<Value> {
+        let level_bytes = self.severity.execute(state, object)?.try_bytes()?;
+
+        let level = String::from_utf8_lossy(level_bytes);
+
+        // Severity levels: https://en.wikipedia.org/wiki/Syslog#Severity_level
+        let severity = match &level[..] {
+            "emerg" | "panic" => Ok(0),
+            "alert" => Ok(1),
+            "crit" => Ok(2),
+            "err" | "error" => Ok(3),
+            "warning" | "warn" => Ok(4),
+            "notice" => Ok(5),
+            "info" => Ok(6),
+            "debug" => Ok(7),
+            _ => Err(format!("level {} not valid", level))
+        }
+
+        match severity {
+            Ok(severity) => Ok(Value::from(severity)),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn type_def(&self, _: &state::Compiler) -> TypeDef {
+        TypeDef {
+            kind: value::Kind::Integer,
+            ..Default::default()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    test_function![
+        to_level => ToSeverity;
+
+        emergency {
+            args: func_args![level: value!(["emerge"])],
+            want: Ok(value!([0])),
+        }
+
+        alert {
+            args: func_args![level: value!["alert"]],
+            want: Ok(value!([1])),
+        }
+
+        critical {
+            args: func_args![severity: value!["crit"]],
+            want: Ok(value!([2])),
+        }
+
+        error {
+            args: func_args![severity: value!["err"]],
+            want: Ok(value!([3])),
+        }
+
+        warning {
+            args: func_args![severity: value!["warn"]],
+            want: Ok(value!([4])),
+        }
+
+        notice {
+            args: func_args![severity: value!["notice"]],
+            want: Ok(value!([5])),
+        }
+
+        informational {
+            args: func_args![severity: value!["info"]],
+            want: Ok(value!([6])),
+        }
+
+        debug {
+            args: func_args![severity: value!["debug"]],
+            want: Ok(value!([7])),
+        }
+
+        invalid_level_1 {
+            args: func_args![severity: value!["oopsie"]],
+            want: Err("level oopsie not valid"),
+        }
+
+        invalid_severity_2 {
+            args: func_args![severity: value!["aww shucks"]],
+            want: Err("level aww schucks not valid"),
+        }
+    ];
+}
