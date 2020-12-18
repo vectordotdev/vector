5488be0a6252efe152bde9fd4babbffb7970fb75
Kirill Fomichev <fanatid@ya.ru>
2020-12-18 16:03:10 +0300

    enhancement(remap transform): add `parse_aws_vpc_flow_log` function (#5504)
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>

--- lib/remap-functions/src/lib.rs
+++ lib/remap-functions/src/lib.rs
@@ -48,6 +48,8 @@ mod ok;
 mod only_fields;
 #[cfg(feature = "parse_aws_alb_log")]
 mod parse_aws_alb_log;
+#[cfg(feature = "parse_aws_vpc_flow_log")]
+mod parse_aws_vpc_flow_log;
 #[cfg(feature = "parse_duration")]
 mod parse_duration;
 #[cfg(feature = "parse_grok")]
@@ -151,6 +153,8 @@ pub use ok::OK;
 pub use only_fields::OnlyFields;
 #[cfg(feature = "parse_aws_alb_log")]
 pub use parse_aws_alb_log::ParseAwsAlbLog;
+#[cfg(feature = "parse_aws_vpc_flow_log")]
+pub use parse_aws_vpc_flow_log::ParseAwsVpcFlowLog;
 #[cfg(feature = "parse_duration")]
 pub use parse_duration::ParseDuration;
 #[cfg(feature = "parse_grok")]
@@ -254,6 +258,8 @@ pub fn all() -> Vec<Box<dyn remap::Function>> {
         Box::new(OnlyFields),
         #[cfg(feature = "parse_aws_alb_log")]
         Box::new(ParseAwsAlbLog),
+        #[cfg(feature = "parse_aws_vpc_flow_log")]
+        Box::new(ParseAwsVpcFlowLog),
         #[cfg(feature = "parse_duration")]
         Box::new(ParseDuration),
         #[cfg(feature = "parse_grok")]
