51bda217d8178d35e06a8fb5310260be8881bf4f
Ana Hobden <operator@hoverbear.org>
2020-12-28 14:14:44 -0800

    Fmt
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- lib/shared/src/event/error.rs
+++ lib/shared/src/event/error.rs
@@ -1,23 +1,26 @@
 use crate::{event::*, lookup::*};
-pub use EventError::*;
 use snafu::Snafu;
+pub use EventError::*;
 
 #[derive(Debug, Snafu)]
 pub enum EventError {
-    #[snafu(display("Cannot insert value nested inside primitive located at {}. {} was the original target.", primitive_at, original_target))]
+    #[snafu(display(
+        "Cannot insert value nested inside primitive located at {}. {} was the original target.",
+        primitive_at,
+        original_target
+    ))]
     PrimitiveDescent {
         primitive_at: LookupBuf,
         original_target: LookupBuf,
         original_value: Option<Value>,
     },
     #[snafu(display("Lookup Error: {}", source))]
-    LookupError { source: crate::lookup::LookupError},
+    LookupError { source: crate::lookup::LookupError },
     #[snafu(display("Empty coalesce subsegment found."))]
     EmptyCoalesceSubSegment,
     #[snafu(display("Cannot remove self."))]
     RemovingSelf,
 }
-
 
 impl From<crate::lookup::LookupError> for EventError {
     fn from(v: crate::lookup::LookupError) -> Self {
