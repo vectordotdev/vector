11a92d2a73f961e7709d9198fbe17695c260d7ed
Ana Hobden <operator@hoverbear.org>
2020-12-28 12:54:05 -0800

    Use snafu
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- lib/shared/src/event/error.rs
+++ lib/shared/src/event/error.rs
@@ -1,48 +1,26 @@
 use crate::{event::*, lookup::*};
-use std::fmt;
 pub use EventError::*;
+use snafu::Snafu;
 
-#[derive(Debug)]
+#[derive(Debug, Snafu)]
 pub enum EventError {
+    #[snafu(display("Cannot insert value nested inside primitive located at {}. {} was the original target.", primitive_at, original_target))]
     PrimitiveDescent {
         primitive_at: LookupBuf,
         original_target: LookupBuf,
         original_value: Option<Value>,
     },
-    LookupError(crate::lookup::LookupError),
+    #[snafu(display("Lookup Error: {}", source))]
+    LookupError { source: crate::lookup::LookupError},
+    #[snafu(display("Empty coalesce subsegment found."))]
     EmptyCoalesceSubSegment,
+    #[snafu(display("Cannot remove self."))]
     RemovingSelf,
 }
 
-impl fmt::Display for EventError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            PrimitiveDescent { primitive_at, original_target, .. } => write!(
-                f,
-                "Cannot insert value nested inside primitive located at {}. {} was the original target.",
-                primitive_at,
-                original_target,
-            ),
-            LookupError(e) => write!(f, "Lookup Error: {:?}", e,),
-            EmptyCoalesceSubSegment => write!(f, "Empty coalesce subsegment found."),
-            RemovingSelf =>  write!(f, "Cannot remove self."),
-        }
-    }
-}
-
-impl std::error::Error for EventError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            PrimitiveDescent { .. } => None,
-            LookupError(source) => Some(source),
-            EmptyCoalesceSubSegment { .. } => None,
-            RemovingSelf => None,
-        }
-    }
-}
 
 impl From<crate::lookup::LookupError> for EventError {
     fn from(v: crate::lookup::LookupError) -> Self {
-        Self::LookupError(v)
+        Self::LookupError { source: v }
     }
 }
