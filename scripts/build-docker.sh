25ddecb2e2f1ceacbdfa5728b27cde5f1e26e070
James Turnbull <james@lovedthanlost.net>
2021-01-03 15:40:26 -0500

    Stripped out old Docker shit in favor of GithUb Actions
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -13,7 +13,6 @@ set -x
 CHANNEL="${CHANNEL:-"$(scripts/release-channel.sh)"}"
 VERSION="${VECTOR_VERSION:-"$(scripts/version.sh)"}"
 DATE="${DATE:-"$(date -u +%Y-%m-%d)"}"
-PUSH="${PUSH:-}"
 PLATFORM="${PLATFORM:-}"
 REPO="${REPO:-"timberio/vector"}"
 
@@ -29,31 +28,17 @@ build() {
   local DOCKERFILE="distribution/docker/$BASE/Dockerfile"
 
   if [ -n "$PLATFORM" ]; then
-    export DOCKER_CLI_EXPERIMENTAL=enabled
-    docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
-    docker buildx rm vector-builder || true
-    docker buildx create --use --name vector-builder
-    docker buildx install
-
-    ARGS=()
-    if [[ -n "${PUSH:-}" ]]; then
-      ARGS+=(--push)
-    fi
-
     docker buildx build \
       --platform="$PLATFORM" \
       --tag "$TAG" \
       target/artifacts \
-      -f "$DOCKERFILE" "${ARGS[@]}"
+      -f "$DOCKERFILE" --push
   else
     docker build \
       --tag "$TAG" \
       target/artifacts \
       -f "$DOCKERFILE"
-
-    if [ -n "$PUSH" ]; then
       docker push "$TAG"
-    fi
   fi
 }
 
