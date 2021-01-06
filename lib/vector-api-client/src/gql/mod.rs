042008b0dae2b823aaf8957d023044b142f59437
Lee Benson <lee@leebenson.com>
2021-01-06 14:14:12 +0000

    reorder imports
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- lib/vector-api-client/src/gql/mod.rs
+++ lib/vector-api-client/src/gql/mod.rs
@@ -1,12 +1,12 @@
 //! Queries, subscriptions, and extension methods for executing them
 
+mod components;
+mod health;
+mod meta;
+mod metrics;
+
 pub use components::*;
 pub use health::*;
 pub use metrics::*;
 
 pub use self::meta::*;
-
-mod components;
-mod health;
-mod meta;
-mod metrics;
