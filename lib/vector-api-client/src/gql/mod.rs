9f3f479df35fdd4a7d092b950a35e0d1b60b1409
Lee Benson <lee@leebenson.com>
2021-01-06 14:07:40 +0000

    update tests
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- lib/vector-api-client/src/gql/mod.rs
+++ lib/vector-api-client/src/gql/mod.rs
@@ -1,11 +1,12 @@
 //! Queries, subscriptions, and extension methods for executing them
 
+pub use components::*;
+pub use health::*;
+pub use metrics::*;
+
+pub use self::meta::*;
+
 mod components;
 mod health;
 mod meta;
 mod metrics;
-
-pub use self::meta::*;
-pub use components::*;
-pub use health::*;
-pub use metrics::*;
