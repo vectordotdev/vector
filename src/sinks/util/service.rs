0dfa82ab2a0ce3d0895890d4d98d5af94d50411d
ktf <krunotf@gmail.com>
2021-01-08 23:24:17 +0100

    Avoid alias
    Signed-off-by: ktf <krunotf@gmail.com>

--- src/sinks/util/service.rs
+++ src/sinks/util/service.rs
@@ -163,11 +163,13 @@ impl ConcurrencyOption for Concurrency {
 #[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
 pub struct TowerRequestConfig<T: ConcurrencyOption = Concurrency> {
     #[serde(default)]
-    #[serde(
-        alias = "in_flight_limit",
-        skip_serializing_if = "ConcurrencyOption::is_none"
-    )]
+    #[serde(skip_serializing_if = "ConcurrencyOption::is_none")]
     pub concurrency: T, // 5
+    /// The same as concurrency but with old deprecated name.
+    /// Alias couldn't be used because of https://github.com/serde-rs/serde/issues/1504
+    #[serde(default)]
+    #[serde(skip_serializing_if = "ConcurrencyOption::is_none")]
+    pub in_flight_limit: T, // 5
     pub timeout_secs: Option<u64>,             // 60
     pub rate_limit_duration_secs: Option<u64>, // 1
     pub rate_limit_num: Option<u64>,           // 5
@@ -180,8 +182,12 @@ pub struct TowerRequestConfig<T: ConcurrencyOption = Concurrency> {
 
 impl<T: ConcurrencyOption> TowerRequestConfig<T> {
     pub fn unwrap_with(&self, defaults: &Self) -> TowerRequestSettings {
+        // let concurrency
+
         TowerRequestSettings {
-            concurrency: self.concurrency.parse_concurrency(&defaults.concurrency),
+            concurrency: self
+                .concurrency()
+                .parse_concurrency(&defaults.concurrency()),
             timeout: Duration::from_secs(self.timeout_secs.or(defaults.timeout_secs).unwrap_or(60)),
             rate_limit_duration: Duration::from_secs(
                 self.rate_limit_duration_secs
@@ -204,6 +210,17 @@ impl<T: ConcurrencyOption> TowerRequestConfig<T> {
                     .unwrap_or(1),
             ),
             adaptive_concurrency: self.adaptive_concurrency,
+        }
+    }
+
+    fn concurrency(&self) -> &T {
+        match (self.concurrency.is_none(), self.in_flight_limit.is_none()) {
+            (_, true) => &self.concurrency,
+            (true, false) => &self.in_flight_limit,
+            (false, false) => {
+                warn!("`in_flight_limit` option has been renamed to `concurrency`. Ignoring `in_flight_limit` and using `concurrency` option");
+                &self.concurrency
+            }
         }
     }
 }
