ff13bf30df66302630233d90cd8823fadddf6336
Duy Do <juchiast@gmail.com>
2021-01-19 11:02:31 +0700

    Print out-of-order warning
    Signed-off-by: Duy Do <juchiast@gmail.com>

--- src/sinks/util/buffer/loki.rs
+++ src/sinks/util/buffer/loki.rs
@@ -153,6 +153,7 @@ impl Batch for LokiBuffer {
                 .cloned()
                 .unwrap_or(item.event.timestamp);
             if item.event.timestamp < latest_timestamp {
+                warn!(msg = "Received out-of-order event.", rate_limit = 30);
                 match self.out_of_order_action {
                     OutOfOrderAction::Drop => return PushResult::Ok(false),
                     OutOfOrderAction::RewriteTimestamp => {
