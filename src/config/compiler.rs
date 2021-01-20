1abfd3bed93d068240090cc7ac0efdc5c18a3601
Luke Steensen <luke.steensen@gmail.com>
2021-01-20 13:12:19 -0600

    feat(config): add wildcard expansion to inputs
    Closes #2550
    
    This allows for very simple wildcards in input lists. The `*` must be
    the last character in the string and we do a simple prefix match against
    all valid inputs (i.e. self is excluded). We could choose to expand this
    to a more full-featured glob syntax in the future, but there were enough
    questions there that this seemed like a better start. It is also my
    expectation that this should cover >95% of valuable uses cases, so it
    may well be all we ever need.
    
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- src/config/compiler.rs
+++ src/config/compiler.rs
@@ -16,6 +16,8 @@ pub fn compile(raw: ConfigBuilder, deny_warnings: bool) -> Result<Config, Vec<St
 
     let mut errors = Vec::new();
 
+    expand_wildcards(&mut config);
+
     expand_macros(&mut config)?;
 
     if let Err(warn) = handle_warnings(validation::warnings(&config), deny_warnings) {
@@ -80,5 +82,141 @@ pub(super) fn expand_macros(config: &mut Config) -> Result<(), Vec<String>> {
     } else {
         config.expansions = expansions;
         Ok(())
+    }
+}
+
+/// Expand trailing `*` wildcards in input lists
+fn expand_wildcards(config: &mut Config) {
+    let candidates = config
+        .sources
+        .keys()
+        .chain(config.transforms.keys())
+        .cloned()
+        .collect::<Vec<String>>();
+
+    for (name, transform) in config.transforms.iter_mut() {
+        expand_inner(&mut transform.inputs, name, &candidates);
+    }
+
+    for (name, sink) in config.sinks.iter_mut() {
+        expand_inner(&mut sink.inputs, name, &candidates);
+    }
+}
+
+fn expand_inner(inputs: &mut Vec<String>, name: &str, candidates: &[String]) {
+    let raw_inputs = inputs.drain(..).collect::<Vec<_>>();
+    for raw_input in raw_inputs {
+        if raw_input.ends_with("*") {
+            let prefix = &raw_input[0..raw_input.len() - 1];
+            for input in candidates {
+                if input.starts_with(prefix) && input != name {
+                    inputs.push(input.clone())
+                }
+            }
+        } else {
+            inputs.push(raw_input);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{
+        config::{DataType, GlobalOptions, SinkConfig, SinkContext, SourceConfig, TransformConfig},
+        shutdown::ShutdownSignal,
+        sinks::{Healthcheck, VectorSink},
+        sources::Source,
+        transforms::Transform,
+        Pipeline,
+    };
+    use async_trait::async_trait;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MockSourceConfig;
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    struct MockTransformConfig;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MockSinkConfig;
+
+    #[async_trait]
+    #[typetag::serde(name = "mock")]
+    impl SourceConfig for MockSourceConfig {
+        async fn build(
+            &self,
+            _name: &str,
+            _globals: &GlobalOptions,
+            _shutdown: ShutdownSignal,
+            _out: Pipeline,
+        ) -> crate::Result<Source> {
+            unimplemented!()
+        }
+
+        fn source_type(&self) -> &'static str {
+            "mock"
+        }
+
+        fn output_type(&self) -> DataType {
+            DataType::Any
+        }
+    }
+
+    #[async_trait]
+    #[typetag::serde(name = "mock")]
+    impl TransformConfig for MockTransformConfig {
+        async fn build(&self) -> crate::Result<Transform> {
+            unimplemented!()
+        }
+
+        fn transform_type(&self) -> &'static str {
+            "mock"
+        }
+
+        fn input_type(&self) -> DataType {
+            DataType::Any
+        }
+
+        fn output_type(&self) -> DataType {
+            DataType::Any
+        }
+    }
+
+    #[async_trait]
+    #[typetag::serde(name = "mock")]
+    impl SinkConfig for MockSinkConfig {
+        async fn build(&self, _cx: SinkContext) -> crate::Result<(VectorSink, Healthcheck)> {
+            unimplemented!()
+        }
+
+        fn sink_type(&self) -> &'static str {
+            "mock"
+        }
+
+        fn input_type(&self) -> DataType {
+            DataType::Any
+        }
+    }
+
+    #[test]
+    fn wildcard_expansion() {
+        let mut builder = ConfigBuilder::default();
+        builder.add_source("foo1", MockSourceConfig);
+        builder.add_source("foo2", MockSourceConfig);
+        builder.add_source("bar", MockSourceConfig);
+        builder.add_transform("foos", &["foo*"], MockTransformConfig);
+        builder.add_sink("baz", &["foos*", "b*"], MockSinkConfig);
+        builder.add_sink("quux", &["*"], MockSinkConfig);
+
+        let config = builder.build().expect("build should succeed");
+
+        assert_eq!(config.transforms["foos"].inputs, vec!["foo1", "foo2"]);
+        assert_eq!(config.sinks["baz"].inputs, vec!["foos", "bar"]);
+        assert_eq!(
+            config.sinks["quux"].inputs,
+            vec!["foo1", "foo2", "bar", "foos"]
+        );
     }
 }
