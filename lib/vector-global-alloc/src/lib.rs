52414e5cc26cd5091d11f9e007b2101edbc738be
MOZGIII <mike-n@narod.ru>
2021-01-21 21:24:49 +0300

    Rely on autodetection by default (solves issues on Windows)
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- lib/vector-global-alloc/src/lib.rs
+++ lib/vector-global-alloc/src/lib.rs
@@ -14,18 +14,37 @@
 )]
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "force-system")] {
-        // Ensure we're setting a global allocator either way to "grab the spot" and
-        // prevent it from being used anywhere else if no other allocator is set.
+    if #[cfg(any(
+        // When system allocator is forced...
+        feature = "force-system",
+        // Or when we want to use it as a platform default.
+        target_os = "windows"
+    ))] {
+        // Use system allocator.
+        // Ensure we're setting a global allocator either way to "grab the spot"
+        // and prevent it from being defined anywhere else.
         #[global_allocator]
         static ALLOC: std::alloc::System = std::alloc::System;
-    } else if #[cfg(feature = "jemallocator")] {
+    } else if #[cfg(any(
+        // When jemallocator is forced...
+        feature = "force-jemallocator",
+        // Or when we want to use it as a platform default.
+        target_os = "linux",
+        target_os = "android",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "freebsd",
+        target_os = "openbsd",
+        target_os = "netbsd"
+    ))] {
         // Use jemalloc as global allocator.
         #[global_allocator]
         static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
     } else {
         // No allocator was picked, fail fast to alert the user about it.
-        // `jemallocator` is supposed to be in the `default` featureset.
-        compile_error!("Either \"force-system\" or \"jemallocator\" feature has to be enabled");
+        compile_error!(concat!(
+            "No feature forcing a particular allocator was passed, and we didn't default to something meaningful.\n",
+            "You have to excplicitly pass a feature to force a particulr allocator on this system.",
+        ));
     }
 }
