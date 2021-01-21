ac9fb950f83f08a8e810274853a643b9332f77c1
Pablo Sichert <mail@pablosichert.com>
2021-01-21 14:16:14 +0100

    Configure buffer sizes on UDP socket for statsd source
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- src/sources/statsd/mod.rs
+++ src/sources/statsd/mod.rs
@@ -170,6 +170,26 @@ async fn statsd_udp(
         .map_err(|error| emit!(StatsdSocketError::bind(error)))
         .await?;
 
+    #[cfg(unix)]
+    {
+        // SAFETY: We temporarily take ownership of the socket and return it by the end of this block scope.
+        let socket = unsafe { socket2::Socket::from_raw_fd(socket.as_raw_fd()) };
+
+        if let Some(send_buffer_bytes) = config.send_buffer_bytes {
+            if let Err(error) = socket.set_send_buffer_size(send_buffer_bytes) {
+                warn!(message = "Failed configuring send buffer size on UDP socket.", %error);
+            }
+        }
+
+        if let Some(receive_buffer_bytes) = config.receive_buffer_bytes {
+            if let Err(error) = socket.set_recv_buffer_size(receive_buffer_bytes) {
+                warn!(message = "Failed configuring receive buffer size on UDP socket.", %error);
+            }
+        }
+
+        socket.into_raw_fd();
+    }
+
     info!(
         message = "Listening.",
         addr = %config.address,
