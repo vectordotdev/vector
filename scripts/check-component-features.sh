7dd346499a3947c2b290923641e0f9cffad6654a
Bruce Guenter <bruce@timber.io>
2021-01-27 12:29:32 -0600

    fix(ci): Extract new second-level features from Cargo.toml
    Signed-off-by: Bruce Guenter <bruce@timber.io>

--- scripts/check-component-features.sh
+++ scripts/check-component-features.sh
@@ -16,6 +16,15 @@ toml-extract() {
   remarshal --if toml --of json | jq -r "$WHAT"
 }
 
+extract-features() {
+  features=( $(toml-extract ".features.\"$1\"|.[]" < Cargo.toml) )
+  # The main "features.NAME" only lists categories, so extract from the categories
+  for feature in ${features[*]}
+  do
+    toml-extract ".features.\"$feature\"|.[]" < Cargo.toml
+  done | grep "^${1}-" | sort --unique
+}
+
 check-listed-features() {
   xargs -I{} sh -cx "(cargo check --tests --no-default-features --features {}) || exit 255"
 }
@@ -33,7 +42,7 @@ if (echo "$COMPONENTS" | grep -E -v "(Log level|^(Sources:|Transforms:|Sinks:|)$
 fi
 
 echo "Checking that each source feature can be built without other features..."
-toml-extract ".features.sources|.[]" < Cargo.toml | check-listed-features
+extract-features sources | check-listed-features
 
 if (${CI:-false}); then
   echo "Cleaning to save some disk space"
@@ -41,7 +50,7 @@ if (${CI:-false}); then
 fi
 
 echo "Checking that each transform feature can be built without other features..."
-toml-extract ".features.transforms|.[]" < Cargo.toml | check-listed-features
+extract-features transforms | check-listed-features
 
 if (${CI:-false}); then
   echo "Cleaning to save some disk space"
@@ -49,4 +58,4 @@ if (${CI:-false}); then
 fi
 
 echo "Checking that each sink feature can be built without other features..."
-toml-extract ".features.sinks|.[]" < Cargo.toml | check-listed-features
+extract-features sinks | check-listed-features
