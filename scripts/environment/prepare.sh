ffb94353e9e9fb88692629e01c4655f07ac10800
James Turnbull <james@lovedthanlost.net>
2021-02-02 20:52:54 -0500

    Fix #5642 - build deb packages that install on Deb 8
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -8,7 +8,7 @@ rustup default "$(cat rust-toolchain)"
 rustup component add rustfmt
 rustup component add clippy
 rustup target add wasm32-wasi
-rustup run stable cargo install cargo-deb --version '^1.28.0'
+rustup run stable cargo install cargo-deb --git https://github.com/mmstick/cargo-deb.git
 
 cd scripts
 bundle update --bundler
