06bdd5f937c1a427131ceebab6c6d0051bd2dc65
Lee Benson <lee@leebenson.com>
2021-02-04 14:06:35 +0000

    log events
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/api/schema/events/log.rs
+++ src/api/schema/events/log.rs
@@ -1,14 +1,31 @@
 use crate::event;
 use async_graphql::Object;
+use serde_json::json;
 
 #[derive(Debug)]
 /// A log event
 pub struct LogEvent(event::LogEvent);
 
+impl LogEvent {
+    pub fn new(ev: event::LogEvent) -> Self {
+        Self(ev)
+    }
+}
+
 #[Object]
 impl LogEvent {
-    /// Get the log event as a string
-    async fn string(&self) -> &str {
-        "test"
+    /// Log message
+    async fn message(&self) -> Option<String> {
+        serde_json::to_string(self.0.get("message")?).ok()
+    }
+
+    /// Get the log event as a JSON string
+    async fn json(&self) -> String {
+        json!(self.0).to_string()
+    }
+
+    /// Get the log event as a YAML string
+    async fn yaml(&self) -> Option<String> {
+        serde_yaml::to_string(&self.0).ok()
     }
 }
--- src/api/schema/events/mod.rs
+++ src/api/schema/events/mod.rs
@@ -3,9 +3,9 @@ mod log;
 use crate::event::EventInspector;
 use async_graphql::{Context, Subscription};
 use async_stream::stream;
-use serde_json::json;
+use log::LogEvent;
 use std::sync::Arc;
-use tokio::stream::{self, Stream};
+use tokio::stream::Stream;
 
 #[derive(Default)]
 pub struct EventsSubscription;
@@ -13,18 +13,18 @@ pub struct EventsSubscription;
 #[Subscription]
 impl EventsSubscription {
     /// A stream of component log events
-    pub async fn events<'a>(
+    pub async fn log_events<'a>(
         &'a self,
         ctx: &'a Context<'a>,
         component_name: String,
-    ) -> impl Stream<Item = String> + 'a {
+    ) -> impl Stream<Item = LogEvent> + 'a {
         stream! {
             if let Some(mut receiver) = ctx
                 .data_unchecked::<Arc<EventInspector>>()
                 .subscribe(&component_name)
                 {
-                    while let Ok(msg) = receiver.recv().await {
-                        yield json!(msg).to_string();
+                    while let Ok(ev) = receiver.recv().await {
+                        yield LogEvent::new(ev)
                     }
                 }
         }
