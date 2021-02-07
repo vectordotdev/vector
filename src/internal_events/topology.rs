94778dcced3462b117c4987932fa8de3f51a2b85
ktf <krunotf@gmail.com>
2021-02-07 20:03:47 +0100

    Add events
    Signed-off-by: ktf <krunotf@gmail.com>

--- src/internal_events/topology.rs
+++ src/internal_events/topology.rs
@@ -9,3 +9,21 @@ impl InternalEvent for EventProcessed {
         counter!("processed_events_total", 1);
     }
 }
+
+#[derive(Debug)]
+pub struct EventIn;
+
+impl InternalEvent for EventIn {
+    fn emit_metrics(&self) {
+        counter!("events_in_total", 1);
+    }
+}
+
+#[derive(Debug)]
+pub struct EventOut;
+
+impl InternalEvent for EventOut {
+    fn emit_metrics(&self) {
+        counter!("events_out_total", 1);
+    }
+}
