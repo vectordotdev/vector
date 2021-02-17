2318b3098929720b7d6d56e015e604c1fafd8c10
Brian L. Troutwine <brian@troutwine.us>
2021-02-17 12:01:21 -0800

    Adjust the strength of `gauge_add` atomics
    This commit adjusts the strength of the `gauge_add` atomics used by
    `OpenGauge`. Previous implementation called `emitter` potentially `n^2 / 2`
    times, where `n` is the number of participants. Current implementation calls
    `emitter` only once after the participant CPU manages to adjust `OpenGauge`
    value, though no guarantee is made about the ordering of participant calls to
    `emitter`. That is, this commit guarantees that thread `A` will sequence its call
    to `emitter` after modification of the internal `gauge` but makes no guarantee
    about sequence of thread `B`'s parallel call to its `emitter`, relative to `A`.
    
    If the point of this code is to sequence calls to `emitter` between threads we
    can close this out and I'll make a different PR to clarify in the
    documentation.
    
    Closes #6477
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/internal_events/open.rs
+++ src/internal_events/open.rs
@@ -61,26 +61,38 @@ impl<E: Fn(usize)> Drop for OpenToken<E> {
 /// This function performs a synchronization procedure that corrects that.
 fn gauge_add(gauge: &AtomicUsize, add: isize, emitter: impl Fn(usize)) {
     // Lock-free procedure with eventual consistency
-
-    // Initialize value and acquire older writes to gauge metric.
-    let mut value = gauge.load(Ordering::Acquire);
+    //
+    // This function will emit only once the current thread has managed to
+    // modify `gauge`. Because of the Acquire/Release semantics of success used
+    // here it is guaranteed that emission will sequence after the `gauge`
+    // modification but there is not guarantee of the ordering of the call to
+    // `emitter` relative to other threads.
+    //
+    // The previous implementation of this function guaranteed a worst case
+    // emission of `n^2 / 2` times, where `n` is the number of parallel updates
+    // in progress. This was achieved by bounding the call to `emitter` in an
+    // Acquire/AcqRel pair, equivalent to Acquire / Release.
+    //
+    // Current implementation will load `value` with Relaxed semantics except in
+    // the case where `compare_exchange_weak` succeeds in which the load of
+    // `value` is converted to an Acquire fence.
+    let mut value = gauge.load(Ordering::Relaxed);
     loop {
         let new_value = (value as isize + add) as usize;
-        emitter(new_value);
-        // Try to update gauge to new value and releasing writes to gauge metric in the process.
-        // Otherwise acquire new writes to gauge metric.
-        let latest = gauge.compare_and_swap(value, new_value, Ordering::AcqRel);
-        if value == latest {
-            // Success
-            break;
+        // Try to update gauge to new value and releasing writes to gauge metric
+        // in the process.  Otherwise acquire new writes to gauge metric.
+        //
+        // When `compare_exchange_weak` returns Ok our `new_value` is now the
+        // current value in memory across all CPUs. When the return is Err we
+        // retry with the now current value.
+        match gauge.compare_exchange_weak(value, new_value, Ordering::AcqRel, Ordering::Relaxed) {
+            Ok(_) => {
+                emitter(new_value);
+                break;
+            }
+            Err(x) => value = x,
         }
-        // Try again with new value
-        value = latest;
     }
-
-    // In the worst case scenario we will emit `n^2 / 2` times when there are `n` parallel
-    // updates in proggress. This scenario has higher chance of happening during shutdown.
-    // In most cases `n` will be small, and futher limited by the number of active threads.
 }
 
 #[cfg(test)]
@@ -89,7 +101,7 @@ mod tests {
     use std::mem::drop;
     use std::thread;
 
-    /// If this failes at any run, then the algorithm in `gauge_add` is faulty.
+    /// If this fails at any run, then the algorithm in `gauge_add` is faulty.
     #[test]
     fn eventually_consistent() {
         let n = 8;
