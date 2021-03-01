vectordotdev/vector

commit 4cab9842b153ff01c8fac9ea63a5a481e4bf912c

fix(external docs): Fix cargo doc errors (#6553)
* Add make command for building Rustdoc

Signed-off-by: Luc Perkins <luc@timber.io>

* Add comment to clarify purpose of docs feature

Signed-off-by: Luc Perkins <luc@timber.io>

* Fix cargo doc errors

Signed-off-by: Luc Perkins <luc@timber.io>

Cargo.toml
@@ -276,6 +276,7 @@ default-msvc = ["api", "api-client", "leveldb", "rdkafka-cmake", "sinks", "sourc
 default-musl = ["api", "api-client", "leveldb", "rdkafka-cmake", "sinks", "sources", "transforms", "unix", "vendor-all", "vrl-cli"]
 default-no-api-client = ["api", "leveldb", "rdkafka-plain", "sinks", "sources", "transforms", "unix", "vendor-all", "vrl-cli"]
 default-no-vrl-cli = ["api", "leveldb", "rdkafka-plain", "sinks", "sources", "transforms", "unix", "vendor-all"]
+# The docs feature specifies which packages are included in the Rustdoc available at https://rustdoc.vector.dev
 docs = ["api", "sinks", "sources", "transforms"]
 
 all-logs = ["sinks-logs", "sources-logs", "transforms-logs"]

Makefile
@@ -643,6 +643,11 @@ check-kubernetes-yaml: ## Check that the generated Kubernetes YAML configs are u
 check-events: ## Check that events satisfy patterns set in https://github.com/timberio/vector/blob/master/rfcs/2020-03-17-2064-event-driven-observability.md
 	${MAYBE_ENVIRONMENT_EXEC} ./scripts/check-events.sh
 
+##@ Rustdoc
+build-rustdoc: ## Build a subset of Vector's Rustdocs (as specified by the "docs" feature)
+	# This command is mostly intended for use by the build process in timberio/vector-rustdoc
+	${MAYBE_ENVIRONMENT_EXEC} cargo doc --no-deps --no-default-features --features docs
+
 ##@ Packaging
 
 # archives

src/event/util/log/all_fields.rs
@@ -5,7 +5,7 @@ use std::{
     iter, slice,
 };
 
-/// Iterates over all paths in form "a.b[0].c[1]" in alphabetical order
+/// Iterates over all paths in form `a.b[0].c[1]` in alphabetical order
 /// and their corresponding values.
 pub fn all_fields(
     fields: &BTreeMap<String, Value>,

src/event/util/log/keys.rs
@@ -1,7 +1,7 @@
 use super::{all_fields, Value};
 use std::collections::BTreeMap;
 
-/// Iterates over all paths in form "a.b[0].c[1]" in alphabetical order.
+/// Iterates over all paths in form `a.b[0].c[1]` in alphabetical order.
 /// It is implemented as a wrapper around `all_fields` to reduce code
 /// duplication.
 pub fn keys(fields: &BTreeMap<String, Value>) -> impl Iterator<Item = String> + '_ {

src/event/util/log/path_iter.rs
@@ -9,15 +9,15 @@ lazy_static! {
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub enum PathComponent {
-    /// For example, in "a.b[0].c[2]" the keys are "a", "b", and "c".
+    /// For example, in `a.b[0].c[2]` the keys are "a", "b", and "c".
     Key(String),
-    /// For example, in "a.b[0].c[2]" the indexes are 0 and 2.
+    /// For example, in `a.b[0].c[2]` the indexes are 0 and 2.
     Index(usize),
     /// Indicates that a parsing error occurred.
     Invalid,
 }
 
-/// Iterator over components of paths specified in form "a.b[0].c[2]".
+/// Iterator over components of paths specified in form `a.b[0].c[2]`.
 pub struct PathIter<'a> {
     path: &'a str,
     chars: Chars<'a>,

src/kubernetes/debounce.rs
@@ -45,8 +45,8 @@ impl Debounce {
     }
 
     /// This function exposes the state of the debounce logic.
-    /// If this returns `false`, you shouldn't `poll` on [`debounced`], as it's
-    /// pending indefinitely.
+    /// If this returns `false`, you shouldn't `poll` on [`Self::debounced`], as
+    /// it's pending indefinitely.
     pub fn is_debouncing(&self) -> bool {
         self.sequence_start.is_some()
     }

src/kubernetes/reflector.rs
@@ -40,7 +40,7 @@ where
     <W as Watcher>::Object: Metadata<Ty = ObjectMeta> + Send,
     S: state::MaintainedWrite<Item = <W as Watcher>::Object>,
 {
-    /// Create a new [`Cache`].
+    /// Create a new [`Reflector`].
     pub fn new(
         watcher: W,
         state_writer: S,

src/kubernetes/state/delayed_delete.rs
@@ -23,7 +23,7 @@ where
     T: super::Write + Send,
     <T as super::Write>::Item: Send + Sync,
 {
-    /// Take a [`super::Write`] and return it wrapped with [`Self`].
+    /// Take a [`super::Write`] and return it wrapped with [`Writer`].
     pub fn new(inner: T, delay_for: Duration) -> Self {
         let queue = VecDeque::new();
         Self {

commit 2c9415fa0d85277e30716ea1c3ef77ca6d11adf6

chore(external docs): Build complete vector docs
I think it useful to build vector's docs with all features.

Broken off from
https://github.com/timberio/vector/pull/6553#discussion_r583105047

Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

Cargo.toml
@@ -276,8 +276,6 @@ default-msvc = ["api", "api-client", "leveldb", "rdkafka-cmake", "sinks", "sourc
 default-musl = ["api", "api-client", "leveldb", "rdkafka-cmake", "sinks", "sources", "transforms", "unix", "vendor-all", "vrl-cli"]
 default-no-api-client = ["api", "leveldb", "rdkafka-plain", "sinks", "sources", "transforms", "unix", "vendor-all", "vrl-cli"]
 default-no-vrl-cli = ["api", "leveldb", "rdkafka-plain", "sinks", "sources", "transforms", "unix", "vendor-all"]
-# The docs feature specifies which packages are included in the Rustdoc available at https://rustdoc.vector.dev
-docs = ["api", "sinks", "sources", "transforms"]
 
 all-logs = ["sinks-logs", "sources-logs", "transforms-logs"]
 all-metrics = ["sinks-metrics", "sources-metrics", "transforms-metrics"]

Makefile
@@ -644,9 +644,9 @@ check-events: ## Check that events satisfy patterns set in https://github.com/ti
 	${MAYBE_ENVIRONMENT_EXEC} ./scripts/check-events.sh
 
 ##@ Rustdoc
-build-rustdoc: ## Build a subset of Vector's Rustdocs (as specified by the "docs" feature)
+build-rustdoc: ## Build Vector's Rustdocs
 	# This command is mostly intended for use by the build process in timberio/vector-rustdoc
-	${MAYBE_ENVIRONMENT_EXEC} cargo doc --no-deps --no-default-features --features docs
+	${MAYBE_ENVIRONMENT_EXEC} cargo doc --no-deps
 
 ##@ Packaging
 