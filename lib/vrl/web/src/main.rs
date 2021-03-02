2fa6ce3bd813b614ee989875b2718b4db7e03429
Luc Perkins <luc@timber.io>
2021-03-01 21:25:44 -0800

    Add initial scaffolding for Trunk
    Signed-off-by: Luc Perkins <luc@timber.io>

--- lib/vrl/web/src/main.rs
+++ lib/vrl/web/src/main.rs
@@ -1,3 +1,36 @@
+use yew::prelude::*;
+
+struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+                <main>
+                    <h1>
+                        { "Hello from VRL!" }
+                    </h1>
+                </main>
+            </>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
