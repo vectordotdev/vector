6bca14f123a8452bd0069627eea4ac99ac2873f1
Luc Perkins <luc@timber.io>
2021-03-02 16:50:58 -0800

    Add initial wasm-bindgen scaffolding
    Signed-off-by: Luc Perkins <luc@timber.io>

---
+++ lib/vrl/wasm/src/lib.rs
@@ -0,0 +1,11 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
