0a1f06757928c3a855305ee48dc1b10856d25890
Luc Perkins <luc@timber.io>
2021-03-02 17:59:11 -0800

    Add initial JSON parsing into Wasm function
    Signed-off-by: Luc Perkins <luc@timber.io>

--- lib/vrl/wasm/src/lib.rs
+++ lib/vrl/wasm/src/lib.rs
@@ -1,11 +1,24 @@
+use serde::{Deserialize, Serialize};
+use vrl::Value;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Deserialize, Serialize)]
+struct Input {
+    program: String,
+    event: Value,
 }
 
+fn vrl_resolve(input: Input) -> Result<Value, String> {
+    Ok(input.event)
+}
+
+
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn resolve(incoming: &JsValue) -> JsValue {
+    let input: Input = incoming.into_serde().unwrap();
+
+    match vrl_resolve(input) {
+        Ok(event) => JsValue::from_str(&event.to_string()),
+        Err(err) => JsValue::from_str(&err.to_string())
+    }
 }
