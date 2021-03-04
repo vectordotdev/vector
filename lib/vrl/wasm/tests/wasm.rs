f2f8fa76c1ac7af2fa272fdb941f65e48d120353
Luc Perkins <luc@timber.io>
2021-03-04 15:47:41 -0800

    Add more involved test
    Signed-off-by: Luc Perkins <luc@timber.io>

--- lib/vrl/wasm/tests/wasm.rs
+++ lib/vrl/wasm/tests/wasm.rs
@@ -1,45 +1,42 @@
-use std::collections::BTreeMap;
-use vrl::Value;
+use indoc::indoc;
+use vrl::{value, Value};
 use vrl_wasm::{resolve, Input, VrlCompileResult};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
 
-macro_rules! to_json {
-    ($v:tt) => {
-        JsValue::from_serde(&$v).unwrap()
-    }
-}
-
-macro_rules! from_json {
-    ($v:tt) => {
-        $v.into_serde().unwrap()
-    }
-}
-
-macro_rules! test {
-    ($program:tt, $event:tt, $expected:tt) => {
-        let input = Input::new($program, $event);
-        let input_js = to_json!(input);
-        let result_js = resolve(&input_js);
-        let result: VrlCompileResult = from_json!(result_js);
-        assert_eq!(result.result, $expected);
-    }
-}
-
 #[wasm_bindgen_test]
-fn vrl_resolve() {
-    let mut event: BTreeMap<String, Value> = BTreeMap::new();
-    event.insert("bar".to_owned(), "baz".to_owned().into());
-
-    let mut expected: BTreeMap<String, Value> = BTreeMap::new();
-    expected.insert("bar".to_owned(), "baz".to_owned().into());
-    expected.insert("foo".to_owned(), "bar".to_owned().into());
-
-    let cases: Vec<(&str, Value, Value)> = vec![
-        (r#".foo = "bar""#, Value::Object(event), Value::Object(expected)),
+fn vrl_resolve_success() {
+    let cases: Vec<(&str, Value, Value, Value)> = vec![
+        (
+            ".",
+            value!({}),
+            value!({}),
+            value!({}),
+        ),
+        (
+            r#".foo = "bar""#, // Program
+            value!("bar"), // Expected output
+            value!({"bar": "baz"}), // Input event
+            value!({"foo": "bar", "bar": "baz"}) // Expected output event
+        ),
+        (
+            indoc! {r#"
+                del(.number)
+                .quote = upcase(string!(.quote))
+                .boolean = false
+            "#},
+            value!(false),
+            value!({"number": 37, "boolean": true, "quote": "testing"}),
+            value!({"boolean": false, "quote": "TESTING"}),
+        ),
     ];
 
-    for (program, event, expected) in cases {
-        test!(program, event, expected);
+    for (program, expected_output, event, expected_event) in cases {
+        let input = Input::new(program, event);
+        let input_js = JsValue::from_serde(&input).unwrap();
+        let result_js = resolve(&input_js);
+        let result: VrlCompileResult = result_js.into_serde().unwrap();
+        assert_eq!(result.result, expected_event);
+        assert_eq!(result.output, expected_output);
     }
 }
