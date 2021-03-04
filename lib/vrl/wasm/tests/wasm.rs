1cc50f7f18514d14cd38659a78f654025f9c7d1f
Luc Perkins <luc@timber.io>
2021-03-04 15:17:04 -0800

    Add helper macros for testing
    Signed-off-by: Luc Perkins <luc@timber.io>

--- lib/vrl/wasm/tests/wasm.rs
+++ lib/vrl/wasm/tests/wasm.rs
@@ -4,6 +4,28 @@ use vrl_wasm::{resolve, Input, VrlCompileResult};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
 
+macro_rules! to_json {
+    ($v:tt) => {
+        JsValue::from_serde(&$v).unwrap()
+    }
+}
+
+macro_rules! from_json {
+    ($v:tt) => {
+        $v.into_serde().unwrap()
+    }
+}
+
+macro_rules! test {
+    ($program:tt, $event:tt, $expected:tt) => {
+        let input = Input::new($program, $event);
+        let input_js = to_json!(input);
+        let result_js = resolve(&input_js);
+        let result: VrlCompileResult = from_json!(result_js);
+        assert_eq!(result.result, $expected);
+    }
+}
+
 #[wasm_bindgen_test]
 fn vrl_resolve() {
     let mut event: BTreeMap<String, Value> = BTreeMap::new();
@@ -17,11 +39,7 @@ fn vrl_resolve() {
         (r#".foo = "bar""#, Value::Object(event), Value::Object(expected)),
     ];
 
-    for (program, event, expected_event) in cases {
-        let input = Input::new(program, event);
-        let input_js = JsValue::from_serde(&input).unwrap();
-        let result_js = resolve(&input_js);
-        let result: VrlCompileResult = result_js.into_serde().unwrap();
-        assert_eq!(result.result, expected_event);
+    for (program, event, expected) in cases {
+        test!(program, event, expected);
     }
 }
