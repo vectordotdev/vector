77e003b2974a1f17d38c13ed2598c4223cc64739
Jesse Szwedko <jesse@szwedko.me>
2021-03-08 16:48:22 -0400

    Revert "chore: Extract global allocator into a crate (#6181)" (#6670)
    This broke the integration tests since they don't specify an allocator.
    As I was fixing this, I started to come around to the idea that maybe it
    is fine to just default to the system allocator and override for benches
    so reverting to revisit this.
    
    This reverts commit 25b1c59649b24a281673a5c404bd80c74c9da008.
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- lib/vector-global-alloc/tests/ui/conflict.rs
+++
@@ -1,9 +0,0 @@
-//! Shared UI test to ensure only one global allocator can be defined.
-
-extern crate vector_global_alloc;
-
-// Should conflict with the declaration from the `vector_global_alloc`.
-#[global_allocator]
-static ALLOC: std::alloc::System = std::alloc::System;
-
-fn main() {}
