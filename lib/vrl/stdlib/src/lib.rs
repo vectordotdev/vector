25a7bd9d243c8c23bd524d1f1b7873e09c4dc2fb
FungusHumungus <fungus.humungus@gmail.com>
2021-03-08 14:48:58 +0000

    enhancement(remap): added the parse_logfmt remap function (#6659)
    * Added the parse_logfmt remap function.
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>
    
    * Updated docs
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- lib/vrl/stdlib/src/lib.rs
+++ lib/vrl/stdlib/src/lib.rs
@@ -96,6 +96,8 @@ mod parse_grok;
 mod parse_json;
 #[cfg(feature = "parse_key_value")]
 mod parse_key_value;
+#[cfg(feature = "parse_logfmt")]
+mod parse_logfmt;
 #[cfg(feature = "parse_regex")]
 mod parse_regex;
 #[cfg(feature = "parse_regex_all")]
@@ -257,6 +259,8 @@ pub use parse_grok::ParseGrok;
 pub use parse_json::ParseJson;
 #[cfg(feature = "parse_key_value")]
 pub use parse_key_value::ParseKeyValue;
+#[cfg(feature = "parse_logfmt")]
+pub use parse_logfmt::ParseLogFmt;
 #[cfg(feature = "parse_regex")]
 pub use parse_regex::ParseRegex;
 #[cfg(feature = "parse_regex_all")]
@@ -420,6 +424,8 @@ pub fn all() -> Vec<Box<dyn vrl::Function>> {
         Box::new(ParseCommonLog),
         #[cfg(feature = "parse_key_value")]
         Box::new(ParseKeyValue),
+        #[cfg(feature = "parse_logfmt")]
+        Box::new(ParseLogFmt),
         #[cfg(feature = "parse_regex")]
         Box::new(ParseRegex),
         #[cfg(feature = "parse_regex_all")]
