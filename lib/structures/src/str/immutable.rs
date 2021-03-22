ed203b7c57fd0f6f0b6d5648aa2b80d8779dfb08
Brian L. Troutwine <brian@troutwine.us>
2021-03-22 17:02:34 +0000

    Update lib/structures/src/str/immutable.rs
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    Co-authored-by: Bruce Guenter <bruce@untroubled.org>

--- lib/structures/src/str/immutable.rs
+++ lib/structures/src/str/immutable.rs
@@ -1,7 +1,7 @@
 /// An immutable string
 ///
 /// This `String` differs from the basic Rust `String` in that it is not mutable
-/// -- once it's created it's created -- and as a result it cannot change
+/// -- once it's created it's fixed -- and as a result it cannot change
 /// size. It consumes 2*usize words overhead where `std::string::String` has
 /// 3*usize.
 pub type String = Box<str>;
