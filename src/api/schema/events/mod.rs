6d80f8eeb42697af5750c2fbccca71fab26c87a0
Lee Benson <lee@leebenson.com>
2021-03-31 20:02:01 +0100

    TapSink sink
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/api/schema/events/mod.rs
+++ src/api/schema/events/mod.rs
@@ -6,7 +6,7 @@ mod output;
 use encoding::EventEncodingType;
 use output::OutputEventsPayload;
 
-use crate::{api::tap::TapSink, topology::WatchRx};
+use crate::{api::tap::TapController, topology::WatchRx};
 
 use async_graphql::{validators::IntRange, Context, Subscription};
 use futures::StreamExt;
@@ -53,9 +53,9 @@ fn create_events_stream(
     let (mut event_tx, event_rx) = mpsc::channel::<Vec<OutputEventsPayload>>(10);
 
     tokio::spawn(async move {
-        // Create a tap sink. When this drops out of scope, clean up will be performed on the
-        // event handlers and topology observation that the tap sink provides.
-        let _tap_sink = TapSink::new(watch_rx, tap_tx, &component_names);
+        // Create a tap controller. When this drops out of scope, clean up will be performed on the
+        // event handlers and topology observation that the tap controller provides.
+        let _tap_controller = TapController::new(watch_rx, tap_tx, &component_names);
 
         // A tick interval to represent when to 'cut' the results back to the client.
         let mut interval = time::interval(time::Duration::from_millis(interval));
