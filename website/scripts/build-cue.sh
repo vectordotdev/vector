d506ab2565841b5994065acbdb07dbf33a5b59d9
Luc Perkins <luc.perkins@datadoghq.com>
2021-04-05 13:20:11 -0700

    Add gitkeep to data dir and add package.json
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/scripts/build-cue.sh
+++ website/scripts/build-cue.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-cue version
-
 ROOT="$(git rev-parse --show-toplevel)"
 DOCS_DIR="${ROOT}/docs"
 SITE_DIR="${ROOT}/website"
 DATA_DIR="${SITE_DIR}/data"
 JSON_OUT="${DATA_DIR}/docs.json"
 
-mkdir -p "${DATA_DIR}"
+# Display the CUE version for CI debugging purposes
+cue version
 
+# The docs JSON file needs to be removed or else CUE errors
 rm "${JSON_OUT}" 2> /dev/null
 
+# Build the docs JSON object out of the CUE sources
 find "${DOCS_DIR}" -name "*.cue" | xargs cue export --all-errors "$@" --outfile "${JSON_OUT}"
