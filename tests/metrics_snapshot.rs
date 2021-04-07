f53f89bf3db3a0bc27fadf5747339cf74346eba7
Brian L. Troutwine <brian@troutwine.us>
2021-04-07 15:29:57 -0700

    cardinality always include cardinality itself
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- tests/metrics_snapshot.rs
+++ tests/metrics_snapshot.rs
@@ -7,7 +7,10 @@ fn prepare_metrics(cardinality: usize) -> &'static vector::metrics::Controller {
         metrics::counter!("test", 1, "idx" => format!("{}", idx));
     }
 
-    assert_cardinality_matches(&vector::metrics::capture_metrics(controller), cardinality);
+    assert_cardinality_matches(
+        &vector::metrics::capture_metrics(controller),
+        cardinality + 1,
+    );
 
     controller
 }
@@ -21,6 +24,6 @@ fn cardinality_matches() {
     for cardinality in &[0, 1, 10, 100, 1000, 10000] {
         let controller = prepare_metrics(*cardinality);
         let iter = vector::metrics::capture_metrics(controller);
-        assert_cardinality_matches(&iter, *cardinality);
+        assert_cardinality_matches(&iter, *cardinality + 1);
     }
 }
