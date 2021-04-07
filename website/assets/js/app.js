98c59015b02f0e0f60ea8882db211c3509a1ba24
Luc Perkins <luc.perkins@datadoghq.com>
2021-04-06 17:43:10 -0700

    Add Tocbot support
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,3 +1,11 @@
 import 'alpinejs';
+import 'tocbot/dist/tocbot';
+
+tocbot.init({
+  tocSelector: '#docs-toc',
+  contentSelector: '#docs-content',
+  headingSelector: 'h1, h2, h3',
+  ignoreSelector: 'no-toc'
+});
 
 console.log('Welcome to the Vector website and documentation!');
