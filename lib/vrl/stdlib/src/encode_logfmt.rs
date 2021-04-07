0064966ba3edc0a23f43d056e93e23014585e118
Mathieu Stefani <auquetal@gmail.com>
2021-04-07 14:59:31 +0200

    remove key-value pair argument type and introduce an optional `fields_ordering` parameter to guarantee ordering of fields
    Signed-off-by: Mathieu Stefani <auquetal@gmail.com>

--- lib/vrl/stdlib/src/encode_logfmt.rs
+++ lib/vrl/stdlib/src/encode_logfmt.rs
@@ -1,45 +1,7 @@
 use vrl::prelude::*;
 
-#[derive(Clone, Copy, Debug)]
-pub struct EncodeLogfmt;
-
-impl Function for EncodeLogfmt {
-    fn identifier(&self) -> &'static str {
-        "encode_logfmt"
-    }
-
-    fn parameters(&self) -> &'static [Parameter] {
-        &[Parameter {
-            keyword: "value",
-            kind: kind::OBJECT | kind::ARRAY,
-            required: true,
-        }]
-    }
-
-    fn compile(&self, mut arguments: ArgumentList) -> Compiled {
-        let value = arguments.required("value");
-
-        Ok(Box::new(EncodeLogfmtFn { value }))
-    }
-
-    fn examples(&self) -> &'static [Example] {
-        &[
-            Example {
-                title: "encode object",
-                source: r#"encode_logfmt({"lvl": "info", "msg": "This is a message", "log_id": 12345})"#,
-                result: Ok(r#"s'log_id=12345 lvl=info msg="This is a message"'"#),
-            },
-            Example {
-                title: "encode array",
-                source: r#"encode_logfmt([["lvl", "info"], ["msg", "This is a message"], ["log_id", 12345]])"#,
-                result: Ok(r#"s'lvl=info msg="This is a message" log_id=12345'"#),
-            },
-        ]
-    }
-}
-
 mod logfmt {
-    use std::collections::BTreeMap;
+    use std::collections::{BTreeMap, HashSet};
     use std::fmt::{self, Write};
     use std::result::Result;
 
@@ -94,63 +56,114 @@ mod logfmt {
         encode_value(output, value)
     }
 
-    pub fn encode_object(input: &BTreeMap<String, Value>) -> Result<String, String> {
+    pub fn encode(input: &BTreeMap<String, Value>, fields: &[String]) -> Result<String, String> {
         let mut output = String::new();
+        let mut seen_fields = HashSet::new();
+
+        for (idx, field) in fields.iter().enumerate() {
+            if let Some(val) = input.get(field) {
+                if idx > 0 {
+                    output.write_char(' ').map_err(|_| "write error")?;
+                }
+                encode_field(&mut output, field, val).map_err(|_| "write error")?;
+                seen_fields.insert(field);
+            }
+        }
 
         for (idx, (key, value)) in input.iter().enumerate() {
-            if idx > 0 {
-                output.write_char(' ').map_err(|_| "write error")?;
+            if seen_fields.contains(key) {
+                continue;
             }
 
+            if idx > 0 || seen_fields.len() > 0 {
+                output.write_char(' ').map_err(|_| "write error")?;
+            }
             encode_field(&mut output, key, value).map_err(|_| "write error")?;
         }
 
         Ok(output)
     }
+}
 
-    pub fn encode_array(input: &Vec<Value>) -> std::result::Result<String, String> {
-        let mut output = String::new();
+#[derive(Clone, Copy, Debug)]
+pub struct EncodeLogfmt;
 
-        for (idx, value) in input.iter().enumerate() {
-            if idx > 0 {
-                output.write_char(' ').map_err(|_| "write error")?;
-            }
+impl Function for EncodeLogfmt {
+    fn identifier(&self) -> &'static str {
+        "encode_logfmt"
+    }
 
-            match value {
-                Value::Array(arr) if arr.len() == 2 => {
-                    let (key, value) = (&arr[0], &arr[1]);
-                    if let Value::Bytes(b) = key {
-                        let key_str = String::from_utf8_lossy(b);
-                        encode_field(&mut output, &key_str, value).map_err(|_| "write error")?;
-                    } else {
-                        return Err(format!("invalid key type at index {} ({})", idx, key));
-                    }
-                }
-                _ => {
-                    return Err(format!(
-                        "invalid key-value pair at index {} ({})",
-                        idx, value
-                    ))
-                }
-            }
-        }
+    fn parameters(&self) -> &'static [Parameter] {
+        &[
+            Parameter {
+                keyword: "value",
+                kind: kind::OBJECT,
+                required: true,
+            },
+            Parameter {
+                keyword: "fields_ordering",
+                kind: kind::ARRAY,
+                required: false,
+            },
+        ]
+    }
 
-        Ok(output)
+    fn compile(&self, mut arguments: ArgumentList) -> Compiled {
+        let value = arguments.required("value");
+        let fields = arguments.optional("fields_ordering");
+
+        Ok(Box::new(EncodeLogfmtFn { value, fields }))
+    }
+
+    fn examples(&self) -> &'static [Example] {
+        &[
+            Example {
+                title: "encode object",
+                source: r#"encode_logfmt!({"lvl": "info", "msg": "This is a message", "log_id": 12345})"#,
+                result: Ok(r#"s'log_id=12345 lvl=info msg="This is a message"'"#),
+            },
+            Example {
+                title: "encode object with fields ordering",
+                source: r#"encode_logfmt!({"msg": "This is a message", "lod_id": 12345, "lvl": "info"}, ["lvl", "msg"])"#,
+                result: Ok(r#"s'lvl=info msg="This is a message" log_id=12345'"#),
+            },
+        ]
     }
 }
 
 #[derive(Clone, Debug)]
 struct EncodeLogfmtFn {
     value: Box<dyn Expression>,
+    fields: Option<Box<dyn Expression>>,
+}
+
+fn resolve_fields(fields: &Value) -> Vec<String> {
+    match fields {
+        Value::Array(arr) => arr
+            .iter()
+            .filter_map(|v| match v {
+                Value::Bytes(bytes) => Some(String::from_utf8_lossy(&bytes)),
+                _ => None,
+            })
+            .map(Into::into)
+            .collect(),
+        _ => vec![],
+    }
 }
 
 impl Expression for EncodeLogfmtFn {
     fn resolve(&self, ctx: &mut Context) -> Resolved {
         let value = self.value.resolve(ctx)?;
+        let fields = match &self.fields {
+            None => vec![],
+            Some(expr) => match expr.resolve(ctx) {
+                Ok(val) => resolve_fields(&val),
+                Err(_) => vec![],
+            },
+        };
 
         let logfmt = match value {
-            Value::Object(map) => logfmt::encode_object(&map),
-            Value::Array(arr) => logfmt::encode_array(&arr),
+            Value::Object(map) => logfmt::encode(&map, &fields[..]),
             _ => Err("unsupported value-type".into()),
         };
 
@@ -167,106 +180,62 @@ impl Expression for EncodeLogfmtFn {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use shared::btreemap;
 
     test_function![
         encode_logfmt  => EncodeLogfmt;
 
-        array_with_single_element_type {
-            args: func_args![value: value!(
-                      vec![value!(vec!["lvl", "info"])]
-                      )
-                    ],
+        single_element {
+            args: func_args![value:
+                btreemap! {
+                    "lvl" => "info"
+                }
+            ],
             want: Ok("lvl=info"),
             tdef: TypeDef::new().bytes().fallible(),
         }
 
-        array_with_multiple_element_types {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("lvl"), value!("info")]),
-                          value!(vec![value!("log_id"), value!(12345)]),
-                      ]
-                  )],
-            want: Ok("lvl=info log_id=12345"),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        array_with_string_and_spaces {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("lvl"), value!("info")]),
-                          value!(vec![value!("msg"), value!("This is a log message")]),
-                      ]
-                  )],
-            want: Ok(r#"lvl=info msg="This is a log message""#),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        array_with_string_and_characters_to_escape {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("lvl"), value!("info")]),
-                          value!(vec![value!("msg"), value!(r#"payload: {"code": 200}\n"#)]),
-                      ]
-                  )],
-            want: Ok(r#"lvl=info msg="payload: {\"code\": 200}\\n""#),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        array_with_too_many_items_in_key_pair_error {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("lvl"), value!("info"), value!("error")]),
-                          value!(vec![value!("log_id"), value!(12345)]),
-                      ]
-                  )],
-            want: Err(r#"failed to encode logfmt: invalid key-value pair at index 0 (["lvl", "info", "error"])"#),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        array_with_missing_items_key_pair_error {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("log_id"), value!(12345)]),
-                          value!(vec![value!("lvl")]),
-                      ]
-                  )],
-            want: Err(r#"failed to encode logfmt: invalid key-value pair at index 1 (["lvl"])"#),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        array_with_invalid_key_type_error {
-            args: func_args![value: value!(
-                      vec![
-                          value!(vec![value!("lvl"), value!("info")]),
-                          value!(vec![value!(10), value!(20)]),
-                      ]
-                  )],
-            want: Err("failed to encode logfmt: invalid key type at index 1 (10)"),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        map_with_single_element_type {
-            args: func_args![value: value!(map!["lvl": value!("info")])],
-            want: Ok("lvl=info"),
-            tdef: TypeDef::new().bytes().fallible(),
-        }
-
-        map_with_multiple_element_types {
-            args: func_args![value: value!(map!["lvl": value!("info"), "log_id": value!(12345)])],
+        multiple_elements {
+            args: func_args![value:
+                btreemap! {
+                    "lvl" => "info",
+                    "log_id" => 12345
+                }
+            ],
             want: Ok("log_id=12345 lvl=info"),
             tdef: TypeDef::new().bytes().fallible(),
         }
 
-        map_with_string_and_spaces {
-            args: func_args![value: value!(map!["lvl": value!("info"), "msg": value!("This is a log message")])],
+        string_with_spaces {
+            args: func_args![value:
+                btreemap! {
+                    "lvl" => "info",
+                    "msg" => "This is a log message"
+                }],
             want: Ok(r#"lvl=info msg="This is a log message""#),
             tdef: TypeDef::new().bytes().fallible(),
         }
 
-        map_with_string_and_characters_to_escape {
-            args: func_args![value: value!(map!["lvl": value!("info"), "msg": value!(r#"payload: {"code": 200}\n"#)])],
+        string_with_characters_to_escape {
+            args: func_args![value:
+                btreemap! {
+                    "lvl" => "info",
+                    "msg" => r#"payload: {"code": 200}\n"#
+                }],
             want: Ok(r#"lvl=info msg="payload: {\"code\": 200}\\n""#),
+            tdef: TypeDef::new().bytes().fallible(),
+        }
+
+        fields_ordering {
+            args: func_args![value:
+                btreemap! {
+                    "lvl" => "info",
+                    "msg" => "This is a log message",
+                    "log_id" => 12345,
+                },
+                fields_ordering: value!(["lvl", "msg"])
+            ],
+            want: Ok(r#"lvl=info msg="This is a log message" log_id=12345"#),
             tdef: TypeDef::new().bytes().fallible(),
         }
     ];
