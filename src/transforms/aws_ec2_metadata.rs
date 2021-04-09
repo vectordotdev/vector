vectordotdev/vector

commit 0135730ee37bc4113c03db5fb8db008a44ea997a

chore(data model): Rework tests to properly handle metadata (#6931)
* Use EventMetadata::default everywhere to create metadata

* Rework tests to ensure metadata is passed through

* Ensure merge transform handles metadata

* Update the reduce transform to handle metadata

* Drop `impl IntoIterator for LogEvent`

It no longer makes sense to convert an event directly into an iterator,
as that will implicitly drop the metadata. This has to go through
`deconstruct` first, which will at least mark the points where this
metadata is moved.

Signed-off-by: Bruce Guenter <bruce.guenter@datadoghq.com>

src/transforms/aws_ec2_metadata.rs
@@ -511,7 +511,7 @@ enum Ec2MetadataError {
 #[cfg(test)]
 mod integration_tests {
     use super::*;
-    use crate::{config::GlobalOptions, event::Event, test_util::trace_init};
+    use crate::{config::GlobalOptions, event::LogEvent, test_util::trace_init};
     use futures::{SinkExt, StreamExt};
 
     const HOST: &str = "http://localhost:8111";
@@ -541,27 +541,26 @@ mod integration_tests {
         // We need to sleep to let the background task fetch the data.
         sleep(Duration::from_secs(1)).await;
 
-        let event = Event::new_empty_log();
-        tx.send(event).await.unwrap();
+        let log = LogEvent::default();
+        let mut expected = log.clone();
+        tx.send(log.into()).await.unwrap();
 
         let event = stream.next().await.unwrap();
-        let log = event.as_log();
 
-        assert_eq!(log.get("availability-zone"), Some(&"ww-region-1a".into()));
-        assert_eq!(log.get("public-ipv4"), Some(&"192.1.1.1".into()));
-        assert_eq!(
-            log.get("public-hostname"),
-            Some(&"mock-public-hostname".into())
-        );
-        assert_eq!(log.get(&"local-ipv4"), Some(&"192.1.1.2".into()));
-        assert_eq!(log.get("local-hostname"), Some(&"mock-hostname".into()));
-        assert_eq!(log.get("instance-id"), Some(&"i-096fba6d03d36d262".into()));
-        assert_eq!(log.get("ami-id"), Some(&"ami-05f27d4d6770a43d2".into()));
-        assert_eq!(log.get("instance-type"), Some(&"t2.micro".into()));
-        assert_eq!(log.get("region"), Some(&"us-east-1".into()));
-        assert_eq!(log.get("vpc-id"), Some(&"mock-vpc-id".into()));
-        assert_eq!(log.get("subnet-id"), Some(&"mock-subnet-id".into()));
-        assert_eq!(log.get("role-name[0]"), Some(&"mock-user".into()));
+        expected.insert("availability-zone", "ww-region-1a");
+        expected.insert("public-ipv4", "192.1.1.1");
+        expected.insert("public-hostname", "mock-public-hostname");
+        expected.insert("local-ipv4", "192.1.1.2");
+        expected.insert("local-hostname", "mock-hostname");
+        expected.insert("instance-id", "i-096fba6d03d36d262");
+        expected.insert("ami-id", "ami-05f27d4d6770a43d2");
+        expected.insert("instance-type", "t2.micro");
+        expected.insert("region", "us-east-1");
+        expected.insert("vpc-id", "mock-vpc-id");
+        expected.insert("subnet-id", "mock-subnet-id");
+        expected.insert("role-name[0]", "mock-user");
+
+        assert_eq!(event.into_log(), expected);
     }
 
     #[tokio::test]
@@ -583,21 +582,15 @@ mod integration_tests {
         // We need to sleep to let the background task fetch the data.
         sleep(Duration::from_secs(1)).await;
 
-        let event = Event::new_empty_log();
-        tx.send(event).await.unwrap();
+        let log = LogEvent::default();
+        let mut expected = log.clone();
+        tx.send(log.into()).await.unwrap();
 
         let event = stream.next().await.unwrap();
-        let log = event.as_log();
 
-        assert_eq!(log.get("availability-zone"), None);
-        assert_eq!(log.get("public-ipv4"), Some(&"192.1.1.1".into()));
-        assert_eq!(log.get("public-hostname"), None);
-        assert_eq!(log.get("local-ipv4"), None);
-        assert_eq!(log.get("local-hostname"), None);
-        assert_eq!(log.get("instance-id"), None,);
-        assert_eq!(log.get("instance-type"), None,);
-        assert_eq!(log.get("ami-id"), None);
-        assert_eq!(log.get("region"), Some(&"us-east-1".into()));
+        expected.insert("public-ipv4", "192.1.1.1");
+        expected.insert("region", "us-east-1");
+        assert_eq!(event.into_log(), expected);
     }
 
     #[tokio::test]
@@ -620,20 +613,15 @@ mod integration_tests {
             // We need to sleep to let the background task fetch the data.
             sleep(Duration::from_secs(1)).await;
 
-            let event = Event::new_empty_log();
-            tx.send(event).await.unwrap();
+            let log = LogEvent::default();
+            let mut expected = log.clone();
+            tx.send(log.into()).await.unwrap();
 
             let event = stream.next().await.unwrap();
-            let log = event.as_log();
 
-            assert_eq!(
-                log.get("ec2.metadata.availability-zone"),
-                Some(&"ww-region-1a".into())
-            );
-            assert_eq!(
-                log.get("ec2.metadata.public-ipv4"),
-                Some(&"192.1.1.1".into())
-            );
+            expected.insert("ec2.metadata.availability-zone", "ww-region-1a");
+            expected.insert("ec2.metadata.public-ipv4", "192.1.1.1");
+            assert_eq!(event.into_log(), expected);
         }
 
         {
@@ -655,14 +643,15 @@ mod integration_tests {
             // We need to sleep to let the background task fetch the data.
             sleep(Duration::from_secs(1)).await;
 
-            let event = Event::new_empty_log();
-            tx.send(event).await.unwrap();
+            let log = LogEvent::default();
+            let mut expected = log.clone();
+            tx.send(log.into()).await.unwrap();
 
             let event = stream.next().await.unwrap();
-            let log = event.as_log();
 
-            assert_eq!(log.get("availability-zone"), Some(&"ww-region-1a".into()));
-            assert_eq!(log.get("public-ipv4"), Some(&"192.1.1.1".into()));
+            expected.insert("availability-zone", "ww-region-1a");
+            expected.insert("public-ipv4", "192.1.1.1");
+            assert_eq!(event.into_log(), expected);
         }
     }
 }

lib/prometheus-parser/Cargo.toml
@@ -14,10 +14,8 @@ nom = "6.0.1"
 num_enum = "0.5.1"
 prost = "0.7.0"
 prost-types = "0.7.0"
+shared = { path = "../shared", features = ["btreemap"] }
 snafu = { version = "0.6" }
 
 [build-dependencies]
 prost-build = "0.7.0"
-
-[dev-dependencies]
-shared = { path = "../shared", features = ["btreemap"] }

lib/prometheus-parser/src/lib.rs
@@ -58,6 +58,8 @@ pub enum ParserError {
     RequestNoNameLabel,
 }
 
+shared::impl_event_data_eq!(ParserError);
+
 #[derive(Debug, Eq, Hash, PartialEq)]
 pub struct GroupKey {
     pub timestamp: Option<i64>,

lib/shared/src/event_data_eq.rs
@@ -42,13 +42,19 @@ macro_rules! impl_event_data_eq {
     };
 }
 
-impl<T: EventDataEq> EventDataEq for Vec<T> {
+impl<T: EventDataEq> EventDataEq for &[T] {
     fn event_data_eq(&self, other: &Self) -> bool {
         self.len() == other.len()
             && self
                 .iter()
                 .zip(other.iter())
                 .all(|(a, b)| a.event_data_eq(b))
+    }
+}
+
+impl<T: EventDataEq> EventDataEq for Vec<T> {
+    fn event_data_eq(&self, other: &Self) -> bool {
+        self.as_slice().event_data_eq(&other.as_slice())
     }
 }
 

src/event/log_event.rs
@@ -20,6 +20,17 @@ pub struct LogEvent {
 }
 
 impl LogEvent {
+    pub fn new_with_metadata(metadata: EventMetadata) -> Self {
+        Self {
+            fields: Default::default(),
+            metadata,
+        }
+    }
+
+    pub fn into_parts(self) -> (BTreeMap<String, Value>, EventMetadata) {
+        (self.fields, self.metadata)
+    }
+
     #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn get(&self, key: impl AsRef<str>) -> Option<&Value> {
         util::log::get(&self.fields, key.as_ref())
@@ -194,7 +205,7 @@ impl From<BTreeMap<String, Value>> for LogEvent {
     fn from(map: BTreeMap<String, Value>) -> Self {
         LogEvent {
             fields: map,
-            metadata: EventMetadata,
+            metadata: EventMetadata::default(),
         }
     }
 }
@@ -209,7 +220,7 @@ impl From<HashMap<String, Value>> for LogEvent {
     fn from(map: HashMap<String, Value>) -> Self {
         LogEvent {
             fields: map.into_iter().collect(),
-            metadata: EventMetadata,
+            metadata: EventMetadata::default(),
         }
     }
 }
@@ -273,19 +284,9 @@ where
 // Allow converting any kind of appropriate key/value iterator directly into a LogEvent.
 impl<K: AsRef<str>, V: Into<Value>> FromIterator<(K, V)> for LogEvent {
     fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
-        let mut log_event = LogEvent::default();
+        let mut log_event = Self::default();
         log_event.extend(iter);
         log_event
-    }
-}
-
-/// Converts event into an iterator over top-level key/value pairs.
-impl IntoIterator for LogEvent {
-    type Item = (String, Value);
-    type IntoIter = std::collections::btree_map::IntoIter<String, Value>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.fields.into_iter()
     }
 }
 
@@ -631,7 +632,7 @@ mod test {
                 vrl::Target::insert(&mut event, &path, value.clone()),
                 result
             );
-            assert_eq!(event, expect);
+            shared::assert_event_data_eq!(event, expect);
             assert_eq!(vrl::Target::get(&event, &path), Ok(Some(value)));
         }
     }

src/event/metric.rs
@@ -21,7 +21,7 @@ pub struct Metric {
     #[serde(flatten)]
     pub data: MetricData,
     #[getset(get = "pub")]
-    #[serde(skip)]
+    #[serde(skip_serializing, default = "EventMetadata::default")]
     metadata: EventMetadata,
 }
 
@@ -233,6 +233,15 @@ pub enum StatisticKind {
 
 impl Metric {
     pub fn new<T: Into<String>>(name: T, kind: MetricKind, value: MetricValue) -> Self {
+        Self::new_with_metadata(name, kind, value, EventMetadata::default())
+    }
+
+    pub fn new_with_metadata<T: Into<String>>(
+        name: T,
+        kind: MetricKind,
+        value: MetricValue,
+        metadata: EventMetadata,
+    ) -> Self {
         Self {
             series: MetricSeries {
                 name: MetricName {
@@ -246,7 +255,7 @@ impl Metric {
                 kind,
                 value,
             },
-            metadata: EventMetadata,
+            metadata,
         }
     }
 
@@ -275,7 +284,7 @@ impl Metric {
         Self {
             series: self.series,
             data: self.data.into_absolute(),
-            metadata: EventMetadata,
+            metadata: self.metadata,
         }
     }
 
@@ -284,7 +293,7 @@ impl Metric {
         Self {
             series: self.series,
             data: self.data.into_incremental(),
-            metadata: EventMetadata,
+            metadata: self.metadata,
         }
     }
 
@@ -372,7 +381,7 @@ impl Metric {
         Self {
             series: self.series.clone(),
             data: self.data.zero(),
-            metadata: EventMetadata,
+            metadata: self.metadata.clone(),
         }
     }
 }
@@ -913,6 +922,7 @@ fn write_word(fmt: &mut Formatter<'_>, word: &str) -> Result<(), fmt::Error> {
 mod test {
     use super::*;
     use chrono::{offset::TimeZone, DateTime, Utc};
+    use pretty_assertions::assert_eq;
     use shared::btreemap;
     use std::str::FromStr;
     use vrl::{Path, Value};
@@ -948,16 +958,13 @@ mod test {
         .with_tags(Some(tags()))
         .with_timestamp(Some(ts()));
 
+        let expected = counter
+            .clone()
+            .with_value(MetricValue::Counter { value: 3.0 })
+            .with_timestamp(Some(ts()));
+
         counter.data.add(&delta.data);
-        assert_eq!(
-            counter,
-            Metric::new(
-                "counter",
-                MetricKind::Incremental,
-                MetricValue::Counter { value: 3.0 },
-            )
-            .with_timestamp(Some(ts()))
-        )
+        assert_eq!(counter, expected);
     }
 
     #[test]
@@ -977,16 +984,13 @@ mod test {
         .with_tags(Some(tags()))
         .with_timestamp(Some(ts()));
 
+        let expected = gauge
+            .clone()
+            .with_value(MetricValue::Gauge { value: -1.0 })
+            .with_timestamp(Some(ts()));
+
         gauge.data.add(&delta.data);
-        assert_eq!(
-            gauge,
-            Metric::new(
-                "gauge",
-                MetricKind::Incremental,
-                MetricValue::Gauge { value: -1.0 },
-            )
-            .with_timestamp(Some(ts()))
-        )
+        assert_eq!(gauge, expected);
     }
 
     #[test]
@@ -1010,18 +1014,15 @@ mod test {
         .with_tags(Some(tags()))
         .with_timestamp(Some(ts()));
 
+        let expected = set
+            .clone()
+            .with_value(MetricValue::Set {
+                values: vec!["old".into(), "new".into()].into_iter().collect(),
+            })
+            .with_timestamp(Some(ts()));
+
         set.data.add(&delta.data);
-        assert_eq!(
-            set,
-            Metric::new(
-                "set",
-                MetricKind::Incremental,
-                MetricValue::Set {
-                    values: vec!["old".into(), "new".into()].into_iter().collect()
-                },
-            )
-            .with_timestamp(Some(ts()))
-        )
+        assert_eq!(set, expected);
     }
 
     #[test]
@@ -1047,19 +1048,16 @@ mod test {
         .with_tags(Some(tags()))
         .with_timestamp(Some(ts()));
 
+        let expected = dist
+            .clone()
+            .with_value(MetricValue::Distribution {
+                samples: samples![1.0 => 10, 1.0 => 20],
+                statistic: StatisticKind::Histogram,
+            })
+            .with_timestamp(Some(ts()));
+
         dist.data.add(&delta.data);
-        assert_eq!(
-            dist,
-            Metric::new(
-                "hist",
-                MetricKind::Incremental,
-                MetricValue::Distribution {
-                    samples: samples![1.0 => 10, 1.0 => 20],
-                    statistic: StatisticKind::Histogram
-                },
-            )
-            .with_timestamp(Some(ts()))
-        )
+        assert_eq!(dist, expected);
     }
 
     #[test]

src/event/mod.rs
@@ -312,7 +312,8 @@ impl From<Event> for proto::EventWrapper {
     fn from(event: Event) -> Self {
         match event {
             Event::Log(log_event) => {
-                let fields = log_event
+                let (fields, _metadata) = log_event.into_parts();
+                let fields = fields
                     .into_iter()
                     .map(|(k, v)| (k, encode_value(v)))
                     .collect::<BTreeMap<_, _>>();

src/sinks/statsd.rs
@@ -301,7 +301,7 @@ mod test {
         let event = Event::Metric(metric1.clone());
         let frame = &encode_event(event, None).unwrap();
         let metric2 = parse(from_utf8(&frame).unwrap().trim()).unwrap();
-        assert_eq!(metric1, metric2);
+        shared::assert_event_data_eq!(metric1, metric2);
     }
 
     #[cfg(feature = "sources-statsd")]
@@ -331,7 +331,7 @@ mod test {
         let event = Event::Metric(metric1.clone());
         let frame = &encode_event(event, None).unwrap();
         let metric2 = parse(from_utf8(&frame).unwrap().trim()).unwrap();
-        assert_eq!(metric1, metric2);
+        shared::assert_event_data_eq!(metric1, metric2);
     }
 
     #[cfg(feature = "sources-statsd")]
@@ -346,7 +346,7 @@ mod test {
         let event = Event::Metric(metric1.clone());
         let frame = &encode_event(event, None).unwrap();
         let metric2 = parse(from_utf8(&frame).unwrap().trim()).unwrap();
-        assert_eq!(metric1, metric2);
+        shared::assert_event_data_eq!(metric1, metric2);
     }
 
     #[cfg(feature = "sources-statsd")]
@@ -364,7 +364,7 @@ mod test {
         let event = Event::Metric(metric1.clone());
         let frame = &encode_event(event, None).unwrap();
         let metric2 = parse(from_utf8(&frame).unwrap().trim()).unwrap();
-        assert_eq!(metric1, metric2);
+        shared::assert_event_data_eq!(metric1, metric2);
     }
 
     #[cfg(feature = "sources-statsd")]
@@ -381,7 +381,7 @@ mod test {
         let event = Event::Metric(metric1.clone());
         let frame = &encode_event(event, None).unwrap();
         let metric2 = parse(from_utf8(&frame).unwrap().trim()).unwrap();
-        assert_eq!(metric1, metric2);
+        shared::assert_event_data_eq!(metric1, metric2);
     }
 
     #[tokio::test]

commit 181c706d8f713f69c8b1f7e24e6d10cfdd29733d

fix(tests): Fix aws_ec2_metadata transform test
Broken by 0135730ee37bc4113c03db5fb8db008a44ea997a

This replaces the partial assertions for the namespace test which
I think is sufficient to test that part of the functionality. It was
previously failing because it matching two log events where the actual
log event had many more fields enriched than the expected event.

Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

src/transforms/aws_ec2_metadata.rs
@@ -614,14 +614,14 @@ mod integration_tests {
             sleep(Duration::from_secs(1)).await;
 
             let log = LogEvent::default();
-            let mut expected = log.clone();
             tx.send(log.into()).await.unwrap();
 
             let event = stream.next().await.unwrap();
 
-            expected.insert("ec2.metadata.availability-zone", "ww-region-1a");
-            expected.insert("ec2.metadata.public-ipv4", "192.1.1.1");
-            assert_eq!(event.into_log(), expected);
+            assert_eq!(
+                event.as_log().get("ec2.metadata.availability-zone"),
+                Some(&"ww-region-1a".into())
+            );
         }
 
         {
@@ -644,14 +644,14 @@ mod integration_tests {
             sleep(Duration::from_secs(1)).await;
 
             let log = LogEvent::default();
-            let mut expected = log.clone();
             tx.send(log.into()).await.unwrap();
 
             let event = stream.next().await.unwrap();
 
-            expected.insert("availability-zone", "ww-region-1a");
-            expected.insert("public-ipv4", "192.1.1.1");
-            assert_eq!(event.into_log(), expected);
+            assert_eq!(
+                event.as_log().get("availability-zone"),
+                Some(&"ww-region-1a".into())
+            );
         }
     }
 }