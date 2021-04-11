6b48c18ae0882d547d3bbf8f19cd188e643fdf41
Or Ricon <rikonor@gmail.com>
2021-04-11 10:14:34 -0400

    fix test
    Signed-off-by: Or Ricon <rikonor@gmail.com>

--- src/sources/splunk_hec.rs
+++ src/sources/splunk_hec.rs
@@ -870,7 +870,7 @@ mod tests {
         api: &str,
         message: &str,
         token: &str,
-        channel: Channel,
+        channel: Channel<'_>,
     ) -> u16 {
         let wrap_with_channel =
             |c: Channel, b: reqwest::RequestBuilder| -> reqwest::RequestBuilder {
@@ -880,11 +880,10 @@ mod tests {
                 }
             };
 
-        let b = reqwest::Client::new()
+        let mut b = reqwest::Client::new()
             .post(&format!("http://{}/{}", address, api))
-            .header("Authorization", format!("Splunk {}", token))
-            .header("x-splunk-request-channel", "guid");
-        b = wrap_with_channel(Channel, b);
+            .header("Authorization", format!("Splunk {}", token));
+        b = wrap_with_channel(channel, b);
         b.body(message.to_owned())
             .send()
             .await
