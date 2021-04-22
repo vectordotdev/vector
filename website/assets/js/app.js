16105f0c3618444ce887cc3f17fcd69a0fb3d4f8
Luc Perkins <luc.perkins@datadoghq.com>
2021-04-22 16:51:21 -0700

    Add dark mode toggler
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,7 +1,10 @@
+import '@ryangjchandler/spruce';
 import 'alpinejs';
 import 'tocbot/dist/tocbot';
 
 // Table of contents for documentation pages
+
+/*
 tocbot.init({
   tocSelector: '#docs-toc',
   contentSelector: '#docs-content',
@@ -9,5 +12,16 @@ tocbot.init({
   ignoreSelector: 'no-toc',
   scrollSmoothDuration: 400
 });
+*/
+
+/* Global state management */
+
+// Dark mode state
+window.Spruce.store('dark', {
+  enabled: null,
+  toggle() {
+    this.enabled = !this.enabled;
+  }
+}, true);
 
 console.log('Welcome to the Vector website and documentation!');
