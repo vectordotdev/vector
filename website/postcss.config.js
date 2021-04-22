466a985efad7d75a7dd4ba27116434f505c037e9
Luc Perkins <luc.perkins@datadoghq.com>
2021-04-22 12:13:55 -0700

    Add dark mode support
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,7 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: (content) => {
+      let els = JSON.parse(content).htmlElements;
+      return els.tags.concat(els.classes, els.ids);
+  }
+})
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
   ]
 }
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
   darkMode: 'class',
   theme: {
     extend: {
