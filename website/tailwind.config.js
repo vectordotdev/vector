98168a27b8879f2190757f2af36faaa94201cfd9
Luc Perkins <luc.perkins@datadoghq.com>
2021-04-22 18:42:53 -0700

    Add RSS icon to footer
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         'primary': '#28d9f2',
         'secondary': '#f44af5',
         'twitter-blue': '#1DA1F2',
-        'discord-purple': '#7289DA'
+        'discord-purple': '#7289DA',
+        'rss-orange': '#f26522'
       },
       fontFamily: {
         sans: ['Segoe UI', ...defaultTheme.fontFamily.sans],
