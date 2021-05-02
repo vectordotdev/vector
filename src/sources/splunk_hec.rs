60d0eeb8a01d21b166340fa5ada02a1eab8da2e8
ktf <krunotf@gmail.com>
2021-05-02 10:43:02 +0200

    Add issue link
    Signed-off-by: ktf <krunotf@gmail.com>

--- src/sources/splunk_hec.rs
+++ src/sources/splunk_hec.rs
@@ -1210,6 +1210,7 @@ mod tests {
     /// This test will fail once `warp` crate fixes support for
     /// custom connection listener, at that point this test can be
     /// modified to pass.
+    /// https://github.com/timberio/vector/issues/7097
     /// https://github.com/seanmonstar/warp/issues/830
     /// https://github.com/seanmonstar/warp/pull/713
     #[tokio::test]
