1f39b2d18a0c1097c99dd675a74bf008f90ed574
Jesse Szwedko <jesse@szwedko.me>
2021-05-24 17:42:06 -0400

    Initial implementation for decoding logstash frames
    Decoding "data" frames is still unimplemented.
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sources/logstash.rs
+++ src/sources/logstash.rs
@@ -1,14 +1,26 @@
 use super::util::{SocketListenAddr, TcpIsErrorFatal, TcpSource};
 use crate::{
-    config::{DataType, GenerateConfig, Resource, SourceConfig, SourceContext, SourceDescription},
-    event::Event,
+    config::{
+        log_schema, DataType, GenerateConfig, Resource, SourceConfig, SourceContext,
+        SourceDescription,
+    },
+    event::{Event, LogEvent, Value},
     tcp::TcpKeepaliveConfig,
     tls::{MaybeTlsSettings, TlsConfig},
 };
-use bytes::{Bytes, BytesMut};
+use bytes::{Buf, Bytes, BytesMut};
+use flate2::read::ZlibDecoder;
 use serde::{Deserialize, Serialize};
-use std::io;
+use std::{
+    collections::{BTreeMap, VecDeque},
+    convert::TryInto,
+    io::{self, Read},
+};
 use tokio_util::codec::Decoder;
+
+// TODO
+// * Handle window size and acking
+// * usize casts bounds
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct LogstashConfig {
@@ -76,17 +88,42 @@ impl TcpSource for LogstashSource {
         LogstashDecoder::new()
     }
 
-    fn build_event(&self, _frame: String, _host: Bytes) -> Option<Event> {
-        None
+    fn build_event(
+        &self,
+        frame: BTreeMap<String, serde_json::Value>,
+        host: Bytes,
+    ) -> Option<Event> {
+        let mut log = LogEvent::from(
+            frame
+                .into_iter()
+                .map(|(key, value)| (key, Value::from(value)))
+                .collect::<BTreeMap<_, _>>(),
+        );
+        if log.get(log_schema().host_key()).is_none() {
+            log.insert(log_schema().host_key(), host);
+        }
+        Some(Event::from(log))
     }
 }
 
+#[derive(Debug, Clone)]
+enum LogstashDecoderReadState {
+    ReadProtocol,
+    ReadType(LogstashProtocolVersion),
+    ReadFrame(LogstashProtocolVersion, LogstashFrameType),
+    PendingFrames(VecDeque<BTreeMap<String, serde_json::Value>>),
+}
+
 #[derive(Clone, Debug)]
-struct LogstashDecoder {}
+struct LogstashDecoder {
+    state: LogstashDecoderReadState,
+}
 
 impl LogstashDecoder {
     fn new() -> Self {
-        Self {}
+        Self {
+            state: LogstashDecoderReadState::ReadProtocol,
+        }
     }
 }
 
@@ -116,12 +153,169 @@ impl From<io::Error> for DecodeError {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+enum LogstashProtocolVersion {
+    V1, // 1
+    V2, // 2
+}
+
+#[derive(Debug, Clone, Copy)]
+enum LogstashFrameType {
+    WindowSize, // W
+    Data,       // D
+    Json,       // J
+    Compressed, // C
+}
+
+// Based on implementation from logstash: https://github.com/logstash-plugins/logstash-input-beats/blob/27bad62a26a81fc000a9d21495b8dc7174ab63e9/src/main/java/org/logstash/beats/BeatsParser.java
+// There is a protocol spec too but it appears to be out-of-date: https://github.com/logstash-plugins/logstash-input-beats/blob/master/PROTOCOL.md
 impl Decoder for LogstashDecoder {
-    type Item = String;
+    type Item = BTreeMap<String, serde_json::Value>;
     type Error = DecodeError;
 
-    fn decode(&mut self, _src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        Ok(None)
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        loop {
+            match self.state {
+                LogstashDecoderReadState::PendingFrames(ref mut frames) => {
+                    match frames.pop_front() {
+                        Some(frame) => return Ok(Some(frame)),
+                        None => {
+                            self.state = LogstashDecoderReadState::ReadProtocol;
+                        }
+                    }
+                }
+                LogstashDecoderReadState::ReadProtocol => {
+                    if src.remaining() < 1 {
+                        return Ok(None);
+                    }
+                    match src.get_u8() {
+                        b'1' => {
+                            self.state =
+                                LogstashDecoderReadState::ReadType(LogstashProtocolVersion::V1);
+                        }
+                        b'2' => {
+                            self.state =
+                                LogstashDecoderReadState::ReadType(LogstashProtocolVersion::V2);
+                        }
+                        // TODO decode error
+                        version => panic!("ohno {:?}", version),
+                    }
+                }
+                LogstashDecoderReadState::ReadType(version) => {
+                    if src.remaining() < 1 {
+                        return Ok(None);
+                    }
+
+                    match src.get_u8() {
+                        b'W' => {
+                            self.state = LogstashDecoderReadState::ReadFrame(
+                                version,
+                                LogstashFrameType::WindowSize,
+                            );
+                        }
+                        b'D' => {
+                            self.state = LogstashDecoderReadState::ReadFrame(
+                                version,
+                                LogstashFrameType::Data,
+                            )
+                        }
+                        b'J' => {
+                            self.state = LogstashDecoderReadState::ReadFrame(
+                                version,
+                                LogstashFrameType::Json,
+                            )
+                        }
+                        b'C' => {
+                            self.state = LogstashDecoderReadState::ReadFrame(
+                                version,
+                                LogstashFrameType::Compressed,
+                            )
+                        }
+                        // TODO decode error
+                        code => panic!("ohno: {:?}", code),
+                    }
+                }
+                LogstashDecoderReadState::ReadFrame(_version, LogstashFrameType::WindowSize) => {
+                    if src.remaining() < 4 {
+                        return Ok(None);
+                    }
+
+                    let _window_size = src.get_u32();
+                    self.state = LogstashDecoderReadState::ReadProtocol;
+                }
+                LogstashDecoderReadState::ReadFrame(_version, LogstashFrameType::Data) => {
+                    unimplemented!("TODO")
+                }
+                LogstashDecoderReadState::ReadFrame(_version, LogstashFrameType::Json) => {
+                    match (src.get(0..4), src.get(4..8)) {
+                        (None, _) | (_, None) => return Ok(None),
+                        (Some(_sequence_number), Some(payload_size)) => {
+                            let payload_size = u32::from_be_bytes(
+                                payload_size.try_into().expect("exactly 4 bytes"),
+                            );
+                            match &src.get(8..8 + payload_size as usize) {
+                                None => {
+                                    src.reserve(8 + payload_size as usize);
+                                    return Ok(None);
+                                }
+
+                                Some(slice) => {
+                                    let fields: BTreeMap<String, serde_json::Value> =
+                                        serde_json::from_slice(&slice[..]).expect("TODO");
+
+                                    src.advance(8 + payload_size as usize);
+
+                                    self.state = LogstashDecoderReadState::ReadProtocol;
+
+                                    return Ok(Some(fields));
+                                }
+                            }
+                        }
+                    }
+                }
+                LogstashDecoderReadState::ReadFrame(_version, LogstashFrameType::Compressed) => {
+                    match src.get(0..4) {
+                        None => return Ok(None),
+                        Some(bytes) => {
+                            let payload_size =
+                                u32::from_be_bytes(bytes.try_into().expect("exactly 4 bytes"));
+
+                            match &src.get(4..4 + payload_size as usize) {
+                                None => {
+                                    src.reserve(4 + payload_size as usize);
+                                    return Ok(None);
+                                }
+                                Some(slice) => {
+                                    let mut buf = {
+                                        let mut buf = Vec::new();
+
+                                        if let Err(err) =
+                                            ZlibDecoder::new(io::Cursor::new(&slice[..]))
+                                                .read_to_end(&mut buf)
+                                        {
+                                            return Err(err.into());
+                                        }
+
+                                        BytesMut::from(&buf[..])
+                                    };
+
+                                    let mut decoder = LogstashDecoder::new();
+
+                                    let mut frames = VecDeque::new();
+
+                                    while let Some(s) = decoder.decode(&mut buf)? {
+                                        frames.push_back(s);
+                                    }
+
+                                    src.advance(4 + payload_size as usize);
+                                    self.state = LogstashDecoderReadState::PendingFrames(frames);
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
     }
 }
 
