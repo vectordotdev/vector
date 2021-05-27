02112884e9e32de4b7b1aeed8fc596f6eaeed518
Toby Lawrence <toby@nuclearfurnace.com>
2021-05-27 11:00:12 -0400

    invert usage so we only apply it to actual binary builds
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -11,12 +11,12 @@ rustup target add wasm32-wasi
 rustup run stable cargo install cargo-deb --version 1.29.2
 rustup run stable cargo install cross --version 0.2.1
 
-# If we're running under CI (GH Actions), and we haven't been told to avoid it,
+# If we're running under CI (GH Actions), and we've been instructed to add it,
 # set VECTOR_BUILD_DESC to add in pertinent build information.  We typically only
-# disable this when running benchmarks to avoid file changes that trigger needless
-# rebuilding/recompilation.
-VECTOR_USE_BUILD_DESC=${VECTOR_USE_BUILD_DESC:-"1"}
-if [[ -f "${GITHUB_ENV}" && "${VECTOR_USE_BUILD_DESC}" == "1" ]]; then
+# enable this when generating binaries that will be in the hands of users so that
+# know which Git commit they were built from, etc.
+VECTOR_USE_BUILD_DESC=${VECTOR_USE_BUILD_DESC:-"false"}
+if [[ -f "${GITHUB_ENV}" && "${VECTOR_USE_BUILD_DESC}" == "true" ]]; then
     GIT_SHA=$(git rev-parse --short HEAD)
     CURRENT_DATE=$(date +%Y-%m-%d)
     echo VECTOR_BUILD_DESC="${GIT_SHA} ${CURRENT_DATE}" >> $GITHUB_ENV
