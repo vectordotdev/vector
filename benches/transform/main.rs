d262f5927988a27e9a94810791ae95e4743b1836
Brian L. Troutwine <brian@troutwine.us>
2021-06-09 15:38:42 -0700

    introduce reduce
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- benches/transform/main.rs
+++ benches/transform/main.rs
@@ -3,5 +3,6 @@ use criterion::criterion_main;
 mod common;
 mod dedupe;
 mod filter;
+mod reduce;
 
-criterion_main!(dedupe::benches, filter::benches,);
+criterion_main!(reduce::benches, dedupe::benches, filter::benches,);
