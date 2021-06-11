527e5a7bcbfe334771ce6b3b89ccb8dfd7695b72
Lee Benson <lee@leebenson.com>
2021-06-11 15:02:02 +0100

    tests
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/conditions/datadog.rs
+++ src/conditions/datadog.rs
@@ -20,7 +20,6 @@ impl ConditionConfig for DatadogConfig {
     fn build(&self) -> crate::Result<Box<dyn Condition>> {
         // Attempt to parse the Datadog search query.
         let query_node = parse(&self.source)?;
-
         let builder = Builder::new();
 
         let program = compile(builder.build(&query_node)).map_err(|diagnostics| {
@@ -34,3 +33,40 @@ impl ConditionConfig for DatadogConfig {
 }
 
 //------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::log_event;
+
+    #[test]
+    fn generate_config() {
+        crate::test_util::test_generate_config::<DatadogConfig>();
+    }
+
+    #[test]
+    fn check_datadog() {
+        let checks = vec![
+            // Tag exists.
+            (
+                "_exists_:a",                        // Source
+                log_event!["tags" => vec!["a:foo"]], // Pass
+                log_event!["tags" => vec!["b:foo"]], // Fail
+            ),
+        ];
+
+        for (source, pass, fail) in checks {
+            let config = DatadogConfig {
+                source: source.to_owned(),
+            };
+
+            // Every query should build successfully.
+            let cond = config
+                .build()
+                .unwrap_or_else(|_| panic!("build failed: {}", source));
+
+            assert!(cond.check_with_context(&pass).is_ok());
+            assert!(cond.check_with_context(&fail).is_err());
+        }
+    }
+}
