c5048dfa1d8442af0aab119666bac9758f4b4d02
Toby Lawrence <toby@nuclearfurnace.com>
2021-06-14 15:50:46 -0400

    fix incorrect lto rustflag value
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- scripts/environment/release-flags.sh
+++ scripts/environment/release-flags.sh
@@ -6,5 +6,5 @@ set -e -o verbose
 # code, but increases compilation time.  We only set this if we're in CI _and_ we haven't
 # been instructed to use the debug profile (via PROFILE environment variable).
 if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
-    echo  "RUSTFLAGS=\"${RUSTFLAGS} -C lto=true -C codegen-units=1\"" >> $GITHUB_ENV
+    echo  "RUSTFLAGS=\"${RUSTFLAGS} -C lto=fat -C codegen-units=1\"" >> $GITHUB_ENV
 fi
