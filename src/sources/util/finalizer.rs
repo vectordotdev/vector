12a1fc38defb875e16da5dc8f2622bd518128bf3
Bruce Guenter <bruce.guenter@datadoghq.com>
2021-06-14 18:05:37 -0600

    Use FuturesOrdered in the ordered finalizer to reduce code
    Signed-off-by: Bruce Guenter <bruce.guenter@datadoghq.com>

--- src/sources/util/finalizer.rs
+++ src/sources/util/finalizer.rs
@@ -1,7 +1,6 @@
 use crate::event::BatchStatusReceiver;
 use crate::shutdown::ShutdownSignal;
-use futures::{future::Shared, stream::FuturesUnordered, FutureExt, StreamExt};
-use std::collections::VecDeque;
+use futures::{future::Shared, stream::FuturesOrdered, FutureExt, StreamExt};
 use std::future::Future;
 use std::pin::Pin;
 use std::task::Poll;
@@ -45,24 +44,22 @@ async fn run_finalizer<T>(
     mut new_entries: mpsc::UnboundedReceiver<(BatchStatusReceiver, T)>,
     apply_done: impl Fn(T),
 ) {
-    let mut status_receivers = FuturesUnordered::default();
-    let mut store = FinalizerStore::new();
+    let mut status_receivers = FuturesOrdered::default();
 
     loop {
         tokio::select! {
             _ = shutdown.clone() => break,
             new_entry = new_entries.recv() => match new_entry {
                 Some((receiver, entry)) => {
-                    let index = store.add_entry(entry);
-                    status_receivers.push(FinalizerFuture { receiver, index });
+                    status_receivers.push(FinalizerFuture {
+                        receiver,
+                        entry: Some(entry),
+                    });
                 }
                 None => break,
             },
             finished = status_receivers.next(), if !status_receivers.is_empty() => match finished {
-                Some((_status, index)) => {
-                    store.mark_done(index);
-                    store.extract_done(&apply_done);
-                }
+                Some((_status, entry)) => apply_done(entry),
                 // The is_empty guard above prevents this from being reachable.
                 None => unreachable!(),
             },
@@ -71,68 +68,24 @@ async fn run_finalizer<T>(
     // We've either seen a shutdown signal or the new entry sender was
     // closed. Wait for the last statuses to come in before indicating
     // we are done.
-    while let Some((_status, index)) = status_receivers.next().await {
-        store.mark_done(index);
-        store.extract_done(&apply_done);
+    while let Some((_status, entry)) = status_receivers.next().await {
+        apply_done(entry);
     }
     drop(shutdown);
 }
 
-struct FinalizerFuture {
+#[pin_project::pin_project]
+struct FinalizerFuture<T> {
     receiver: BatchStatusReceiver,
-    index: u64,
+    entry: Option<T>,
 }
 
-impl Future for FinalizerFuture {
-    type Output = (<BatchStatusReceiver as Future>::Output, u64);
+impl<T> Future for FinalizerFuture<T> {
+    type Output = (<BatchStatusReceiver as Future>::Output, T);
     fn poll(mut self: Pin<&mut Self>, ctx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        self.receiver
-            .poll_unpin(ctx)
-            .map(|status| (status, self.index))
-    }
-}
-
-struct FinalizerStore<T> {
-    first: u64,
-    entries: VecDeque<(bool, T)>,
-}
-
-impl<T> FinalizerStore<T> {
-    fn new() -> Self {
-        Self {
-            first: 0,
-            entries: VecDeque::new(),
-        }
-    }
-
-    /// Add the needed data from the given message into the store and
-    /// return the index.
-    #[must_use]
-    fn add_entry(&mut self, entry: T) -> u64 {
-        let index = self.first + self.entries.len() as u64;
-        self.entries.push_back((false, entry));
-        index
-    }
-
-    fn mark_done(&mut self, index: u64) {
-        // Under normal usage, both of these conditions should be true,
-        // but they are guarded to avoid panics.
-        if index >= self.first {
-            let offset = (index - self.first) as usize;
-            if let Some(entry) = self.entries.get_mut(offset) {
-                entry.0 = true;
-            }
-        }
-    }
-
-    fn extract_done(&mut self, apply: impl Fn(T)) {
-        while let Some(entry) = self.entries.front() {
-            if !entry.0 {
-                break;
-            }
-            let entry = self.entries.pop_front().unwrap().1;
-            self.first += 1;
-            apply(entry);
-        }
+        let status = futures::ready!(self.receiver.poll_unpin(ctx));
+        // The use of this above in a `FuturesOrdered` will only take
+        // this once before dropping the future.
+        Poll::Ready((status, self.entry.take().unwrap_or_else(|| unreachable!())))
     }
 }
