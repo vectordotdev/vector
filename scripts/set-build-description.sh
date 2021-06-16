b13866ecaa5b3cbcd2c15c60e3315136b40127a3
Brian L. Troutwine <brian@troutwine.us>
2021-06-16 16:17:46 -0700

    chore: Build a fat release binary (#7895)
    Build a fat release binary
    
    This commit builds a release binary with debuginfo included. We build only a
    tarball to avoid the situation where a user might install our CD-sized binary
    accidentally. I have not made any changes to release-s3.sh as it didn't seem
    like most other artifacts were explicitly mentioned there.
    
    Resolves #7892
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- scripts/set-build-description.sh
+++ scripts/set-build-description.sh
@@ -1,5 +1,8 @@
-#! /usr/bin/env bash
-set -e -o verbose
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o verbose
 
 # Set VECTOR_BUILD_DESC to add in pertinent build information.  We typically only
 # enable this when generating binaries that will be in the hands of users so that
@@ -7,6 +10,15 @@ set -e -o verbose
 GIT_SHA=$(git rev-parse --short HEAD)
 CURRENT_DATE=$(date +%Y-%m-%d)
 BUILD_DESC="${GIT_SHA} ${CURRENT_DATE}"
+
+# We do not add 'debug' to the BUILD_DESC unless the caller has flagged on line
+# or full debug symbols. See the Cargo Book profiling section for value meaning:
+# https://doc.rust-lang.org/cargo/reference/profiles.html#debug
+if [ "$CARGO_PROFILE_RELEASE_DEBUG" == 1 ] ||
+       [ "$CARGO_PROFILE_RELEASE_DEBUG" == 2 ] ||
+       [ "$CARGO_PROFILE_RELEASE_DEBUG" == true ]; then
+    export BUILD_DESC="${BUILD_DESC} debug"
+fi
 
 # If we're in Github CI, set it in the special environment variables file. Otherwise,
 # export the variable.  This requires sourcing the file instead of simply running it.
