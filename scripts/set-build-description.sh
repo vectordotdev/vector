5cddd4b21eb06332245e8058ab78c18098272b71
Jesse Szwedko <jesse@szwedko.me>
2021-06-17 15:11:31 -0700

    chore(releasing): Move where the `DEBUG` info is sensed during builds
    Moves where we include how debug information is injected into the binary
    to `build.rs` rather than CI. This feels a bit safer in that it will
    check even outside of CI and augment the version string accordingly.
    
    A release build of vector looks like:
    
    ```
    vector 0.15.0 (x86_64-apple-darwin)
    ```
    
    That is, no debug info. A debug build of vector looks like:
    
    ```
    vector 0.15.0 (x86_64-apple-darwin debug=full)
    ```
    
    We currently just have a binary `debug` or nothing, which we could
    switch back to here. I'm not really sure how often `debug` is set to `1`
    to only include line info and if that is substantially different in
    meaning than including full debug info.
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/set-build-description.sh
+++ scripts/set-build-description.sh
@@ -11,15 +11,6 @@ GIT_SHA=$(git rev-parse --short HEAD)
 CURRENT_DATE=$(date +%Y-%m-%d)
 BUILD_DESC="${GIT_SHA} ${CURRENT_DATE}"
 
-# We do not add 'debug' to the BUILD_DESC unless the caller has flagged on line
-# or full debug symbols. See the Cargo Book profiling section for value meaning:
-# https://doc.rust-lang.org/cargo/reference/profiles.html#debug
-if [ "$CARGO_PROFILE_RELEASE_DEBUG" == 1 ] ||
-       [ "$CARGO_PROFILE_RELEASE_DEBUG" == 2 ] ||
-       [ "$CARGO_PROFILE_RELEASE_DEBUG" == true ]; then
-    export BUILD_DESC="${BUILD_DESC} debug"
-fi
-
 # If we're in Github CI, set it in the special environment variables file. Otherwise,
 # export the variable.  This requires sourcing the file instead of simply running it.
 if [[ -f "${GITHUB_ENV}" ]]; then
