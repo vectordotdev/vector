2b21c643ea5f0fb5690defa0ef33bb903f505929
Luc Perkins <luc.perkins@datadoghq.com>
2021-06-23 14:05:16 -0700

    Fix JS console errors due to non-existing elements on specific pages
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- docs/assets/js/below.js
+++ docs/assets/js/below.js
@@ -9,13 +9,15 @@ import 'tocbot/dist/tocbot';
 
 // Table of contents for documentation pages
 const tableOfContents = () => {
-  tocbot.init({
-    tocSelector: '#toc',
-    contentSelector: '#page-content',
-    headingSelector: 'h1, h2, h3, h4, h5',
-    ignoreSelector: 'no-toc',
-    scrollSmoothDuration: 400
-  });
+  if (document.getElementById('toc')) {
+    tocbot.init({
+      tocSelector: '#toc',
+      contentSelector: '#page-content',
+      headingSelector: 'h1, h2, h3, h4, h5',
+      ignoreSelector: 'no-toc',
+      scrollSmoothDuration: 400
+    });
+  }
 }
 
 // Algolia search
