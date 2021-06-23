4a1fff7270bee058f4a0f9424e3e6cbb71dd8fd6
Luc Perkins <luc.perkins@datadoghq.com>
2021-06-23 16:00:07 -0700

    Provide initial search results dropdown implementation
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- docs/assets/js/below.js
+++ docs/assets/js/below.js
@@ -5,7 +5,8 @@
 
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js';
-import { searchBox, hits } from 'instantsearch.js/es/widgets';
+import { searchBox } from 'instantsearch.js/es/widgets';
+import { connectHits } from 'instantsearch.js/es/connectors';
 
 import 'tocbot/dist/tocbot';
 
@@ -21,9 +22,32 @@ const searchInput = searchBox({
   container: '#algolia-search-box'
 });
 
-const searchResults = hits({
-  container: '#algolia-search-results'
-})
+const renderHits = (renderOptions, _isFirstRender) => {
+  const { hits, widgetParams } = renderOptions;
+
+  widgetParams.container.innerHTML = `
+    <ul class="flex flex-col divide-y dark:divide-gray-700">
+      ${hits
+        .map(
+          item =>
+            `<li class="text-dark dark:text-gray-200 py-2 hover:text-secondary dark:hover:text-primary">
+              <a href="${item.url}">
+              ${instantsearch.highlight({ attribute: 'title', hit: item })}
+              </a>
+            </li>`
+        )
+        .join('')}
+    </ul>
+  `;
+};
+
+const customHits = connectHits(
+  renderHits
+);
+
+const searchResults = customHits({
+  container: document.querySelector('#algolia-search-results')
+});
 
 search.addWidgets([
   searchInput,
