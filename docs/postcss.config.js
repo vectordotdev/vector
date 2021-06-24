1ea03e4da838b8c772f98ab89676ae3f0e35deb9
Luc Perkins <luc.perkins@datadoghq.com>
2021-06-24 12:38:19 -0700

    Add autoprefixer config
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,3 +1,9 @@
+const postcssImport = require('postcss-import');
+const tailwindCss = require('tailwindcss');
+const autoprefixer = require('autoprefixer')({
+  browsers: ['last 2 versions']
+});
+
 // These are classes for things that are applied by JS, and thus missed by Hugo.
 // See assets/js/*.js for places where this happens.
 const safeClasses = [
@@ -23,7 +29,6 @@ const safeClasses = [
   'tracking-wide',
 ];
 
-
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./hugo_stats.json'],
   safelist: safeClasses,
@@ -35,9 +40,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
+    postcssImport,
+    tailwindCss,
+    autoprefixer,
     ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
   ]
 }
