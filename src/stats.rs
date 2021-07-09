fc4d693cf6d41d53947044337c42b0711082bdc0
Bruce Guenter <bruce.guenter@datadoghq.com>
2021-07-09 09:41:04 -0600

    Add EWMA type with variance calculations
    Signed-off-by: Bruce Guenter <bruce.guenter@datadoghq.com>

--- src/stats.rs
+++ src/stats.rs
@@ -26,6 +26,58 @@ impl Ewma {
     }
 }
 
+/// Exponentially Weighted Moving Average with variance calculation
+#[derive(Clone, Copy, Debug)]
+pub struct EwmaVar {
+    state: Option<MeanVariance>,
+    alpha: f64,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MeanVariance {
+    pub mean: f64,
+    pub variance: f64,
+}
+
+impl EwmaVar {
+    pub fn new(alpha: f64) -> Self {
+        let state = None;
+        Self { state, alpha }
+    }
+
+    pub fn state(&self) -> Option<MeanVariance> {
+        self.state
+    }
+
+    #[cfg(test)]
+    pub fn average(&self) -> Option<f64> {
+        self.state.map(|state| state.mean)
+    }
+
+    #[cfg(test)]
+    pub fn variance(&self) -> Option<f64> {
+        self.state.map(|state| state.variance)
+    }
+
+    /// Update the current average and variance, and return them for convenience
+    pub fn update(&mut self, point: f64) -> MeanVariance {
+        let (mean, variance) = match self.state {
+            None => (point, 0.0),
+            Some(state) => {
+                let difference = point - state.mean;
+                let increment = self.alpha * difference;
+                (
+                    state.mean + increment,
+                    (1.0 - self.alpha) * (state.variance + difference * increment),
+                )
+            }
+        };
+        let state = MeanVariance { mean, variance };
+        self.state = Some(state);
+        state
+    }
+}
+
 /// Simple unweighted arithmetic mean
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Mean {
@@ -83,6 +135,34 @@ mod tests {
         assert_eq!(mean.average(), Some(1.5));
         mean.update(2.0);
         assert_eq!(mean.average(), Some(1.75));
+
         assert_eq!(mean.average, Some(1.75));
+    }
+
+    #[test]
+    fn ewma_variance_update_works() {
+        let mut mean = EwmaVar::new(0.5);
+        assert_eq!(mean.average(), None);
+        assert_eq!(mean.variance(), None);
+        mean.update(2.0);
+        assert_eq!(mean.average(), Some(2.0));
+        assert_eq!(mean.variance(), Some(0.0));
+        mean.update(2.0);
+        assert_eq!(mean.average(), Some(2.0));
+        assert_eq!(mean.variance(), Some(0.0));
+        mean.update(1.0);
+        assert_eq!(mean.average(), Some(1.5));
+        assert_eq!(mean.variance(), Some(0.25));
+        mean.update(2.0);
+        assert_eq!(mean.average(), Some(1.75));
+        assert_eq!(mean.variance(), Some(0.1875));
+
+        assert_eq!(
+            mean.state,
+            Some(MeanVariance {
+                mean: 1.75,
+                variance: 0.1875
+            })
+        );
     }
 }
