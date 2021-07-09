d2bb972045a39ce357f257e120a968d2637dad64
Luc Perkins <luc.perkins@datadoghq.com>
2021-07-09 16:18:10 -0700

    Fix margins on re-worked templates
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- docs/assets/js/below.js
+++ docs/assets/js/below.js
@@ -6,7 +6,7 @@ const tableOfContents = () => {
     tocbot.init({
       tocSelector: '#toc',
       contentSelector: '#page-content',
-      headingSelector: 'h1, h2, h3, h4, h5',
+      headingSelector: 'h2, h3, h4, h5',
       ignoreSelector: 'no-toc',
       scrollSmoothDuration: 400
     });
