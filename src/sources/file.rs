b2f96d0eaae12be4dbc5496096480ed2c89e077b
Brian L. Troutwine <brian@troutwine.us>
2021-07-22 14:56:22 -0700

    chore: Ignore flakey file-source tests (#8424)
    * Ignore flakey file-source tests
    
    This commit marks two tests in the file-source as ignore. Both deal with
    restarts. We've fiddled with this mechanism quite a bit lately. My argument is
    that file-source needs a fundamental rethink -- I did the basic work in
    [cernan](https://github.com/postmates/cernan) before `Future` existed -- and
    tinkering with restart isn't working.
    
    The test appears to be flakey only on Mac and Windows, our slowest CI machines,
    which drives out CI turn-around time. Consider that #8421 has failed CI in two
    runs because of one or other of these tests.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * Run the previously ignored tests only on Linux
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/sources/file.rs
+++ src/sources/file.rs
@@ -953,16 +953,19 @@ mod tests {
         }
     }
 
+    #[cfg(target_os = "linux")] // see #7988
     #[tokio::test]
     async fn file_start_position_server_restart_acknowledged() {
         file_start_position_server_restart(Acks).await
     }
 
+    #[cfg(target_os = "linux")] // see #7988
     #[tokio::test]
     async fn file_start_position_server_restart_nonacknowledged() {
         file_start_position_server_restart(NoAcks).await
     }
 
+    #[cfg(target_os = "linux")] // see #7988
     async fn file_start_position_server_restart(acking: AckingMode) {
         let dir = tempdir().unwrap();
         let config = file::FileConfig {
