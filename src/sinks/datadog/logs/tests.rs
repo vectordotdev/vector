c618734895ccb04d6dbba929719ff0ec97a28645
Brian L. Troutwine <brian@troutwine.us>
2021-07-28 10:09:38 -0700

    Document datadog sink tests
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/sinks/datadog/logs/tests.rs
+++ src/sinks/datadog/logs/tests.rs
@@ -25,55 +25,15 @@ fn event_with_api_key(msg: &str, key: &str) -> Event {
     e
 }
 
-#[tokio::test]
-async fn smoke_json() {
-    let (expected, output) = smoke_test().await;
-
-    for (i, val) in output.iter().enumerate() {
-        assert_eq!(
-            val.0.headers.get("Content-Type").unwrap(),
-            "application/json"
-        );
-
-        let mut json = serde_json::Deserializer::from_slice(&val.1[..])
-            .into_iter::<serde_json::Value>()
-            .map(|v| v.expect("decoding json"));
-
-        let json = json.next().unwrap();
-
-        // The json we send to Datadog is an array of events.
-        // As we have set batch.max_events to 1, each entry will be
-        // an array containing a single record.
-        let message = json
-            .get(0)
-            .unwrap()
-            .get("message")
-            .unwrap()
-            .as_str()
-            .unwrap();
-        assert_eq!(message, expected[i]);
-    }
-}
-
-async fn smoke_test() -> (Vec<String>, Vec<(http::request::Parts, Bytes)>) {
-    let (expected, rx) = start_test(StatusCode::OK, BatchStatus::Delivered).await;
-
-    let output = rx.take(expected.len()).collect::<Vec<_>>().await;
-
-    (expected, output)
-}
-
-#[tokio::test]
-async fn handles_failure_json() {
-    handles_failure().await;
-}
-
-async fn handles_failure() {
-    let (_expected, mut rx) = start_test(StatusCode::FORBIDDEN, BatchStatus::Failed).await;
-
-    assert!(matches!(rx.try_next(), Err(TryRecvError { .. })));
-}
-
+/// Starts a test sink with random lines running into it
+///
+/// This function starts a Datadog Logs sink with a simplistic configuration and
+/// runs random lines through it, returning a vector of the random lines and a
+/// Receiver populated with the result of the sink's operation.
+///
+/// Testers may set `http_status` and `batch_status`. The first controls what
+/// status code faked HTTP responses will have, the second acts as a check on
+/// the `Receiver`'s status before being returned to the caller.
 async fn start_test(
     http_status: StatusCode,
     batch_status: BatchStatus,
@@ -107,6 +67,62 @@ async fn start_test(
 }
 
 #[tokio::test]
+/// Assert the basic functionality of the sink in good conditions
+///
+/// This test rigs the sink to return StatusCode::OK to responses, checks that
+/// all batches were delivered and then asserts that every message is able to be
+/// deserialized.
+async fn smoke() {
+    let (expected, rx) = start_test(StatusCode::OK, BatchStatus::Delivered).await;
+
+    let output = rx.take(expected.len()).collect::<Vec<_>>().await;
+
+    for (i, val) in output.iter().enumerate() {
+        assert_eq!(
+            val.0.headers.get("Content-Type").unwrap(),
+            "application/json"
+        );
+
+        let mut json = serde_json::Deserializer::from_slice(&val.1[..])
+            .into_iter::<serde_json::Value>()
+            .map(|v| v.expect("decoding json"));
+
+        let json = json.next().unwrap();
+
+        // The json we send to Datadog is an array of events.
+        // As we have set batch.max_events to 1, each entry will be
+        // an array containing a single record.
+        let message = json
+            .get(0)
+            .unwrap()
+            .get("message")
+            .unwrap()
+            .as_str()
+            .unwrap();
+        assert_eq!(message, expected[i]);
+    }
+}
+
+#[tokio::test]
+/// Assert delivery error behavior
+///
+/// In the event that delivery fails -- in this case becaues it is FORBIDDEN --
+/// there should be no outbound messages from the sink. That is, receiving from
+/// its Receiver must fail.
+async fn handles_failure() {
+    let (_expected, mut rx) = start_test(StatusCode::FORBIDDEN, BatchStatus::Failed).await;
+    let res = rx.try_next();
+
+    assert!(matches!(res, Err(TryRecvError { .. })));
+}
+
+#[tokio::test]
+/// Assert that metadata API keys are passed correctly
+///
+/// Datadog sink payloads come with an associated API key. This key can be set
+/// per-event or set in the message for an entire payload. This test asserts
+/// that, for successful transmission, the API key set in metadata is
+/// propagated.
 async fn api_key_in_metadata() {
     let (mut config, cx) = load_sink::<DatadogLogsConfig>(indoc! {r#"
             default_api_key = "atoken"
@@ -128,10 +144,11 @@ async fn api_key_in_metadata() {
 
     let (expected, events) = random_lines_with_stream(100, 10, None);
 
+    let api_key = "0xDECAFBAD";
     let mut events = events.map(|mut e| {
         e.as_mut_log()
             .metadata_mut()
-            .set_datadog_api_key(Some(Arc::from("from_metadata")));
+            .set_datadog_api_key(Some(Arc::from(api_key)));
         Ok(e)
     });
 
@@ -139,7 +156,7 @@ async fn api_key_in_metadata() {
     let output = rx.take(expected.len()).collect::<Vec<_>>().await;
 
     for (i, val) in output.iter().enumerate() {
-        assert_eq!(val.0.headers.get("DD-API-KEY").unwrap(), "from_metadata");
+        assert_eq!(val.0.headers.get("DD-API-KEY").unwrap(), api_key);
 
         assert_eq!(
             val.0.headers.get("Content-Type").unwrap(),
@@ -167,6 +184,12 @@ async fn api_key_in_metadata() {
 }
 
 #[tokio::test]
+/// Assert that events with explicit keys have those keys preserved
+///
+/// Datadog sink payloads come with an associated API key. This key can be set
+/// per-event or set in the message for an entire payload. This test asserts
+/// that, for successful transmission, per-event API keys are propagated
+/// correctly.
 async fn multiple_api_keys() {
     let (mut config, cx) = load_sink::<DatadogLogsConfig>(indoc! {r#"
             default_api_key = "atoken"
