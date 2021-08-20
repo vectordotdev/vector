0031084f8c1554a42511e51397a0275ed3dba85c
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2021-08-20 08:32:20 +0200

    feat(pipelines): load pipelines for unit test
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/config/unit_test.rs
+++ src/config/unit_test.rs
@@ -11,7 +11,9 @@ use std::collections::HashMap;
 pub async fn build_unit_tests_main(paths: &[ConfigPath]) -> Result<Vec<UnitTest>, Vec<String>> {
     config::init_log_schema(paths, false)?;
 
-    let (config, _) = super::loading::load_builder_from_paths(paths)?;
+    let pipelines = super::loading::load_pipelines_from_paths(paths)?;
+    let (mut config, _) = super::loading::load_builder_from_paths(paths)?;
+    config.set_pipelines(pipelines);
 
     build_unit_tests(config).await
 }
