17966e4219ad19d617f063aaea6997569cb352d0
bgdeutsch <bgdeutsch@gmail.com>
2021-08-24 16:45:10 -0400

    RUM config

--- website/assets/js/dd-browser-logs-rum.js
+++ website/assets/js/dd-browser-logs-rum.js
@@ -1,5 +1,7 @@
-// {{ $latest := index site.Data.docs.versions 0 }}
-// import { datadogRum } from '@datadog/browser-rum';
+{{ $latest := index site.Data.docs.versions 0 }}
+
+import { ddConfig } from './config-vector';
+import { datadogRum } from '@datadog/browser-rum';
 
 console.log('Hello from DD Browser Logs and Rum init JS file.');
 
@@ -18,39 +20,17 @@ const getEnv = () => {
   return env;
 }
 
-const getVersion = () => {
-  const spruceConfig = localStorage.getItem('__spruce:global');
-  const spruceConfigToJSON = JSON.parse(spruceConfig)
-  return spruceConfigToJSON["version"]
+const env = getEnv();
+
+if (datadogRum) {
+  if (env === 'preview' || env === 'live') {
+    datadogRum.init({
+      applicationId: ddConfig.applicationID,
+      clientToken: ddConfig.clientToken,
+      env,
+      service: 'vector',
+      version: '{{ $latest }}',
+      trackInteractions: true
+    });
+  }
 }
-
-// const test = localStorage.getItem('__spruce:global');
-// const parsed = JSON.parse(test)
-
-// console.log(parsed["version"]);
-
-
-// if (window.DD_RUM) {
-//   window.DD_RUM.init({
-//     applicationId: 'test',
-//     clientToken: 'test',
-//     env: getEnv(),
-//     service: 'vector',
-//     version: CI_COMMIT_SHORT_SHA,
-//     trackInteractions: true,
-//     allowedTracingOrigins: [window.location.origin]
-//   })
-// }
-
-// import { datadogRum } from '@datadog/browser-rum';
-
-// datadogRum.init({
-//     applicationId: '0b95923b-b06d-445b-893f-a861e93d6ea3',
-//     clientToken: 'puba5f23a97d613091ae2ca8c0f4a135af4',
-//     site: 'datadoghq.com',
-//     service:'vector',
-//     // Specify a version number to identify the deployed version of your application in Datadog 
-//     // version: '1.0.0',
-//     sampleRate: 100,
-//     trackInteractions: true
-// });
