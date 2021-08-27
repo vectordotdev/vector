270931ff5d7ac00aad5fab0b208420c09fa3ff09
Jesse Szwedko <jesse@szwedko.me>
2021-08-27 20:29:55 +0000

    Install llvm 9
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/cross/bootstrap-rhel.sh
+++ scripts/cross/bootstrap-rhel.sh
@@ -2,6 +2,9 @@
 
 yum makecache
 
+# Needed for llvm 9 required by onig_sys
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
 # needed by onig_sys
 yum install -y \
       clang \
