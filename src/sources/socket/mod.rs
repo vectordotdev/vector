9891bd3fcb2253f9b96ba2b808cabc057ed7a6c5
Brian L. Troutwine <brian@troutwine.us>
2021-09-04 21:56:21 -0700

    ignore flaky test, see #9046
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/sources/socket/mod.rs
+++ src/sources/socket/mod.rs
@@ -320,6 +320,7 @@ mod test {
     }
 
     #[tokio::test]
+    #[ignore] // see issue #9046
     async fn tcp_with_tls_intermediate_ca() {
         let (tx, mut rx) = Pipeline::new_test();
         let addr = next_addr();
