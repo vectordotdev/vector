528bd5ee56d2ee00ccbc42de28513cba32de208f
Pablo Sichert <mail@pablosichert.com>
2021-09-06 15:08:57 +0000

    Implement `bytes` parser
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- src/codecs/mod.rs
+++ src/codecs/mod.rs
@@ -158,7 +158,7 @@ impl DecodingConfig {
             .decoding
             .as_ref()
             .map(|config| config.build())
-            .unwrap_or_else(|| Err("Fallback to bytes parser is not implemented yet.".into()))?;
+            .unwrap_or_else(|| BytesParserConfig::new().build())?;
 
         Ok(Decoder::new(framer, parser))
     }
