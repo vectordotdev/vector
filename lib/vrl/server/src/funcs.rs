2a9e9483ec3f9441fe1d6c0f77237dc123746848
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-10 16:56:33 -0700

    Return functions list as JSON object
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- lib/vrl/server/src/funcs.rs
+++ lib/vrl/server/src/funcs.rs
@@ -1,7 +1,15 @@
+use serde::Serialize;
 use std::convert::Infallible;
 use warp::{reply::json, Reply};
 
+#[derive(Serialize)]
+struct FunctionList {
+    functions: Vec<&'static str>,
+}
+
 pub async fn function_metadata() -> Result<impl Reply, Infallible> {
     let functions: Vec<&'static str> = stdlib::all().iter().map(|f| f.identifier()).collect();
-    Ok(json(&functions))
+    let functions_list = FunctionList { functions };
+
+    Ok(json(&functions_list))
 }
