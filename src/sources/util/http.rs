72159b5d485bdd5cda4782700b27aa9b4415839d
Pierre Rognant <59561183+prognant@users.noreply.github.com>
2021-09-16 10:19:23 +0200

    fix(http source): adjust deflate compression handling (#9121)
    Signed-off-by: prognant <pierre.rognant@datadoghq.com>

--- src/sources/util/http.rs
+++ src/sources/util/http.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 use async_trait::async_trait;
 use bytes::{Buf, Bytes};
-use flate2::read::{DeflateDecoder, MultiGzDecoder};
+use flate2::read::{MultiGzDecoder, ZlibDecoder};
 use futures::{FutureExt, SinkExt, StreamExt, TryFutureExt};
 use headers::{Authorization, HeaderMapExt};
 use serde::{Deserialize, Serialize};
@@ -141,7 +141,7 @@ pub fn decode(header: &Option<String>, mut body: Bytes) -> Result<Bytes, ErrorMe
                 }
                 "deflate" => {
                     let mut decoded = Vec::new();
-                    DeflateDecoder::new(body.reader())
+                    ZlibDecoder::new(body.reader())
                         .read_to_end(&mut decoded)
                         .map_err(|error| handle_decode_error(encoding, error))?;
                     decoded.into()
