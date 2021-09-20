a50cc41a0bfc76714736e936cea5bf451e7502ae
Brian L. Troutwine <brian@troutwine.us>
2021-09-19 19:39:48 -0700

    Flip on runtime/tokio traces for console
    After getting help from @hawkw on discord it turns out the thing the last PR was
    missing were targets for `runtime` and `tokio` at trace level in our environment
    filter. For now I've set these only to flag on if the console is enabled, as
    they're relatively chatty.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/app.rs
+++ src/app.rs
@@ -55,6 +55,19 @@ impl Application {
 
         let level = std::env::var("LOG").unwrap_or_else(|_| match opts.log_level() {
             "off" => "off".to_owned(),
+            #[cfg(feature = "tokio-console")]
+            level => [
+                format!("vector={}", level),
+                format!("codec={}", level),
+                format!("vrl={}", level),
+                format!("file_source={}", level),
+                "tower_limit=trace".to_owned(),
+                "runtime=trace".to_owned(),
+                "tokio=trace".to_owned(),
+                format!("rdkafka={}", level),
+            ]
+            .join(","),
+            #[cfg(not(feature = "tokio-console"))]
             level => [
                 format!("vector={}", level),
                 format!("codec={}", level),
