e030129f4e30777b105ca0cb5755f6b459e44db4
001wwang <will.wang@datadoghq.com>
2021-09-20 17:54:00 -0400

    Add crate intro and clippy preamble
    Signed-off-by: 001wwang <will.wang@datadoghq.com>

--- lib/vector-core/internal-event/src/lib.rs
+++ lib/vector-core/internal-event/src/lib.rs
@@ -1,3 +1,11 @@
+//! The Vector Core Internal Event library
+//!
+//! This library powers the Event-driven Observability pattern (RFC 2064) that
+//! vector uses for internal instrumentation
+
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+
 pub trait InternalEvent {
     fn emit_logs(&self) {}
     fn emit_metrics(&self) {}
