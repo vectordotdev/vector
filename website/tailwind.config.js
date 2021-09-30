303caa790e1d4108195fc90938089761c958f12c
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-30 15:46:07 -0700

    Revert font color changes
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -114,11 +114,10 @@ module.exports = {
             },
             code: {
               color: theme('colors.primary', 'currentColor'),
-              backgroundColor: theme('colors.gray.700'),
-            },
-            'code[class^="language-"]': {
-              color: theme('colors.gray.100', 'currentColor'),
-              backgroundColor: theme('colors.black'),
+              '&:not([class^="language-"])': {
+                color: theme('colors.gray.100'),
+                'background-color': theme('colors.gray.700'),
+              }
             },
             color: theme('colors.gray.200'),
             'p, h1, h2, h3, h4, h5, h6, li': {
