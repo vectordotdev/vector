2a65d42a074e847501265597809440792303dd4b
Luke Steensen <luke.steensen@gmail.com>
2021-10-01 08:01:40 -0500

    remove unused method
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- src/config/id.rs
+++ src/config/id.rs
@@ -101,13 +101,6 @@ impl ComponentKey {
     pub const fn is_global(&self) -> bool {
         matches!(self.scope, ComponentScope::Global)
     }
-
-    pub fn join(&self, s: &str) -> Self {
-        let mut other = self.clone();
-        other.id.push('.');
-        other.id.push_str(s);
-        other
-    }
 }
 
 impl From<(Option<String>, String)> for ComponentKey {
