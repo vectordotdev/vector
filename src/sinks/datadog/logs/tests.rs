c8adf01d581c766ef63ea0bb0ae4b3ce5093f354
Vladimir Zhuk <vladimir.zhuk@datadoghq.com>
2021-10-04 17:57:57 +0200

    fix tests
    Signed-off-by: Vladimir Zhuk <vladimir.zhuk@datadoghq.com>

--- src/sinks/datadog/logs/tests.rs
+++ src/sinks/datadog/logs/tests.rs
@@ -67,11 +67,11 @@ async fn start_test(
 #[tokio::test]
 /// Assert the basic functionality of the sink in good conditions
 ///
-/// This test rigs the sink to return StatusCode::OK to responses, checks that
+/// This test rigs the sink to return StatusCode::ACCEPTED to responses, checks that
 /// all batches were delivered and then asserts that every message is able to be
 /// deserialized.
 async fn smoke() {
-    let (expected, rx) = start_test(StatusCode::OK, BatchStatus::Delivered).await;
+    let (expected, rx) = start_test(StatusCode::ACCEPTED, BatchStatus::Delivered).await;
 
     let output = rx.take(expected.len()).collect::<Vec<_>>().await;
 
@@ -133,7 +133,7 @@ async fn api_key_in_metadata() {
 
     let (sink, _) = config.build(cx).await.unwrap();
 
-    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::OK);
+    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::ACCEPTED);
     tokio::spawn(server);
 
     let (expected_messages, events) = random_lines_with_stream(100, 10, None);
@@ -198,7 +198,7 @@ async fn multiple_api_keys() {
 
     let (sink, _) = config.build(cx).await.unwrap();
 
-    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::OK);
+    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::ACCEPTED);
     tokio::spawn(server);
 
     let events = vec![
@@ -235,7 +235,7 @@ async fn enterprise_headers() {
     cx.globals.enterprise = true;
     let (sink, _) = config.build(cx).await.unwrap();
 
-    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::OK);
+    let (rx, _trigger, server) = build_test_server_status(addr, StatusCode::ACCEPTED);
     tokio::spawn(server);
 
     let (_expected_messages, events) = random_lines_with_stream(100, 10, None);
