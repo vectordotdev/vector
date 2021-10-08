03873963e98e3815aa2f948a6c7f6994152aa530
Nathan Fox <fuchsnj@gmail.com>
2021-10-07 22:17:30 -0400

    I guess that looks a little nicer
    Signed-off-by: Nathan Fox <fuchsnj@gmail.com>

--- scripts/environment/release-flags.sh
+++ scripts/environment/release-flags.sh
@@ -6,7 +6,9 @@ set -e -o verbose
 # code, but increases compilation time.  We only set this if we're in CI _and_ we haven't
 # been instructed to use the debug profile (via PROFILE environment variable).
 if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
-    echo "CARGO_PROFILE_RELEASE_LTO=fat" >> "${GITHUB_ENV}"
-    echo "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1" >> "${GITHUB_ENV}"
-    echo "CARGO_PROFILE_RELEASE_DEBUG=false" >> "${GITHUB_ENV}"
+  {
+    echo "CARGO_PROFILE_RELEASE_LTO=fat";
+    echo "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1";
+    echo "CARGO_PROFILE_RELEASE_DEBUG=false";
+  } >> "${GITHUB_ENV}"
 fi
