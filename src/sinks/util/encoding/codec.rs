545cdacc993a17b519288baf4aef9840b9f69c5e
Toby Lawrence <toby@nuclearfurnace.com>
2021-10-07 23:49:14 -0400

    fix tests and such
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- src/sinks/util/encoding/codec.rs
+++ src/sinks/util/encoding/codec.rs
@@ -27,59 +27,113 @@ pub enum StandardEncodings {
 
 impl StandardEncodings {
     fn batch_pre_hook(self, writer: &mut dyn io::Write) -> io::Result<usize> {
-        match self {
-            StandardEncodings::Json => {
-                let buf = &[b'['];
-                writer.write_all(buf).map(|()| buf.len())
-            }
-            _ => Ok(0),
+        let buf = match self {
+            StandardEncodings::Json => Some(&[b'[']),
+            _ => None,
+        };
+
+        if let Some(buf) = buf {
+            writer.write_all(buf).map(|()| buf.len())
+        } else {
+            Ok(0)
         }
     }
 
     fn batch_post_hook(self, writer: &mut dyn io::Write) -> io::Result<usize> {
-        match self {
-            StandardEncodings::Json => {
-                let buf = &[b']'];
-                writer.write_all(buf).map(|()| buf.len())
-            }
-            _ => Ok(0),
+        let buf = match self {
+            StandardEncodings::Json => Some(&[b']']),
+            _ => None,
+        };
+
+        if let Some(buf) = buf {
+            writer.write_all(buf).map(|()| buf.len())
+        } else {
+            Ok(0)
         }
     }
 
-    const fn batch_delimiter(&self) -> &[u8] {
-        match self {
-            StandardEncodings::Text | StandardEncodings::Ndjson => &[b'\n'],
-            StandardEncodings::Json => &[b','],
+    fn batch_delimiter_hook(self, writer: &mut dyn io::Write) -> io::Result<usize> {
+        let buf = match self {
+            StandardEncodings::Json => Some(&[b',']),
+            StandardEncodings::Text => Some(&[b'\n']),
+            _ => None,
+        };
+
+        if let Some(buf) = buf {
+            writer.write_all(buf).map(|()| buf.len())
+        } else {
+            Ok(0)
+        }
+    }
+
+    fn batch_trailer_hook(self, writer: &mut dyn io::Write) -> io::Result<usize> {
+        let buf = match self {
+            StandardEncodings::Ndjson => Some(&[b'\n']),
+            _ => None,
+        };
+
+        if let Some(buf) = buf {
+            writer.write_all(buf).map(|()| buf.len())
+        } else {
+            Ok(0)
+        }
+    }
+
+    fn single_trailer_hook(self, writer: &mut dyn io::Write) -> io::Result<usize> {
+        let buf = match self {
+            StandardEncodings::Ndjson => Some(&[b'\n']),
+            _ => None,
+        };
+
+        if let Some(buf) = buf {
+            writer.write_all(buf).map(|()| buf.len())
+        } else {
+            Ok(0)
         }
     }
 }
 
 impl Encoder<Event> for StandardEncodings {
     fn encode_input(&self, input: Event, writer: &mut dyn io::Write) -> io::Result<usize> {
-        match self {
+        let mut written = 0;
+
+        let n = match self {
             StandardEncodings::Text => DEFAULT_TEXT_ENCODER.encode_input(input, writer),
             StandardEncodings::Json => DEFAULT_JSON_ENCODER.encode_input(input, writer),
             StandardEncodings::Ndjson => DEFAULT_JSON_ENCODER.encode_input(input, writer),
-        }
+        }?;
+        written += n;
+
+        let n = self.single_trailer_hook(writer)?;
+        written += n;
+
+        Ok(written)
     }
 }
 
 impl Encoder<Vec<Event>> for StandardEncodings {
     fn encode_input(&self, input: Vec<Event>, writer: &mut dyn io::Write) -> io::Result<usize> {
         let mut written = 0;
-        let delimiter = self.batch_delimiter();
 
         let n = self.batch_pre_hook(writer)?;
         written += n;
 
         let last = input.len();
         for (i, event) in input.into_iter().enumerate() {
-            let n = self.encode_input(event, writer)?;
+            let n = match self {
+                StandardEncodings::Text => DEFAULT_TEXT_ENCODER.encode_input(event, writer),
+                StandardEncodings::Json => DEFAULT_JSON_ENCODER.encode_input(event, writer),
+                StandardEncodings::Ndjson => DEFAULT_JSON_ENCODER.encode_input(event, writer),
+            }?;
             written += n;
-            if i != last {
-                let _ = writer.write_all(delimiter)?;
-                written += delimiter.len();
+
+            if i != last - 1 {
+                let n = self.batch_delimiter_hook(writer)?;
+                written += n;
             }
+
+            let n = self.batch_trailer_hook(writer)?;
+            written += n;
         }
 
         let n = self.batch_post_hook(writer)?;
@@ -181,47 +235,97 @@ mod tests {
         result.map(|_| buf)
     }
 
+    fn encode_events(events: Vec<Event>, encoding: StandardEncodings) -> io::Result<Vec<u8>> {
+        let mut buf = Vec::new();
+        let result = encoding.encode_input(events, &mut buf);
+        result.map(|_| buf)
+    }
+
     #[test]
-    fn test_standard_text() {
+    fn test_standard_text_log_single() {
         let encoding = StandardEncodings::Text;
 
         let message = "log event";
-        let log_event = Event::from(message.to_string());
+        let event = Event::from(message.to_string());
 
-        let result = encode_event(log_event, encoding).expect("should not have failed");
+        let result = encode_event(event, encoding).expect("should not have failed");
         let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
 
-        let expected = format!("{}\n", message);
+        let expected = message;
         assert_eq!(expected, encoded);
     }
 
     #[test]
-    #[should_panic]
-    fn test_standard_text_panics_with_metric_event() {
+    fn test_standard_text_log_multiple() {
         let encoding = StandardEncodings::Text;
 
-        let metric_event = Metric::new(
+        let message1 = "log event 1";
+        let event1 = Event::from(message1.to_string());
+
+        let message2 = "log event 2";
+        let event2 = Event::from(message2.to_string());
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected = format!("{}\n{}", message1, message2);
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_text_metric_single() {
+        let encoding = StandardEncodings::Text;
+
+        let event = Metric::new(
             "namespace",
             MetricKind::Absolute,
             MetricValue::Counter { value: 1.23 },
         )
         .into();
 
-        let _result = encode_event(metric_event, encoding);
+        let result = encode_event(event, encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected = "namespace{} = 1.23";
+        assert_eq!(expected, encoded);
     }
 
     #[test]
-    fn test_standard_json() {
+    fn test_standard_text_metric_multiple() {
+        let encoding = StandardEncodings::Text;
+
+        let event1 = Metric::new(
+            "namespace1",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 1.23 },
+        )
+        .into();
+        let event2 = Metric::new(
+            "namespace2",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 3.14 },
+        )
+        .into();
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected = "namespace1{} = 1.23\nnamespace2{} = 3.14";
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_json_log_single() {
         let msg_key = log_schema().message_key();
         let ts_key = log_schema().timestamp_key();
         let now = Utc::now();
         let encoding = StandardEncodings::Json;
 
         let message = "log event";
-        let mut log_event = Event::from(message.to_string());
-        log_event.as_mut_log().insert(ts_key, now);
+        let mut event = Event::from(message.to_string());
+        event.as_mut_log().insert(ts_key, now);
 
-        let result = encode_event(log_event, encoding).expect("should not have failed");
+        let result = encode_event(event, encoding).expect("should not have failed");
         let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
 
         // We have to hard-code the transformation of the timestamp here, as `chrono::DateTime`
@@ -237,17 +341,100 @@ mod tests {
     }
 
     #[test]
-    fn test_standard_ndjson() {
+    fn test_standard_json_log_multiple() {
+        let msg_key = log_schema().message_key();
+        let ts_key = log_schema().timestamp_key();
+        let now = Utc::now();
+        let encoding = StandardEncodings::Json;
+
+        let message1 = "log event1";
+        let mut event1 = Event::from(message1.to_string());
+        event1.as_mut_log().insert(ts_key, now);
+
+        let message2 = "log event2";
+        let mut event2 = Event::from(message2.to_string());
+        event2.as_mut_log().insert(ts_key, now);
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        // We have to hard-code the transformation of the timestamp here, as `chrono::DateTime`
+        // uses a more timezone-explicit format in its `Display` implementation, while its
+        // `Serialize` implementation uses RFC3339.
+        let expected = format!(
+            "[{{\"{}\":\"{}\",\"{}\":\"{}\"}},{{\"{}\":\"{}\",\"{}\":\"{}\"}}]",
+            msg_key,
+            message1,
+            ts_key,
+            now.to_rfc3339_opts(SecondsFormat::AutoSi, true),
+            msg_key,
+            message2,
+            ts_key,
+            now.to_rfc3339_opts(SecondsFormat::AutoSi, true),
+        );
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_json_metric_single() {
+        let encoding = StandardEncodings::Json;
+
+        let event = Metric::new(
+            "namespace",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 1.23 },
+        )
+        .into();
+
+        let result = encode_event(event, encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected =
+            "{\"name\":\"namespace\",\"kind\":\"absolute\",\"counter\":{\"value\":1.23}}";
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_json_metric_multiple() {
+        let encoding = StandardEncodings::Json;
+
+        let event1 = Metric::new(
+            "namespace1",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 1.23 },
+        )
+        .into();
+
+        let event2 = Metric::new(
+            "namespace2",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 3.14 },
+        )
+        .into();
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected1 =
+            "{\"name\":\"namespace1\",\"kind\":\"absolute\",\"counter\":{\"value\":1.23}}";
+        let expected2 =
+            "{\"name\":\"namespace2\",\"kind\":\"absolute\",\"counter\":{\"value\":3.14}}";
+        let expected = format!("[{},{}]", expected1, expected2);
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_ndjson_log_single() {
         let msg_key = log_schema().message_key();
         let ts_key = log_schema().timestamp_key();
         let now = Utc::now();
         let encoding = StandardEncodings::Ndjson;
 
         let message = "log event";
-        let mut log_event = Event::from(message.to_string());
-        log_event.as_mut_log().insert(ts_key, now);
+        let mut event = Event::from(message.to_string());
+        event.as_mut_log().insert(ts_key, now);
 
-        let result = encode_event(log_event, encoding).expect("should not have failed");
+        let result = encode_event(event, encoding).expect("should not have failed");
         let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
 
         // We have to hard-code the transformation of the timestamp here, as `chrono::DateTime`
@@ -259,6 +446,89 @@ mod tests {
             ts_key,
             now.to_rfc3339_opts(SecondsFormat::AutoSi, true)
         );
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_ndjson_log_multiple() {
+        let msg_key = log_schema().message_key();
+        let ts_key = log_schema().timestamp_key();
+        let now = Utc::now();
+        let encoding = StandardEncodings::Ndjson;
+
+        let message1 = "log event1";
+        let mut event1 = Event::from(message1.to_string());
+        event1.as_mut_log().insert(ts_key, now);
+
+        let message2 = "log event2";
+        let mut event2 = Event::from(message2.to_string());
+        event2.as_mut_log().insert(ts_key, now);
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        // We have to hard-code the transformation of the timestamp here, as `chrono::DateTime`
+        // uses a more timezone-explicit format in its `Display` implementation, while its
+        // `Serialize` implementation uses RFC3339.
+        let expected = format!(
+            "{{\"{}\":\"{}\",\"{}\":\"{}\"}}\n{{\"{}\":\"{}\",\"{}\":\"{}\"}}\n",
+            msg_key,
+            message1,
+            ts_key,
+            now.to_rfc3339_opts(SecondsFormat::AutoSi, true),
+            msg_key,
+            message2,
+            ts_key,
+            now.to_rfc3339_opts(SecondsFormat::AutoSi, true),
+        );
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_ndjson_metric_single() {
+        let encoding = StandardEncodings::Ndjson;
+
+        let event = Metric::new(
+            "namespace",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 1.24 },
+        )
+        .into();
+
+        let result = encode_event(event, encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected =
+            "{\"name\":\"namespace\",\"kind\":\"absolute\",\"counter\":{\"value\":1.24}}\n";
+        assert_eq!(expected, encoded);
+    }
+
+    #[test]
+    fn test_standard_ndjson_metric_multiple() {
+        let encoding = StandardEncodings::Ndjson;
+
+        let event1 = Metric::new(
+            "namespace1",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 1.24 },
+        )
+        .into();
+
+        let event2 = Metric::new(
+            "namespace2",
+            MetricKind::Absolute,
+            MetricValue::Counter { value: 3.15 },
+        )
+        .into();
+
+        let result = encode_events(vec![event1, event2], encoding).expect("should not have failed");
+        let encoded = std::str::from_utf8(&result).expect("result should be valid UTF-8");
+
+        let expected1 =
+            "{\"name\":\"namespace1\",\"kind\":\"absolute\",\"counter\":{\"value\":1.24}}";
+        let expected2 =
+            "{\"name\":\"namespace2\",\"kind\":\"absolute\",\"counter\":{\"value\":3.15}}";
+        let expected = format!("{}\n{}\n", expected1, expected2);
         assert_eq!(expected, encoded);
     }
 }
