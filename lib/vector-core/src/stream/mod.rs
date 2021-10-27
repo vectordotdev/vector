87cfdf783c2c2223f5dae2fe2ad9c1b5f500afe2
Nathan Fox <fuchsnj@gmail.com>
2021-10-27 12:29:27 -0400

    enhancement(sinks): enforce `EventsSent` instrumentation for new-style sinks (#9801)
    Signed-off-by: Nathan Fox <fuchsnj@gmail.com>

--- lib/vector-core/src/stream/mod.rs
+++ lib/vector-core/src/stream/mod.rs
@@ -2,6 +2,8 @@ mod batcher;
 mod concurrent_map;
 mod driver;
 
+pub use driver::DriverResponse;
+
 pub use batcher::{Batcher, BatcherSettings, ExpirationQueue};
 pub use concurrent_map::ConcurrentMap;
 pub use driver::Driver;
