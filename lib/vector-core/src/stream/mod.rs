760cc936b3bc7922a9396fa88350a4fc128f4563
Toby Lawrence <tobz@users.noreply.github.com>
2021-10-27 15:31:26 -0400

    fix(architecture): fix bug in Driver that causes hang (#9804)
    * fix(architecture): fix bug in Driver that causes hang
    
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- lib/vector-core/src/stream/mod.rs
+++ lib/vector-core/src/stream/mod.rs
@@ -1,9 +1,11 @@
 mod batcher;
 mod concurrent_map;
 mod driver;
+mod futures_unordered_chunked;
 
 pub use driver::DriverResponse;
 
 pub use batcher::{Batcher, BatcherSettings, ExpirationQueue};
 pub use concurrent_map::ConcurrentMap;
 pub use driver::Driver;
+pub use futures_unordered_chunked::FuturesUnorderedChunked;
