fdb01c0434d9a70c8f3f02e2544367c10e41b346
Brian L. Troutwine <brian@troutwine.us>
2021-11-01 11:30:56 -0700

    chore: Run soaks in CI  (#9818)
    * Run soaks in CI
    
    This commit introduces a new workflow 'soak' that runs the soaks
    introduced in #9699 in Github Action CI. There is, at present, no PR
    reporting and every push to the project generates a soak run, except
    those done by dependabot.
    
    The soak.sh script has been changed in its interface as well. Please
    see README for details.
    
    Closes #9619
    Closes #9618
    Closes #9620
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * fix check-script dings
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * remove build-args, clean runner
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * pushd into baseline-vector to calculate sha
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * ignore master <> master soak
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * adjust tag structure, documentation
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>
    
    * correctly compute tag for baseline
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- soaks/syslog_loki/terraform/variables.tf
+++ soaks/syslog_loki/terraform/variables.tf
@@ -7,8 +7,3 @@ variable "vector_image" {
   description = "The image of vector to use in this investigation"
   type        = string
 }
-
-variable "sha" {
-  description = "The commit SHA from the Vector project under investigation"
-  type        = string
-}
