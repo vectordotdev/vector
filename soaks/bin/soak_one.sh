089a6af1e1483e4be89cd49bdbfd44f332da2061
Brian L. Troutwine <brian@troutwine.us>
2021-11-01 10:42:23 -0700

    adjust tag structure, documentation
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- soaks/bin/soak_one.sh
+++ soaks/bin/soak_one.sh
@@ -9,24 +9,24 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 display_usage() {
     echo ""
-    echo "Usage: $0 USE_LOCAL_IMAGE SOAK_NAME VARIANT SHA CAPTURE_DIR"
+    echo "Usage: $0 USE_LOCAL_IMAGE SOAK_NAME VARIANT TAG CAPTURE_DIR"
 }
 
 USE_LOCAL_IMAGE="${1}"
 SOAK_NAME="${2}"
 VARIANT="${3}"
-SHA="${4}"
+TAG="${4}"
 CAPTURE_DIR="${5}"
 
 pushd "${__dir}"
 
-IMAGE="vector:${SHA}"
+IMAGE="vector:${TAG}"
 if [ "${USE_LOCAL_IMAGE}" = "true" ]; then
     echo "Building images locally..."
 
-    ./build_container.sh "${SHA}" "${IMAGE}"
+    ./build_container.sh "${TAG}" "${IMAGE}"
 else
-    REMOTE_IMAGE="ghcr.io/vectordotdev/vector/soak-vector:${SHA}"
+    REMOTE_IMAGE="ghcr.io/vectordotdev/vector/soak-vector:${TAG}"
     docker pull "${REMOTE_IMAGE}"
     docker image tag "${REMOTE_IMAGE}" "${IMAGE}"
 fi
