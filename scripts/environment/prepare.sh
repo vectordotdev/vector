a7f8fbe3cb6fffb002e05dade0c06f13710d608a
Jesse Szwedko <jesse@szwedko.me>
2021-11-01 21:24:23 -0600

    default rust profile already installs rustfmt and clippy
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -3,8 +3,6 @@ set -e -o verbose
 
 rustup show # causes installation of version from rust-toolchain.toml
 rustup default "$(rustup show active-toolchain | awk '{print $1;}')"
-rustup component add rustfmt
-rustup component add clippy
 rustup run stable cargo install cargo-deb --version 1.29.2
 rustup run stable cargo install cross --version 0.2.1
 
