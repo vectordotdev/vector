544e4c74bc53e97fc98315dfcd8842187bc3e305
Lee Benson <lee@leebenson.com>
2021-11-03 20:32:52 +0000

    enhancement(pipelines): Report `datadog.configuration` as a metrics tag for DD Pipelines (#9850)
    * order test
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * zip
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * eq
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * -warning
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * hash test
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * rename config_hash
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * version
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * Config should be versioned
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * commentary
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * datadog.configuration
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * wip
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * commentary
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * default enabled
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * version tidy up
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * fix tests
    
    Signed-off-by: Lee Benson <lee@leebenson.com>
    
    * configuration_key
    
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/config/datadog.rs
+++ src/config/datadog.rs
@@ -11,13 +11,15 @@ static INTERNAL_METRICS_KEY: &str = "#datadog_internal_metrics";
 static DATADOG_METRICS_KEY: &str = "#datadog_metrics";
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
-#[serde(default, deny_unknown_fields)]
+#[serde(deny_unknown_fields)]
 pub struct Options {
     #[serde(default = "default_enabled")]
     pub enabled: bool,
 
     #[serde(default)]
     pub api_key: Option<String>,
+
+    pub configuration_key: String,
 
     #[serde(default = "default_reporting_interval_secs")]
     pub reporting_interval_secs: u64,
@@ -28,6 +30,7 @@ impl Default for Options {
         Self {
             enabled: default_enabled(),
             api_key: None,
+            configuration_key: "".to_owned(),
             reporting_interval_secs: default_reporting_interval_secs(),
         }
     }
@@ -35,7 +38,7 @@ impl Default for Options {
 
 /// By default, the Datadog feature is enabled.
 const fn default_enabled() -> bool {
-    false
+    true
 }
 
 /// By default, report to Datadog every 5 seconds.
@@ -46,8 +49,14 @@ const fn default_reporting_interval_secs() -> u64 {
 /// Augment configuration with observability via Datadog if the feature is enabled and
 /// an API key is provided.
 pub fn try_attach(config: &mut Config) -> bool {
+    // Only valid if a [datadog] section is present in config.
+    let datadog = match config.datadog.as_ref() {
+        Some(datadog) => datadog,
+        _ => return false,
+    };
+
     // Return early if an API key is missing, or the feature isn't enabled.
-    let api_key = match (&config.datadog.api_key, config.datadog.enabled) {
+    let api_key = match (&datadog.api_key, datadog.enabled) {
         // API key provided explicitly.
         (Some(api_key), true) => api_key.clone(),
         // No API key; attempt to get it from the environment.
@@ -60,20 +69,21 @@ pub fn try_attach(config: &mut Config) -> bool {
 
     info!("Datadog API key provided. Host and internal metrics will be sent to Datadog.");
 
+    let version = config.version.as_ref().expect("Config should be versioned");
+
     let host_metrics_id = OutputId::from(ComponentKey::from(HOST_METRICS_KEY));
     let internal_metrics_id = OutputId::from(ComponentKey::from(INTERNAL_METRICS_KEY));
     let datadog_metrics_id = ComponentKey::from(DATADOG_METRICS_KEY);
 
-    let version = config.version.as_ref().expect("Config should be versioned");
-
     // Create internal sources for host and internal metrics. We're using distinct sources here and
     // not attempting to reuse existing ones, to configure according to enterprise requirements.
-    let mut host_metrics = HostMetricsConfig::enterprise(version);
-    let mut internal_metrics = InternalMetricsConfig::enterprise(version);
+    let mut host_metrics = HostMetricsConfig::enterprise(version, &datadog.configuration_key);
+    let mut internal_metrics =
+        InternalMetricsConfig::enterprise(version, &datadog.configuration_key);
 
     // Override default scrape intervals.
-    host_metrics.scrape_interval_secs(config.datadog.reporting_interval_secs);
-    internal_metrics.scrape_interval_secs(config.datadog.reporting_interval_secs);
+    host_metrics.scrape_interval_secs(datadog.reporting_interval_secs);
+    internal_metrics.scrape_interval_secs(datadog.reporting_interval_secs);
 
     config.sources.insert(
         host_metrics_id.component.clone(),
@@ -113,11 +123,8 @@ mod tests {
     fn default() {
         let config = default_with_hash();
 
-        // The Datadog config should be disabled by default.
-        assert!(!config.datadog.enabled);
-
-        // There should be no API key.
-        assert_eq!(config.datadog.api_key, None);
+        // The Datadog config shouldn't exist by default.
+        assert!(config.datadog.is_none());
     }
 
     #[test]
@@ -142,10 +149,13 @@ mod tests {
     fn enabled() {
         let mut config = default_with_hash();
 
-        // Explicitly set to enabled and provide an API key to activate.
-        config.datadog.enabled = true;
-        config.datadog.api_key = Some("xxx".to_string());
+        config.datadog = Some(Options {
+            api_key: Some("xxx".to_owned()),
+            configuration_key: "zzz".to_owned(),
+            ..Options::default()
+        });
 
+        // Explicitly set to enabled and provide an API key to activate.
         assert!(try_attach(&mut config));
 
         assert!(config
@@ -157,13 +167,5 @@ mod tests {
         assert!(config
             .sinks
             .contains_key(&ComponentKey::from(DATADOG_METRICS_KEY)));
-    }
-
-    #[test]
-    fn default_reporting_interval_secs() {
-        let config = default_with_hash();
-
-        // Reporting interval should default to 5 seconds.
-        assert_eq!(config.datadog.reporting_interval_secs, 5);
     }
 }
