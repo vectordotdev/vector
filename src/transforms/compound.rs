54deadeb41a51fcbc1ac7f5dcc254615721295ed
Jérémie Drouet <jdrouet@users.noreply.github.com>
2021-11-04 14:06:46 +0100

    feat(pipelines transform): load and handle pipelines tranforms (#9733)
    * feat(pipelines transform): create config structure
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>
    
    * make it work with configs
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * recursive expansion
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * make the graph works
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * refactor serial expansion with alias flag
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * remove manual aliases
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * remove hack
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add transforms-pipelines to default features
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add some pipelines behavior tests
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * remove redundant lifetime
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * update clippy
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * fix tests
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * fix behavior tests
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * fix transform name on in behavior test
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add documentation for pipelines transform
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * use indoc for configuration sample
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * move config and remove useless comments
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add some doc around ExpandType
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add some more doc
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>
    
    * add some doc and an expansion example
    
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/transforms/compound.rs
+++ src/transforms/compound.rs
@@ -55,7 +55,7 @@ impl TransformConfig for CompoundConfig {
         }
 
         if !map.is_empty() {
-            Ok(Some((map, ExpandType::Serial)))
+            Ok(Some((map, ExpandType::Serial { alias: false })))
         } else {
             Err("must specify at least one transform".into())
         }
@@ -106,7 +106,7 @@ mod test {
 
         assert_eq!(
             serde_json::to_string(&config).unwrap(),
-            r#"[{"0":{"type":"mock"},"foo":{"type":"mock"}},"Serial"]"#
+            r#"[{"0":{"type":"mock"},"foo":{"type":"mock"}},{"Serial":{"alias":false}}]"#
         );
     }
 }
