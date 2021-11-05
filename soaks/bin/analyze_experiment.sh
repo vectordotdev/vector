1ff5364fa1c010bc8ea71b99ef10efdd5cf4dab0
Brian L. Troutwine <brian@troutwine.us>
2021-11-04 20:26:52 -0700

    Update comment aesthetic
    Closes #9926
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- soaks/bin/analyze_experiment.sh
+++ soaks/bin/analyze_experiment.sh
@@ -14,29 +14,31 @@ CAPTURE_DIR="${1}"
 BASELINE_SHA="${2}"
 COMPARISON_SHA="${3}"
 
-echo "Captures recorded between ${BASELINE_SHA} and ${COMPARISON_SHA}"
+echo "# Soak Test Results"
+echo "Baseline: ${BASELINE_SHA}"
+echo "Comparison: ${COMPARISON_SHA}"
 echo ""
-echo "Here is a statistical summary of the soak captures between the SHAs given"
-echo "above. Units are bytes, except for 'skewness' and 'kurtosis'. Higher numbers in 'comparison'"
-echo "is generally better. Higher skewness or kurtosis numbers indicate a"
-echo "lack of consistency in behavior, making predictions of fitness in the"
-echo "field challenging."
+echo "What follows is a statistical summary of the soak captures between the SHAs given above. Units are bytes/second, except for 'skewness' and 'kurtosis'. Higher numbers in 'comparison' is generally better. Higher skewness or kurtosis numbers indicate a lack of consistency in behavior, making predictions of fitness in the field challenging."
 echo ""
 for soak_dir in "${CAPTURE_DIR}"/*; do
     SOAK_NAME=$(basename "${soak_dir}")
     echo " --- "
-    echo "# \`${SOAK_NAME}\`"
+    echo "## \`${SOAK_NAME}\`"
     # NOTE if you change the statistics being pulled here please update the
     # header/body divisor below. Consider that you need one column for the group
     # and one for each statistic.
-    OUTPUT=$(mlr --tsv \
-        --from "${soak_dir}/baseline.captures" \
-        --from "${soak_dir}/comparison.captures" \
-        stats1 -a 'min,p90,p99,max,skewness,kurtosis' -g EXPERIMENT -f VALUE)
+    OUTPUT=$(
+        mlr --itsv --ocsv \
+            --from "${soak_dir}/baseline.captures" \
+            --from "${soak_dir}/comparison.captures" \
+            stats1 -a 'min,p90,p99,max,skewness,kurtosis' -g EXPERIMENT -f VALUE | \
+        numfmt --header=1 --to=iec-i --format="%.2f" --field="2-5" --delimiter="," | \
+        numfmt --header=1 --to=none  --format="%.2f" --field="6-7" --delimiter=","
+    )
     HEADER=$(echo "${OUTPUT}" | head -n1)
     BODY=$(echo "${OUTPUT}" | tail -n+2)
 
     echo "${HEADER}" | sed 's/^/|\ /g' | sed 's/\t/\ |\ /g' | sed 's/$/\ |/g'
     echo "| --- | --- | --- | --- | --- | --- | --- |"
-    echo "${BODY}" | sed 's/^/|\ /g' | sed 's/\t/\ |\ /g' | sed 's/$/\ |/g'
+    echo "${BODY}" | sed 's/,/\ \|\ /g' | sed 's/^/|\ /g' | sed 's/$/\ |/g'
 done
