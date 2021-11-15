8aa646909a9132a2d61d39b656159f4f9fcc3a2d
001wwang <will.wang@datadoghq.com>
2021-11-15 17:13:12 -0500

    Add lading_splunk_hec_gen terraform module
    Signed-off-by: 001wwang <will.wang@datadoghq.com>

---
+++ soaks/common/terraform/modules/lading_splunk_hec_gen/variables.tf
@@ -0,0 +1,14 @@
+variable "type" {
+  description = "The type of the vector install, whether 'baseline' or 'comparison'"
+  type        = string
+}
+
+variable "namespace" {
+  description = "The namespace in which to run"
+  type        = string
+}
+
+variable "splunk-hec-gen-toml" {
+  description = "The rendered splunk_hec_gen.toml for this test"
+  type        = string
+}
