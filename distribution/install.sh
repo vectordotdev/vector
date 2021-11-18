b6236fce375cd2c8a2b62b9c8c6a405feea9c191
Jesse Szwedko <jesse@szwedko.me>
2021-11-18 09:05:57 -0500

    Version bump
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- distribution/install.sh
+++ distribution/install.sh
@@ -12,7 +12,7 @@ set -u
 
 # If PACKAGE_ROOT is unset or empty, default it.
 PACKAGE_ROOT="${PACKAGE_ROOT:-"https://packages.timber.io/vector"}"
-VECTOR_VERSION="0.17.3"
+VECTOR_VERSION="0.18.0"
 _divider="--------------------------------------------------------------------------------"
 _prompt=">>>"
 _indent="   "
