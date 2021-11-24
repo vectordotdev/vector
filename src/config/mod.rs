b516b116c3ff9b744fad398b3c549a700feacfcf
Jérémie Drouet <jdrouet@users.noreply.github.com>
2021-11-24 19:42:53 +0100

    feat(pipelines transform): forbid nesting pipelines transform (#10098)
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -439,13 +439,25 @@ impl TransformOuter<String> {
     pub(crate) fn expand(
         mut self,
         key: ComponentKey,
+        parent_types: &HashSet<&'static str>,
         transforms: &mut IndexMap<ComponentKey, TransformOuter<String>>,
         expansions: &mut IndexMap<ComponentKey, Vec<ComponentKey>>,
     ) -> Result<(), String> {
+        if !self.inner.nestable(parent_types) {
+            return Err(format!(
+                "the component {} cannot be nested in {:?}",
+                self.inner.transform_type(),
+                parent_types
+            ));
+        }
+
         let expansion = self
             .inner
             .expand()
             .map_err(|err| format!("failed to expand transform '{}': {}", key, err))?;
+
+        let mut ptypes = parent_types.clone();
+        ptypes.insert(self.inner.transform_type());
 
         if let Some((expanded, expand_type)) = expansion {
             let mut children = Vec::new();
@@ -458,7 +470,7 @@ impl TransformOuter<String> {
                     inputs,
                     inner: content,
                 };
-                child.expand(full_name.clone(), transforms, expansions)?;
+                child.expand(full_name.clone(), &ptypes, transforms, expansions)?;
                 children.push(full_name.clone());
 
                 inputs = match expand_type {
@@ -1137,5 +1149,51 @@ mod resource_tests {
             Some(Format::Toml),
         )
         .is_err());
+    }
+}
+
+#[cfg(all(
+    test,
+    feature = "sources-stdin",
+    feature = "sinks-console",
+    feature = "transforms-pipelines",
+    feature = "transforms-filter"
+))]
+mod pipelines_tests {
+    use super::{load_from_str, Format};
+    use indoc::indoc;
+
+    #[test]
+    fn forbid_pipeline_nesting() {
+        let res = load_from_str(
+            indoc! {r#"
+                [sources.in]
+                  type = "stdin"
+
+                [transforms.processing]
+                  inputs = ["in"]
+                  type = "pipelines"
+
+                  [transforms.processing.logs.pipelines.foo]
+                    name = "foo"
+
+                    [[transforms.processing.logs.pipelines.foo.transforms]]
+                      type = "pipelines"
+
+                      [transforms.processing.logs.pipelines.foo.transforms.logs.pipelines.bar]
+                        name = "bar"
+
+                          [[transforms.processing.logs.pipelines.foo.transforms.logs.pipelines.bar.transforms]]
+                            type = "filter"
+                            condition = ""
+
+                [sinks.out]
+                  type = "console"
+                  inputs = ["processing"]
+                  encoding = "json"
+            "#},
+            Some(Format::Toml),
+        );
+        assert!(res.is_err(), "should error");
     }
 }
