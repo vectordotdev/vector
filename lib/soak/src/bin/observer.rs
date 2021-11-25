9cdc72f245e35c6c2186e59091ddf4501661f694
Brian L. Troutwine <brian@troutwine.us>
2021-11-24 19:08:30 -0800

    now able to generate sensible tables
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lib/soak/src/bin/observer.rs
+++ lib/soak/src/bin/observer.rs
@@ -2,10 +2,10 @@
 //! writes the result out to disk. It replaces curl-in-a-loop in our soak infra.
 use argh::FromArgs;
 use reqwest::Url;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use snafu::Snafu;
-use std::io::Read;
 use std::time::Duration;
+use std::{borrow::Cow, io::Read};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use tokio::runtime::Builder;
@@ -24,6 +24,14 @@ struct Opts {
     config_path: String,
 }
 
+/// Query configuration
+#[derive(Debug, Deserialize)]
+pub struct Query {
+    id: String,
+    query: String,
+    unit: soak::Unit,
+}
+
 /// Main configuration struct for this program
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -36,7 +44,7 @@ pub struct Config {
     /// The vector ID associated with the experiment
     pub vector_id: String,
     /// The queries to make of the experiment
-    pub queries: Vec<String>,
+    pub queries: Vec<Query>,
     /// The file to record captures into
     pub capture_path: String,
 }
@@ -53,20 +61,6 @@ enum Error {
     ParseFloat { error: std::num::ParseFloatError },
     #[snafu(display("Could not serialize output: {}", error))]
     Json { error: serde_json::Error },
-}
-
-#[derive(Debug, Serialize)]
-pub struct Query<'a> {
-    query: &'a str,
-    value: f64,
-}
-
-#[derive(Debug, Serialize)]
-pub struct Output<'a> {
-    experiment: &'a str,
-    vector_id: &'a str,
-    time: f64,
-    queries: Vec<Query<'a>>,
 }
 
 impl From<reqwest::Error> for Error {
@@ -134,24 +128,23 @@ struct QueryResponse {
 }
 
 struct Worker {
-    url: Url,
     vector_id: String,
     experiment_name: String,
-    queries: Vec<String>,
+    queries: Vec<(Query, Url)>,
     startup_delay: u64,
     capture_path: String,
 }
 
 impl Worker {
     fn new(config: Config) -> Self {
-        let queries = Vec::new();
+        let mut queries = Vec::with_capacity(config.queries.len());
         for query in config.queries {
-            let url = format!("{}/api/v1/query?query={}", config.prometheus, query);
-            queries.push(url);
+            let url = format!("{}/api/v1/query?query={}", config.prometheus, query.query);
+            let url = url.parse::<Url>().unwrap();
+            queries.push((query, url));
         }
 
         Self {
-            url: url.parse::<Url>().unwrap(),
             vector_id: config.vector_id,
             experiment_name: config.experiment_name,
             queries,
@@ -172,38 +165,47 @@ impl Worker {
         sleep(Duration::from_secs(self.startup_delay)).await;
         info!("finished with sleep");
 
-        file.write_all(b"EXPERIMENT\tVECTOR-ID\tTIME\tQUERY\tVALUE\n")
-            .await?;
-        file.flush().await?;
-        loop {
-            let request = client.get(self.url.clone()).build()?;
-            let body = client
-                .execute(request)
-                .await?
-                .json::<QueryResponse>()
-                .await?;
-            debug!("body: {:?}", body.data);
+        for fetch_index in 0..u64::max_value() {
+            for (query, url) in &self.queries {
+                let request = client.get(url.clone()).build()?;
+                let body = client
+                    .execute(request)
+                    .await?
+                    .json::<QueryResponse>()
+                    .await?;
+                debug!("body: {:?}", body.data);
 
-            if !body.data.result.is_empty() {
-                let time = body.data.result[0].value.time;
-                let value = body.data.result[0].value.value.parse::<f64>()?;
-                let output = serde_json::to_string(&Output {
-                    experiment: &self.experiment_name,
-                    vector_id: &self.vector_id,
-                    time,
-                    queries: vec![Query {
-                        query: &self.query,
-                        value,
-                    }],
-                })?;
-                file.write_all(output.as_bytes()).await?;
-                file.write_all(b"\n").await?;
-                file.flush().await?;
-            } else {
-                error!("failed to request body: {:?}", body.data);
+                if !body.data.result.is_empty() {
+                    let time = body.data.result[0].value.time;
+                    let value = body.data.result[0].value.value.parse::<f64>()?;
+                    let output = serde_json::to_string(&soak::Output {
+                        experiment: Cow::Borrowed(&self.experiment_name),
+                        vector_id: Cow::Borrowed(&self.vector_id),
+                        time,
+                        query: soak::Query {
+                            id: Cow::Borrowed(&query.id),
+                            query: Cow::Borrowed(&query.query),
+                            value,
+                            unit: query.unit,
+                        },
+                        fetch_index,
+                    })?;
+                    info!("{}", output);
+                    file.write_all(output.as_bytes()).await?;
+                    file.write_all(b"\n").await?;
+                    file.flush().await?;
+                } else {
+                    error!("failed to request body: {:?}", body.data);
+                }
             }
             sleep(Duration::from_secs(1)).await;
         }
+        // SAFETY: The only way to reach this point is to break the above loop
+        // -- which we do not -- or to traverse u64::MAX seconds, implying that
+        // the computer running this program has been migrated away from the
+        // Earth meanwhile as our dear craddle is now well encompassed by the
+        // sun. Unless we moved the Earth.
+        unreachable!()
     }
 }
 
@@ -215,7 +217,7 @@ fn get_config() -> Config {
         .unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    toml::from_str(&contents).unwrap()
+    serde_yaml::from_str(&contents).unwrap()
 }
 
 fn main() {
--- lib/soak/src/lib.rs
+++ lib/soak/src/lib.rs
@@ -1,1 +1,35 @@
-// nothing, intentionally
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+pub enum Unit {
+    Raw,
+    Bytes,
+}
+
+pub fn default_unit() -> Unit {
+    Unit::Raw
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Query<'a> {
+    #[serde(borrow)]
+    pub query: Cow<'a, str>,
+    #[serde(borrow)]
+    pub id: Cow<'a, str>,
+    pub value: f64,
+    #[serde(default = "default_unit")]
+    pub unit: Unit,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output<'a> {
+    #[serde(borrow)]
+    pub experiment: Cow<'a, str>,
+    #[serde(borrow)]
+    pub vector_id: Cow<'a, str>,
+    pub time: f64,
+    pub fetch_index: u64,
+    pub query: Query<'a>,
+}
