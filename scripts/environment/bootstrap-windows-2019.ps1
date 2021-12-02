db382c8d4213cb062cc681efe91b1e527d11cbe1
Pierre Rognant <59561183+prognant@users.noreply.github.com>
2021-12-02 16:48:15 +0100

    chore(ci): lower `CARGO_BUILD_JOBS` for Windows runner (#10235)
    Signed-off-by: prognant <pierre.rognant@datadoghq.com>

--- scripts/environment/bootstrap-windows-2019.ps1
+++ scripts/environment/bootstrap-windows-2019.ps1
@@ -1,5 +1,6 @@
 if ($env:CI -ne $null) {
     echo "$HOME\.cargo\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+    echo "CARGO_BUILD_JOBS=1" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 } else {
     $env:Path += ";$HOME\.cargo\bin"
 }
