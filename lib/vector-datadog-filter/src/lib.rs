615e5af42af502017255029d597cca968bf2a436
Lee Benson <lee@leebenson.com>
2021-12-06 13:08:45 +0000

    first passing test
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- lib/vector-datadog-filter/src/lib.rs
+++ lib/vector-datadog-filter/src/lib.rs
@@ -1,6 +1,7 @@
-use datadog_filter::{Filter, Matcher, Resolver};
+use datadog_filter::{Filter, Matcher, Resolver, Run};
 use datadog_search_syntax::{Comparison, ComparisonValue, Field};
-use vector_core::event::Event;
+use std::borrow::{Borrow, Cow};
+use vector_core::event::{Event, Value};
 
 #[derive(Default, Clone)]
 pub struct EventFilter;
@@ -9,7 +10,26 @@ impl Resolver for EventFilter {}
 
 impl Filter<Event> for EventFilter {
     fn exists(&self, field: Field) -> Box<dyn Matcher<Event>> {
-        todo!()
+        match field {
+            Field::Tag(tag) => {
+                let starts_with = format!("{}:", tag);
+
+                Run::boxed(move |ev: &Event| match ev {
+                    Event::Log(log) => match log.get("tags") {
+                        Some(Value::Array(v)) => v.iter().any(|v| {
+                            let bytes = v.as_bytes();
+                            let str_value = String::from_utf8_lossy(&bytes);
+
+                            // The tag matches using either 'key' or 'key:value' syntax.
+                            str_value == tag || str_value.starts_with(&starts_with)
+                        }),
+                        _ => false,
+                    },
+                    _ => false,
+                })
+            }
+            _ => todo!(),
+        }
     }
 
     fn equals(&self, field: Field, to_match: &str) -> Box<dyn Matcher<Event>> {
@@ -31,5 +51,54 @@ impl Filter<Event> for EventFilter {
         comparison_value: ComparisonValue,
     ) -> Box<dyn Matcher<Event>> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! log_event {
+    ($($key:expr => $value:expr),*  $(,)?) => {
+        #[allow(unused_variables)]
+        {
+            let mut event = Event::Log(vector_core::event::LogEvent::default());
+            let log = event.as_mut_log();
+            $(
+                log.insert($key, $value);
+            )*
+            event
+        }
+    };
+}
+
+#[cfg(test)]
+pub fn get_checks() -> Vec<(&'static str, Event, Event)> {
+    vec![
+        // Tag exists.
+        (
+            "_exists_:a",                        // Source
+            log_event!["tags" => vec!["a:foo"]], // Pass
+            log_event!["tags" => vec!["b:foo"]], // Fail
+        ),
+    ]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use datadog_filter::build_matcher;
+    use datadog_search_syntax::parse;
+
+    #[test]
+    fn bla() {
+        let checks = get_checks();
+        let filter = EventFilter::default();
+
+        for (source, pass, fail) in checks {
+            let node = parse(source).unwrap();
+            let matcher = build_matcher(&node, &filter);
+
+            assert!(matcher.run(&pass));
+            assert!(!matcher.run(&fail));
+        }
     }
 }
