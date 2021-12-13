c021374d62521ba7ba7a1f1f7091f141ec3dbde1
Pablo Sichert <mail@pablosichert.com>
2021-12-13 18:40:35 +0100

    Implement <Abort as Expression>::emit_llvm
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- lib/vrl/compiler/src/precompiled/src/lib.rs
+++ lib/vrl/compiler/src/precompiled/src/lib.rs
@@ -1,4 +1,4 @@
-use vrl_core::{Context, Resolved};
+use vrl_core::{Context, ExpressionError, Resolved, Span, Value};
 
 // We only want to precompile the stub for this function, and therefore don't
 // reference the function arguments.
@@ -6,3 +6,30 @@ use vrl_core::{Context, Resolved};
 #[allow(unused_variables)]
 #[no_mangle]
 pub extern "C" fn vrl_execute(context: &mut Context, result: &mut Resolved) {}
+
+#[no_mangle]
+pub extern "C" fn vrl_resolved_initialize(result: *mut Resolved) {
+    unsafe { result.write(Ok(Value::Null)) };
+}
+
+#[no_mangle]
+pub extern "C" fn vrl_resolved_drop(result: *mut Resolved) {
+    drop(unsafe { result.read() });
+}
+
+#[no_mangle]
+pub extern "C" fn vrl_expression_abort_impl(
+    span: &Span,
+    message: &Resolved,
+    result: &mut Resolved,
+) {
+    let message = match message {
+        Ok(Value::Null) => None,
+        Ok(Value::Bytes(bytes)) => Some(String::from_utf8_lossy(bytes).into()),
+        _ => panic!(r#"abort message "{:?}" is not a string"#, message),
+    };
+    *result = Err(ExpressionError::Abort {
+        span: *span,
+        message,
+    });
+}
