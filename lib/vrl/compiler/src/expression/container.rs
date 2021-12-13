21b898ca4e02b7f9fe17d2402eabf9a549abf8e2
Pablo Sichert <mail@pablosichert.com>
2021-12-14 00:05:33 +0100

    Implement <Container as Expression>::emit_llvm
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- lib/vrl/compiler/src/expression/container.rs
+++ lib/vrl/compiler/src/expression/container.rs
@@ -67,8 +67,15 @@ impl Expression for Container {
     }
 
     #[cfg(feature = "llvm")]
-    fn emit_llvm<'ctx>(&self, _: &mut crate::llvm::Context<'ctx>) -> Result<(), String> {
-        todo!()
+    fn emit_llvm<'ctx>(&self, ctx: &mut crate::llvm::Context<'ctx>) -> Result<(), String> {
+        use Variant::*;
+
+        match &self.variant {
+            Group(v) => v.emit_llvm(ctx),
+            Block(v) => v.emit_llvm(ctx),
+            Array(v) => v.emit_llvm(ctx),
+            Object(v) => v.emit_llvm(ctx),
+        }
     }
 }
 
