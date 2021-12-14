342ceba6be12c4c75211ed42c6c011f79baeae26
Jesse Szwedko <jesse@szwedko.me>
2021-12-14 15:12:37 -0800

    Gate jemalloc in vrl tests
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- lib/vrl/tests/src/main.rs
+++ lib/vrl/tests/src/main.rs
@@ -14,6 +14,7 @@ use vrl::{diagnostic::Formatter, state, Runtime, Terminate, Value};
 
 use vrl_tests::{docs, Test};
 
+#[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
