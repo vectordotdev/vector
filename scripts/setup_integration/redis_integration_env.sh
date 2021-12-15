d28580b7cc91ad18c5ec8e4c6fd19b4d5ccba993
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2021-12-15 08:25:32 +0000

    remove podman script
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- scripts/setup_integration/redis_integration_env.sh
+++ scripts/setup_integration/redis_integration_env.sh
@@ -18,27 +18,15 @@ ACTION=$1
 # Functions
 #
 
-start_podman () {
-  podman pod create --replace --name vector-test-integration-redis -p 6379:6379
-  podman run -d --pod=vector-test-integration-redis  --name vector_redis \
-	 redis
-}
-
-start_docker () {
+start () {
   docker-compose -f scripts/setup_integration/docker-compose.redis.yml up -d
 }
 
-stop_podman () {
-  podman rm --force vector_redis 2>/dev/null; true
-  podman pod stop vector-test-integration-redis 2>/dev/null; true
-  podman pod rm --force vector-test-integration-redis 2>/dev/null; true
-}
-
-stop_docker () {
+stop () {
   docker-compose -f scripts/setup_integration/docker-compose.redis.yml stop
   docker-compose -f scripts/setup_integration/docker-compose.redis.yml rm -f
 }
 
 echo "Running $ACTION action for Redis integration tests environment"
 
-"${ACTION}"_"${CONTAINER_TOOL}"
+"${ACTION}"
