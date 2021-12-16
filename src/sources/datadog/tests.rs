d1095f1da82cfed879f2b7b9bfd1d7dae863db8e
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2021-12-16 13:29:31 +0000

    add more checks on test
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/sources/datadog/tests.rs
+++ src/sources/datadog/tests.rs
@@ -26,5 +26,11 @@ async fn wait_for_message() {
         2,
     )
     .await;
-    assert!(!events.is_empty());
+    assert_eq!(events.len(), 2);
+    let event = events.get(0).unwrap().as_log();
+    let msg = event.get("message").unwrap().as_bytes();
+    assert_eq!(msg, "hello world");
+    let event = events.get(1).unwrap().as_log();
+    let msg = event.get("message").unwrap().as_bytes();
+    assert_eq!(msg, "it's vector speaking");
 }
