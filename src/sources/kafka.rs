ca42a63466ef2a6b2cad1f1d0508032526783b56
Bruce Guenter <bruce.guenter@datadoghq.com>
2021-12-29 11:33:29 -0600

    Revert "Fix write-back committing of offsets for acknowledgements"

--- src/sources/kafka.rs
+++ src/sources/kafka.rs
@@ -135,7 +135,7 @@ impl_generate_config_from_default!(KafkaSourceConfig);
 impl SourceConfig for KafkaSourceConfig {
     async fn build(&self, cx: SourceContext) -> crate::Result<super::Source> {
         let acknowledgements = self.acknowledgements.enabled;
-        let consumer = create_consumer(self, !acknowledgements)?;
+        let consumer = create_consumer(self)?;
 
         let decoder = DecodingConfig::new(self.framing.clone(), self.decoding.clone()).build()?;
 
@@ -321,20 +321,13 @@ impl<'a> From<BorrowedMessage<'a>> for FinalizerEntry {
 
 fn mark_done(consumer: Arc<StreamConsumer<CustomContext>>) -> impl Fn(FinalizerEntry) {
     move |entry| {
-        let mut tpl = TopicPartitionList::new();
-        tpl.add_partition(&entry.topic, entry.partition)
-            .set_offset(Offset::from_raw(entry.offset + 1))
-            .unwrap();
-        if let Err(error) = consumer.commit(&tpl, CommitMode::Sync) {
+        if let Err(error) = consumer.store_offset(&entry.topic, entry.partition, entry.offset) {
             emit!(&KafkaOffsetUpdateFailed { error });
         }
     }
 }
 
-fn create_consumer(
-    config: &KafkaSourceConfig,
-    auto_commit: bool,
-) -> crate::Result<StreamConsumer<CustomContext>> {
+fn create_consumer(config: &KafkaSourceConfig) -> crate::Result<StreamConsumer<CustomContext>> {
     let mut client_config = ClientConfig::new();
     client_config
         .set("group.id", &config.group_id)
@@ -344,10 +337,7 @@ fn create_consumer(
         .set("socket.timeout.ms", &config.socket_timeout_ms.to_string())
         .set("fetch.wait.max.ms", &config.fetch_wait_max_ms.to_string())
         .set("enable.partition.eof", "false")
-        .set(
-            "enable.auto.commit",
-            if auto_commit { "true" } else { "false" },
-        )
+        .set("enable.auto.commit", "true")
         .set(
             "auto.commit.interval.ms",
             &config.commit_interval_ms.to_string(),
@@ -426,7 +416,7 @@ mod test {
     #[tokio::test]
     async fn consumer_create_ok() {
         let config = make_config("topic", "group");
-        assert!(create_consumer(&config, true).is_ok());
+        assert!(create_consumer(&config).is_ok());
     }
 
     #[tokio::test]
@@ -435,7 +425,7 @@ mod test {
             auto_offset_reset: "incorrect-auto-offset-reset".to_string(),
             ..make_config("topic", "group")
         };
-        assert!(create_consumer(&config, true).is_err());
+        assert!(create_consumer(&config).is_err());
     }
 }
 
@@ -588,7 +578,7 @@ mod integration_test {
     ) -> (Trigger, Tripwire) {
         let (trigger_shutdown, shutdown, shutdown_done) = ShutdownSignal::new_wired();
         tokio::spawn(kafka_source(
-            create_consumer(&config, !acknowledgements).unwrap(),
+            create_consumer(&config).unwrap(),
             config.key_field,
             config.topic_key,
             config.partition_key,
