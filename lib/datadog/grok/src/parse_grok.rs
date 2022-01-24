2ca93f5c74b5f71ae85d5e5b7a0551e2bf8514ed
Vladimir Zhuk <vladimir.zhuk@datadoghq.com>
2022-01-24 15:30:59 +0100

    add a comment about .sorted()
    Signed-off-by: Vladimir Zhuk <vladimir.zhuk@datadoghq.com>

--- lib/datadog/grok/src/parse_grok.rs
+++ lib/datadog/grok/src/parse_grok.rs
@@ -46,6 +46,9 @@ fn apply_grok_rule(source: &str, grok_rule: &GrokRule, remove_empty: bool) -> Re
     let mut parsed = Value::from(btreemap! {});
 
     if let Some(ref matches) = grok_rule.pattern.match_against(source) {
+        // Extracted fields do not preserve the order(stored in a hashmap)
+        // in which they've been seen in the source expression, which matters for arrays,
+        // so we need to sort them first by keys(grok0, grok1, ...).
         for (name, value) in matches.iter().sorted() {
             let mut value = Some(Value::from(value));
 
