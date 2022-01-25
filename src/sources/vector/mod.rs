4bc85228396cbfce4fa1d02012054857bd3d7163
Jesse Szwedko <jesse@szwedko.me>
2022-01-25 09:25:19 -0800

    clippy and fixing broken link
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sources/vector/mod.rs
+++ src/sources/vector/mod.rs
@@ -51,7 +51,7 @@ impl GenerateConfig for VectorConfig {
         let config =
             toml::Value::try_into::<v2::VectorConfig>(v2::VectorConfig::generate_config()).unwrap();
         toml::Value::try_from(VectorConfigV2 {
-            version: V2::V2,
+            version: Some(V2::V2),
             config,
         })
         .unwrap()
