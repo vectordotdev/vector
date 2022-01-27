c33fce300b235d0980929c2216da1cf62c31d8e7
Brian L. Troutwine <brian@troutwine.us>
2022-01-26 19:10:52 -0800

    grep exit 1 again
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- soaks/bin/run_experiment.sh
+++ soaks/bin/run_experiment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -o pipefail
+#set -o pipefail # grep will exit 1 without a match, even with --count
 set -o nounset
 #set -o xtrace
 
@@ -114,9 +114,10 @@ periods=0
 (( target_samples = TOTAL_SAMPLES + WARMUP_SECONDS ))
 while [ $recorded_samples -le $target_samples ]
 do
+    observed_samples=$(grep --count "bytes_written" "${SOAK_CAPTURE_FILE}")
+
     # Check that the capture file grows monotonically. If it shrinks this
     # indicates a serious problem.
-    observed_samples=$(grep --count "bytes_written" "${SOAK_CAPTURE_FILE}")
     # shellcheck disable=SC2086
     if [ $recorded_samples -gt $observed_samples ]; then
         echo "SAMPLES LOST. THIS IS A CATASTROPHIC, UNRECOVERABLE FAILURE."
