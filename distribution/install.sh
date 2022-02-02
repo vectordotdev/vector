e37070a635d1ae68239cbb48ab7fa457ed612d11
Jesse Szwedko <jesse@szwedko.me>
2022-02-02 14:08:17 -0800

    chore(releasing): Prepare 0.20.0 release
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- distribution/install.sh
+++ distribution/install.sh
@@ -12,7 +12,7 @@ set -u
 
 # If PACKAGE_ROOT is unset or empty, default it.
 PACKAGE_ROOT="${PACKAGE_ROOT:-"https://packages.timber.io/vector"}"
-VECTOR_VERSION="0.19.3"
+VECTOR_VERSION="0.20.0"
 _divider="--------------------------------------------------------------------------------"
 _prompt=">>>"
 _indent="   "
