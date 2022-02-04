fa66d85e97dcdd3c0ce2b9f17fe665f9a5652747
Jesse Szwedko <jesse@szwedko.me>
2022-02-04 13:21:52 -0800

    Share deserialization of path components
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sinks/util/encoding/fixed.rs
+++ src/sinks/util/encoding/fixed.rs
@@ -3,9 +3,9 @@ use std::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    event::{PathComponent, PathIter},
+    event::PathComponent,
     serde::skip_serializing_if_default,
-    sinks::util::encoding::{EncodingConfiguration, TimestampFormat},
+    sinks::util::encoding::{deserialize_path_components, EncodingConfiguration, TimestampFormat},
 };
 
 /// A structure to wrap sink encodings and enforce field privacy.
@@ -21,12 +21,12 @@ pub struct EncodingConfigFixed<E: Default + PartialEq> {
     #[serde(default, skip_serializing_if = "skip_serializing_if_default")]
     pub(crate) schema: Option<String>,
     /// Keep only the following fields of the message. (Items mutually exclusive with `except_fields`)
+    // TODO(2410): Using PathComponents here is a hack for #2407, #2410 should fix this fully.
     #[serde(
         default,
         skip_serializing_if = "skip_serializing_if_default",
         deserialize_with = "deserialize_path_components"
     )]
-    // TODO(2410): Using PathComponents here is a hack for #2407, #2410 should fix this fully.
     pub(crate) only_fields: Option<Vec<Vec<PathComponent<'static>>>>,
     /// Remove the following fields of the message. (Items mutually exclusive with `only_fields`)
     #[serde(default, skip_serializing_if = "skip_serializing_if_default")]
@@ -74,23 +74,4 @@ where
             timestamp_format: Default::default(),
         }
     }
-}
-
-fn deserialize_path_components<'de, D>(
-    deserializer: D,
-) -> Result<Option<Vec<Vec<PathComponent<'static>>>>, D::Error>
-where
-    D: serde::de::Deserializer<'de>,
-{
-    let fields: Option<Vec<&str>> = serde::de::Deserialize::deserialize(deserializer)?;
-    Ok(fields.map(|fields| {
-        fields
-            .iter()
-            .map(|only| {
-                PathIter::new(only)
-                    .map(|component| component.into_static())
-                    .collect()
-            })
-            .collect()
-    }))
 }
