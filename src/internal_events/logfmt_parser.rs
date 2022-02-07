6db36e58b2bd4eb0e0ca3c4d7d2962f7db9e6519
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-02-07 14:08:10 +0000

    logfmt_parser: rename errors
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/internal_events/logfmt_parser.rs
+++ src/internal_events/logfmt_parser.rs
@@ -2,40 +2,66 @@ use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
-pub struct LogfmtParserMissingField<'a> {
+pub struct LogfmtParserMissingFieldError<'a> {
     pub field: &'a str,
 }
 
-impl InternalEvent for LogfmtParserMissingField<'_> {
+impl InternalEvent for LogfmtParserMissingFieldError<'_> {
     fn emit_logs(&self) {
-        debug!(message = "Field does not exist.", field = %self.field);
+        error!(
+            message = "Field does not exist.",
+            error = "Field does not exist.",
+            error_type = "condition_failed",
+            stage = "processing",
+            field = %self.field,
+        );
     }
 
     fn emit_metrics(&self) {
-        counter!("processing_errors_total", 1,
+        counter!(
+            "component_errors_total", 1,
+            "error" => "Field does not exist.",
+            "error_type" => "condition_failed",
+            "stage" => "processing",
+            "field" => self.field.to_string(),
+        );
+        // deprecated
+        counter!(
+            "processing_errors_total", 1,
             "error_type" => "missing_field",
         );
     }
 }
 
 #[derive(Debug)]
-pub struct LogfmtParserConversionFailed<'a> {
+pub struct LogfmtParserConversionError<'a> {
     pub name: &'a str,
     pub error: crate::types::Error,
 }
 
-impl<'a> InternalEvent for LogfmtParserConversionFailed<'a> {
+impl<'a> InternalEvent for LogfmtParserConversionError<'a> {
     fn emit_logs(&self) {
-        debug!(
+        error!(
             message = "Could not convert types.",
             name = %self.name,
             error = ?self.error,
+            error_type = "conversion_failed",
+            stage = "processing",
             internal_log_rate_secs = 30
         );
     }
 
     fn emit_metrics(&self) {
-        counter!("processing_errors_total", 1,
+        counter!(
+            "component_errors_total", 1,
+            "error" => self.error.to_string(),
+            "error_type" => "conversion_failed",
+            "stage" => "processing",
+            "name" => self.name.to_string(),
+        );
+        // deprecated
+        counter!(
+            "processing_errors_total", 1,
             "error_type" => "type_conversion_failed",
         );
     }
