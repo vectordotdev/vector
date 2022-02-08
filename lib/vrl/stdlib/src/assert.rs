a63863eb7ee59c4b33cd9e3ce531712a09002366
Jean Mertz <git@jeanmertz.com>
2022-02-08 16:08:27 +0100

    fix bad merge
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- lib/vrl/stdlib/src/assert.rs
+++ lib/vrl/stdlib/src/assert.rs
@@ -1,4 +1,4 @@
-use vrl::prelude::*;
+use vrl::{diagnostic::Note, prelude::*};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Assert;
@@ -66,19 +66,30 @@ impl Expression for AssertFn {
     fn resolve(&self, ctx: &mut Context) -> Resolved {
         match self.condition.resolve(ctx)?.try_boolean()? {
             true => Ok(true.into()),
-            false => Err(self
-                .message
-                .as_ref()
-                .map(|m| {
-                    m.resolve(ctx)
-                        .and_then(|v| Ok(v.try_bytes_utf8_lossy()?.into_owned()))
-                })
-                .transpose()?
-                .unwrap_or_else(|| match self.condition.format() {
-                    Some(string) => format!("assertion failed: {}", string),
-                    None => "assertion failed".to_owned(),
-                })
-                .into()),
+            false => {
+                let message = self
+                    .message
+                    .as_ref()
+                    .map(|m| {
+                        m.resolve(ctx)
+                            .and_then(|v| Ok(v.try_bytes_utf8_lossy()?.into_owned()))
+                    })
+                    .transpose()?;
+
+                if let Some(message) = message {
+                    Err(ExpressionError::Error {
+                        message: message.clone(),
+                        labels: vec![],
+                        notes: vec![Note::UserErrorMessage(message)],
+                    })
+                } else {
+                    let message = match self.condition.format() {
+                        Some(string) => format!("assertion failed: {}", string),
+                        None => "assertion failed".to_owned(),
+                    };
+                    Err(ExpressionError::from(message))
+                }
+            }
         }
     }
 
