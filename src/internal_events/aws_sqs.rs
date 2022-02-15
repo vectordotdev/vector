197e1cde3ae1c7a3fafcc8f5b92f51a2b58e3cc0
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-02-15 13:50:17 +0000

    aws_sqs: add missing events
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/internal_events/aws_sqs.rs
+++ src/internal_events/aws_sqs.rs
@@ -2,17 +2,20 @@ use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
-pub struct AwsSqsEventSent<'a> {
+pub struct AwsSqsEventsSent<'a> {
     pub byte_size: usize,
     pub message_id: Option<&'a String>,
 }
 
-impl InternalEvent for AwsSqsEventSent<'_> {
+impl InternalEvent for AwsSqsEventsSent<'_> {
     fn emit_logs(&self) {
-        trace!(message = "Event sent.", message_id = ?self.message_id);
+        trace!(message = "Events sent.", message_id = ?self.message_id);
     }
 
     fn emit_metrics(&self) {
+        counter!("component_sent_events_total", 1);
+        counter!("component_sent_event_bytes_total", self.byte_size as u64);
+        // deprecated
         counter!("processed_bytes_total", self.byte_size as u64);
     }
 }
