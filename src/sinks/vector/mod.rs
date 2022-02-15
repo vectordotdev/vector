83f9aae4d9da3d441d0d97eadb3c44ebb64db128
Brian L. Troutwine <brian@troutwine.us>
2022-02-15 14:43:45 -0800

    [RETYPIST] applied modifications

--- src/sinks/vector/mod.rs
+++ src/sinks/vector/mod.rs
@@ -35,7 +35,7 @@ pub struct VectorConfigV2 {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
-pub enum VectorConfig {
+pub(self) enum VectorConfig {
     V1(VectorConfigV1),
     V2(VectorConfigV2),
 }
