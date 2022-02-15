8c7ff6984c209d31ec2df947fd4d4d5a1c9374d2
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-02-15 09:56:50 +0000

    aws_cloudwatch_logs: add missing events
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/internal_events/aws_cloudwatch_logs_subscription_parser.rs
+++ src/internal_events/aws_cloudwatch_logs_subscription_parser.rs
@@ -12,6 +12,7 @@ impl InternalEvent for AwsCloudwatchLogsSubscriptionParserError {
         error!(
             message = "Event failed to parse as a CloudWatch Logs subscription JSON message.",
             error = ?self.error,
+            error_code = "parsing_cloudwatch_logs",
             error_type = "parser_failed",
             stage = error_stage::PROCESSING,
             internal_log_rate_secs = 30
@@ -21,13 +22,79 @@ impl InternalEvent for AwsCloudwatchLogsSubscriptionParserError {
     fn emit_metrics(&self) {
         counter!(
             "component_errors_total", 1,
-            "error" => self.error.to_string(),
+            "error_code" => "parsing_cloudwatch_logs",
             "error_type" => "parser_failed",
             "stage" => error_stage::PROCESSING,
         );
         // deprecated
-        counter!("processing_errors_total", 1,
+        counter!(
+            "processing_errors_total", 1,
             "error_type" => "failed_parse",
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct AwsCloudwatchLogsMessageSizeError {
+    pub size: usize,
+    pub max_size: usize,
+}
+
+impl InternalEvent for AwsCloudwatchLogsMessageSizeError {
+    fn emit_logs(&self) {
+        error!(
+            message = %format!("Encoded event is too long: {} > {}", self.size, self.max_size),
+            error_code = "message_too_long",
+            error_type = "encoder_failed",
+            stage = error_stage::PROCESSING,
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!(
+            "component_errors_total", 1,
+            "error_code" => "message_too_long",
+            "error_type" => "encoder_failed",
+            "stage" => error_stage::PROCESSING,
+        );
+        counter!(
+            "component_discarded_events_total", 1,
+            "error_code" => "message_too_long",
+            "error_type" => "encoder_failed",
+            "stage" => error_stage::PROCESSING,
+        );
+    }
+}
+
+#[derive(Debug)]
+pub struct AwsCloudwatchLogsEncoderError {
+    pub error: std::io::Error,
+}
+
+impl InternalEvent for AwsCloudwatchLogsEncoderError {
+    fn emit_logs(&self) {
+        error!(
+            message = "Error when encoding event.",
+            error = %self.error,
+            error_code = "failed_encoding_event",
+            error_type = "encoder_failed",
+            stage = error_stage::PROCESSING,
+            internal_log_rate_secs = 10,
+        );
+    }
+
+    fn emit_metrics(&self) {
+        counter!(
+            "component_errors_total", 1,
+            "error_code" => "failed_encoding_event",
+            "error_type" => "encoder_failed",
+            "stage" => error_stage::PROCESSING,
+        );
+        counter!(
+            "component_discarded_events_total", 1,
+            "error_code" => "failed_encoding_event",
+            "error_type" => "encoder_failed",
+            "stage" => error_stage::PROCESSING,
         );
     }
 }
