a12e8439c1f342cd60a1c54207639e0fda6b7c5e
Brian L. Troutwine <brian@troutwine.us>
2022-02-16 05:28:31 -0800

    [RETYPIST] applied 2 modifications

--- src/sources/util/body_decoding.rs
+++ src/sources/util/body_decoding.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug, Eq, PartialEq, Clone, Derivative, Copy)]
 #[serde(rename_all = "snake_case")]
 #[derivative(Default)]
-pub enum Encoding {
+pub(crate) enum Encoding {
     #[derivative(Default)]
     Text,
     Ndjson,
