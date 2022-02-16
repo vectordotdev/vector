ebed064abb65f3410e87b1d2edfce0cb40d6b739
Brian L. Troutwine <brian@troutwine.us>
2022-02-15 22:32:22 -0800

    [RETYPIST] applied 1 modifications

--- src/transforms/geoip.rs
+++ src/transforms/geoip.rs
@@ -15,7 +15,7 @@ use crate::{
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
-pub struct GeoipConfig {
+pub(self) struct GeoipConfig {
     pub source: String,
     pub database: String,
     #[serde(default = "default_geoip_target_field")]
