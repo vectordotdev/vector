165c05ee940ecf0b9d9cc1214107c72dd25d2db5
Brian L. Troutwine <brian@troutwine.us>
2022-02-15 17:37:00 -0800

    [RETYPIST] applied 1 modifications

--- src/config/datadog.rs
+++ src/config/datadog.rs
@@ -24,7 +24,7 @@ pub struct Options {
     pub(super) configuration_key: String,
 
     #[serde(default = "default_reporting_interval_secs")]
-    pub reporting_interval_secs: u64,
+    pub(super) reporting_interval_secs: u64,
 }
 
 impl Default for Options {
