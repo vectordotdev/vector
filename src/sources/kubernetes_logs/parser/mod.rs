efd17a78b42d358e6d22c759b053780d3b615924
Brian L. Troutwine <brian@troutwine.us>
2022-02-16 03:38:12 -0800

    [RETYPIST] applied 2 modifications

--- src/sources/kubernetes_logs/parser/mod.rs
+++ src/sources/kubernetes_logs/parser/mod.rs
@@ -6,7 +6,7 @@ mod picker;
 mod test_util;
 
 /// Parser for any log format supported by `kubelet`.
-pub type Parser = picker::Picker;
+pub(super) type Parser = picker::Picker;
 
 /// Build a parser for any log format supported by `kubelet`.
 pub const fn build(timezone: TimeZone) -> Parser {
