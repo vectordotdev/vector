3702def8c53a52cb0434afc1f76dcac2227625d4
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 15:03:43 -0800

    [RETYPIST] applied 1 modifications

--- src/config/api.rs
+++ src/config/api.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 #[serde(default, deny_unknown_fields)]
 pub struct Options {
     #[serde(default = "default_enabled")]
-    pub enabled: bool,
+    pub(crate) enabled: bool,
 
     #[serde(default = "default_address")]
     pub(crate) address: Option<SocketAddr>,
