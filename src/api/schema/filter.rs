7b13792e930e86cc8909afefcb124fde3b92fe97
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 11:09:22 -0800

    [RETYPIST] applied 1 modifications

--- src/api/schema/filter.rs
+++ src/api/schema/filter.rs
@@ -20,7 +20,7 @@ macro_rules! filter_check {
 pub struct StringFilter {
     pub equals: Option<String>,
     pub not_equals: Option<String>,
-    pub contains: Option<String>,
+    pub(crate) contains: Option<String>,
     pub not_contains: Option<String>,
     pub starts_with: Option<String>,
     pub(super) ends_with: Option<String>,
