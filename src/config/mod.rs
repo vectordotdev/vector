7ae9793c37c3e3b90c408f6a43e9804ef1d3237b
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 11:46:51 -0800

    [RETYPIST] applied 1 modifications

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -107,7 +107,7 @@ pub struct Config {
     pub healthchecks: HealthcheckOptions,
     pub sources: IndexMap<ComponentKey, SourceOuter>,
     pub sinks: IndexMap<ComponentKey, SinkOuter<OutputId>>,
-    pub transforms: IndexMap<ComponentKey, TransformOuter<OutputId>>,
+    pub(super) transforms: IndexMap<ComponentKey, TransformOuter<OutputId>>,
     pub enrichment_tables: IndexMap<ComponentKey, EnrichmentTableOuter>,
     tests: Vec<TestDefinition>,
     expansions: IndexMap<ComponentKey, Vec<ComponentKey>>,
