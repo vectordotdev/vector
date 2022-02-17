241327debf46abb85ca0d07702a0c4025a7691f7
Brian L. Troutwine <brian@troutwine.us>
2022-02-16 22:43:17 -0800

    [RETYPIST] applied 1 modifications

--- src/api/schema/components/sink.rs
+++ src/api/schema/components/sink.rs
@@ -17,7 +17,7 @@ use crate::{
 pub struct Data {
     pub component_key: ComponentKey,
     pub component_type: String,
-    pub inputs: Vec<OutputId>,
+    pub(crate) inputs: Vec<OutputId>,
 }
 
 #[derive(Debug, Clone)]
