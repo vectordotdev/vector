acd07c78eb7014af9b521c5073930ff44b6862d1
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 12:02:10 -0800

    [RETYPIST] applied 2 modifications

--- src/config/unit_test/unit_test_components.rs
+++ src/config/unit_test/unit_test_components.rs
@@ -68,7 +68,7 @@ impl Default for UnitTestSinkCheck {
 }
 
 #[derive(Debug)]
-pub struct UnitTestSinkResult {
+pub(crate) struct UnitTestSinkResult {
     pub(crate) test_name: String,
     pub test_errors: Vec<String>,
 }
