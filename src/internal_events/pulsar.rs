666925608f1edbc56c5f0c13a2fff406a713d7f7
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-02-18 12:23:03 +0000

    pulsar: refactor error
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/internal_events/pulsar.rs
+++ src/internal_events/pulsar.rs
@@ -1,23 +1,32 @@
-// ## skip check-events ##
-
+use super::prelude::error_stage;
 use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
-pub(crate) struct PulsarEncodeEventFailed<'a> {
-    pub error: &'a str,
+pub(crate) struct PulsarEncodeEventError<E> {
+    pub error: E,
 }
 
-impl<'a> InternalEvent for PulsarEncodeEventFailed<'a> {
+impl<E: std::fmt::Display> InternalEvent for PulsarEncodeEventError<E> {
     fn emit_logs(&self) {
         error!(
             message = "Event encode failed; dropping event.",
             error = %self.error,
+            error_code = "pulsar_encoding_failed",
+            error_type = "encoder_failed",
+            stage = error_stage::PROCESSING,
             internal_log_rate_secs = 30,
         );
     }
 
     fn emit_metrics(&self) {
+        counter!(
+            "component_errors_total", 1,
+            "error_code" => "pulsar_encoding_failed",
+            "error_type" => "encoder_failed",
+            "stage" => error_stage::PROCESSING,
+        );
+        // deprecated
         counter!("encode_errors_total", 1);
     }
 }
