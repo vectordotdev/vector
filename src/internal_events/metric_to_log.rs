d8b8a9d588a3554476ab80be4c9baf1633680a3a
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 21:46:16 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/metric_to_log.rs
+++ src/internal_events/metric_to_log.rs
@@ -5,7 +5,7 @@ use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
 pub struct MetricToLogSerializeError {
-    pub error: Error,
+    pub(crate) error: Error,
 }
 
 impl<'a> InternalEvent for MetricToLogSerializeError {
