57c825113b57a82238185c04fdad083327c43a08
Brian L. Troutwine <brian@troutwine.us>
2022-02-18 10:49:12 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/kubernetes/mod.rs
+++ src/internal_events/kubernetes/mod.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "kubernetes")]
 
-pub mod api_watcher;
+pub(crate) mod api_watcher;
 pub(crate) mod instrumenting_state;
 pub(crate) mod instrumenting_watcher;
 pub(crate) mod reflector;
