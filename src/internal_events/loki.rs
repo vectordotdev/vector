2bc1332ace806f3c8137d6558eba82d4041c18e6
Brian L. Troutwine <brian@troutwine.us>
2022-02-18 08:31:22 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/loki.rs
+++ src/internal_events/loki.rs
@@ -2,7 +2,7 @@ use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
-pub struct LokiEventUnlabeled;
+pub(crate) struct LokiEventUnlabeled;
 
 impl InternalEvent for LokiEventUnlabeled {
     fn emit_metrics(&self) {
