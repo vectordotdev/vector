a18504d96439e52aef6b09cf4a4456b513a5a743
Brian L. Troutwine <brian@troutwine.us>
2022-02-18 13:52:33 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/rename_fields.rs
+++ src/internal_events/rename_fields.rs
@@ -12,7 +12,7 @@ impl<'a> InternalEvent for RenameFieldsFieldOverwritten<'a> {
 }
 
 #[derive(Debug)]
-pub struct RenameFieldsFieldDoesNotExist<'a> {
+pub(crate) struct RenameFieldsFieldDoesNotExist<'a> {
     pub(crate) field: &'a str,
 }
 
