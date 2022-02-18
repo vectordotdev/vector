946d0e47b1b365f66af07dce21962f518e768957
Brian L. Troutwine <brian@troutwine.us>
2022-02-18 12:45:07 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/prometheus.rs
+++ src/internal_events/prometheus.rs
@@ -11,7 +11,7 @@ use vector_core::internal_event::InternalEvent;
 
 #[derive(Debug)]
 pub(crate) struct PrometheusEventsReceived {
-    pub byte_size: usize,
+    pub(crate) byte_size: usize,
     pub(crate) count: usize,
     pub(crate) uri: http::Uri,
 }
