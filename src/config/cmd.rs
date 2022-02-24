0c16349210039a50a50876bbd211fd8b8ceb858b
Lee Benson <lee@leebenson.com>
2022-02-24 10:15:29 +0000

    add test
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/config/cmd.rs
+++ src/config/cmd.rs
@@ -1,6 +1,7 @@
 use super::{load_builder_from_paths, load_source_from_paths, process_paths, ConfigPath};
 use crate::cli::handle_config_errors;
 use clap::Parser;
+use serde_json::Value;
 
 #[derive(Parser, Debug, Clone)]
 #[clap(rename_all = "kebab-case")]
@@ -12,6 +13,26 @@ pub struct Opts {
     /// Include default values where missing from config
     #[clap(short, long)]
     include_defaults: bool,
+}
+
+/// Helper to merge JSON. Handles objects and array concatenation.
+fn merge_json(a: &mut Value, b: Value) {
+    match (a, b) {
+        (Value::Object(ref mut a), Value::Object(b)) => {
+            for (k, v) in b {
+                merge_json(a.entry(k).or_insert(Value::Null), v);
+            }
+        }
+        (Value::Array(ref mut a), Value::Object(b)) => {
+            a.extend([Value::Object(b)]);
+        }
+        (Value::Array(ref mut a), Value::Array(b)) => {
+            a.extend(b);
+        }
+        (a, b) => {
+            *a = b;
+        }
+    }
 }
 
 /// Function used by the `vector config` subcommand for outputting a normalized configuration.
@@ -29,30 +50,64 @@ pub fn cmd(opts: &Opts, config_paths: &[ConfigPath]) -> exitcode::ExitCode {
         None => return exitcode::CONFIG,
     };
 
+    // Serialize source against normalized paths, and get a TOML `Table` as JSON.
+    let mut source = match load_source_from_paths(&paths) {
+        Ok((map, _)) => serde_json::to_value(map)
+            .expect("should serialize config source to JSON. Please report."),
+        Err(errs) => return handle_config_errors(errs),
+    };
+
     // If a user has requested default fields, we'll serialize a `ConfigBuilder`. Otherwise,
     // we'll serialize the raw user provided config (without interpolated env vars, to preserve
     // the original source).
-    let json = if opts.include_defaults {
-        if opts.pretty {
-            serde_json::to_string_pretty(&builder)
-        } else {
-            serde_json::to_string(&builder)
-        }
-    } else {
-        // Serialize source against normalized paths, and get a TOML `Table`.
-        let map = match load_source_from_paths(&paths) {
-            Ok((map, _)) => map,
-            Err(errs) => return handle_config_errors(errs),
-        };
+    if opts.include_defaults {
+        // For security, we don't want environment variables to be interpolated in the final
+        // output, but we *do* want defaults. To work around this, we'll serialize `ConfigBuilder`
+        // to JSON, and merge in the raw config which will contain the pre-interpolated strings.
+        let mut builder = serde_json::to_value(&builder)
+            .expect("should serialize ConfigBuilder to JSON. Please report.");
 
-        if opts.pretty {
-            serde_json::to_string_pretty(&map)
-        } else {
-            serde_json::to_string(&map)
-        }
+        merge_json(&mut builder, source);
+
+        source = builder
+    }
+
+    // Get a JSON string. This will either be pretty printed or (default) minified.
+    let json = if opts.pretty {
+        serde_json::to_string_pretty(&source)
+    } else {
+        serde_json::to_string(&source)
     };
 
     println!("{}", json.expect("config should be serializable"));
 
     exitcode::OK
+}
+
+#[cfg(test)]
+mod tests {
+    use super::merge_json;
+    use serde_json::json;
+
+    #[test]
+    fn test_array_concat() {
+        let mut json = json!({
+            "arr": [
+                "value1", "value2"
+            ]
+        });
+
+        let to_add = json!({
+            "arr": [
+                "value3", "value4"
+            ]
+        });
+
+        merge_json(&mut json, to_add);
+
+        assert_eq!(
+            *json.get("arr").unwrap(),
+            json!(["value1", "value2", "value3", "value4"])
+        )
+    }
 }
