c3dc7c86e82a345a2201e9eae68707661d8a6dde
Brian L. Troutwine <brian@troutwine.us>
2022-02-28 11:21:50 -0800

    [RETYPIST] applied 1 modifications

--- src/expiring_hash_map.rs
+++ src/expiring_hash_map.rs
@@ -15,7 +15,7 @@ use tokio_util::time::{delay_queue, DelayQueue};
 
 /// An expired item, holding the value and the key with an expiration
 /// information.
-pub type ExpiredItem<K, V> = (V, delay_queue::Expired<K>);
+pub(crate) type ExpiredItem<K, V> = (V, delay_queue::Expired<K>);
 
 /// A [`HashMap`] that maintains deadlines for the keys via a [`DelayQueue`].
 pub struct ExpiringHashMap<K, V> {
