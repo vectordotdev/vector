762a5e623fb1cbdf6c81aec506c0f80cd6aee9db
Nathan Fox <fuchsnj@gmail.com>
2022-02-28 12:03:19 -0500

    chore(core): Remove instrumentation on LogEvent (#11592)

--- lib/vector-core/src/event/log_event.rs
+++ lib/vector-core/src/event/log_event.rs
@@ -109,27 +109,22 @@ impl LogEvent {
         self.metadata.add_finalizer(finalizer);
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn get(&self, key: impl AsRef<str>) -> Option<&Value> {
         util::log::get(self.as_map(), key.as_ref())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn get_flat(&self, key: impl AsRef<str>) -> Option<&Value> {
         self.as_map().get(key.as_ref())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn get_mut(&mut self, key: impl AsRef<str>) -> Option<&mut Value> {
         util::log::get_mut(self.as_map_mut(), key.as_ref())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn contains(&self, key: impl AsRef<str>) -> bool {
         util::log::contains(self.as_map(), key.as_ref())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn insert(
         &mut self,
         key: impl AsRef<str>,
@@ -138,7 +133,6 @@ impl LogEvent {
         util::log::insert(self.as_map_mut(), key.as_ref(), value.into())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn try_insert(&mut self, key: impl AsRef<str>, value: impl Into<Value> + Debug) {
         let key = key.as_ref();
         if !self.contains(key) {
@@ -146,7 +140,6 @@ impl LogEvent {
         }
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = ?key))]
     pub fn insert_path<V>(&mut self, key: Vec<PathComponent>, value: V) -> Option<Value>
     where
         V: Into<Value> + Debug,
@@ -163,8 +156,8 @@ impl LogEvent {
     /// This function is a no-op if `from_key` and `to_key` are identical. If
     /// `to_key` already exists in the structure its value will be overwritten
     /// silently.
-    #[instrument(level = "trace", skip(self, from_key, to_key), fields(key = %from_key))]
     #[inline]
+    #[allow(clippy::needless_pass_by_value)] // will be fixed by #11570
     pub fn rename_key_flat<K>(&mut self, from_key: K, to_key: K)
     where
         K: AsRef<str> + Into<String> + PartialEq + Display,
@@ -184,7 +177,6 @@ impl LogEvent {
     /// This function will insert a key in place without reference to any
     /// pathing information in the key. It will insert over the top of any value
     /// that exists in the map already.
-    #[instrument(level = "trace", skip(self, key), fields(key = %key))]
     pub fn insert_flat<K, V>(&mut self, key: K, value: V) -> Option<Value>
     where
         K: Into<String> + Display,
@@ -193,7 +185,6 @@ impl LogEvent {
         self.as_map_mut().insert(key.into(), value.into())
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn try_insert_flat(&mut self, key: impl AsRef<str>, value: impl Into<Value> + Debug) {
         let key = key.as_ref();
         if !self.as_map().contains_key(key) {
@@ -201,35 +192,29 @@ impl LogEvent {
         }
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn remove(&mut self, key: impl AsRef<str>) -> Option<Value> {
         util::log::remove(self.as_map_mut(), key.as_ref(), false)
     }
 
-    #[instrument(level = "trace", skip(self, key), fields(key = %key.as_ref()))]
     pub fn remove_prune(&mut self, key: impl AsRef<str>, prune: bool) -> Option<Value> {
         util::log::remove(self.as_map_mut(), key.as_ref(), prune)
     }
 
-    #[instrument(level = "trace", skip(self))]
-    pub fn keys<'a>(&'a self) -> impl Iterator<Item = String> + 'a {
+    pub fn keys(&self) -> impl Iterator<Item = String> + '_ {
         match self.fields.as_ref() {
             Value::Object(map) => util::log::keys(map),
             _ => unreachable!(),
         }
     }
 
-    #[instrument(level = "trace", skip(self))]
     pub fn all_fields(&self) -> impl Iterator<Item = (String, &Value)> + Serialize {
         util::log::all_fields(self.as_map())
     }
 
-    #[instrument(level = "trace", skip(self))]
     pub fn is_empty(&self) -> bool {
         self.as_map().is_empty()
     }
 
-    #[instrument(level = "trace", skip(self))]
     pub fn as_map(&self) -> &BTreeMap<String, Value> {
         match self.fields.as_ref() {
             Value::Object(map) => map,
@@ -237,7 +222,6 @@ impl LogEvent {
         }
     }
 
-    #[instrument(level = "trace", skip(self))]
     pub fn as_map_mut(&mut self) -> &mut BTreeMap<String, Value> {
         match Arc::make_mut(&mut self.fields) {
             Value::Object(ref mut map) => map,
