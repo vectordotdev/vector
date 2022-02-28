7548687a91e5d2b4f86b4bbb6ecd3082fa04c057
Brian L. Troutwine <brian@troutwine.us>
2022-02-28 12:27:27 -0800

    [RETYPIST] applied 1 modifications

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -35,7 +35,7 @@ pub struct Opts {
         env = "VECTOR_CONFIG_DIR",
         use_value_delimiter(true)
     )]
-    pub config_dirs: Vec<PathBuf>,
+    pub(crate) config_dirs: Vec<PathBuf>,
 }
 
 impl Opts {
