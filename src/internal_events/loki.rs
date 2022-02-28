a673ccaffb337522ce7bde051a0fb9deabbdab21
Brian L. Troutwine <brian@troutwine.us>
2022-02-28 12:06:36 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/loki.rs
+++ src/internal_events/loki.rs
@@ -13,7 +13,7 @@ impl InternalEvent for LokiEventUnlabeled {
 
 #[derive(Debug)]
 pub struct LokiEventsProcessed {
-    pub byte_size: usize,
+    pub(crate) byte_size: usize,
 }
 
 impl InternalEvent for LokiEventsProcessed {
