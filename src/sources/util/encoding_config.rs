36fa2a7e4ee40842500fef5e7c1fd1deb94daad7
Brian L. Troutwine <brian@troutwine.us>
2022-02-28 14:54:31 -0800

    [RETYPIST] applied 1 modifications

--- src/sources/util/encoding_config.rs
+++ src/sources/util/encoding_config.rs
@@ -3,5 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct EncodingConfig {
-    pub charset: &'static encoding_rs::Encoding,
+    pub(crate) charset: &'static encoding_rs::Encoding,
 }
