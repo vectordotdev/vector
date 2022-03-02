14c56ca9983ca43b5970974e5a32e4f26e7b259d
Nathan Fox <fuchsnj@gmail.com>
2022-03-02 09:05:49 -0500

    re-enable log::remove tests

--- lib/vector-core/src/event/util/log/remove.rs
+++ lib/vector-core/src/event/util/log/remove.rs
@@ -73,120 +73,120 @@ fn array_remove(values: &mut Vec<Value>, index: usize) -> Option<Value> {
     }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use serde_json::json;
-//
-//     use super::{super::test::fields_from_json, *};
-//
-//     #[test]
-//     fn array_remove_from_middle() {
-//         let mut array = vec![Value::Null, Value::Integer(3)];
-//         assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
-//         assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
-//
-//         assert_eq!(array_remove(&mut array, 1), Some(Value::Integer(3)));
-//         assert_eq!(array_remove(&mut array, 1), None);
-//
-//         assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
-//         assert_eq!(array_remove(&mut array, 0), None);
-//     }
-//
-//     #[test]
-//     fn remove_simple() {
-//         let mut fields = fields_from_json(json!({
-//             "field": 123
-//         }));
-//         assert_eq!(
-//             remove(&mut fields, "field", false),
-//             Some(Value::Integer(123))
-//         );
-//         assert_eq!(remove(&mut fields, "field", false), None);
-//     }
-//
-//     #[test]
-//     fn remove_nested() {
-//         let mut fields = fields_from_json(json!({
-//             "a": {
-//                 "b": {
-//                     "c": 5
-//                 },
-//                 "d": 4,
-//                 "array": [null, 3, {
-//                     "x": 1
-//                 }, [2]]
-//             }
-//         }));
-//         let queries = [
-//             ("a.b.c", Some(Value::Integer(5)), None),
-//             ("a.d", Some(Value::Integer(4)), None),
-//             ("a.array[1]", Some(Value::Integer(3)), Some(Value::Null)),
-//             ("a.array[2].x", Some(Value::Integer(1)), None),
-//             ("a.array[3][0]", Some(Value::Integer(2)), None),
-//             ("a.array[3][1]", None, None),
-//             ("a.x", None, None),
-//             ("z", None, None),
-//             (".123", None, None),
-//             ("", None, None),
-//         ];
-//
-//         for (query, expected_first, expected_second) in &queries {
-//             assert_eq!(
-//                 remove(&mut fields, query, false),
-//                 *expected_first,
-//                 "{}",
-//                 query
-//             );
-//             assert_eq!(
-//                 remove(&mut fields, query, false),
-//                 *expected_second,
-//                 "{}",
-//                 query
-//             );
-//         }
-//         assert_eq!(
-//             fields,
-//             fields_from_json(json!({
-//                 "a": {
-//                     "b": {},
-//                     "array": [
-//                         null,
-//                         null,
-//                         {},
-//                         [],
-//                     ],
-//                 },
-//             }))
-//         );
-//     }
-//
-//     #[test]
-//     fn remove_prune() {
-//         let mut fields = fields_from_json(json!({
-//             "a": {
-//                 "b": {
-//                     "c": vec![5]
-//                 },
-//                 "d": 4,
-//             }
-//         }));
-//
-//         assert_eq!(remove(&mut fields, "a.d", true), Some(Value::Integer(4)));
-//         assert_eq!(
-//             fields,
-//             fields_from_json(json!({
-//                 "a": {
-//                     "b": {
-//                         "c": vec![5]
-//                     }
-//                 }
-//             }))
-//         );
-//
-//         assert_eq!(
-//             remove(&mut fields, "a.b.c[0]", true),
-//             Some(Value::Integer(5))
-//         );
-//         assert_eq!(fields, fields_from_json(json!({})));
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use super::{super::test::fields_from_json, *};
+
+    #[test]
+    fn array_remove_from_middle() {
+        let mut array = vec![Value::Null, Value::Integer(3)];
+        assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
+        assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
+
+        assert_eq!(array_remove(&mut array, 1), Some(Value::Integer(3)));
+        assert_eq!(array_remove(&mut array, 1), None);
+
+        assert_eq!(array_remove(&mut array, 0), Some(Value::Null));
+        assert_eq!(array_remove(&mut array, 0), None);
+    }
+
+    #[test]
+    fn remove_simple() {
+        let mut fields = fields_from_json(json!({
+            "field": 123
+        }));
+        assert_eq!(
+            remove(&mut fields, "field", false),
+            Some(Value::Integer(123))
+        );
+        assert_eq!(remove(&mut fields, "field", false), None);
+    }
+
+    #[test]
+    fn remove_nested() {
+        let mut fields = fields_from_json(json!({
+            "a": {
+                "b": {
+                    "c": 5
+                },
+                "d": 4,
+                "array": [null, 3, {
+                    "x": 1
+                }, [2]]
+            }
+        }));
+        let queries = [
+            ("a.b.c", Some(Value::Integer(5)), None),
+            ("a.d", Some(Value::Integer(4)), None),
+            ("a.array[1]", Some(Value::Integer(3)), Some(Value::Null)),
+            ("a.array[2].x", Some(Value::Integer(1)), None),
+            ("a.array[3][0]", Some(Value::Integer(2)), None),
+            ("a.array[3][1]", None, None),
+            ("a.x", None, None),
+            ("z", None, None),
+            (".123", None, None),
+            ("", None, None),
+        ];
+
+        for (query, expected_first, expected_second) in &queries {
+            assert_eq!(
+                remove(&mut fields, query, false),
+                *expected_first,
+                "{}",
+                query
+            );
+            assert_eq!(
+                remove(&mut fields, query, false),
+                *expected_second,
+                "{}",
+                query
+            );
+        }
+        assert_eq!(
+            fields,
+            fields_from_json(json!({
+                "a": {
+                    "b": {},
+                    "array": [
+                        null,
+                        null,
+                        {},
+                        [],
+                    ],
+                },
+            }))
+        );
+    }
+
+    #[test]
+    fn remove_prune() {
+        let mut fields = fields_from_json(json!({
+            "a": {
+                "b": {
+                    "c": vec![5]
+                },
+                "d": 4,
+            }
+        }));
+
+        assert_eq!(remove(&mut fields, "a.d", true), Some(Value::Integer(4)));
+        assert_eq!(
+            fields,
+            fields_from_json(json!({
+                "a": {
+                    "b": {
+                        "c": vec![5]
+                    }
+                }
+            }))
+        );
+
+        assert_eq!(
+            remove(&mut fields, "a.b.c[0]", true),
+            Some(Value::Integer(5))
+        );
+        assert_eq!(fields, fields_from_json(json!({})));
+    }
+}
