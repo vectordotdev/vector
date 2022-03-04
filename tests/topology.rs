4016629c89fd6584674187d253910947f97126ad
Luke Steensen <luke.steensen@gmail.com>
2022-03-04 07:51:45 -0600

    fix(topology): correct fix for topology reloading around rebuilt sinks (#11680)
    Fixes #10412
    
    It turns out that the fix for #9173 in #9536 was based on
    a misunderstanding of the underlying issue. In that commit, I noted
    that:
    
    > When replacing the upstream component, the new input for the rebuilt
    > downstream component has not yet been registered and therefore doesn't
    > get readded to the rebuilt `Fanout`.
    
    It's correct that the new input has not yet been registered, but it
    misses that the previous version of the input is still present and does
    get readded to the rebuilt `Fanout`. This is because we only remove
    existing inputs for components that are being removed from the topology
    entirely.
    
    The actual issue in that case stemmed from the logic we have to reuse
    buffers for rebuilt sinks where the buffer config itself has not
    changed. To facilitate that reuse, we removed the existing input of the
    sink (i.e. the buffer sender) in order to stash it in a collection of
    buffers to be passed along and reused later in the reload process. This
    created a special case where the previous input actually would not be
    present and therefore not readded to a rebuilt `Fanout` that previously
    contained it.
    
    The fix we implemented for that issue (based on the misunderstood cause)
    was to send `Add` instead of `Replace` messages to `Fanout`s that had
    been rebuilt when the downstream component itself was being rebuilt.
    This worked for sinks that fell into the category described above, but
    created issues for other types of components whose inputs were not
    removed. That is the direct cause of #10412, where we're sending an
    `Add` message for a rebuilt transform when its previous input was
    already present.
    
    This change undoes the previous fix and makes it so we simply don't
    remove the input for sinks that are being rebuilt. This eliminates the
    special case and lets the original logic work fine in both scenarios.
    
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- tests/topology.rs
+++ tests/topology.rs
@@ -522,6 +522,51 @@ async fn topology_rebuild_connected() {
 }
 
 #[tokio::test]
+async fn topology_rebuild_connected_transform() {
+    vector::trace::init(true, false, "info");
+
+    let (mut in1, source1) = source_with_data("v1");
+    let transform1 = transform(" transformed", 0.0);
+    let transform2 = transform(" transformed", 0.0);
+    let (_out1, sink1) = sink_with_data(10, "v1");
+
+    let mut config = Config::builder();
+    config.add_source("in1", source1);
+    config.add_transform("t1", &["in1"], transform1);
+    config.add_transform("t2", &["t1"], transform2);
+    config.add_sink("out1", &["t2"], sink1);
+
+    let (mut topology, _crash) = start_topology(config.build().unwrap(), false).await;
+
+    let (_in1, source1) = source_with_data("v1"); // not changing
+    let transform1 = transform("", 0.0);
+    let transform2 = transform("", 0.0);
+    let (out1, sink1) = sink_with_data(10, "v2");
+
+    let mut config = Config::builder();
+    config.add_source("in1", source1);
+    config.add_transform("t1", &["in1"], transform1);
+    config.add_transform("t2", &["t1"], transform2);
+    config.add_sink("out1", &["t2"], sink1);
+
+    assert!(topology
+        .reload_config_and_respawn(config.build().unwrap())
+        .await
+        .unwrap());
+    sleep(Duration::from_millis(10)).await;
+
+    let event1 = Event::from("this");
+    let event2 = Event::from("that");
+    let h_out1 = tokio::spawn(out1.collect::<Vec<_>>());
+    in1.send(event1.clone()).await.unwrap();
+    in1.send(event2.clone()).await.unwrap();
+    topology.stop().await;
+
+    let res = h_out1.await.unwrap();
+    assert_eq!(vec![event1, event2], res);
+}
+
+#[tokio::test]
 async fn topology_required_healthcheck_fails_start() {
     let mut config = basic_config_with_sink_failing_healthcheck();
     config.healthchecks.require_healthy = true;
