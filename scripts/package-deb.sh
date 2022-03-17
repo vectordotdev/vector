63b68d0fa9d818677c26e256ca078c30896bfc8a
Jesse Szwedko <jesse@szwedko.me>
2022-03-17 22:21:25 +0000

    fix(releasing): Fix debian packaging
    Accidentally broke this in
    https://github.com/vectordotdev/vector/pull/11887
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/package-deb.sh
+++ scripts/package-deb.sh
@@ -77,7 +77,8 @@ cargo deb --target "$TARGET" --deb-version "${PACKAGE_VERSION}-1" --variant "$TA
 for file in target/"${TARGET}"/debian/*.deb; do
   base=$(basename "${file}")
   nbase=${base//-${TARGET}/}
-  mv "${file}" target/"${TARGET}"/debian/"${nbase}";
+  # -f since the deb won't have TARGET in its name if not a "variant"
+  mv -f "${file}" target/"${TARGET}"/debian/"${nbase}"
 done
 
 #
