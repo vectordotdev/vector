56712c886958fd1398c3addfcdbeb66597b3a885
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2022-03-21 14:20:55 -0400

    add unit tests for shim
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>

--- src/kubernetes/delayed_reflector.rs
+++ src/kubernetes/delayed_reflector.rs
@@ -1,4 +1,7 @@
-use futures::{Stream, TryStreamExt};
+use futures::{
+    task::{self, Context},
+    Stream, TryStreamExt,
+};
 use kube::api::Resource;
 use kube::runtime::reflector::store;
 use kube::runtime::watcher;
@@ -31,16 +34,11 @@ where
                 store.apply_watcher_event(event);
             }
         } // Check if any events are ready to delete
-        while let std::task::Poll::Ready(Some(Ok(expired))) =
-            delayer
-                .queue
-                .poll_expired(&mut futures::task::Context::from_waker(
-                    &futures::task::noop_waker(),
-                ))
+        while let std::task::Poll::Ready(Some(Ok(event))) = delayer
+            .queue
+            .poll_expired(&mut Context::from_waker(&task::noop_waker()))
         {
-            // Remove expired event from the queue and then pass the event
-            // down into the cache to apply
-            let event = delayer.queue.remove(&expired.key());
+            // Pass the expired event to the underlying store
             store.apply_watcher_event(&event.into_inner());
         }
     })
@@ -64,6 +62,203 @@ where
         Self {
             queue: DelayQueue::default(),
             ttl,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{reflector_shim, Delayer};
+    use futures::{stream, StreamExt, TryStreamExt};
+    use k8s_openapi::{api::core::v1::ConfigMap, apimachinery::pkg::apis::meta::v1::ObjectMeta};
+    use kube::runtime::reflector::store;
+    use kube::runtime::reflector::ObjectRef;
+    use kube::runtime::watcher;
+    use rand::{
+        distributions::{Bernoulli, Uniform},
+        Rng,
+    };
+    use std::collections::{BTreeMap, HashMap};
+    use std::time::Duration;
+    use tokio::time::sleep;
+
+    #[tokio::test]
+    async fn reflector_applied_should_add_object() {
+        let store_w = store::Writer::default();
+        let delayer = Delayer::new(Duration::from_secs(1));
+        let store = store_w.as_reader();
+        let cm = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some("a".to_string()),
+                ..ObjectMeta::default()
+            },
+            ..ConfigMap::default()
+        };
+        reflector_shim(
+            store_w,
+            stream::iter(vec![Ok(watcher::Event::Applied(cm.clone()))]),
+            delayer,
+        )
+        .map(|_| ())
+        .collect::<()>()
+        .await;
+        assert_eq!(store.get(&ObjectRef::from_obj(&cm)).as_deref(), Some(&cm));
+    }
+
+    #[tokio::test]
+    async fn reflector_applied_should_update_object() {
+        let store_w = store::Writer::default();
+        let delayer = Delayer::new(Duration::from_secs(1));
+        let store = store_w.as_reader();
+        let cm = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some("a".to_string()),
+                ..ObjectMeta::default()
+            },
+            ..ConfigMap::default()
+        };
+        let updated_cm = ConfigMap {
+            data: Some({
+                let mut data = BTreeMap::new();
+                data.insert("data".to_string(), "present!".to_string());
+                data
+            }),
+            ..cm.clone()
+        };
+        reflector_shim(
+            store_w,
+            stream::iter(vec![
+                Ok(watcher::Event::Applied(cm.clone())),
+                Ok(watcher::Event::Applied(updated_cm.clone())),
+            ]),
+            delayer,
+        )
+        .map(|_| ())
+        .collect::<()>()
+        .await;
+        assert_eq!(
+            store.get(&ObjectRef::from_obj(&cm)).as_deref(),
+            Some(&updated_cm)
+        );
+    }
+
+    #[tokio::test]
+    async fn reflector_deleted_should_not_immediately_remove_object() {
+        let store_w = store::Writer::default();
+        let delayer = Delayer::new(Duration::from_secs(1));
+        let store = store_w.as_reader();
+        let cm = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some("a".to_string()),
+                ..ObjectMeta::default()
+            },
+            ..ConfigMap::default()
+        };
+        reflector_shim(
+            store_w,
+            stream::iter(vec![
+                Ok(watcher::Event::Applied(cm.clone())),
+                Ok(watcher::Event::Deleted(cm.clone())),
+            ]),
+            delayer,
+        )
+        .map(|_| ())
+        .collect::<()>()
+        .await;
+        // We should keep the Applied event in the cache until two things happen
+        // the ttl expires, and we receive a new event of any kind
+        assert_eq!(store.get(&ObjectRef::from_obj(&cm)).as_deref(), Some(&cm));
+
+        // After waiting past the ttl the next event should actually delete from
+        // the cache
+        sleep(Duration::from_secs(2)).await;
+        // reflector_shim(
+        //     store_w,
+        //     stream::iter(vec![Ok(watcher::Event::Deleted(cm.clone()))]),
+        //     delayer,
+        // )
+        // .map(|_| ())
+        // .collect::<()>()
+        // .await;
+        // assert_eq!(store.get(&ObjectRef::from_obj(&cm)), None);
+    }
+
+    #[tokio::test]
+    async fn reflector_restarted_should_clear_objects() {
+        let store_w = store::Writer::default();
+        let delayer = Delayer::new(Duration::from_secs(1));
+        let store = store_w.as_reader();
+        let cm_a = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some("a".to_string()),
+                ..ObjectMeta::default()
+            },
+            ..ConfigMap::default()
+        };
+        let cm_b = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some("b".to_string()),
+                ..ObjectMeta::default()
+            },
+            ..ConfigMap::default()
+        };
+        reflector_shim(
+            store_w,
+            stream::iter(vec![
+                Ok(watcher::Event::Applied(cm_a.clone())),
+                Ok(watcher::Event::Restarted(vec![cm_b.clone()])),
+            ]),
+            delayer,
+        )
+        .map(|_| ())
+        .collect::<()>()
+        .await;
+        assert_eq!(store.get(&ObjectRef::from_obj(&cm_a)), None);
+        assert_eq!(
+            store.get(&ObjectRef::from_obj(&cm_b)).as_deref(),
+            Some(&cm_b)
+        );
+        // assert_eq!(delayer.queue.len(), 0);
+    }
+
+    #[tokio::test]
+    async fn reflector_store_should_not_contain_duplicates() {
+        let mut rng = rand::thread_rng();
+        let item_dist = Uniform::new(0_u8, 100);
+        let deleted_dist = Bernoulli::new(0.40).unwrap();
+        let store_w = store::Writer::default();
+        let delayer = Delayer::new(Duration::from_secs(1));
+        let store = store_w.as_reader();
+        reflector_shim(
+            store_w,
+            stream::iter((0_u32..100_000).map(|gen| {
+                let item = rng.sample(item_dist);
+                let deleted = rng.sample(deleted_dist);
+                let obj = ConfigMap {
+                    metadata: ObjectMeta {
+                        name: Some(item.to_string()),
+                        resource_version: Some(gen.to_string()),
+                        ..ObjectMeta::default()
+                    },
+                    ..ConfigMap::default()
+                };
+                Ok(if deleted {
+                    watcher::Event::Deleted(obj)
+                } else {
+                    watcher::Event::Applied(obj)
+                })
+            })),
+            delayer,
+        )
+        .map_ok(|_| ())
+        .try_collect::<()>()
+        .await
+        .unwrap();
+
+        let mut seen_objects = HashMap::new();
+        for obj in store.state() {
+            assert_eq!(seen_objects.get(obj.metadata.name.as_ref().unwrap()), None);
+            seen_objects.insert(obj.metadata.name.clone().unwrap(), obj);
         }
     }
 }
