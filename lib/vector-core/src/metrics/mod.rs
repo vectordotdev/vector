ded0d167539e28f242c0fff6331bf194b7ecc60e
Jesse Szwedko <jesse@szwedko.me>
2022-03-25 12:38:54 -0700

    Remove stray debug
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- lib/vector-core/src/metrics/mod.rs
+++ lib/vector-core/src/metrics/mod.rs
@@ -144,7 +144,6 @@ impl Controller {
         // Add alias `processed_events_total` for `component_sent_events_total`.
         for i in 0..metrics.len() {
             let metric = &metrics[i];
-            dbg!(&metric);
             match metric.name() {
                 "component_sent_events_total" => {
                     let alias = metric.clone().with_name("processed_events_total");
