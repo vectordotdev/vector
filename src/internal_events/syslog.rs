59cb2ce295b29ffa16e92165866a5d1193b8bd11
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-03-28 09:51:42 +0000

    syslog: update errors
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

--- src/internal_events/syslog.rs
+++ src/internal_events/syslog.rs
@@ -1,3 +1,4 @@
+use super::prelude::{error_stage, error_type};
 use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
@@ -10,7 +11,19 @@ pub struct SyslogUdpReadError {
 #[cfg(feature = "codecs")]
 impl InternalEvent for SyslogUdpReadError {
     fn emit(self) {
-        error!(message = "Error reading datagram.", error = ?self.error, internal_log_rate_secs = 10);
+        error!(
+            message = "Error reading datagram.",
+            error = ?self.error,
+            internal_log_rate_secs = 10,
+            error_type = error_type::READER_FAILED,
+            stage = error_stage::RECEIVING,
+        );
+        counter!(
+            "component_errors_total", 1,
+            "error_type" => error_type::READER_FAILED,
+            "stage" => error_stage::RECEIVING,
+        );
+        // deprecated
         counter!("connection_read_errors_total", 1, "mode" => "udp");
     }
 }
@@ -21,7 +34,19 @@ pub(crate) struct SyslogConvertUtf8Error {
 
 impl InternalEvent for SyslogConvertUtf8Error {
     fn emit(self) {
-        error!(message = "Error converting bytes to UTF-8 string.", error = %self.error, internal_log_rate_secs = 10);
+        error!(
+            message = "Error converting bytes to UTF-8 string.",
+            error = %self.error,
+            internal_log_rate_secs = 10,
+            error_type = error_type::PARSER_FAILED,
+            stage = error_stage::RECEIVING,
+        );
+        counter!(
+            "component_errors_total", 1,
+            "error_type" => error_type::PARSER_FAILED,
+            "stage" => error_stage::RECEIVING,
+        );
+        // deprecated
         counter!("utf8_convert_errors_total", 1);
     }
 }
