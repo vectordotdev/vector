39ff4d655d974c1573063ee568be8757fa532b3b
Jesse Szwedko <jesse@szwedko.me>
2022-03-30 16:25:29 -0400

    fix(aws_s3 sink): Conditionally set tagging
    Setting the `x-amz-tagging` header with no value seems to not work with
    Ceph, an S3 compatible endpoint.
    
    Addresses: https://github.com/vectordotdev/vector/issues/1498#issuecomment-1083490832
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sinks/s3_common/service.rs
+++ src/sinks/s3_common/service.rs
@@ -111,13 +111,13 @@ impl Service<S3Request> for S3Service {
 
         let content_md5 = base64::encode(md5::Md5::digest(&request.body));
 
-        let mut tagging = url::form_urlencoded::Serializer::new(String::new());
-        if let Some(tags) = options.tags {
+        let tagging = options.tags.map(|tags| {
+            let mut tagging = url::form_urlencoded::Serializer::new(String::new());
             for (p, v) in tags {
                 tagging.append_pair(&p, &v);
             }
-        }
-        let tagging = tagging.finish();
+            tagging.finish()
+        });
         let count = request.metadata.count;
         let events_byte_size = request.metadata.byte_size;
 
@@ -141,7 +141,7 @@ impl Service<S3Request> for S3Service {
                 .set_server_side_encryption(options.server_side_encryption.map(Into::into))
                 .set_ssekms_key_id(options.ssekms_key_id)
                 .set_storage_class(options.storage_class.map(Into::into))
-                .tagging(tagging)
+                .set_tagging(tagging)
                 .content_md5(content_md5)
                 .send()
                 .in_current_span()
