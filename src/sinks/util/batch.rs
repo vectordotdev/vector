35442d659fc48163c5dd03a0e09d0bd1dd9d3941
Jesse Szwedko <jesse@szwedko.me>
2022-03-31 16:48:13 -0400

    clippy
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sinks/util/batch.rs
+++ src/sinks/util/batch.rs
@@ -101,7 +101,7 @@ impl<D: SinkBatchSettings> BatchConfig<D, Unmerged> {
         let config = BatchConfig {
             max_bytes: self.max_bytes.or(D::MAX_BYTES),
             max_events: self.max_events.or(D::MAX_EVENTS),
-            timeout_secs: self.timeout_secs.or_else(|| Some(D::TIMEOUT_SECS)),
+            timeout_secs: self.timeout_secs.or(D::TIMEOUT_SECS),
             _d: PhantomData,
             _s: PhantomData,
         };
