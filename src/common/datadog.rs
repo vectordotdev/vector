1df17f71c7af485636d94529c265330c813aefc4
Lee Benson <lee@leebenson.com>
2022-04-01 13:25:48 +0100

    lint
    Signed-off-by: Lee Benson <lee@leebenson.com>

--- src/common/datadog.rs
+++ src/common/datadog.rs
@@ -1,3 +1,6 @@
+// Allow unused imports here, since use of these functions will differ depending on the
+// Datadog component type, whether it's used in integration tests, etc.
+#![allow(dead_code)]
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
@@ -26,14 +29,9 @@ pub(crate) enum DatadogMetricType {
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub(crate) struct DatadogPoint<T>(pub(crate) i64, pub(crate) T);
 
-#[cfg(any(
-    feature = "sinks-datadog_logs",
-    feature = "sinks-datadog_events",
-    feature = "enterprise"
-))]
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-pub enum Region {
+pub(crate) enum Region {
     Us,
     Eu,
 }
@@ -42,7 +40,6 @@ pub enum Region {
 ///
 /// If `site` is not specified, we fallback to `region`, and if that is not specified, we
 /// fallback to the Datadog US domain.
-#[cfg(any(feature = "sinks-datadog_metrics", feature = "enterprise"))]
 pub(crate) fn get_base_domain(site: Option<&String>, region: Option<Region>) -> &str {
     site.map(|s| s.as_str()).unwrap_or_else(|| match region {
         Some(Region::Eu) => "datadoghq.eu",
@@ -54,7 +51,6 @@ pub(crate) fn get_base_domain(site: Option<&String>, region: Option<Region>) -> 
 ///
 /// If `site` is not specified, we fallback to `region`, and if that is not specified, we fallback
 /// to the Datadog US domain.
-#[cfg(any(feature = "sinks-datadog_metrics", feature = "enterprise"))]
 pub(crate) fn get_api_base_endpoint(
     endpoint: Option<&String>,
     site: Option<&String>,
