6a930b5d1f57c3c40b80d5a881982190286e4966
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-04-07 17:54:26 -0600

    fix(splunk_hec source): Fix handling of acknowledgements (#12125)
    * Introduce generic FuturesSet type for finalizers
    
    * Make the finalizer set implementation generic over the ordering
    
    * Use new unordered finalizer in splunk_hec
    
    * Only apply batches as done if they received a positive acknowledgement
    
    * Add a test

--- src/sources/util/finalizer.rs
+++ src/sources/util/finalizer.rs
@@ -1,35 +1,55 @@
+use std::marker::{PhantomData, Unpin};
 use std::{future::Future, pin::Pin, task::Poll};
 
-use futures::{future::Shared, stream::FuturesOrdered, FutureExt, StreamExt};
+use futures::stream::{FuturesOrdered, FuturesUnordered};
+use futures::{future::Shared, FutureExt, Stream, StreamExt};
 use tokio::sync::mpsc;
 
-use crate::{event::BatchStatusReceiver, shutdown::ShutdownSignal};
+use crate::event::{BatchStatus, BatchStatusReceiver};
+use crate::shutdown::ShutdownSignal;
 
-/// The `OrderedFinalizer` framework here is a mechanism for marking
-/// events from a source as done in a single background task *in the
-/// order they are received from the source*. The type `T` is the
-/// source-specific data associated with each entry to be used to
-/// complete the finalization.
-pub(crate) struct OrderedFinalizer<T> {
+/// The `OrderedFinalizer` framework marks events from a source as
+/// done in a single background task *in the order they are received
+/// from the source*, using `FinalizerSet`.
+#[cfg(any(feature = "sources-file", feature = "sources-kafka",))]
+pub(crate) type OrderedFinalizer<T> = FinalizerSet<T, FuturesOrdered<FinalizerFuture<T>>>;
+
+/// The `UnorderedFinalizer` framework marks events from a source as
+/// done in a single background task *in the order the finalization
+/// happens on the event batches*, using `FinalizerSet`.
+#[cfg(feature = "sources-splunk_hec")]
+pub(crate) type UnorderedFinalizer<T> = FinalizerSet<T, FuturesUnordered<FinalizerFuture<T>>>;
+
+/// The `FinalizerSet` framework here is a mechanism for marking
+/// events from a source as done in a single background task. The type
+/// `T` is the source-specific data associated with each entry to be
+/// used to complete the finalization.
+pub(crate) struct FinalizerSet<T, F> {
     sender: Option<mpsc::UnboundedSender<(BatchStatusReceiver, T)>>,
+    _finalizer: PhantomData<F>,
 }
 
-impl<T: Send + 'static> OrderedFinalizer<T> {
+impl<T, F> FinalizerSet<T, F>
+where
+    T: Send + 'static,
+    F: FuturesSet<FinalizerFuture<T>> + Default + Send + Unpin + 'static,
+{
     pub(crate) fn new(
         shutdown: Shared<ShutdownSignal>,
         apply_done: impl Fn(T) + Send + 'static,
     ) -> Self {
         let (sender, receiver) = mpsc::unbounded_channel();
-        tokio::spawn(run_finalizer(shutdown, receiver, apply_done));
+        tokio::spawn(run_finalizer(shutdown, receiver, apply_done, F::default()));
         Self {
             sender: Some(sender),
+            _finalizer: Default::default(),
         }
     }
 
     pub(crate) fn add(&self, entry: T, receiver: BatchStatusReceiver) {
         if let Some(sender) = &self.sender {
             if let Err(error) = sender.send((receiver, entry)) {
-                error!(message = "OrderedFinalizer task ended prematurely.", %error);
+                error!(message = "FinalizerSet task ended prematurely.", %error);
             }
         }
     }
@@ -39,9 +59,8 @@ async fn run_finalizer<T>(
     shutdown: Shared<ShutdownSignal>,
     mut new_entries: mpsc::UnboundedReceiver<(BatchStatusReceiver, T)>,
     apply_done: impl Fn(T),
+    mut status_receivers: impl FuturesSet<FinalizerFuture<T>> + Unpin,
 ) {
-    let mut status_receivers = FuturesOrdered::default();
-
     loop {
         tokio::select! {
             _ = shutdown.clone() => break,
@@ -55,7 +74,9 @@ async fn run_finalizer<T>(
                 None => break,
             },
             finished = status_receivers.next(), if !status_receivers.is_empty() => match finished {
-                Some((_status, entry)) => apply_done(entry),
+                Some((status, entry)) => if status == BatchStatus::Delivered {
+                    apply_done(entry);
+                }
                 // The is_empty guard above prevents this from being reachable.
                 None => unreachable!(),
             },
@@ -64,14 +85,41 @@ async fn run_finalizer<T>(
     // We've either seen a shutdown signal or the new entry sender was
     // closed. Wait for the last statuses to come in before indicating
     // we are done.
-    while let Some((_status, entry)) = status_receivers.next().await {
-        apply_done(entry);
+    while let Some((status, entry)) = status_receivers.next().await {
+        if status == BatchStatus::Delivered {
+            apply_done(entry);
+        }
     }
     drop(shutdown);
 }
 
+pub(crate) trait FuturesSet<Fut: Future>: Stream<Item = Fut::Output> {
+    fn is_empty(&self) -> bool;
+    fn push(&mut self, future: Fut);
+}
+
+impl<Fut: Future> FuturesSet<Fut> for FuturesOrdered<Fut> {
+    fn is_empty(&self) -> bool {
+        Self::is_empty(self)
+    }
+
+    fn push(&mut self, future: Fut) {
+        Self::push(self, future)
+    }
+}
+
+impl<Fut: Future> FuturesSet<Fut> for FuturesUnordered<Fut> {
+    fn is_empty(&self) -> bool {
+        Self::is_empty(self)
+    }
+
+    fn push(&mut self, future: Fut) {
+        Self::push(self, future)
+    }
+}
+
 #[pin_project::pin_project]
-struct FinalizerFuture<T> {
+pub(crate) struct FinalizerFuture<T> {
     receiver: BatchStatusReceiver,
     entry: Option<T>,
 }
@@ -80,8 +128,8 @@ impl<T> Future for FinalizerFuture<T> {
     type Output = (<BatchStatusReceiver as Future>::Output, T);
     fn poll(mut self: Pin<&mut Self>, ctx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
         let status = futures::ready!(self.receiver.poll_unpin(ctx));
-        // The use of this above in a `FuturesOrdered` will only take
-        // this once before dropping the future.
+        // The use of this above in a `Futures{Ordered|Unordered|`
+        // will only take this once before dropping the future.
         Poll::Ready((status, self.entry.take().unwrap_or_else(|| unreachable!())))
     }
 }
