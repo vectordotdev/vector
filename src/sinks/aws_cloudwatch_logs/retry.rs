9c0eec6db925f7a3c3057123f8fc31e16f6af758
Nathan Fox <fuchsnj@gmail.com>
2022-04-19 15:15:28 -0400

    retry cloudwatch logs ThrottlingException

--- src/sinks/aws_cloudwatch_logs/retry.rs
+++ src/sinks/aws_cloudwatch_logs/retry.rs
@@ -1,3 +1,4 @@
+use crate::aws::is_retriable_error;
 use aws_sdk_cloudwatchlogs::error::{
     CreateLogStreamErrorKind, DescribeLogStreamsErrorKind, PutLogEventsErrorKind,
 };
@@ -33,13 +34,13 @@ impl<T: Send + Sync + 'static> RetryLogic for CloudwatchRetryLogic<T> {
     #[allow(clippy::cognitive_complexity)] // long, but just a hair over our limit
     fn is_retriable_error(&self, error: &Self::Error) -> bool {
         match error {
-            CloudwatchError::Put(err) => match err {
+            CloudwatchError::Put(sdk_err) => match sdk_err {
                 SdkError::ServiceError { err, raw: _ } => match err.kind {
                     PutLogEventsErrorKind::ServiceUnavailableException(_) => {
                         error!(message = "Put logs service unavailable.", %error);
                         true
                     }
-                    _ => false,
+                    _ => is_retriable_error(sdk_err),
                 },
                 SdkError::DispatchFailure(_err) => {
                     error!(message = "Put logs HTTP dispatch.", %error);
@@ -60,13 +61,13 @@ impl<T: Send + Sync + 'static> RetryLogic for CloudwatchRetryLogic<T> {
                 SdkError::ConstructionFailure(_) => false,
                 SdkError::TimeoutError(_) => true,
             },
-            CloudwatchError::Describe(err) => match err {
+            CloudwatchError::Describe(sdk_err) => match sdk_err {
                 SdkError::ServiceError { err, raw: _ } => match err.kind {
                     DescribeLogStreamsErrorKind::ServiceUnavailableException(_) => {
                         error!(message = "Describe streams service unavailable.", %error);
                         true
                     }
-                    _ => false,
+                    _ => is_retriable_error(sdk_err),
                 },
                 SdkError::TimeoutError(_) => true,
                 SdkError::DispatchFailure(_) => {
@@ -86,13 +87,13 @@ impl<T: Send + Sync + 'static> RetryLogic for CloudwatchRetryLogic<T> {
                 }
                 SdkError::ConstructionFailure(_) => false,
             },
-            CloudwatchError::CreateStream(err) => match err {
+            CloudwatchError::CreateStream(sdk_err) => match sdk_err {
                 SdkError::ServiceError { err, raw: _ } => match err.kind {
                     CreateLogStreamErrorKind::ServiceUnavailableException(_) => {
                         error!(message = "Create stream service unavailable.", %error);
                         true
                     }
-                    _ => false,
+                    _ => is_retriable_error(sdk_err),
                 },
                 SdkError::TimeoutError(_) => true,
                 SdkError::DispatchFailure(_) => {
@@ -114,5 +115,40 @@ impl<T: Send + Sync + 'static> RetryLogic for CloudwatchRetryLogic<T> {
             },
             _ => false,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::sinks::aws_cloudwatch_logs::retry::CloudwatchRetryLogic;
+    use crate::sinks::aws_cloudwatch_logs::service::CloudwatchError;
+    use crate::sinks::util::retries::RetryLogic;
+    use aws_sdk_cloudwatchlogs::error::{PutLogEventsError, PutLogEventsErrorKind};
+    use aws_sdk_cloudwatchlogs::types::SdkError;
+    use aws_smithy_http::body::SdkBody;
+    use aws_smithy_http::operation::Response;
+
+    #[test]
+    fn test_throttle_retry() {
+        let retry_logic: CloudwatchRetryLogic<()> = CloudwatchRetryLogic::new();
+
+        let meta_err = aws_smithy_types::Error::builder()
+            .code("ThrottlingException")
+            .message("Rate exceeded for logStreamName log-test-1.us-east-1.compute.internal")
+            .request_id("0ac34e43-f6ff-4e1b-96be-7d03b2be8376")
+            .build();
+
+        let mut http_response = http::Response::new(SdkBody::from("{\"__type\":\"ThrottlingException\",\"message\":\"Rate exceeded for logStreamName log-test-1.us-east-1.compute.internal\"}"));
+        *http_response.status_mut() = http::StatusCode::BAD_REQUEST;
+        let raw = Response::new(http_response);
+
+        let err = CloudwatchError::Put(SdkError::ServiceError {
+            err: PutLogEventsError::new(
+                PutLogEventsErrorKind::Unhandled(Box::new(meta_err.clone())),
+                meta_err,
+            ),
+            raw,
+        });
+        assert!(retry_logic.is_retriable_error(&err));
     }
 }
