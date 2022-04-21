c53d01c0e0209958647121a6d3cccd47c8305c55
Will <43912346+001wwang@users.noreply.github.com>
2022-04-21 19:19:20 +0200

    fix(cli): Use user-provided array values in vector config --include-defaults (#12337)
    * Use user configured value for arrays
    
    * Fix array test
    
    * Fix formatting
    
    * Override array value with user-provided object value

--- src/config/cmd.rs
+++ src/config/cmd.rs
@@ -83,12 +83,6 @@ fn merge_json(a: &mut Value, b: Value) {
                 merge_json(a.entry(k).or_insert(Value::Null), v);
             }
         }
-        (Value::Array(ref mut a), Value::Object(b)) => {
-            a.extend([Value::Object(b)]);
-        }
-        (Value::Array(ref mut a), Value::Array(b)) => {
-            a.extend(b);
-        }
         (a, b) => {
             *a = b;
         }
@@ -154,24 +148,21 @@ mod tests {
     use serde_json::json;
 
     #[test]
-    fn test_array_concat() {
+    fn test_array_override() {
         let mut json = json!({
             "arr": [
                 "value1", "value2"
             ]
         });
 
-        let to_add = json!({
+        let to_override = json!({
             "arr": [
                 "value3", "value4"
             ]
         });
 
-        merge_json(&mut json, to_add);
+        merge_json(&mut json, to_override);
 
-        assert_eq!(
-            *json.get("arr").unwrap(),
-            json!(["value1", "value2", "value3", "value4"])
-        )
+        assert_eq!(*json.get("arr").unwrap(), json!(["value3", "value4"]))
     }
 }
