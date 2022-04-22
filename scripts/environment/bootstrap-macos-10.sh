304ef545b826260c294fdcc208b148c78d12c905
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-04-22 16:06:36 -0600

    Fix MacOS no vendor env too

--- scripts/environment/bootstrap-macos-10.sh
+++ scripts/environment/bootstrap-macos-10.sh
@@ -10,8 +10,9 @@ gem install bundler
 echo "export PATH=\"/usr/local/opt/ruby/bin:\$PATH\"" >> "$HOME/.bash_profile"
 
 # Force the proto-build crate to avoid building the vendored protoc.
-echo "export PROTO_NO_VENDOR=1" >> "$HOME/.bash_profile"
+echo "export PROTOC_NO_VENDOR=1" >> "$HOME/.bash_profile"
 
 if [ -n "${CI-}" ] ; then
   echo "/usr/local/opt/ruby/bin" >> "$GITHUB_PATH"
+  echo PROTOC_NO_VENDOR=1 >> "$GITHUB_ENV"
 fi
