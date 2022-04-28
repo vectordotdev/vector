85acde55291dd7bce5c0949c8dbae24ee90a4563
Justin Knueppel <Justin_Knueppel@comcast.com>
2022-04-28 14:26:52 -0400

    Attempt integration test

--- src/sources/socket/mod.rs
+++ src/sources/socket/mod.rs
@@ -238,7 +238,7 @@ impl SourceConfig for SocketConfig {
 #[cfg(test)]
 mod test {
     use std::{
-        collections::HashMap,
+        collections::{HashMap, BTreeMap},
         net::{SocketAddr, UdpSocket},
         sync::{
             atomic::{AtomicBool, Ordering},
@@ -256,6 +256,7 @@ mod test {
         task::JoinHandle,
         time::{timeout, Duration, Instant},
     };
+    use vector_common::btreemap;
     use vector_core::event::EventContainer;
     #[cfg(unix)]
     use {
@@ -415,11 +416,11 @@ mod test {
             let (tx, mut rx) = SourceSender::new_test();
             let addr = next_addr();
 
-        let mut config = TcpConfig::from_address(addr.into());
-        config.set_tls(Some(TlsSourceConfig {
-            tls_config: TlsEnableableConfig::test_config(),
-            peer_key: None,
-        }));
+            let mut config = TcpConfig::from_address(addr.into());
+            config.set_tls(Some(TlsSourceConfig {
+                tls_config: TlsEnableableConfig::test_config(),
+                peer_key: Some("tls_peer".into()),
+            }));
 
             let server = SocketConfig::from(config)
                 .build(SourceContext::new_test(tx, None))
@@ -430,7 +431,13 @@ mod test {
             let lines = vec!["one line".to_owned(), "another line".to_owned()];
 
             wait_for_tcp(addr).await;
-            send_lines_tls(addr, "localhost".into(), lines.into_iter(), None)
+            send_lines_tls(
+                addr, "localhost".into(), 
+                lines.into_iter(), 
+                std::path::Path::new(tls::TEST_PEM_CA_PATH),
+                std::path::Path::new("tests/data/tls_meta_client.crt"),
+                std::path::Path::new("tests/data/tls_meta_client.key"),
+            )
                 .await
                 .unwrap();
 
@@ -438,12 +445,28 @@ mod test {
             assert_eq!(
                 event.as_log()[log_schema().message_key()],
                 "one line".into()
+            
+            );
+
+            let tls_meta: BTreeMap<String, value::Value> = btreemap!(
+                "subject" => "CN=localhost,OU=TLS Tests,O=Timber.io,L=Brooklyn,ST=New York,C=US"
+            );
+
+            assert_eq!(
+                event.as_log()["tls_peer"],
+                tls_meta.clone().into(),
             );
 
             let event = rx.next().await.unwrap();
             assert_eq!(
                 event.as_log()[log_schema().message_key()],
                 "another line".into()
+            );
+
+            assert_eq!(
+                event.as_log()["tls_peer"],
+                tls_meta.clone().into(),
+    
             );
         })
         .await;
@@ -455,18 +478,18 @@ mod test {
             let (tx, mut rx) = SourceSender::new_test();
             let addr = next_addr();
 
-        let mut config = TcpConfig::from_address(addr.into());
-        config.set_tls(Some(TlsSourceConfig {
-            tls_config: TlsEnableableConfig {
-                enabled: Some(true),
-                options: TlsConfig {
-                    crt_file: Some("tests/data/Chain_with_intermediate.crt".into()),
-                    key_file: Some("tests/data/Crt_from_intermediate.key".into()),
-                    ..Default::default()
+            let mut config = TcpConfig::from_address(addr.into());
+            config.set_tls(Some(TlsSourceConfig {
+                tls_config: TlsEnableableConfig {
+                    enabled: Some(true),
+                    options: TlsConfig {
+                        crt_file: Some("tests/data/Chain_with_intermediate.crt".into()),
+                        key_file: Some("tests/data/Crt_from_intermediate.key".into()),
+                        ..Default::default()
+                    },
                 },
-            },
-            peer_key: None,
-        }));
+                peer_key: None,
+            }));
 
             let server = SocketConfig::from(config)
                 .build(SourceContext::new_test(tx, None))
@@ -482,6 +505,8 @@ mod test {
                 "localhost".into(),
                 lines.into_iter(),
                 std::path::Path::new(tls::TEST_PEM_CA_PATH),
+                None,
+                None,
             )
             .await
             .unwrap();
