81cc105fbed702adfbfa6e52292dd1f0192fa544
Luke Steensen <luke.steensen@gmail.com>
2022-05-05 17:50:57 -0500

    enhancement(observability): add batch size histogram metric (#11290)
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- lib/vector-common/src/internal_event/events_received.rs
+++ lib/vector-common/src/internal_event/events_received.rs
@@ -1,4 +1,4 @@
-use metrics::counter;
+use metrics::{counter, histogram};
 use tracing::trace;
 
 use crate::internal_event::InternalEvent;
@@ -12,6 +12,10 @@ pub struct EventsReceived {
 impl InternalEvent for EventsReceived {
     fn emit(self) {
         trace!(message = "Events received.", count = %self.count, byte_size = %self.byte_size);
+
+        #[allow(clippy::cast_precision_loss)]
+        let fcount = self.count as f64;
+        histogram!("component_received_events_count", fcount);
         counter!("component_received_events_total", self.count as u64);
         counter!("events_in_total", self.count as u64);
         counter!(
