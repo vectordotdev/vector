b37ad9b7d6f21859b38f2fd91bdcfdeb739d973e
Jesse Szwedko <jesse@szwedko.me>
2022-05-05 08:06:24 -0700

    fix(aws provider): Disable retries
    We have our own retry layer wired up with ARC so we disable AWS's retries.
    
    This also appears to match the AWS component's previous behavior with
    rusoto (see https://github.com/rusoto/rusoto/issues/234 and discussion).
    
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- src/sinks/aws_kinesis_firehose/config.rs
+++ src/sinks/aws_kinesis_firehose/config.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 use aws_sdk_firehose::{Client as KinesisFirehoseClient, Endpoint, Region};
 use aws_smithy_async::rt::sleep::AsyncSleep;
 use aws_smithy_client::erase::DynConnector;
+use aws_smithy_types::retry::RetryConfig;
 use aws_types::credentials::SharedCredentialsProvider;
 use futures::FutureExt;
 use serde::{Deserialize, Serialize};
@@ -138,6 +139,13 @@ impl ClientBuilder for KinesisFirehoseClientBuilder {
         sleep_impl: Arc<dyn AsyncSleep>,
     ) -> Self::ConfigBuilder {
         builder.sleep_impl(sleep_impl)
+    }
+
+    fn with_retry_config(
+        builder: Self::ConfigBuilder,
+        retry_config: RetryConfig,
+    ) -> Self::ConfigBuilder {
+        builder.retry_config(retry_config)
     }
 
     fn client_from_conf_conn(
