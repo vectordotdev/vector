751a7f206ca477fe6cee33722bba27aedeec609c
001wwang <will.wang@datadoghq.com>
2022-05-12 03:36:25 +0000

    Add prop test infrastructure for vector config

--- src/config/cmd.rs
+++ src/config/cmd.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use serde_json::Value;
 
-use super::{load_builder_from_paths, load_source_from_paths, process_paths};
+use super::{load_builder_from_paths, load_source_from_paths, process_paths, ConfigBuilder};
 use crate::cli::handle_config_errors;
 use crate::config;
 
@@ -89,6 +89,40 @@ fn merge_json(a: &mut Value, b: Value) {
     }
 }
 
+/// Convert a raw user config to a JSON string
+fn serialize_to_json(
+    source: toml::value::Table,
+    source_builder: &ConfigBuilder,
+    include_defaults: bool,
+    pretty_print: bool,
+) -> serde_json::Result<String> {
+    // Convert table to JSON
+    let mut source_json = serde_json::to_value(source)
+        .expect("should serialize config source to JSON. Please report.");
+
+    // If a user has requested default fields, we'll serialize a `ConfigBuilder`. Otherwise,
+    // we'll serialize the raw user provided config (without interpolated env vars, to preserve
+    // the original source).
+    if include_defaults {
+        // For security, we don't want environment variables to be interpolated in the final
+        // output, but we *do* want defaults. To work around this, we'll serialize `ConfigBuilder`
+        // to JSON, and merge in the raw config which will contain the pre-interpolated strings.
+        let mut builder = serde_json::to_value(&source_builder)
+            .expect("should serialize ConfigBuilder to JSON. Please report.");
+
+        merge_json(&mut builder, source_json);
+
+        source_json = builder
+    }
+
+    // Get a JSON string. This will either be pretty printed or (default) minified.
+    if pretty_print {
+        serde_json::to_string_pretty(&source_json)
+    } else {
+        serde_json::to_string(&source_json)
+    }
+}
+
 /// Function used by the `vector config` subcommand for outputting a normalized configuration.
 /// The purpose of this func is to combine user configuration after processing all paths,
 /// Pipelines expansions, etc. The JSON result of this serialization can itself be used as a config,
@@ -105,34 +139,13 @@ pub fn cmd(opts: &Opts) -> exitcode::ExitCode {
         None => return exitcode::CONFIG,
     };
 
-    // Serialize source against normalized paths, and get a TOML `Table` as JSON.
-    let mut source = match load_source_from_paths(&paths) {
-        Ok((map, _)) => serde_json::to_value(map)
-            .expect("should serialize config source to JSON. Please report."),
+    // Load source TOML.
+    let source = match load_source_from_paths(&paths) {
+        Ok((map, _)) => map,
         Err(errs) => return handle_config_errors(errs),
     };
 
-    // If a user has requested default fields, we'll serialize a `ConfigBuilder`. Otherwise,
-    // we'll serialize the raw user provided config (without interpolated env vars, to preserve
-    // the original source).
-    if opts.include_defaults {
-        // For security, we don't want environment variables to be interpolated in the final
-        // output, but we *do* want defaults. To work around this, we'll serialize `ConfigBuilder`
-        // to JSON, and merge in the raw config which will contain the pre-interpolated strings.
-        let mut builder = serde_json::to_value(&builder)
-            .expect("should serialize ConfigBuilder to JSON. Please report.");
-
-        merge_json(&mut builder, source);
-
-        source = builder
-    }
-
-    // Get a JSON string. This will either be pretty printed or (default) minified.
-    let json = if opts.pretty {
-        serde_json::to_string_pretty(&source)
-    } else {
-        serde_json::to_string(&source)
-    };
+    let json = serialize_to_json(source, &builder, opts.include_defaults, opts.pretty);
 
     #[allow(clippy::print_stdout)]
     {
@@ -142,9 +155,19 @@ pub fn cmd(opts: &Opts) -> exitcode::ExitCode {
     exitcode::OK
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "sources", feature = "transforms", feature = "sinks"))]
 mod tests {
+    use proptest::{prelude::*, sample};
+    use rand::{prelude::SliceRandom, thread_rng};
     use serde_json::json;
+
+    use crate::{
+        config::{
+            cmd::serialize_to_json, ConfigBuilder, SinkDescription, SourceDescription,
+            TransformDescription,
+        },
+        generate::{generate_example, TransformInputsStrategy},
+    };
 
     use super::merge_json;
 
@@ -165,5 +188,101 @@ mod tests {
         merge_json(&mut json, to_override);
 
         assert_eq!(*json.get("arr").unwrap(), json!(["value3", "value4"]))
+    }
+
+    /// Select any 2-4 sources
+    fn arb_sources() -> impl Strategy<Value = Vec<&'static str>> {
+        sample::subsequence(SourceDescription::types(), 2..=4)
+    }
+
+    /// Select any 2-4 transforms
+    fn arb_transforms() -> impl Strategy<Value = Vec<&'static str>> {
+        sample::subsequence(TransformDescription::types(), 2..=4)
+    }
+
+    /// Select any 2-4 sinks
+    fn arb_sinks() -> impl Strategy<Value = Vec<&'static str>> {
+        sample::subsequence(SinkDescription::types(), 2..=4)
+    }
+
+    fn create_config_source(sources: &[&str], transforms: &[&str], sinks: &[&str]) -> String {
+        // This creates a string in the syntax expected by the `vector generate`
+        // command whose internal mechanics we are using to create valid Vector
+        // configurations.
+        //
+        // Importantly, we have to name the components (in this case, simply by
+        // their type as each type of component is guaranteed to only appear
+        // once), because (in some tests) we'd like to shuffle the configuration
+        // later in a way that does not change its actual semantics. Otherwise,
+        // an autogenerated ID like `source0` could correspond to different
+        // sources depending on the ordering of the `vector generate` input.
+        //
+        // We also append a fixed `remap` transform to the transforms list. This
+        // ensures sink inputs are consistent since `generate` uses the last
+        // transform the input for each sink.
+        let generate_config_str = format!(
+            "{}/{}/{}",
+            sources
+                .iter()
+                .map(|source| format!("{}:{}", source, source))
+                .collect::<Vec<_>>()
+                .join(","),
+            transforms
+                .iter()
+                .map(|transform| format!("{}:{}", transform, transform))
+                .chain(vec!["manually-added-remap:remap".to_string()])
+                .collect::<Vec<_>>()
+                .join(","),
+            sinks
+                .iter()
+                .map(|sink| format!("{}:{}", sink, sink))
+                .collect::<Vec<_>>()
+                .join(","),
+        );
+        generate_example(
+            false,
+            generate_config_str.as_ref(),
+            &None,
+            TransformInputsStrategy::All,
+        )
+        .expect("invalid config generated")
+    }
+
+    // Properties to test
+    // - Config JSON output is in the same order/format regardless of the input order
+    // - Config JSON output is a valid ConfigBuilder/Config
+    // - Include defaults includes defaults but does not include sensitive information
+
+    proptest! {
+        #[test]
+        /// Output should be the same regardless of input config ordering
+        fn output_has_consistent_ordering(mut sources in arb_sources(), mut transforms in arb_transforms(), mut sinks in arb_sinks()) {
+            let config_source = create_config_source(sources.as_ref(), transforms.as_ref(), sinks.as_ref());
+
+            // Shuffle the ordering of components which shuffles the order in
+            // which items appear in the final config
+            let mut rng = thread_rng();
+            sources.shuffle(&mut rng);
+            transforms.shuffle(&mut rng);
+            sinks.shuffle(&mut rng);
+            let shuffled_config_source = create_config_source(sources.as_ref(), transforms.as_ref(), sinks.as_ref());
+
+            let json = serialize_to_json(
+                toml::from_str(config_source.as_ref()).unwrap(),
+                &ConfigBuilder::from_toml(config_source.as_ref()),
+                false,
+                false
+            )
+            .unwrap();
+            let shuffled_json = serialize_to_json(
+                toml::from_str(shuffled_config_source.as_ref()).unwrap(),
+                &ConfigBuilder::from_toml(shuffled_config_source.as_ref()),
+                false,
+                false
+            )
+            .unwrap();
+
+            assert_eq!(json, shuffled_json);
+        }
     }
 }
