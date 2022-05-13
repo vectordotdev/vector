61d1622446fa16aa629c71310309e852447c1c42
prognant <pierre.rognant@datadoghq.com>
2022-05-13 14:07:00 +0200

    +aggregator skeleton

--- src/sinks/datadog/traces/stats.rs
+++ src/sinks/datadog/traces/stats.rs
@@ -1,17 +1,39 @@
 use crate::{event::TraceEvent, sinks::datadog::traces::sink::PartitionKey};
+use std::collections::BTreeMap;
+use crate::event::Value;
 mod dd_proto {
     include!(concat!(env!("OUT_DIR"), "/dd_trace.rs"));
 }
 
+const TOP_LEVEL_KEY: &str = "_dd.top_level";
+
+struct Aggregator {}
+
+impl Aggregator {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn handle_trace(&self, _trace: &TraceEvent) {}
+
+    fn handle_span(&self, _span: &BTreeMap<String, Value>) {}
+
+    fn get_client_stats_payload(&self) -> Vec<dd_proto::ClientStatsPayload> {
+        vec![]
+    }
+}
 pub(crate) fn compute_apm_stats(
-    _key: &PartitionKey,
-    _events: &[TraceEvent],
+    key: &PartitionKey,
+    traces: &[TraceEvent],
 ) -> dd_proto::StatsPayload {
+    let aggregator = Aggregator::new();
+    traces.iter().for_each(|t| aggregator.handle_trace(t));
+
     dd_proto::StatsPayload {
-        agent_hostname: "dummy".to_string(),
-        agent_env: "dummy".to_string(),
-        stats: vec![],
-        agent_version: "dummy".to_string(),
+        agent_hostname: key.hostname.clone().unwrap_or_else(|| "".to_string()),
+        agent_env: key.env.clone().unwrap_or_else(|| "".to_string()),
+        stats: aggregator.get_client_stats_payload(),
+        agent_version: key.agent_version.clone().unwrap_or_else(|| "".to_string()),
         client_computed: false,
     }
 }
