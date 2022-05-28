2f068efb7d79d86080d79a80dda59f1052f9a697
caibirdme <englanq@126.com>
2022-05-28 23:28:01 +0800

    feat: move convert into lib

--- lib/opentelemetry/src/lib.rs
+++ lib/opentelemetry/src/lib.rs
@@ -1,22 +1,10 @@
-mod proto;
-
 pub use proto::resource::v1::Resource;
 pub use proto::common::v1 as Common;
 pub use proto::logs::v1 as Logs;
 pub use proto::collector::logs::v1 as LogService;
 
-impl From<Logs::InstrumentationLibraryLogs> for Logs::ScopeLogs {
-    fn from(v: Logs::InstrumentationLibraryLogs) -> Self {
-        Self {
-            scope: v.instrumentation_library.map(|v| Common::InstrumentationScope{
-                name: v.name,
-                version: v.version,
-            }),
-            log_records: v.log_records,
-            schema_url: v.schema_url,
-        }
-    }
-}
+mod proto;
+mod convert;
 
 #[cfg(test)]
 mod tests {
