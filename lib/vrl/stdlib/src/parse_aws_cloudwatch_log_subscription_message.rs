b84fe11739d237eb20557e196bf9a6ce5358c86b
Nathan Fox <fuchsnj@gmail.com>
2022-06-16 12:47:04 -0400

    fix type def

--- lib/vrl/stdlib/src/parse_aws_cloudwatch_log_subscription_message.rs
+++ lib/vrl/stdlib/src/parse_aws_cloudwatch_log_subscription_message.rs
@@ -138,11 +138,13 @@ fn inner_kind() -> BTreeMap<Field, Kind> {
         ),
         (
             Field::from("log_events"),
-            Kind::object(BTreeMap::from([
-                (Field::from("id"), Kind::bytes()),
-                (Field::from("timestamp"), Kind::timestamp()),
-                (Field::from("message"), Kind::bytes()),
-            ])),
+            Kind::array(Collection::from_unknown(
+                Kind::object(BTreeMap::from([
+                    (Field::from("id"), Kind::bytes()),
+                    (Field::from("timestamp"), Kind::timestamp()),
+                    (Field::from("message"), Kind::bytes()),
+                ])),
+            ))
         ),
     ])
 }
