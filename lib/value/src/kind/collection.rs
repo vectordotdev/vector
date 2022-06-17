ef9225f73bf4de48ade56d9c6456017c25778c51
Nathan Fox <fuchsnj@gmail.com>
2022-06-17 10:47:53 -0400

    fix(vrl): fix `Kind::reduced_kind` when `known` is empty (#13208)

--- lib/value/src/kind/collection.rs
+++ lib/value/src/kind/collection.rs
@@ -256,29 +256,28 @@ impl<T: Ord> Collection<T> {
     }
 
     /// Return the reduced `Kind` of the items within the collection.
+    /// This only returns the type of _existing_ values in the collection. Accessing
+    /// a non-existing value can return null which is not added to the type here.
     pub fn reduced_kind(&self) -> Kind {
         let strategy = merge::Strategy {
             depth: merge::Depth::Deep,
             indices: merge::Indices::Keep,
         };
 
-        self.known
+        let mut kind = self
+            .known
             .values()
             .cloned()
             .reduce(|mut lhs, rhs| {
                 lhs.merge(rhs, strategy);
                 lhs
             })
-            .map_or_else(Kind::any, |kind| {
-                self.unknown
-                    .as_ref()
-                    .map(|unknown| {
-                        let mut kind = kind.clone();
-                        kind.merge(unknown.to_kind().into_owned(), strategy);
-                        kind
-                    })
-                    .unwrap_or(kind)
-            })
+            .unwrap_or_else(Kind::never);
+
+        if let Some(unknown) = &self.unknown {
+            kind.merge(unknown.to_kind().into_owned(), strategy);
+        }
+        kind
     }
 }
 
