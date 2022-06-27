339c3af55fa6120845564b02684cf49a95526a5b
Toby Lawrence <tobz@users.noreply.github.com>
2022-06-27 13:29:50 -0400

    Don't group by the partial field.

--- src/sources/kubernetes_logs/partial_events_merger.rs
+++ src/sources/kubernetes_logs/partial_events_merger.rs
@@ -22,8 +22,8 @@ pub fn build(enabled: bool) -> PartialEventsMerger {
             MergeStrategy::Concat,
         );
 
-        // Group events by their file and the partial indicator field.
-        let group_by = vec![(&*FILE_KEY).to_string(), event::PARTIAL.to_string()];
+        // Group events by their file.
+        let group_by = vec![(&*FILE_KEY).to_string()];
 
         // As soon as we see an event that has no "partial" field, that's when we've hit the end of the split-up message
         // we've been incrementally aggregating.. or the message was never split up to begin with because it was already
