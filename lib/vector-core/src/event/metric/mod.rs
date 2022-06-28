7a491258c81fe9845b2d67138d9489f2f8a78557
Toby Lawrence <tobz@users.noreply.github.com>
2022-06-28 15:08:02 -0400

    Update lib/vector-core/src/event/metric/mod.rs
    Co-authored-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>

--- lib/vector-core/src/event/metric/mod.rs
+++ lib/vector-core/src/event/metric/mod.rs
@@ -421,7 +421,7 @@ impl Finalizable for Metric {
 /// Metrics can be either absolute of incremental. Absolute metrics represent a sort of "last write wins" scenario,
 /// where the latest absolute value seen is meant to be the actual metric value.  In constrast, and perhaps intuitively,
 /// incremental metrics are meant to be additive, such that we don't know what total value of the metric is, but we know
-/// that we'll be addingb or subtracting the given value from it.
+/// that we'll be adding or subtracting the given value from it.
 ///
 /// Generally speaking, most metrics storage systems deal with incremental updates. A notable exception is Prometheus,
 /// which deals with, and expects, absolute values from clients.
