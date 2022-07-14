fad8796e5601766940d33f17b66359a40ca6b3e9
Kyle Criddle <kyle.criddle@datadoghq.com>
2022-07-14 18:09:27 +0000

    add --locked

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -10,7 +10,7 @@ if [[ "$(cross --version | grep cross)" != "cross 0.2.1" ]] ; then
   rustup run stable cargo install cross --version 0.2.1 --force
 fi
 if [[ "$(cargo-nextest --version)" != "cargo-nextest 0.9.25" ]] ; then
-  rustup run stable cargo install cargo-nextest --version 0.9.25 --force
+  rustup run stable cargo install cargo-nextest --version 0.9.25 --force --locked
 fi
 
 cd scripts
