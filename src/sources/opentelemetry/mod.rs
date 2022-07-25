4d739126c370d78bbe56f92b51334e628642e8aa
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2022-07-25 17:34:47 -0400

    add stubs for http otel source
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>

--- src/sources/opentelemetry/mod.rs
+++ src/sources/opentelemetry/mod.rs
@@ -3,9 +3,12 @@ mod tests;
 
 mod log;
 
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
-use futures::TryFutureExt;
+use bytes::Bytes;
+use futures::{future::join, FutureExt, TryFutureExt};
+
+use http::HeaderMap;
 use vector_config::configurable_component;
 use vector_core::config::LogNamespace;
 
@@ -14,9 +17,14 @@ use crate::{
         AcknowledgementsConfig, DataType, GenerateConfig, Output, Resource, SourceConfig,
         SourceContext, SourceDescription,
     },
+    event::Event,
     opentelemetry::LogService::logs_service_server::LogsServiceServer,
     serde::bool_or_struct,
-    sources::{util::grpc::run_grpc_server, Source},
+    sources::{
+        http::HttpMethod,
+        util::{grpc::run_grpc_server, ErrorMessage, HttpSource},
+        Source,
+    },
     tls::{MaybeTlsSettings, TlsEnableableConfig},
 };
 
@@ -29,6 +37,22 @@ pub const LOGS: &str = "logs";
 #[derive(Clone, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct OpentelemetryConfig {
+    #[configurable(derived)]
+    grpc: GrpcConfig,
+
+    #[configurable(derived)]
+    http: HttpConfig,
+
+    #[configurable(derived)]
+    #[serde(default, deserialize_with = "bool_or_struct")]
+    acknowledgements: AcknowledgementsConfig,
+}
+
+/// Configuration for the `opentelemetry` gRPC server.
+#[configurable_component]
+#[derive(Clone, Debug)]
+#[serde(deny_unknown_fields)]
+struct GrpcConfig {
     /// The address to listen for connections on.
     ///
     /// It _must_ include a port.
@@ -37,17 +61,34 @@ pub struct OpentelemetryConfig {
     #[configurable(derived)]
     #[serde(default)]
     tls: Option<TlsEnableableConfig>,
+}
+
+/// Configuration for the `opentelemetry` HTTP server.
+#[configurable_component]
+#[derive(Clone, Debug)]
+#[serde(deny_unknown_fields)]
+struct HttpConfig {
+    /// The address to listen for connections on.
+    ///
+    /// It _must_ include a port.
+    address: SocketAddr,
 
     #[configurable(derived)]
-    #[serde(default, deserialize_with = "bool_or_struct")]
-    acknowledgements: AcknowledgementsConfig,
+    #[serde(default)]
+    tls: Option<TlsEnableableConfig>,
 }
 
 impl GenerateConfig for OpentelemetryConfig {
     fn generate_config() -> toml::Value {
         toml::Value::try_from(Self {
-            address: "0.0.0.0:4317".parse().unwrap(),
-            tls: Default::default(),
+            grpc: GrpcConfig {
+                address: "0.0.0.0:4317".parse().unwrap(),
+                tls: Default::default(),
+            },
+            http: HttpConfig {
+                address: "0.0.0.0:4318".parse().unwrap(),
+                tls: Default::default(),
+            },
             acknowledgements: Default::default(),
         })
         .unwrap()
@@ -62,19 +103,37 @@ inventory::submit! {
 #[typetag::serde(name = "opentelemetry")]
 impl SourceConfig for OpentelemetryConfig {
     async fn build(&self, cx: SourceContext) -> crate::Result<Source> {
-        let tls_settings = MaybeTlsSettings::from_config(&self.tls, true)?;
         let acknowledgements = cx.do_acknowledgements(&self.acknowledgements);
-        let service = LogsServiceServer::new(Service {
-            pipeline: cx.out,
+
+        let grpc_tls_settings = MaybeTlsSettings::from_config(&self.grpc.tls, true)?;
+        let grpc_service = LogsServiceServer::new(Service {
+            pipeline: cx.out.clone(),
             acknowledgements,
         })
         .accept_gzip();
-        let source =
-            run_grpc_server(self.address, tls_settings, service, cx.shutdown).map_err(|error| {
-                error!(message = "Source future failed.", %error);
-            });
+        let grpc_source = run_grpc_server(
+            self.grpc.address,
+            grpc_tls_settings,
+            grpc_service,
+            cx.shutdown.clone(),
+        )
+        .map_err(|error| {
+            error!(message = "Source future failed.", %error);
+        });
 
-        Ok(Box::pin(source))
+        let http_source = HttpSource::run(
+            OpentelemetryHttpServer {},
+            self.http.address,
+            "/v1/logs",
+            HttpMethod::Post,
+            true,
+            &self.http.tls,
+            &None,
+            cx,
+            self.acknowledgements,
+        )?;
+
+        Ok(join(grpc_source, http_source).map(|_| Ok(())).boxed())
     }
 
     fn outputs(&self, _global_log_namespace: LogNamespace) -> Vec<Output> {
@@ -86,10 +145,35 @@ impl SourceConfig for OpentelemetryConfig {
     }
 
     fn resources(&self) -> Vec<Resource> {
-        vec![Resource::tcp(self.address)]
+        vec![
+            Resource::tcp(self.grpc.address),
+            Resource::tcp(self.http.address),
+        ]
     }
 
     fn can_acknowledge(&self) -> bool {
         true
+    }
+}
+
+#[derive(Clone)]
+struct OpentelemetryHttpServer;
+
+impl OpentelemetryHttpServer {
+    fn decode_body(&self, _body: Bytes) -> Result<Vec<Event>, ErrorMessage> {
+        todo!()
+    }
+}
+
+impl HttpSource for OpentelemetryHttpServer {
+    fn build_events(
+        &self,
+        body: Bytes,
+        _header_map: HeaderMap,
+        _query_parameters: HashMap<String, String>,
+        _path: &str,
+    ) -> Result<Vec<Event>, ErrorMessage> {
+        let events = self.decode_body(body)?;
+        Ok(events)
     }
 }
