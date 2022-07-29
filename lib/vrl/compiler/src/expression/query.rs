4c64c11a8a0b53d84e0b8da3747dac5e80ae650f
Pablo Sichert <mail@pablosichert.com>
2022-07-29 15:31:08 +0200

    Implement `<Query as Expression>::emit_llvm`
    Signed-off-by: Pablo Sichert <mail@pablosichert.com>

--- lib/vrl/compiler/src/expression/query.rs
+++ lib/vrl/compiler/src/expression/query.rs
@@ -134,10 +134,91 @@ impl Expression for Query {
     #[cfg(feature = "llvm")]
     fn emit_llvm<'ctx>(
         &self,
-        _: (&mut LocalEnv, &mut ExternalEnv),
-        _: &mut crate::llvm::Context<'ctx>,
+        state: (&mut LocalEnv, &mut ExternalEnv),
+        ctx: &mut crate::llvm::Context<'ctx>,
     ) -> Result<(), String> {
-        todo!()
+        let query_begin_block = ctx.append_basic_block("query_begin");
+        let query_end_block = ctx.append_basic_block("query_end");
+
+        ctx.build_unconditional_branch(query_begin_block);
+        ctx.position_at_end(query_begin_block);
+
+        let result_ref = ctx.result_ref();
+        let path_name = format!("{}", self.path);
+        let path_ref = ctx
+            .into_const(self.path.clone(), &path_name)
+            .as_pointer_value();
+
+        match &self.target {
+            Target::External => {
+                ctx.fns().vrl_expression_query_target_external.build_call(
+                    ctx.builder(),
+                    ctx.context_ref(),
+                    ctx.cast_lookup_buf_ref_type(path_ref),
+                    result_ref,
+                );
+
+                ctx.build_unconditional_branch(query_end_block);
+                ctx.position_at_end(query_end_block);
+
+                return Ok(());
+            }
+            Target::Internal(variable) => ctx.emit_llvm(
+                variable,
+                ctx.result_ref(),
+                (state.0, state.1),
+                query_end_block,
+                vec![],
+            )?,
+            Target::FunctionCall(call) => ctx.emit_llvm(
+                call,
+                ctx.result_ref(),
+                (state.0, state.1),
+                query_end_block,
+                vec![],
+            )?,
+            Target::Container(container) => ctx.emit_llvm(
+                container,
+                ctx.result_ref(),
+                (state.0, state.1),
+                query_end_block,
+                vec![],
+            )?,
+        };
+
+        let target_fallible = match &self.target {
+            Target::Internal(_) | Target::External => false,
+            Target::FunctionCall(call) => call.type_def((state.0, state.1)).is_fallible(),
+            Target::Container(container) => container.type_def((state.0, state.1)).is_fallible(),
+        };
+
+        if target_fallible {
+            let query_target_ok_block = ctx.append_basic_block("query_target_ok");
+
+            let is_ok = ctx
+                .fns()
+                .vrl_resolved_is_ok
+                .build_call(ctx.builder(), result_ref)
+                .try_as_basic_value()
+                .left()
+                .expect("result is not a basic value")
+                .try_into()
+                .expect("result is not an int value");
+
+            ctx.build_conditional_branch(is_ok, query_target_ok_block, query_end_block);
+            ctx.position_at_end(query_target_ok_block);
+        }
+
+        ctx.fns().vrl_expression_query_target.build_call(
+            ctx.builder(),
+            ctx.cast_lookup_buf_ref_type(path_ref),
+            result_ref,
+        );
+        ctx.build_unconditional_branch(query_end_block);
+
+        ctx.position_at_end(query_end_block);
+
+        Ok(())
     }
 }
 
