ecba1c43079a8e939d1768f83f11f34d5ca1f1b4
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-08-05 15:13:13 -0600

    Set up registered internal event structures

--- lib/vector-common/src/internal_event/mod.rs
+++ lib/vector-common/src/internal_event/mod.rs
@@ -7,12 +7,29 @@ pub use events_received::{EventsReceived, OldEventsReceived};
 pub use events_sent::{EventsSent, DEFAULT_OUTPUT};
 
 pub trait InternalEvent: Sized {
-    fn emit(self) {}
+    fn emit(self);
 
     // Optional for backwards compat until all events implement this
     fn name(&self) -> Option<&'static str> {
         None
     }
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub trait RegisterInternalEvent: Sized {
+    type Handle: InternalEventHandle;
+
+    fn register(self) -> Self::Handle;
+
+    fn name(&self) -> Option<&'static str> {
+        None
+    }
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub trait InternalEventHandle: Sized {
+    type Data: Sized;
+    fn emit(&self, data: Self::Data);
 }
 
 // Sets the name of an event if it doesn't have one
@@ -21,10 +38,7 @@ pub struct DefaultName<E> {
     pub event: E,
 }
 
-impl<E> InternalEvent for DefaultName<E>
-where
-    E: InternalEvent,
-{
+impl<E: InternalEvent> InternalEvent for DefaultName<E> {
     fn emit(self) {
         self.event.emit();
     }
@@ -34,16 +48,50 @@ where
     }
 }
 
+impl<E: RegisterInternalEvent> RegisterInternalEvent for DefaultName<E> {
+    type Handle = E::Handle;
+
+    fn register(self) -> Self::Handle {
+        self.event.register()
+    }
+
+    fn name(&self) -> Option<&'static str> {
+        Some(self.event.name().unwrap_or(self.name))
+    }
+}
+
 #[cfg(any(test, feature = "test"))]
 pub fn emit(event: impl InternalEvent) {
-    let name = event.name();
-    event.emit();
-    if let Some(name) = name {
+    if let Some(name) = event.name() {
         super::event_test_util::record_internal_event(name);
     }
+    event.emit();
 }
 
 #[cfg(not(any(test, feature = "test")))]
 pub fn emit(event: impl InternalEvent) {
     event.emit();
 }
+
+#[cfg(any(test, feature = "test"))]
+pub fn register<E: RegisterInternalEvent>(event: E) -> E::Handle {
+    if let Some(name) = event.name() {
+        super::event_test_util::record_internal_event(name);
+    }
+    event.register()
+}
+
+#[cfg(not(any(test, feature = "test")))]
+pub fn register<E: RegisterInternalEvent>(event: E) -> E::Handle {
+    event.register()
+}
+
+pub fn emit_registered<E, H, D>(event: E, data: D)
+where
+    E: RegisterInternalEvent<Handle = H>,
+    H: InternalEventHandle<Data = D>,
+{
+    event.register().emit(data);
+}
+
+pub type Registered<T> = <T as RegisterInternalEvent>::Handle;
