dc8098d46052ddcabdbd61f6c1e289d11fd89a4c
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2022-08-08 11:50:42 -0400

    remove gosu, document docker-entrypoint

--- distribution/docker/docker-entrypoint.sh
+++ distribution/docker/docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 set -o errexit
 
-# Prevent core dumps
+# Prevent core dumps.
 ulimit -c 0
 
+# If the user is trying to run Vector with options, just pass them along.
 case $1 in -*) set -- vector "$@"; esac
 
+# Look for Vector subcommands.
 if [ "$1" = 'help' ]; then
-    # This needs a special case because there's no help output.
+    # This needs a special case because there's no `help --help` output.
     set -- vector "$@"
+# We grep Vector's help output to check for the provided subcommand.
 elif vector "$1" --help 2>&1 | grep -q "vector $1"; then
   set -- vector "$@"
 fi
@@ -23,10 +26,6 @@ if [ "$1" = 'vector' ]; then
     if [ "$(stat -c %u /var/lib/vector)" != "$(id -u vector)" ]; then
       chown -R vector:vector /var/lib/vector || echo "Could not chown /var/lib/vector (may not have appropriate permissions)"
     fi
-  fi
-
-  if [ "$(id -u)" = '0' ]; then
-    set -- gosu vector "$@"
   fi
 fi
 
