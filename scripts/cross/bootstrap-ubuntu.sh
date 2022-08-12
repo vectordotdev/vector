edbc46eb064690aff1827e65230af2f09ba82353
Jesse Szwedko <jesse@szwedko.me>
2022-08-12 13:43:27 -0700

    Install protoc for cross builds too
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -20,3 +20,13 @@ apt-get update
 apt-get install -y \
       libclang1-9 \
       llvm-9
+
+# Protoc. No guard because we want to override Ubuntu's old version in
+# case it is already installed by a dependency.
+PROTOC_VERSION=3.19.4
+PROTOC_ZIP=protoc-${PROTOC_VERSION}-linux-x86_64.zip
+curl -fsSL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP \
+     --output "$TEMP/$PROTOC_ZIP"
+unzip "$TEMP/$PROTOC_ZIP" bin/protoc -d "$TEMP"
+chmod +x "$TEMP"/bin/protoc
+mv --force --verbose "$TEMP"/bin/protoc /usr/bin/protoc
