4e2bd6f81809a6ac8d8d6179899a09472cf60465
Nathan Fox <fuchsnj@gmail.com>
2022-08-22 10:38:30 -0400

    cleanup

--- lib/vrl/parser/src/lib.rs
+++ lib/vrl/parser/src/lib.rs
@@ -58,26 +58,6 @@ pub fn parse(input: impl AsRef<str>) -> Result<Program, Error> {
         })
 }
 
-// pub fn parse_path(input: impl AsRef<str>) -> Result<LookupBuf, Error> {
-//     let lexer = lex::Lexer::new(input.as_ref());
-//
-//     parser::QueryParser::new()
-//         .parse(input.as_ref(), lexer)
-//         .map_err(|source| Error::ParseError {
-//             span: Span::new(0, input.as_ref().len()),
-//             source: source
-//                 .map_token(|t| t.map(ToOwned::to_owned))
-//                 .map_error(|err| err.to_string()),
-//             dropped_tokens: vec![],
-//         })
-//         .and_then(|query| match query.target.into_inner() {
-//             ast::QueryTarget::External => Ok(query.path.into_inner()),
-//             _ => Err(Error::UnexpectedParseError(
-//                 "unexpected query target".to_owned(),
-//             )),
-//         })
-// }
-
 pub fn parse_literal(input: impl AsRef<str>) -> Result<Literal, Error> {
     let lexer = lex::Lexer::new(input.as_ref());
 
