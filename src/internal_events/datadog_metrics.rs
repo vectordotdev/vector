138b1abbc17c16382d00f130e1f2d325da4f1135
kyle criddle <kyle.criddle@datadoghq.com>
2022-08-23 15:09:03 -0600

    add+use ComponentEventsDropped

--- src/internal_events/datadog_metrics.rs
+++ src/internal_events/datadog_metrics.rs
@@ -1,3 +1,4 @@
+use crate::{emit, internal_events::ComponentEventsDropped};
 use metrics::counter;
 use vector_core::internal_event::InternalEvent;
 
@@ -5,16 +6,17 @@ use super::prelude::{error_stage, error_type};
 
 #[derive(Debug)]
 pub struct DatadogMetricsEncodingError {
-    pub message: &'static str,
+    pub error_message: &'static str,
     pub error_code: &'static str,
     pub dropped_events: u64,
 }
 
 impl InternalEvent for DatadogMetricsEncodingError {
     fn emit(self) {
+        let reason = "Failed to encode Datadog metrics.";
         error!(
-            message = "Failed to encode Datadog metrics.",
-            reason = %self.message,
+            message = reason,
+            error = %self.error_message,
             error_code = %self.error_code,
             error_type = error_type::ENCODER_FAILED,
             intentional = "false",
@@ -28,13 +30,11 @@ impl InternalEvent for DatadogMetricsEncodingError {
         );
 
         if self.dropped_events > 0 {
-            counter!(
-                "component_discarded_events_total", self.dropped_events,
-                "error_code" => self.error_code,
-                "error_type" => error_type::ENCODER_FAILED,
-                "intentional" => "false",
-                "stage" => error_stage::PROCESSING,
-            );
+            emit!(ComponentEventsDropped {
+                count: self.dropped_events,
+                intentional: false,
+                reason,
+            });
         }
     }
 }
