2bb823fd0af2edd8878327aabf0dc9a2ebc0a8c7
Nathan Fox <fuchsnj@gmail.com>
2022-08-31 13:58:56 -0400

    cleanup

--- lib/vrl/stdlib/src/del.rs
+++ lib/vrl/stdlib/src/del.rs
@@ -148,16 +148,6 @@ impl Expression for DelFn {
 
         self.query.delete_type_def(&mut state.external);
 
-        // if self.query.is_external() {
-        //     if let Err(
-        //         value::kind::remove::Error::RootPath
-        //         | value::kind::remove::Error::CoalescedPath
-        //         | value::kind::remove::Error::NegativeIndexPath,
-        //     ) = self.query.delete_type_def(&mut state.external)
-        //     {
-        //
-        //     }
-        // }
         TypeInfo::new(state, return_type)
     }
 }
