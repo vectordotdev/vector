4d74160d371de16f3fa96823b0a65f29ac7df4d0
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-09-22 10:21:51 -0600

    Install protoc in the integration and soak docker file

--- scripts/environment/bootstrap-ubuntu-20.04.sh
+++ scripts/environment/bootstrap-ubuntu-20.04.sh
@@ -110,15 +110,7 @@ if ! [ -x "$(command -v docker)" ]; then
     usermod --append --groups docker ubuntu || true
 fi
 
-# Protoc. No guard because we want to override Ubuntu's old version in
-# case it is already installed by a dependency.
-PROTOC_VERSION=3.19.5 # also update soaks/Dockerfile
-PROTOC_ZIP=protoc-${PROTOC_VERSION}-linux-x86_64.zip
-curl -fsSL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP \
-     --output "$TEMP/$PROTOC_ZIP"
-unzip "$TEMP/$PROTOC_ZIP" bin/protoc -d "$TEMP"
-chmod +x "$TEMP"/bin/protoc
-mv --force --verbose "$TEMP"/bin/protoc /usr/bin/protoc
+bash scripts/environment/install-protoc.sh
 
 # Apt cleanup
 apt clean
