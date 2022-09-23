93ead0036a8ba646ffbf684aa8d1ece0ce3a1726
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-09-23 15:02:01 -0600

    Use install-protoc.sh in cross image build scripts

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -23,15 +23,3 @@ apt-get install -y \
       libclang1-9 \
       llvm-9 \
       unzip
-
-# Protoc. No guard because we want to override Ubuntu's old version in
-# case it is already installed by a dependency.
-TEMP=$(mktemp -d)
-PROTOC_VERSION=3.19.5
-PROTOC_ZIP=protoc-${PROTOC_VERSION}-linux-x86_64.zip
-curl -fsSL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP \
-     --output "$TEMP/$PROTOC_ZIP"
-unzip "$TEMP/$PROTOC_ZIP" bin/protoc -d "$TEMP"
-chmod +x "$TEMP"/bin/protoc
-mv --force --verbose "$TEMP"/bin/protoc /usr/bin/protoc
-rm -fr "$TEMP"
