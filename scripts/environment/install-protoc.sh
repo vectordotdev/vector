e6cfd640462c1dc16b82ab67ac570c1eb6140297
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2022-09-26 14:31:10 -0400

    +shellcheck
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>

--- scripts/environment/install-protoc.sh
+++ scripts/environment/install-protoc.sh
@@ -7,11 +7,13 @@ set -o errexit -o verbose
 # Basis of script copied from:
 # https://github.com/paxosglobal/asdf-protoc/blob/46c2f9349b8420144b197cfd064a9677d21cfb0c/bin/install
 
-readonly TMP_DIR="$(mktemp -d -t "protoc_XXXX")"
+readonly TMP_DIR
+TMP_DIR="$(mktemp -d -t "protoc_XXXX")"
 trap 'rm -rf "${TMP_DIR?}"' EXIT
 
 get_platform() {
-  local os=$(uname)
+  local os
+  os=$(uname)
   if [[ "${os}" == "Darwin" ]]; then
     echo "osx"
   elif [[ "${os}" == "Linux" ]]; then
@@ -22,8 +24,10 @@ get_platform() {
 }
 
 get_arch() {
-  local os=$(uname)
-  local arch=$(uname -m)
+  local os
+  local arch
+  os=$(uname)
+  arch=$(uname -m)
   # On ARM Macs, uname -m returns "arm64", but in protoc releases this architecture is called "aarch_64"
   if [[ "${os}" == "Darwin" && "${arch}" == "arm64" ]]; then
     echo "aarch_64"
@@ -39,7 +43,8 @@ install_protoc() {
   local install_path=$2
 
   local base_url="https://github.com/protocolbuffers/protobuf/releases/download"
-  local url="${base_url}/v${version}/protoc-${version}-$(get_platform)-$(get_arch).zip"
+  local url
+  url="${base_url}/v${version}/protoc-${version}-$(get_platform)-$(get_arch).zip"
   local download_path="${TMP_DIR}/protoc.zip"
 
   echo "Downloading ${url}"
