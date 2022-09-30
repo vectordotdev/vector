e1f29a5bfc9c30a1c622d7572466e9b47c6d70a0
Nathan Fox <fuchsnj@gmail.com>
2022-09-30 16:21:47 -0400

    save

--- lib/value/src/kind/crud/remove.rs
+++ lib/value/src/kind/crud/remove.rs
@@ -1,5 +1,5 @@
 use crate::kind::collection::EmptyState;
-use crate::kind::{Collection, Field};
+use crate::kind::{Collection, Field, Index};
 use crate::Kind;
 use lookup::lookup_v2::OwnedSegment;
 use lookup::OwnedValuePath;
@@ -36,19 +36,21 @@ impl Kind {
         let first = segments.first().expect("must not be empty");
         let second = segments.get(1);
 
-        // match (first, second) {
-        //     (OwnedSegment::Field())
-        // }
-
         if let Some(second) = second {
-            // unimplemented!("more segments");
+            // more than 1 segment left
+
             match first {
                 OwnedSegment::Field(field) => {
                     if let Some(object) = self.as_object_mut() {
                         match object.known_mut().get_mut(&Field::from(field.to_owned())) {
-                            None => {
-                                unimplemented!()
+                            None => CompactOptions {
+                                should_compact: object
+                                    .unknown_kind()
+                                    .at_path(&segments[1..])
+                                    .contains_any_defined(),
+                                should_not_compact: true,
                             }
+                            .apply_global_compact_option(compact),
                             Some(child) => {
                                 let compact_options = child.remove_inner(&segments[1..], compact);
 
@@ -74,22 +76,75 @@ impl Kind {
                                 )
                             }
                         }
-                        // let removed_known = object
-                        //     .known_mut()
-                        //     .remove(&field.as_str().into())
-                        //     .map_or(false, |kind| kind.contains_any_defined());
-                        //
-                        // let maybe_removed_unknown = object.unknown_kind().contains_any_defined();
-                        //
-                        // CompactOptions::from(object.is_empty())
-                        //     .disable_should_compact(!removed_known && !maybe_removed_unknown)
                     } else {
-                        unimplemented!()
+                        // guaranteed to not delete anything
+                        CompactOptions {
+                            should_compact: false,
+                            should_not_compact: true,
+                        }
                     }
                 }
-                OwnedSegment::Index(_) => unimplemented!(),
+                OwnedSegment::Index(index) => {
+                    if let Some(array) = self.as_array_mut() {
+                        let index = *index;
+                        if index < 0 {
+                            unimplemented!()
+                        } else {
+                            unimplemented!()
+                        }
+                        // match array.known_mut().get_mut(&Index::from(*index)) {
+                        //     None => {
+                        //         unimplemented!()
+                        //         // CompactOptions {
+                        //         //     should_compact: object
+                        //         //         .unknown_kind()
+                        //         //         .at_path(&segments[1..])
+                        //         //         .contains_any_defined(),
+                        //         //     should_not_compact: true,
+                        //         // }.apply_global_compact_option(compact)
+                        //     }
+                        //     Some(child) => {
+                        //         unimplemented!()
+                        //         // let compact_options = child.remove_inner(&segments[1..], compact);
+                        //         //
+                        //         // if compact_options.should_compact
+                        //         //     && !compact_options.should_not_compact
+                        //         // {
+                        //         //     // always compact
+                        //         //     object.known_mut().remove(&Field::from(field.to_owned()));
+                        //         // } else if compact_options.should_compact
+                        //         //     && compact_options.should_not_compact
+                        //         // {
+                        //         //     // maybe compact
+                        //         //     let not_compacted = object.clone();
+                        //         //     object.known_mut().remove(&Field::from(field.to_owned()));
+                        //         //     object.merge(not_compacted, false);
+                        //         // } else {
+                        //         //     // never compact: do nothing, already correct.
+                        //         // }
+                        //         //
+                        //         // // The compaction is propagated only if the current collection is also empty
+                        //         // compact_options.disable_should_compact(
+                        //         //     !CompactOptions::from(object.is_empty()).should_compact,
+                        //         // )
+                        //     }
+                        // }
+                    } else {
+                        // guaranteed to not delete anything
+                        CompactOptions {
+                            should_compact: false,
+                            should_not_compact: true,
+                        }
+                    }
+                }
                 OwnedSegment::Coalesce(_) => unimplemented!(),
-                OwnedSegment::Invalid => unimplemented!(),
+                OwnedSegment::Invalid => {
+                    // guaranteed to not delete anything
+                    CompactOptions {
+                        should_compact: false,
+                        should_not_compact: true,
+                    }
+                }
             }
         } else {
             match first {
@@ -109,29 +164,17 @@ impl Kind {
                         unimplemented!()
                     }
                 }
-                OwnedSegment::Index(_) => unimplemented!(),
+                OwnedSegment::Index(index) => {
+                    if let Some(array) = self.as_array_mut() {
+                        unimplemented!("index={:?}", index)
+                    } else {
+                        unimplemented!()
+                    }
+                }
                 OwnedSegment::Coalesce(_) => unimplemented!(),
                 OwnedSegment::Invalid => unimplemented!(),
             }
         }
-
-        // match (first, second) {
-        //     (_, None) => {
-        //
-        //     }
-        //     _ => unimplemented!()
-        // }
-
-        // match segments.first().expect("must not be empty") {
-        //     OwnedSegment::Field(_) => unimplemented!(),
-        //     OwnedSegment::Index(_) => unimplemented!(),
-        //     OwnedSegment::Coalesce(_) => unimplemented!(),
-        //     OwnedSegment::Invalid => {
-        //         // Same behavior as `Value::remove`, do nothing. Eventually
-        //         // this variant should be removed from `OwnedValuePath`.
-        //         return;
-        //     }
-        // }
     }
 }
 
@@ -315,40 +358,99 @@ mod test {
                     ),
                 },
             ),
-            // (
-            //     "remove nested object field: maybe compact",
-            //     TestCase {
-            //         kind: Kind::object(Collection::from(BTreeMap::from([(
-            //             "a".into(),
-            //             Kind::object(Collection::empty().with_unknown(Kind::float())),
-            //         )]))),
-            //         path: owned_value_path!("a", "b"),
-            //         compact: true,
-            //         want: Kind::object(Collection::empty()),
-            //     },
-            // ),
-            // (
-            //     "remove nested object field: compact",
-            //     TestCase {
-            //         kind: Kind::object(Collection::from(BTreeMap::from([(
-            //             "a".into(),
-            //             Kind::object(Collection::from(BTreeMap::from([(
-            //                 "b".into(),
-            //                 Kind::integer(),
-            //             )]))),
-            //         )]))),
-            //         path: owned_value_path!("a", "b"),
-            //         compact: true,
-            //         want: Kind::object(Collection::empty()),
-            //     },
-            // ),
+            (
+                "remove nested object field: maybe compact",
+                TestCase {
+                    kind: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::float())),
+                    )]))),
+                    path: owned_value_path!("a", "b"),
+                    compact: true,
+                    want: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::float()))
+                            .or_undefined(),
+                    )]))),
+                },
+            ),
+            (
+                "remove unknown object field 2",
+                TestCase {
+                    kind: Kind::object(Collection::empty().with_unknown(Kind::integer())),
+                    path: owned_value_path!("a", "b"),
+                    compact: false,
+                    want: Kind::object(Collection::empty().with_unknown(Kind::integer())),
+                },
+            ),
+            (
+                "remove deep nested unknown object field",
+                TestCase {
+                    kind: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::any_object())),
+                    )]))),
+                    path: owned_value_path!("a", "b", "c"),
+                    compact: false,
+                    want: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::any_object())),
+                    )]))),
+                },
+            ),
+            (
+                "remove deep nested unknown object field: compact",
+                TestCase {
+                    kind: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::any_object())),
+                    )]))),
+                    path: owned_value_path!("a", "b", "c"),
+                    compact: true,
+                    want: Kind::object(Collection::from(BTreeMap::from([(
+                        "a".into(),
+                        Kind::object(Collection::empty().with_unknown(Kind::any_object()))
+                            .or_undefined(),
+                    )]))),
+                },
+            ),
+            (
+                "remove field from non object",
+                TestCase {
+                    kind: Kind::integer(),
+                    path: owned_value_path!("a", "b", "c"),
+                    compact: true,
+                    want: Kind::integer(),
+                },
+            ),
+            (
+                "remove index from non array",
+                TestCase {
+                    kind: Kind::integer(),
+                    path: owned_value_path!(1, 2, 3),
+                    compact: true,
+                    want: Kind::integer(),
+                },
+            ),
+            (
+                "remove known index",
+                TestCase {
+                    kind: Kind::array(Collection::from(BTreeMap::from([(
+                        0.into(),
+                        Kind::integer(),
+                    )]))),
+                    path: owned_value_path!(0),
+                    compact: true,
+                    want: Kind::array(Collection::empty()),
+                },
+            ),
         ] {
             println!("Test: {:?}", title);
             let mut actual = kind;
             actual.remove(&path, compact);
             if actual != want {
                 panic!(
-                    "Test failed: {:?}.\nExpected = {:?}\nActual = {:?}",
+                    "Test failed: {:?}.\nExpected = {:?}\nActual =   {:?}",
                     title,
                     want.debug_info(),
                     actual.debug_info()
