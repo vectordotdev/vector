5be4295b7282db8652960106f653e18cd79eaeb2
kyle criddle <kyle.criddle@datadoghq.com>
2022-10-20 08:20:20 -0600

    Revert "merging event files"
    This reverts commit 6f06e53d29e6ac30b1d8e1dfb93822b9d2b47055.

--- src/internal_events/aws_kinesis_firehose.rs
+++ src/internal_events/aws_kinesis_firehose.rs
@@ -89,30 +89,3 @@ impl InternalEvent for AwsKinesisFirehoseAutomaticRecordDecodeError {
         counter!("request_automatic_decode_errors_total", 1);
     }
 }
-
-#[derive(Debug)]
-pub struct AwsKinesisStreamNoPartitionKeyError<'a> {
-    pub partition_key_field: &'a str,
-}
-
-impl InternalEvent for AwsKinesisStreamNoPartitionKeyError<'_> {
-    fn emit(self) {
-        let reason = "Partition key does not exist.";
-
-        error!(
-            message = reason,
-            partition_key_field = %self.partition_key_field,
-            error_type = error_type::PARSER_FAILED,
-            stage = error_stage::PROCESSING,
-            internal_log_rate_limit = true,
-        );
-
-        counter!(
-            "component_errors_total", 1,
-            "error_type" => error_type::PARSER_FAILED,
-            "stage" => error_stage::PROCESSING,
-        );
-
-        emit!(ComponentEventsDropped::<UNINTENTIONAL> { count: 1, reason });
-    }
-}
