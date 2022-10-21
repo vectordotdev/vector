0ca07a3350a9d73218511b2131d594b3fb6edff6
Toby Lawrence <tobz@users.noreply.github.com>
2022-10-21 11:52:23 -0400

    chore(filtering, transforms): remove `check_fields` condition and `lanes` alias in `route` transform (#14903)

--- src/transforms/filter.rs
+++ src/transforms/filter.rs
@@ -27,11 +27,7 @@ impl From<AnyCondition> for FilterConfig {
 
 impl GenerateConfig for FilterConfig {
     fn generate_config() -> toml::Value {
-        toml::from_str(
-            r#"condition.type = "check_fields"
-            condition."message.eq" = "value""#,
-        )
-        .unwrap()
+        toml::from_str(r#"condition = ".message = \"value\"""#).unwrap()
     }
 }
 
