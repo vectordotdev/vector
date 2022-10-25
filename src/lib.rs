2ad8ec2d595e70f1e94f5bb8d299b15131e32c9f
Arshia Soleimani <arshiyasoleimani@gmail.com>
2022-10-25 13:59:39 -0400

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -29,8 +29,11 @@ extern crate derivative;
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[cfg(all(feature = "tikv-jemallocator", feature = "allocation-tracing"))]
-#[cfg(not(target_os = "macos"))]
+#[cfg(all(
+    feature = "tikv-jemallocator",
+    feature = "allocation-tracing",
+    not(target_os = "macos")
+))]
 #[global_allocator]
 static ALLOC: self::internal_telemetry::allocations::Allocator<tikv_jemallocator::Jemalloc> =
     self::internal_telemetry::allocations::get_grouped_tracing_allocator(
