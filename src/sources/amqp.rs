60d039bc143e44dc7f056e00dbed656fe007811c
Jesse Szwedko <jesse@szwedko.me>
2022-10-31 16:24:31 -0700

    fix(observability): Fix a few more usages of internal_log_rate_secs (#15041)
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- src/sources/amqp.rs
+++ src/sources/amqp.rs
@@ -359,7 +359,7 @@ async fn run_amqp_source(
         )
         .await
         .map_err(|error| {
-            error!(message = "Failed to consume.", error = ?error, internal_log_rate_secs = true);
+            error!(message = "Failed to consume.", error = ?error, internal_log_rate_limit = true);
         })?
         .fuse();
     let mut shutdown = shutdown.fuse();
