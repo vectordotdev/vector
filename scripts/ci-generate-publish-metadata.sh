f416c31bda13bbe04d3e3267aa41b938e432167f
Kyle Criddle <kyle.criddle@datadoghq.com>
2022-11-02 15:08:54 +0000

    use the correct new delimiter

--- scripts/ci-generate-publish-metadata.sh
+++ scripts/ci-generate-publish-metadata.sh
@@ -15,19 +15,19 @@ GITHUB_OUTPUT="${GITHUB_OUTPUT:-/dev/stdout}"
 
 # Generate the Vector version, and build description.
 VERSION="${VERSION:-"$(awk -F ' = ' '$1 ~ /^version/ { gsub(/["]/, "", $2); printf("%s",$2) }' Cargo.toml)"}"
-echo "vector_version::${VERSION}" >> "${GITHUB_OUTPUT}"
+echo "vector_version=${VERSION}" >> "${GITHUB_OUTPUT}"
 
 GIT_SHA=$(git rev-parse --short HEAD)
 CURRENT_DATE=$(date +%Y-%m-%d)
-echo "vector_build_desc::${GIT_SHA} ${CURRENT_DATE}" >> "${GITHUB_OUTPUT}"
+echo "vector_build_desc=${GIT_SHA} ${CURRENT_DATE}" >> "${GITHUB_OUTPUT}"
 
 # Figure out what our release channel is.
 CHANNEL="${CHANNEL:-"$(scripts/release-channel.sh)"}"
-echo "vector_release_channel::${CHANNEL}" >> "${GITHUB_OUTPUT}"
+echo "vector_release_channel=${CHANNEL}" >> "${GITHUB_OUTPUT}"
 
 # Depending on the channel, this influences which Cloudsmith repository we publish to.
 if [[ "${CHANNEL}" == "nightly" ]]; then
-	echo "vector_cloudsmith_repo::vector-nightly" >> "${GITHUB_OUTPUT}"
+	echo "vector_cloudsmith_repo=vector-nightly" >> "${GITHUB_OUTPUT}"
 else
-	echo "vector_cloudsmith_repo::vector" >> "${GITHUB_OUTPUT}"
+	echo "vector_cloudsmith_repo=vector" >> "${GITHUB_OUTPUT}"
 fi
