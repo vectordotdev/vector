1e37d54676c47f8ff22e62502c7304a6d9c5d569
Alexander Zaitsev <zamazan4ik@tut.by>
2022-11-11 10:55:39 +0100

    feat(pulsar): another bunch of features
    - add priority_level support
    - add dead letter queue policy support
    - add auth support
    
    Tested:
    - Local build

--- src/sources/pulsar.rs
+++ src/sources/pulsar.rs
@@ -10,7 +10,9 @@ use chrono::Utc;
 use codecs::decoding::{DeserializerConfig, FramingConfig};
 use codecs::StreamDecodingError;
 use futures_util::StreamExt;
-use pulsar::SubType;
+use pulsar::authentication::oauth2::{OAuth2Authentication, OAuth2Params};
+use pulsar::error::AuthenticationError;
+use pulsar::{Authentication, SubType};
 use pulsar::{Consumer, Pulsar, TokioExecutor};
 use tokio_util::codec::FramedRead;
 use vector_common::internal_event::ByteSize;
@@ -44,11 +46,21 @@ pub struct PulsarSourceConfig {
     /// The Pulsar subscription name.
     subscription_name: Option<String>,
 
+    /// Priority level for a consumer to which a broker gives more priority while dispatching messages in Shared subscription type.
+    ///
+    /// The broker follows descending priorities. For example, 0=max-priority, 1, 2,...
+    ///
+    /// In Shared subscription type, the broker first dispatches messages to the max priority level consumers if they have permits. Otherwise, the broker considers next priority level consumers.
+    priority_level: Option<i32>,
+
     /// Max count of messages in a batch.
     batch_size: Option<u32>,
 
     #[configurable(derived)]
     auth: Option<AuthConfig>,
+
+    #[configurable(derived)]
+    dead_letter_queue_policy: Option<DeadLetterQueuePolicy>,
 
     #[configurable(derived)]
     #[serde(default = "default_framing_message_based")]
@@ -81,7 +93,7 @@ struct AuthConfig {
     oauth2: Option<OAuth2Config>,
 }
 
-/// OAuth2-specific authenticatgion configuration.
+/// OAuth2-specific authentication configuration.
 #[configurable_component]
 #[derive(Clone, Debug)]
 pub struct OAuth2Config {
@@ -98,6 +110,17 @@ pub struct OAuth2Config {
 
     /// The OAuth2 scope.
     scope: Option<String>,
+}
+
+/// Dead Letter Queue policy configuration.
+#[configurable_component]
+#[derive(Clone, Debug)]
+struct DeadLetterQueuePolicy {
+    /// Maximum number of times that a message will be redelivered before being sent to the dead letter queue.
+    pub max_redeliver_count: usize,
+
+    /// Name of the dead topic where the failing messages will be sent.
+    pub dead_letter_topic: String,
 }
 
 impl GenerateConfig for PulsarSourceConfig {
@@ -142,13 +165,53 @@ impl SourceConfig for PulsarSourceConfig {
 async fn create_consumer(
     config: &PulsarSourceConfig,
 ) -> crate::Result<pulsar::consumer::Consumer<String, TokioExecutor>> {
-    let pulsar = Pulsar::<TokioExecutor>::builder(&config.endpoint, TokioExecutor)
-        .build()
-        .await?;
+    let mut builder = Pulsar::builder(&config.endpoint, TokioExecutor);
+
+    if let Some(auth) = &config.auth {
+        builder = match (
+            auth.name.as_ref(),
+            auth.token.as_ref(),
+            auth.oauth2.as_ref(),
+        ) {
+            (Some(name), Some(token), None) => builder.with_auth(Authentication {
+                name: name.clone(),
+                data: token.inner().as_bytes().to_vec(),
+            }),
+            (None, None, Some(oauth2)) => {
+                builder.with_auth_provider(OAuth2Authentication::client_credentials(OAuth2Params {
+                    issuer_url: oauth2.issuer_url.clone(),
+                    credentials_url: oauth2.credentials_url.clone(),
+                    audience: oauth2.audience.clone(),
+                    scope: oauth2.scope.clone(),
+                }))
+            }
+            _ => return Err(Box::new(pulsar::error::Error::Authentication(
+                AuthenticationError::Custom(
+                    "Invalid auth config: can only specify name and token or oauth2 configuration"
+                        .to_string(),
+                ),
+            ))),
+        };
+    }
+
+    let pulsar = builder.build().await?;
+
     let mut consumer_builder = pulsar
         .consumer()
         .with_topics(&config.topics)
-        .with_subscription_type(SubType::Shared);
+        .with_subscription_type(SubType::Shared)
+        .with_options(pulsar::consumer::ConsumerOptions {
+            priority_level: config.priority_level,
+            ..Default::default()
+        });
+
+    if let Some(dead_letter_queue_policy) = &config.dead_letter_queue_policy {
+        consumer_builder =
+            consumer_builder.with_dead_letter_policy(pulsar::consumer::DeadLetterPolicy {
+                max_redeliver_count: dead_letter_queue_policy.max_redeliver_count,
+                dead_letter_topic: dead_letter_queue_policy.dead_letter_topic.clone(),
+            });
+    }
 
     if let Some(batch_size) = config.batch_size {
         consumer_builder = consumer_builder.with_batch_size(batch_size);
