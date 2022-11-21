c11143b0a4caded0d8303f0c1c1d03fdfb073e8c
Arshia Soleimani <arshia.soleimani@datadoghq.com>
2022-11-21 11:07:56 -0500

    remove duplicate cfg

--- src/lib.rs
+++ src/lib.rs
@@ -26,22 +26,7 @@ extern crate derivative;
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[cfg(all(
-    feature = "tikv-jemallocator",
-    feature = "allocation-tracing",
-    not(target_os = "macos")
-))]
-#[global_allocator]
-static ALLOC: self::internal_telemetry::allocations::Allocator<tikv_jemallocator::Jemalloc> =
-    self::internal_telemetry::allocations::get_grouped_tracing_allocator(
-        tikv_jemallocator::Jemalloc,
-    );
-
-#[cfg(all(
-    feature = "tikv-jemallocator",
-    feature = "allocation-tracing",
-    target_os = "macos"
-))]
+#[cfg(all(feature = "tikv-jemallocator", feature = "allocation-tracing"))]
 #[global_allocator]
 static ALLOC: self::internal_telemetry::allocations::Allocator<tikv_jemallocator::Jemalloc> =
     self::internal_telemetry::allocations::get_grouped_tracing_allocator(
