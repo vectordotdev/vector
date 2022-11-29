b3481c486fb2995669cabc40d99abc99c58f90fd
Arshia Soleimani <arshia.soleimani@datadoghq.com>
2022-11-29 14:51:43 -0500

    checkpoint

--- src/internal_telemetry/allocations/mod.rs
+++ src/internal_telemetry/allocations/mod.rs
@@ -3,7 +3,7 @@
 mod allocator;
 use std::{
     sync::{
-        atomic::{AtomicBool, AtomicI64, Ordering},
+        atomic::{AtomicBool, AtomicU64, Ordering},
         Mutex,
     },
     thread,
@@ -23,23 +23,29 @@ pub(crate) use self::allocator::{
 const NUM_GROUPS: usize = 128;
 pub static TRACK_ALLOCATIONS: AtomicBool = AtomicBool::new(false);
 
-type GroupMemStatsArray = [AtomicI64; NUM_GROUPS];
+/// Track allocations and deallocations separately.
+struct GroupMemStatsStorage {
+    allocations: [AtomicU64; NUM_GROUPS],
+    deallocations: [AtomicU64; NUM_GROUPS],
+}
 
 /// A registry for tracking each thread's group memory statistics.
-static THREAD_LOCAL_REFS: Mutex<Vec<&'static GroupMemStatsArray>> = Mutex::new(Vec::new());
+static THREAD_LOCAL_REFS: Mutex<Vec<&'static GroupMemStatsStorage>> = Mutex::new(Vec::new());
 
 /// Group memory statistics per thread.
 struct GroupMemStats {
-    stats: &'static GroupMemStatsArray,
+    stats: &'static GroupMemStatsStorage,
 }
 
 impl GroupMemStats {
-    /// Allocates a [`GroupMemStatsArray`], and updates the global [`THREAD_LOCAL_REFS`] registry
+    /// Allocates a [`GroupMemStatsStorage`], and updates the global [`THREAD_LOCAL_REFS`] registry
     /// with a reference to this newly allocated memory.
     pub fn new() -> Self {
         let mut mutex = THREAD_LOCAL_REFS.lock().unwrap();
-        let stats_ref: &'static GroupMemStatsArray =
-            Box::leak(Box::new(arr![AtomicI64::new(0) ; 128]));
+        let stats_ref: &'static GroupMemStatsStorage = Box::leak(Box::new(GroupMemStatsStorage {
+            allocations: arr![AtomicU64::new(0) ; 128],
+            deallocations: arr![AtomicU64::new(0) ; 128],
+        }));
         let group_mem_stats = GroupMemStats { stats: stats_ref };
         mutex.push(stats_ref);
         group_mem_stats
@@ -81,7 +87,8 @@ impl Tracer for MainTracer {
     fn trace_allocation(&self, object_size: usize, group_id: AllocationGroupId) {
         // Handle the case when thread local destructor is ran.
         let _ = GROUP_MEM_STATS.try_with(|t| {
-            t.stats[group_id.as_raw() as usize].fetch_add(object_size as i64, Ordering::Relaxed)
+            t.stats.allocations[group_id.as_raw() as usize]
+                .fetch_add(object_size as u64, Ordering::Relaxed)
         });
     }
 
@@ -89,8 +96,8 @@ impl Tracer for MainTracer {
     fn trace_deallocation(&self, object_size: usize, source_group_id: AllocationGroupId) {
         // Handle the case when thread local destructor is ran.
         let _ = GROUP_MEM_STATS.try_with(|t| {
-            t.stats[source_group_id.as_raw() as usize]
-                .fetch_sub(object_size as i64, Ordering::Relaxed)
+            t.stats.deallocations[source_group_id.as_raw() as usize]
+                .fetch_add(object_size as u64, Ordering::Relaxed)
         });
     }
 }
@@ -111,21 +118,36 @@ pub fn init_allocation_tracing() {
             if TRACK_ALLOCATIONS.load(Ordering::Relaxed) {
                 without_allocation_tracing(|| {
                     for (group_idx, group) in GROUP_INFO.iter().enumerate() {
-                        let mut mem_used = 0;
+                        let mut allocations = 0;
+                        let mut deallocations = 0;
                         let mutex = THREAD_LOCAL_REFS.lock().unwrap();
                         for idx in 0..mutex.len() {
-                            mem_used += mutex[idx][group_idx].load(Ordering::Relaxed);
+                            allocations += mutex[idx].allocations[group_idx].load(Ordering::Relaxed);
+                            deallocations += mutex[idx].deallocations[group_idx].load(Ordering::Relaxed);
                         }
-                        if mem_used == 0 {
+                        if allocations == 0 {
                             continue;
                         }
+                        let mem_used = allocations - deallocations;
                         let group_info = group.lock().unwrap();
                         gauge!(
-                        "component_allocated_bytes",
-                        mem_used.to_f64().expect("failed to convert group_id from int to float"),
-                        "component_kind" => group_info.component_kind.clone(),
-                        "component_type" => group_info.component_type.clone(),
-                        "component_id" => group_info.component_id.clone());
+                            "component_allocated_bytes_total",
+                            allocations.to_f64().expect("failed to convert allocations from int to float"),
+                            "component_kind" => group_info.component_kind.clone(),
+                            "component_type" => group_info.component_type.clone(),
+                            "component_id" => group_info.component_id.clone());
+                        gauge!(
+                            "component_deallocated_bytes_total",
+                            deallocations.to_f64().expect("failed to convert deallocations from int to float"),
+                            "component_kind" => group_info.component_kind.clone(),
+                            "component_type" => group_info.component_type.clone(),
+                            "component_id" => group_info.component_id.clone());
+                        gauge!(
+                            "component_allocated_bytes",
+                            mem_used.to_f64().expect("failed to convert mem_used from int to float"),
+                            "component_kind" => group_info.component_kind.clone(),
+                            "component_type" => group_info.component_type.clone(),
+                            "component_id" => group_info.component_id.clone());
                     }
                 });
             }
