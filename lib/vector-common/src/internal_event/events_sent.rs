898b4fec6b168e6cf2c1877e58ab85d0605aaf6e
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-12-06 15:19:32 -0600

    chore(observability): Add macro to help implementing registered internal events (#14753)
    * Add macro to set up registered events
    
    * Use `paste` to create the handle name programmatically
    
    * Merge `fn register` into handle field definition
    
    * Modify check-events to handle the registered event macro
    
    * Add sample code for registering and emitting an event

--- lib/vector-common/src/internal_event/events_sent.rs
+++ lib/vector-common/src/internal_event/events_sent.rs
@@ -1,60 +1,33 @@
 use metrics::{register_counter, Counter};
 use tracing::trace;
 
-use super::{CountByteSize, InternalEventHandle, Output, RegisterInternalEvent, SharedString};
+use super::{CountByteSize, Output, SharedString};
 
 pub const DEFAULT_OUTPUT: &str = "_default";
 
-#[derive(Debug)]
-pub struct EventsSent {
-    pub output: Option<SharedString>,
-}
-
-impl From<Output> for EventsSent {
-    fn from(output: Output) -> Self {
-        Self { output: output.0 }
-    }
-}
-
-impl RegisterInternalEvent for EventsSent {
-    type Handle = EventsSentHandle;
-
-    fn register(self) -> Self::Handle {
-        if let Some(output) = self.output {
-            EventsSentHandle {
-                events: register_counter!("component_sent_events_total", "output" => output.clone()),
-                events_out: register_counter!("events_out_total", "output" => output.clone()),
-                event_bytes: register_counter!("component_sent_event_bytes_total", "output" => output.clone()),
-                output: Some(output),
-            }
+crate::registered_event!(
+    EventsSent {
+        output: Option<SharedString>,
+    } => {
+        events: Counter = if let Some(output) = &self.output {
+            register_counter!("component_sent_events_total", "output" => output.clone())
         } else {
-            EventsSentHandle {
-                events: register_counter!("component_sent_events_total"),
-                events_out: register_counter!("events_out_total"),
-                event_bytes: register_counter!("component_sent_event_bytes_total"),
-                output: None,
-            }
-        }
+            register_counter!("component_sent_events_total")
+        },
+        events_out: Counter = if let Some(output) = &self.output {
+            register_counter!("events_out_total", "output" => output.clone())
+        } else {
+            register_counter!("events_out_total")
+        },
+        event_bytes: Counter = if let Some(output) = &self.output {
+            register_counter!("component_sent_event_bytes_total", "output" => output.clone())
+        } else {
+            register_counter!("component_sent_event_bytes_total")
+        },
+        output: Option<SharedString> = self.output,
     }
 
-    fn name(&self) -> Option<&'static str> {
-        Some("EventsSent")
-    }
-}
-
-#[allow(clippy::module_name_repetitions)]
-#[derive(Clone)]
-pub struct EventsSentHandle {
-    events: Counter,
-    events_out: Counter,
-    event_bytes: Counter,
-    output: Option<SharedString>,
-}
-
-impl InternalEventHandle for EventsSentHandle {
-    type Data = CountByteSize;
-
-    fn emit(&self, data: Self::Data) {
+    fn emit(&self, data: CountByteSize) {
         let CountByteSize(count, byte_size) = data;
 
         match &self.output {
@@ -69,5 +42,11 @@ impl InternalEventHandle for EventsSentHandle {
         self.events.increment(count as u64);
         self.events_out.increment(count as u64);
         self.event_bytes.increment(byte_size as u64);
+    }
+);
+
+impl From<Output> for EventsSent {
+    fn from(output: Output) -> Self {
+        Self { output: output.0 }
     }
 }
