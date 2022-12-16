a6a678640f331fff6464d10d4b73340fa871dc6d
Bruce Guenter <bruce.guenter@datadoghq.com>
2022-12-16 14:15:46 -0600

    enhancement(metric_to_log transform): Add compatibility support for enhanced tags

--- lib/vector-core/src/event/metric/tags.rs
+++ lib/vector-core/src/event/metric/tags.rs
@@ -118,7 +118,7 @@ impl Display for TagValueSet {
 impl TagValueSet {
     /// Convert this set into a single value, mimicking the behavior of this set being just a plain
     /// single string while still storing all of the values.
-    pub(crate) fn into_single(self) -> Option<String> {
+    pub fn into_single(self) -> Option<String> {
         match self {
             Self::Empty => None,
             Self::Single(tag) => tag.into_option(),
@@ -131,7 +131,7 @@ impl TagValueSet {
 
     /// Get the "single" value of this set, mimicking the behavior of this set being just a plain
     /// single string while still storing all of the values.
-    pub(crate) fn as_single(&self) -> Option<&str> {
+    pub fn as_single(&self) -> Option<&str> {
         match self {
             Self::Empty => None,
             Self::Single(tag) => tag.as_option(),
@@ -149,7 +149,7 @@ impl TagValueSet {
         }
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         match self {
             Self::Empty => 0,
             Self::Single(_) => 1,
@@ -510,7 +510,7 @@ impl MetricTags {
         }
     }
 
-    pub fn retain(&mut self, mut f: impl FnMut(&str, &TagValueSet) -> bool) {
+    pub fn retain(&mut self, mut f: impl FnMut(&str, &mut TagValueSet) -> bool) {
         self.0.retain(|key, tags| f(key.as_str(), tags));
     }
 }
