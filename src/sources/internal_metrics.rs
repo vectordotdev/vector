48490f0d967112d552b51ec02e183ae575e82cd2
May Lee <may.lee@datadoghq.com>
2022-12-29 10:47:36 -0500

    remove vector

--- src/sources/internal_metrics.rs
+++ src/sources/internal_metrics.rs
@@ -29,7 +29,7 @@ pub struct InternalMetricsConfig {
 
     /// Overrides the default namespace for the metrics emitted by the source.
     ///
-    /// By default, `vector` is used.
+    /// Overrides the default namespace.
     pub namespace: Option<String>,
 }
 
