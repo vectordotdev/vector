c491c2dbd0e1d3bf165c33e61a13c3e9b9e85294
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-01-03 18:05:54 -0600

    chore(vrl): Fix year-dependent tests for 2023 (#15797)
    The code side of this could potentially be fixed by formatting the timestamps
    involved. However, all the strings involved have a fixed type of `&'static str`
    which cannot be dynamically formatted. As a result, resolving this involves a
    cascade of type refactoring into either `String` or `Cow<'static, str>`, and the
    return from `examples` into something other than `&'static [Example]` since that
    too is no longer static.
    
    That will not help the documentation tests, though.

--- lib/vrl/stdlib/src/parse_linux_authorization.rs
+++ lib/vrl/stdlib/src/parse_linux_authorization.rs
@@ -28,7 +28,7 @@ impl Function for ParseLinuxAuthorization {
                 "hostname": "localhost",
                 "message": "Accepted publickey for eng from 10.1.1.1 port 8888 ssh2: RSA SHA256:foobar",
                 "procid": 1111,
-                "timestamp": "2022-03-23T01:49:58Z"
+                "timestamp": "2023-03-23T01:49:58Z"
             }"#}),
         }]
     }
