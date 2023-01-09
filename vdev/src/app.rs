36940105f525ceade9276ecd538ec3b6fe044496
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-01-09 15:02:11 -0600

    enhancement(dev): Add dedicated helper tool (#15833)
    * Add dedicated dev tool
    
    * refactor
    
    * Apply suggestions from code review
    
    Co-authored-by: neuronull <kyle.criddle@datadoghq.com>
    
    * address
    
    * add env management commands
    
    * add test command
    
    * refactor with once_cell
    
    * refactor sub-process handling
    
    * improve error messages
    
    * handle case of first time user
    
    * reduce boilerplate with macros
    
    * address
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Wakely <stephen@lisp.space>
    
    * address
    
    * add top-level test command
    
    * add to CI
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Wakely <stephen@lisp.space>
    
    * final update
    
    * Fix clippy lints
    
    * Convert extensions to `process::Command` into an extension trait
    
    * Add `infer_subcommands` option and unabbreviate `int` subcommand
    
    * address
    
    * Flatten out the `cli.rs` files
    
    * Inline the used cfg options
    
    * Drop unused config bits
    
    * Turn on `clippy::pedantic` and fix issues
    
    * Pass command args owned to allow for direct use
    
    * Eliminate some silent error fallbacks
    
    * Simplify logic of `integration show`
    
    * Drop some common parameters in testing functions
    
    * Fix new clippy lints
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    Co-authored-by: neuronull <kyle.criddle@datadoghq.com>
    Co-authored-by: Stephen Wakely <stephen@lisp.space>

---
+++ vdev/src/app.rs
@@ -0,0 +1,107 @@
+use std::{borrow::Cow, process::Command, time::Duration};
+
+use anyhow::{bail, Result};
+use indicatif::{ProgressBar, ProgressStyle};
+use log::LevelFilter;
+use once_cell::sync::OnceCell;
+
+use crate::config::Config;
+
+static VERBOSITY: OnceCell<LevelFilter> = OnceCell::new();
+static CONFIG: OnceCell<Config> = OnceCell::new();
+static PATH: OnceCell<String> = OnceCell::new();
+
+pub fn verbosity() -> &'static LevelFilter {
+    VERBOSITY.get().expect("verbosity is not initialized")
+}
+
+pub fn config() -> &'static Config {
+    CONFIG.get().expect("config is not initialized")
+}
+
+pub fn path() -> &'static String {
+    PATH.get().expect("path is not initialized")
+}
+
+/// Overlay some extra helper functions onto `std::process::Command`
+pub trait CommandExt {
+    fn with_path(program: &str) -> Self;
+    fn capture_output(&mut self) -> Result<String>;
+    fn run(&mut self) -> Result<()>;
+    fn wait(&mut self, message: impl Into<Cow<'static, str>>) -> Result<()>;
+}
+
+impl CommandExt for Command {
+    fn with_path(program: &str) -> Self {
+        let mut command = Command::new(program);
+        command.current_dir(path());
+        command
+    }
+
+    fn capture_output(&mut self) -> Result<String> {
+        Ok(String::from_utf8(self.output()?.stdout)?)
+    }
+
+    fn run(&mut self) -> Result<()> {
+        let status = self.status()?;
+        if status.success() {
+            Ok(())
+        } else {
+            bail!(
+                "command: {} {}\nfailed with exit code: {}",
+                self.get_program().to_str().expect("Invalid program name"),
+                self.get_args()
+                    .map(|arg| arg.to_str().expect("Invalid command argument"))
+                    .collect::<Vec<_>>()
+                    .join(" "),
+                status.code().unwrap()
+            )
+        }
+    }
+
+    fn wait(&mut self, message: impl Into<Cow<'static, str>>) -> Result<()> {
+        let progress_bar = get_progress_bar()?;
+        progress_bar.set_message(message);
+
+        let result = self.output();
+        progress_bar.finish_and_clear();
+        let output = match result {
+            Ok(output) => output,
+            Err(_) => bail!("could not run command"),
+        };
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(
+                "{}\nfailed with exit code: {}",
+                String::from_utf8(output.stdout)?,
+                output.status.code().unwrap()
+            )
+        }
+    }
+}
+
+fn get_progress_bar() -> Result<ProgressBar> {
+    let progress_bar = ProgressBar::new_spinner();
+    progress_bar.enable_steady_tick(Duration::from_millis(125));
+    progress_bar.set_style(
+        ProgressStyle::with_template("{spinner} {msg:.magenta.bold}")?
+            // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+            .tick_strings(&["∙∙∙", "●∙∙", "∙●∙", "∙∙●", "∙∙∙"]),
+    );
+
+    Ok(progress_bar)
+}
+
+pub fn set_global_verbosity(verbosity: LevelFilter) {
+    VERBOSITY.set(verbosity).expect("could not set verbosity");
+}
+
+pub fn set_global_config(config: Config) {
+    CONFIG.set(config).expect("could not set config");
+}
+
+pub fn set_global_path(path: String) {
+    PATH.set(path).expect("could not set path");
+}
