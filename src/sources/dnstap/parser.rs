3c5ec1c1e1856271fff4e12694ae25ca9bb987d4
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2023-01-10 10:33:58 -0500

    chore: fix base64 breaking changes

--- src/sources/dnstap/parser.rs
+++ src/sources/dnstap/parser.rs
@@ -5,6 +5,7 @@ use std::{
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
 };
 
+use base64::prelude::{Engine as _, BASE64_STANDARD};
 use bytes::Bytes;
 use chrono::{TimeZone, Utc};
 use once_cell::sync::Lazy;
@@ -172,7 +173,7 @@ impl<'a> DnstapParser<'a> {
         if need_raw_data {
             self.insert(
                 self.event_schema.dnstap_root_data_schema().raw_data(),
-                base64::encode(&frame),
+                BASE64_STANDARD.encode(&frame),
             );
         }
 
@@ -468,7 +469,7 @@ impl<'a> DnstapParser<'a> {
 
         self.insert(
             self.event_schema.dns_query_message_schema().raw_data(),
-            base64::encode(raw_dns_message),
+            BASE64_STANDARD.encode(raw_dns_message),
         );
 
         self.parent_key_path.segments.pop();
@@ -872,7 +873,7 @@ impl<'a> DnstapParser<'a> {
         if let Some(rdata_bytes) = &record.rdata_bytes {
             self.insert(
                 self.event_schema.dns_record_schema().rdata_bytes(),
-                base64::encode(rdata_bytes),
+                BASE64_STANDARD.encode(rdata_bytes),
             );
         };
     }
@@ -952,7 +953,9 @@ mod tests {
         let raw_dnstap_data = "ChVqYW1lcy1WaXJ0dWFsLU1hY2hpbmUSC0JJTkQgOS4xNi4zcnoIAxACGAEiEAAAAAAAAA\
         AAAAAAAAAAAAAqECABBQJwlAAAAAAAAAAAADAw8+0CODVA7+zq9wVNMU3WNlI2kwIAAAABAAAAAAABCWZhY2Vib29rMQNjb\
         20AAAEAAQAAKQIAAACAAAAMAAoACOxjCAG9zVgzWgUDY29tAHgB";
-        let dnstap_data = base64::decode(raw_dnstap_data).expect("Invalid base64 encoded data.");
+        let dnstap_data = BASE64_STANDARD
+            .decode(raw_dnstap_data)
+            .expect("Invalid base64 encoded data.");
         let parse_result = parser.parse_dnstap_data(Bytes::from(dnstap_data));
         assert!(parse_result.is_ok());
         assert!(log_event
@@ -1002,7 +1005,9 @@ mod tests {
         let raw_dnstap_data = "ChVqYW1lcy1WaXJ0dWFsLU1hY2hpbmUSC0JJTkQgOS4xNi4zcmsIDhABGAEiBH8AAA\
         EqBH8AAAEwrG44AEC+iu73BU14gfofUh1wi6gAAAEAAAAAAAAHZXhhbXBsZQNjb20AAAYAAWC+iu73BW0agDwvch1wi6gAA\
         AEAAAAAAAAHZXhhbXBsZQNjb20AAAYAAXgB";
-        let dnstap_data = base64::decode(raw_dnstap_data).expect("Invalid base64 encoded data.");
+        let dnstap_data = BASE64_STANDARD
+            .decode(raw_dnstap_data)
+            .expect("Invalid base64 encoded data.");
         let parse_result = parser.parse_dnstap_data(Bytes::from(dnstap_data));
         assert!(parse_result.is_ok());
         assert!(log_event
