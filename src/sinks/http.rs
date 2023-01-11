dc06a4708332581acaecf9b46cadbaf28f312898
jakob.diebold <jakob.diebold@idealo.de>
2023-01-11 21:18:55 +0100

    fix formatting

--- src/sinks/http.rs
+++ src/sinks/http.rs
@@ -401,12 +401,16 @@ fn validate_payload_wrapper(
     encoder: &Encoder<Framer>,
 ) -> crate::Result<(String, String)> {
     let payload = [payload_prefix, "{}", payload_suffix].join("");
-    match (encoder.serializer(), encoder.framer(), serde_json::from_str::<serde_json::Value>(&payload) ) {
+    match (
+        encoder.serializer(),
+        encoder.framer(),
+        serde_json::from_str::<serde_json::Value>(&payload),
+    ) {
         (
             Serializer::Json(_),
             Framer::CharacterDelimited(CharacterDelimitedEncoder { delimiter: b',' }),
-            Err(_)
-         ) => Err("Payload prefix and suffix wrapper must produce a valid JSON object.".into()),
+            Err(_),
+        ) => Err("Payload prefix and suffix wrapper must produce a valid JSON object.".into()),
         _ => Ok((payload_prefix.to_owned(), payload_suffix.to_owned())),
     }
 }
