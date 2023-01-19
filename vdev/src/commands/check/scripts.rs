26daa3402fea1e0813f5c4afb27ab5f643058610
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-01-19 15:42:30 -0600

    Refactor invoking `git ls-files` into common code

--- vdev/src/commands/check/scripts.rs
+++ vdev/src/commands/check/scripts.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use crate::app::{self, Command, CommandExt as _};
+use crate::{app, git};
 
 /// Check that shell scripts do not have common mistakes
 #[derive(clap::Args, Debug)]
@@ -12,18 +12,13 @@ impl Cli {
         app::set_repo_dir()?;
 
         #[allow(clippy::case_sensitive_file_extension_comparisons)]
-        let scripts: Vec<_> = Command::new("git")
-            .arg("ls-files")
-            .capture_output()?
-            .lines()
-            .filter_map(|entry| entry.ends_with(".sh").then(|| entry.to_owned()))
-            .collect();
-
         app::exec(
             "shellcheck",
-            ["--external-sources", "--shell", "bash"]
-                .into_iter()
-                .chain(scripts.iter().map(String::as_str)),
+            ["--external-sources", "--shell", "bash"].into_iter().chain(
+                git::list_files()?
+                    .iter()
+                    .filter_map(|name| name.ends_with(".sh").then_some(name.as_str())),
+            ),
             true,
         )
     }
