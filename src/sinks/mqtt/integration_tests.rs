ad67fcb64850a4658b2f698861b08a934b111fb6
Alexander Zaitsev <zamazan4ik@tut.by>
2023-02-05 22:18:23 +0100

    test integration test on CI

--- src/sinks/mqtt/integration_tests.rs
+++ src/sinks/mqtt/integration_tests.rs
@@ -1,4 +1,5 @@
 use crate::config::{SinkConfig, SinkContext};
+use crate::sinks::mqtt::config::MqttQoS;
 use crate::sinks::mqtt::MqttSinkConfig;
 use crate::template::Template;
 use crate::test_util::components::{run_and_assert_sink_compliance, SINK_TAGS};
@@ -7,30 +8,35 @@ use rumqttc::{AsyncClient, MqttOptions, QoS};
 use std::time::Duration;
 
 fn mqtt_broker_address() -> String {
-    std::env::var("MQTT_BROKER_ADDRESS").unwrap_or_else(|_| "127.0.0.1".into())
+    let result = std::env::var("MQTT_BROKER_ADDRESS").unwrap_or_else(|_| "emqx".into());
+    println!("{}", result);
+    result
 }
 
 fn mqtt_broker_port() -> u16 {
-    std::env::var("MQTT_BROKER_PORT")
+    let result = std::env::var("MQTT_BROKER_PORT")
         .unwrap_or_else(|_| "1883".into())
         .parse::<u16>()
-        .expect("Cannot parse as u16")
+        .expect("Cannot parse as u16");
+    println!("{}", result);
+    result
 }
 
 #[tokio::test]
 async fn mqtt_happy() {
     trace_init();
 
-    let topic = "test-topic";
+    let topic = "test";
     let cnf = MqttSinkConfig {
         host: mqtt_broker_address(),
         port: mqtt_broker_port(),
         topic: Template::try_from(topic).expect("Cannot parse the topic template"),
+        quality_of_service: MqttQoS::AtLeastOnce,
         ..Default::default()
     };
 
     let cx = SinkContext::new_test();
-    let (sink, healthcheck) = cnf.build(cx).await.unwrap();
+    let (sink, healthcheck) = cnf.build(cx).await.expect("Cannot build the sink");
     healthcheck.await.expect("Health check failed");
 
     // prepare consumer
@@ -43,19 +49,19 @@ async fn mqtt_happy() {
 
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
     client
-        .subscribe(topic, QoS::ExactlyOnce)
+        .subscribe("test", QoS::AtLeastOnce)
         .await
         .expect("Cannot subscribe to the topic");
 
-    let num_events = 1000;
+    let num_events = 10;
     let (input, events) = random_lines_with_stream(100, num_events, None);
     run_and_assert_sink_compliance(sink, events, &SINK_TAGS).await;
 
     // loop instead of iter so we can set a timeout
     let mut failures = 0;
     let mut out = Vec::new();
-    while failures < 10 && out.len() < input.len() {
-        if let Ok(try_msg) = tokio::time::timeout(Duration::from_secs(10), eventloop.poll()).await {
+    while failures < 5 && out.len() < input.len() {
+        if let Ok(try_msg) = tokio::time::timeout(Duration::from_secs(1), eventloop.poll()).await {
             let msg = try_msg.unwrap();
             println!("Received = {:?}", msg);
             out.push("test_str".to_string());
--- src/sinks/mqtt/sink.rs
+++ src/sinks/mqtt/sink.rs
@@ -6,7 +6,7 @@ use futures::{pin_mut, stream::BoxStream, Stream, StreamExt};
 use rumqttc::{AsyncClient, ClientError, ConnectionError, EventLoop, MqttOptions};
 use snafu::{ResultExt, Snafu};
 use tokio_util::codec::Encoder as _;
-use vector_common::internal_event::{ByteSize, CountByteSize, Output, Protocol, Registered};
+use vector_common::internal_event::{ByteSize, CountByteSize, Output, Protocol};
 use vector_core::{
     event::EventFinalizers,
     internal_event::{BytesSent, EventsSent, InternalEventHandle},
@@ -69,12 +69,11 @@ pub struct MqttSink {
     connector: MqttConnector,
     finalizers_queue: VecDeque<EventFinalizers>,
     quality_of_service: MqttQoS,
-    events_sent: Registered<EventsSent>,
-    bytes_sent: Registered<BytesSent>,
 }
 
 impl MqttSink {
     pub fn new(config: &MqttSinkConfig, connector: MqttConnector) -> crate::Result<Self> {
+        println!("Create sink");
         let transformer = config.encoding.transformer();
         let serializer = config.encoding.build()?;
         let encoder = Encoder::<()>::new(serializer);
@@ -86,8 +85,6 @@ impl MqttSink {
             connector,
             finalizers_queue,
             quality_of_service: config.quality_of_service,
-            events_sent: register!(EventsSent::from(Output(None))),
-            bytes_sent: register!(BytesSent::from(Protocol("mqtt".into()))),
         })
     }
 
@@ -101,10 +98,15 @@ impl MqttSink {
     where
         I: Stream<Item = Event> + Unpin,
     {
+        println!("Handle sink");
+        let events_sent = register!(EventsSent::from(Output(None)));
+        let bytes_sent = register!(BytesSent::from(Protocol("mqtt".into())));
+
         loop {
             tokio::select! {
                 // handle connection errors
                 msg = connection.poll() => {
+                    println!("{:?}", msg);
                     match msg {
                         Ok(rumqttc::Event::Outgoing(rumqttc::Outgoing::PubRel(_))) => {
                             // publish has been acknowledged by the MQTT server
@@ -122,6 +124,7 @@ impl MqttSink {
 
                 // handle outgoing events
                 event = input.next() => {
+                    println!("Event sink");
                     let mut event = if let Some(event) = event {
                         event
                     } else {
@@ -162,12 +165,14 @@ impl MqttSink {
                     let retain = false;
                     match client.publish(&topic, self.quality_of_service.into(), retain, message).await {
                         Ok(()) => {
-                            self.events_sent.emit(CountByteSize(1, event_byte_size));
-                            self.bytes_sent.emit(ByteSize(message_len));
+                            println!("Event sent");
+                            events_sent.emit(CountByteSize(1, event_byte_size));
+                            bytes_sent.emit(ByteSize(message_len));
 
                             self.finalizers_queue.push_back(finalizers);
                         }
                         Err(error) => {
+                            println!("Event error");
                             emit!(MqttClientError { error });
                             finalizers.update_status(EventStatus::Errored);
                             return Err(());
@@ -186,6 +191,7 @@ impl MqttSink {
 #[async_trait]
 impl StreamSink<Event> for MqttSink {
     async fn run(mut self: Box<Self>, input: BoxStream<'_, Event>) -> Result<(), ()> {
+        println!("Run sink");
         let input = input.fuse().peekable();
         pin_mut!(input);
 
@@ -193,6 +199,7 @@ impl StreamSink<Event> for MqttSink {
         pin_mut!(client);
         pin_mut!(connection);
         while input.as_mut().peek().await.is_some() {
+            println!("While sink");
             let _open_token = OpenGauge::new().open(|count| emit!(ConnectionOpen { count }));
 
             let _result = self
