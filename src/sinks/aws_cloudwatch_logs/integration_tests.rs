fec2d3679515ab007287dc3ef8636c070dba430c
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2023-02-08 11:49:08 -0500

    chore(deps): Update aws-sdks to 0.23 and aws supporting crates to 0.53

--- src/sinks/aws_cloudwatch_logs/integration_tests.rs
+++ src/sinks/aws_cloudwatch_logs/integration_tests.rs
@@ -2,14 +2,11 @@
 #![cfg(test)]
 
 use std::convert::TryFrom;
-use std::str::FromStr;
 
 use aws_sdk_cloudwatchlogs::Client as CloudwatchLogsClient;
-use aws_sdk_cloudwatchlogs::{Endpoint, Region};
 use chrono::Duration;
 use codecs::TextSerializerConfig;
 use futures::{stream, StreamExt};
-use http::Uri;
 use similar_asserts::assert_eq;
 
 use super::*;
@@ -451,11 +448,9 @@ async fn cloudwatch_healthcheck() {
 
 async fn create_client_test() -> CloudwatchLogsClient {
     let auth = AwsAuthentication::test_auth();
-    let region = Some(Region::new("localstack"));
+    let region = Some(aws_types::region::Region::new("localstack"));
     let watchlogs_address = watchlogs_address();
-    let endpoint = Some(Endpoint::immutable(
-        Uri::from_str(&watchlogs_address).unwrap(),
-    ));
+    let endpoint = Some(watchlogs_address);
     let proxy = ProxyConfig::default();
 
     create_client::<CloudwatchLogsClientBuilder>(&auth, region, endpoint, &proxy, &None, true)
