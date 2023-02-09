0e548d063acda53348da238a91141fb7e867186d
Stephen Wakely <fungus.humungus@gmail.com>
2023-02-09 10:36:47 +0000

    Feedback from Kyle
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/splunk_hec/logs/config.rs
+++ src/sinks/splunk_hec/logs/config.rs
@@ -42,7 +42,7 @@ pub struct HecLogsSinkConfig {
     /// The base URL of the Splunk instance.
     ///
     /// The scheme (`http` or `https`) must be specified. No path should be included since the paths defined
-    /// by the [`Splunk`][splunk] api are used.
+    /// by the [`Splunk`][splunk] API are used.
     ///
     /// [splunk]: https://docs.splunk.com/Documentation/Splunk/8.0.0/Data/HECRESTendpoints
     #[configurable(metadata(
@@ -67,20 +67,16 @@ pub struct HecLogsSinkConfig {
     #[configurable(metadata(docs::examples = "field1", docs::examples = "field2"))]
     pub indexed_fields: Vec<String>,
 
-    /// The name of the index where to send the events to.
+    /// The name of the index where to send events to.
     ///
-    /// If not specified, the default index is used.
+    /// If not specified, the default index defined within Splunk is used.
     #[configurable(metadata(docs::examples = "{{ host }}", docs::examples = "custom_index"))]
     pub index: Option<Template>,
 
     /// The sourcetype of events sent to this sink.
     ///
     /// If unset, Splunk will default to `httpevent`.
-    #[configurable(metadata(
-        docs::examples = "{{ sourcetype }}",
-        docs::examples = "_json",
-        docs::examples = "httpevent"
-    ))]
+    #[configurable(metadata(docs::examples = "{{ sourcetype }}", docs::examples = "_json",))]
     pub sourcetype: Option<Template>,
 
     /// The source of events sent to this sink.
