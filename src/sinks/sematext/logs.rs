d377e1631df194016bc077384187370a42a41a0b
Stephen Wakely <fungus.humungus@gmail.com>
2023-02-14 17:41:03 +0000

    Update sinks to use log namespacing
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/sematext/logs.rs
+++ src/sinks/sematext/logs.rs
@@ -142,12 +142,12 @@ fn map_timestamp(mut events: EventArray) -> EventArray {
     match &mut events {
         EventArray::Logs(logs) => {
             for log in logs {
-                if let Some(ts) = log.remove(crate::config::log_schema().timestamp_key()) {
-                    log.insert("@timestamp", ts);
+                if let Some(path) = log.timestamp_path() {
+                    log.rename_key(path.as_str(), "@timestamp");
                 }
 
-                if let Some(host) = log.remove(crate::config::log_schema().host_key()) {
-                    log.insert("os.host", host);
+                if let Some(path) = log.get_host() {
+                    log.rename_key(host_path, "os.host");
                 }
             }
         }
