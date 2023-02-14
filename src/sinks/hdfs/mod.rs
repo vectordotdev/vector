9e09fdeae22abb7851cc45e02654f12d1ebd363f
Xuanwo <github@xuanwo.io>
2023-02-14 17:12:50 +0800

    Add docs for hdfs testing
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/sinks/hdfs/mod.rs
+++ src/sinks/hdfs/mod.rs
@@ -12,5 +12,11 @@ pub use self::config::HdfsConfig;
 #[cfg(test)]
 mod test;
 
+/// This test suites requires the following setups:
+///
+/// - `JAVA_HOME`: users must have installed java runtime and configure `JAVA_HOME` correctly.
+/// - `HADOOP_HOME`: users must have installed hadoop and configure `HADOOP_HOME` to the path of hadoop setup. Visit <https://hadoop.apache.org/releases.html> for downloading.
+///
+/// For more information, please refer to [opendal::services::Hdfs](https://docs.rs/opendal/latest/opendal/services/struct.Hdfs.html#environment)
 #[cfg(all(test, feature = "hdfs-integration-tests"))]
 mod integration_tests;
