6e86232ab4c2d970a86c299d60c08db1694cfac6
Xuanwo <github@xuanwo.io>
2023-02-15 12:17:48 +0800

    Add docs
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/sinks/hdfs/mod.rs
+++ src/sinks/hdfs/mod.rs
@@ -12,10 +12,16 @@ pub use self::config::HdfsConfig;
 #[cfg(test)]
 mod test;
 
-/// This test suites requires the following setups:
+/// It's better to run this test inside container:
 ///
-/// - `JAVA_HOME`: users must have installed java runtime and configure `JAVA_HOME` correctly.
-/// - `HADOOP_HOME`: users must have installed hadoop and configure `HADOOP_HOME` to the path of hadoop setup. Visit <https://hadoop.apache.org/releases.html> for downloading.
+/// ```shell
+/// make test-integration-hdfs
+/// ```
+///
+/// However, you can still run it if you have the following setup:
+///
+/// - `JAVA_HOME`: install java runtime and configure `JAVA_HOME` correctly.
+/// - `HADOOP_HOME`: install hadoop and configure `HADOOP_HOME` to the path of hadoop setup. Visit <https://hadoop.apache.org/releases.html> for downloading.
 ///
 /// For more information, please refer to [opendal::services::Hdfs](https://docs.rs/opendal/latest/opendal/services/struct.Hdfs.html#environment)
 #[cfg(all(test, feature = "hdfs-integration-tests"))]
