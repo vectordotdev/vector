1be1f6432d55973af507a8f65b4a7c1dadb1746b
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2023-02-16 13:51:15 -0300

    fix(distribution): Add missing check_proc function to install script
    Missed in #16398. Definitely could use some tests of this script on different platforms.
    
    Fixes: #16472
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- distribution/install.sh
+++ distribution/install.sh
@@ -268,6 +268,14 @@ get_gnu_musl_glibc() {
   fi
 }
 
+check_proc() {
+    # Check for /proc by looking for the /proc/self/exe link
+    # This is only run on Linux
+    if ! test -L /proc/self/exe ; then
+        err "fatal: Unable to find /proc/self/exe.  Is /proc mounted?  Installation cannot proceed without /proc."
+    fi
+}
+
 get_bitness() {
     need_cmd head
     # Architecture detection without dependencies beyond coreutils.
