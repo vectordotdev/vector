8285d292b8f655dd5dbd6a60d515fa6a820aff16
jonathanpv <jonpad512@gmail.com>
2023-03-02 15:50:32 -0500

    Embed signoff script to install_git_hooks.rs, delete signoff-git-hook.sh

--- vdev/src/commands/install_git_hooks.rs
+++ vdev/src/commands/install_git_hooks.rs
@@ -1,8 +1,42 @@
 use anyhow::{Result, Ok};
+use std::fs::File;
+use std::io::Write;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use std::process::Command;
 use crate::git;
 use crate::app::CommandExt as _;
-// use std::{env, path::PathBuf};
+
+const SIGNOFF_HOOK: &str = r#"#!/bin/bash
+set -euo pipefail
+
+# Automatically sign off your commits.
+#
+# Installation:
+#
+#    cp scripts/signoff-git-hook.sh .git/hooks/commit-msg
+#
+# It's also possible to symlink the script, however that's a security hazard and
+# is not recommended.
+
+NAME="$(git config user.name)"
+EMAIL="$(git config user.email)"
+
+if [ -z "$NAME" ]; then
+  echo "empty git config user.name"
+  exit 1
+fi
+
+if [ -z "$EMAIL" ]; then
+  echo "empty git config user.email"
+  exit 1
+fi
+
+git interpret-trailers --if-exists doNothing --trailer \
+  "Signed-off-by: $NAME <$EMAIL>" \
+  --in-place "$1"
+
+"#;
 
 /// Install a Git commit message hook that verifies
 /// that all commits have been signed off.
@@ -27,13 +61,12 @@ impl Cli {
 
         command.check_run()?;
 
-        // Copy the script scripts/signoff-git-hook.sh to the
-        // .git/hooks/commit-msg file
         if mode == "all" || mode == "signoff" {
-            command = Command::new("cp");
-            command.args(["scripts/signoff-git-hook.sh", &format!("{git_dir}/hooks/commit-msg")]);
-            command.check_run()?;
-            println!("Copied scripts/signoff-git-hook.sh to {git_dir}/hooks/commit-msg");
+            let hook_path = Path::new(&git_dir).join("hooks").join("commit-msg");
+            let mut file = File::create(hook_path)?;
+            file.write_all(SIGNOFF_HOOK.as_bytes())?;
+            file.metadata()?.permissions().set_mode(0o755);
+            println!("Created signoff script in {git_dir}/hooks/commit-msg");
         }
         Ok(())
     }
