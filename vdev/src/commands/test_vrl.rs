230ebe6bafe57603f60ecaf517a3255bc61ca075
jonathanpv <jonpad512@gmail.com>
2023-03-02 02:40:04 -0500

    Add test-vrl command to vdev

---
+++ vdev/src/commands/test_vrl.rs
@@ -0,0 +1,24 @@
+use anyhow::{Context as _, Result};
+
+use crate::{app};
+use std::{env, path::PathBuf};
+
+/// Run the Vector Remap Language test suite
+#[derive(clap::Args, Debug)]
+#[command()]
+pub struct Cli {}
+
+impl Cli {
+    pub fn exec(self) -> Result<()> {
+        let path: PathBuf = [app::path(), "lib", "vrl", "tests"].into_iter().collect();
+        let _ = env::set_current_dir(path).context("Could not change directory");
+
+        #[allow(clippy::case_sensitive_file_extension_comparisons)]
+        let res = app::exec(
+            "cargo",
+            ["run", "--", "--runtime=ast"].into_iter(),
+            false,
+        );
+        res
+    }
+}
