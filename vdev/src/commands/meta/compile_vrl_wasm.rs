fe3a9e8f7411e72966de7526d086a22a84eb8ebd
jonathanpv <jonpad512@gmail.com>
2023-03-09 16:51:44 -0500

    chore(vdev): Rewrite compile-vrl-wasm.sh to vdev

---
+++ vdev/src/commands/meta/compile_vrl_wasm.rs
@@ -0,0 +1,36 @@
+use std::path::Path;
+use anyhow::Result;
+use clap::Args;
+
+use crate::app;
+
+/// Compiles VRL crates to wasm32-unknown-unknown
+#[derive(Args, Debug)]
+#[command()]
+pub struct Cli {}
+
+impl Cli {
+    pub fn exec(self) -> Result<()> {
+        let vrl_path = Path::new(app::path()).join("lib").join("vrl");
+        // Can't use cargo vdev build because it defaults to Error: command: "cargo" "build" "--no-default-features" "--release" "--features" "default" "--target" "wasm32-unknown-unknown"
+        // Notice the --features "default", some of the crates below don't have default specified
+        // Causing the error: none of the selected packages contains these features: default
+        let args = vec!["build", "--release", "--target", "wasm32-unknown-unknown"];
+        println!("Compiling lib/vrl/compiler to wasm32-unknown-unknown");
+        std::env::set_current_dir(vrl_path.join("compiler"))?;
+        app::exec("cargo", &args, false)?;
+
+        println!("Compiling lib/vrl/core to wasm32-unknown-unknown");
+        std::env::set_current_dir(vrl_path.join("core"))?;
+        app::exec("cargo", &args, false)?;
+
+        println!("Compiling lib/vrl/diagnostic to wasm32-unknown-unknown");
+        std::env::set_current_dir(vrl_path.join("diagnostic"))?;
+        app::exec("cargo", &args, false)?;
+
+        println!("Compiling lib/vrl/parser to wasm32-unknown-unknown");
+        std::env::set_current_dir(vrl_path.join("parser"))?;
+        app::exec("cargo", &args, false)?;
+        Ok(())
+    }
+}
--- vdev/src/commands/meta/mod.rs
+++ vdev/src/commands/meta/mod.rs
@@ -1,5 +1,6 @@
 crate::cli_subcommands! {
     "Collection of meta-utilities..."
+    mod compile_vrl_wasm,
     mod starship,
     mod install_git_hooks,
 }
