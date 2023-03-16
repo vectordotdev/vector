d2b0fe79673d04cb04fa28038594b20d96862feb
neuronull <neuronull@pm.me>
2023-03-16 09:06:27 -0600

    improve comment

--- scripts/package-msi.sh
+++ scripts/package-msi.sh
@@ -18,12 +18,15 @@ pushd target/msi-x64
 # shellcheck disable=SC2016
 powershell '$progressPreference = "silentlyContinue"; Expand-Archive vector-'"$ARCHIVE_VERSION"'-x86_64-pc-windows-msvc.zip'
 
-# building the MSI package requires the version to be purely numerical (eg 0.0.0),
+# Building the MSI package requires the version to be purely numerical (eg 0.0.0),
 # which is not the case if with custom build workflow.
+# This specifically works around the following issue:
+#     C:\a\vector\vector\target\msi-x64\vector.wxs(6) : error CNDL0108 : The Product/@Version attribute's value, '0.29.0.custom.a28ecdc', is not a valid version.
+#     Legal version values should look like 'x.x.x.x' where x is an integer from 0 to 65534.
+# , by  changing "0.29.0.custom.a28ecdc" -> "0.29.0".
 CHANNEL="${CHANNEL:-"$(cargo vdev release channel)"}"
 
 if [[ "$CHANNEL" == "custom" ]]; then
-    # "0.29.0.custom.a28ecdc" -> "0.29.0"
     PACKAGE_VERSION="${ARCHIVE_VERSION%.custom*}"
 else
     PACKAGE_VERSION="${ARCHIVE_VERSION}"
