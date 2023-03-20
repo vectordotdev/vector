9cbb4911b9d74cd7d939b35cafe79c4d00a33c0a
Stephen Wakely <fungus.humungus@gmail.com>
2023-03-20 13:13:33 +0000

    fix(syslog): add `source_ip` to `syslog` schema (#16837)
    * Add source_ip to syslog schema
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>
    
    * source_ip field is only added by source not codec
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>
    
    * Add source_ip field to tests
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>
    
    ---------
    
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sources/syslog.rs
+++ src/sources/syslog.rs
@@ -522,6 +522,11 @@ mod test {
                     Some("host"),
                 )
                 .with_metadata_field(
+                    &owned_value_path!("syslog", "source_ip"),
+                    Kind::bytes().or_undefined(),
+                    None,
+                )
+                .with_metadata_field(
                     &owned_value_path!("syslog", "severity"),
                     Kind::bytes().or_undefined(),
                     Some("severity"),
@@ -594,6 +599,11 @@ mod test {
             &owned_value_path!("hostname"),
             Kind::bytes().or_undefined(),
             Some("host"),
+        )
+        .with_event_field(
+            &owned_value_path!("source_ip"),
+            Kind::bytes().or_undefined(),
+            None,
         )
         .with_event_field(
             &owned_value_path!("severity"),
