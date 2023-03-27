af88729c2adbe3788cd5cb54249799ec58d12878
Tom de Bruijn <tom@tomdebruijn.com>
2023-03-27 15:43:23 +0200

    Add basic AppSignal module documentation

--- src/sinks/appsignal/mod.rs
+++ src/sinks/appsignal/mod.rs
@@ -1,3 +1,11 @@
+//! The AppSignal sink
+//!
+//! This sink provides downstream support for `AppSignal` to collect logs and a subset of Vector
+//! metric types. These events are sent to the `appsignal-endpoint.net` domain, which is part of
+//! the `appsignal.com` infrastructure.
+//!
+//! Logs and metrics are stored on an per app basis are require an app-level Push API key.
+
 #[cfg(all(test, feature = "appsignal-integration-tests"))]
 mod integration_tests;
 
