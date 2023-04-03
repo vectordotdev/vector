3cfc6336656dee927786aad525d91abe3265571b
May Lee <may.lee@datadoghq.com>
2023-04-03 17:49:57 -0400

    clarify kafka

--- src/kafka.rs
+++ src/kafka.rs
@@ -58,8 +58,10 @@ pub struct KafkaSaslConfig {
     /// Enables SASL authentication.
     ///
     /// Only `PLAIN`- and `SCRAM`-based mechanisms are supported when configuring SASL authentication using `sasl.*`. For
-    /// other mechanisms, `librdkafka_options.*` must be used directly to configure other `librdkafka`-specific values
-    /// for example, `sasl.kerberos.*`.
+    /// other mechanisms, `librdkafka_options.*` must be used directly to configure other `librdkafka`-specific values.
+    /// If using `sasl.kerberos.*` as an example, where `*` is `service.name`, `principal`, `kinit.md`, etc., then
+    /// `librdkafka_options.*` as a result becomes `librdkafka_options.sasl.kerberos.service.name`,
+    /// `librdkafka_options.sasl.kerberos.principal`, etc.
     ///
     /// See the [librdkafka documentation](https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md) for details.
     ///
