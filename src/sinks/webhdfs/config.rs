730c9386f66b6348c64a268ef37e752343d8fb9a
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2023-04-14 15:56:20 -0400

    docs: Adjust doc comment locations (#17154)

--- src/sinks/webhdfs/config.rs
+++ src/sinks/webhdfs/config.rs
@@ -21,38 +21,6 @@ use crate::{
 };
 
 /// Configuration for the `webhdfs` sink.
-///
-/// The Hadoop Distributed File System (HDFS) is a distributed file system
-/// designed to run on commodity hardware. HDFS consists of a namenode and a
-/// datanode. We will send rpc to namenode to know which datanode to send
-/// and receive data to. Also, HDFS will rebalance data across the cluster
-/// to make sure each file has enough redundancy.
-///
-/// ```txt
-///                     ┌───────────────┐
-///                     │  Data Node 2  │
-///                     └───────────────┘
-///                             ▲
-/// ┌───────────────┐           │            ┌───────────────┐
-/// │  Data Node 1  │◄──────────┼───────────►│  Data Node 3  │
-/// └───────────────┘           │            └───────────────┘
-///                     ┌───────┴───────┐
-///                     │   Name Node   │
-///                     └───────────────┘
-///                             ▲
-///                             │
-///                      ┌──────┴─────┐
-///                      │   Vector   │
-///                      └────────────┘
-/// ```
-///
-/// WebHDFS will connect to the HTTP RESTful API of HDFS.
-///
-/// For more information, please refer to:
-///
-/// - [HDFS Users Guide](https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/HdfsUserGuide.html)
-/// - [WebHDFS REST API](https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/WebHDFS.html)
-/// - [opendal::services::webhdfs](https://docs.rs/opendal/latest/opendal/services/struct.Webhdfs.html)
 #[configurable_component(sink("webhdfs"))]
 #[derive(Clone, Debug)]
 #[serde(deny_unknown_fields)]
