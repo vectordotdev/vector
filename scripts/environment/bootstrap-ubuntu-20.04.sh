4b80c714b68bb9acc2869c48b71848d11954c6aa
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2023-04-28 19:58:15 -0500

    chore(dev): Install the correct `mold` based on CPU architecture (#17248)
    Fixes: #17185
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/environment/bootstrap-ubuntu-20.04.sh
+++ scripts/environment/bootstrap-ubuntu-20.04.sh
@@ -133,7 +133,7 @@ if [ -z "${DISABLE_MOLD:-""}" ] ; then
     # first when trying to load the shared object, so we can dodge having to care about the "right" lib folder to put it in.
     TEMP=$(mktemp -d)
     MOLD_VERSION=1.2.1
-    MOLD_TARGET=mold-${MOLD_VERSION}-x86_64-linux
+    MOLD_TARGET=mold-${MOLD_VERSION}-$(uname -m)-linux
     curl -fsSL "https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/${MOLD_TARGET}.tar.gz" \
         --output "$TEMP/${MOLD_TARGET}.tar.gz"
     tar \
