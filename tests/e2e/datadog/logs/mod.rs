62211ab1a791b47d934733c60a4d4af6d22fe3c2
neuronull <neuronull@pm.me>
2023-05-18 14:18:40 -0600

    reorg

--- tests/e2e/datadog/logs/mod.rs
+++ tests/e2e/datadog/logs/mod.rs
@@ -1,62 +1,13 @@
 use indoc::indoc;
-use reqwest::{Client, Method};
-use serde::Deserialize;
 use tokio::sync::mpsc;
 
-use vector::{config::ConfigBuilder, test_util::start_topology, topology::RunningTopology};
+use vector::{
+    config::ConfigBuilder,
+    test_util::{start_topology, trace_init},
+    topology::RunningTopology,
+};
 
-fn vector_receive_port() -> u16 {
-    std::env::var("VECTOR_RECEIVE_PORT")
-        .unwrap_or_else(|_| "8081".to_string())
-        .parse::<u16>()
-        .unwrap()
-}
-
-fn fake_intake_vector_endpoint() -> String {
-    std::env::var("FAKE_INTAKE_VECTOR_ENDPOINT")
-        .unwrap_or_else(|_| "http://127.0.0.1:8082".to_string())
-}
-
-fn fake_intake_agent_endpoint() -> String {
-    std::env::var("FAKE_INTAKE_AGENT_ENDPOINT")
-        .unwrap_or_else(|_| "http://127.0.0.1:8083".to_string())
-}
-
-// Fakeintake response
-#[derive(Deserialize, Debug)]
-struct Payloads {
-    payloads: Vec<Payload>,
-}
-
-#[allow(dead_code)]
-#[derive(Deserialize, Debug)]
-struct Payload {
-    // base64 encoded
-    data: String,
-    encoding: String,
-    timestamp: String,
-}
-
-async fn get_fakeintake_payloads(base: &str, endpoint: &str) -> Payloads {
-    let url = format!("{}/fakeintake/payloads?endpoint={}", base, endpoint,);
-
-    Client::new()
-        .request(Method::GET, &url)
-        .send()
-        .await
-        .unwrap_or_else(|_| panic!("Sending GET request to {} failed", &url))
-        .json::<Payloads>()
-        .await
-        .expect("Parsing fakeintake payloads failed")
-}
-
-async fn get_payloads_agent() -> Payloads {
-    get_fakeintake_payloads(&fake_intake_agent_endpoint(), "TODO logs endpoint").await
-}
-
-async fn get_payloads_vector() -> Payloads {
-    get_fakeintake_payloads(&fake_intake_vector_endpoint(), "TODO logs endpoint").await
-}
+use super::*;
 
 async fn start_vector() -> (
     RunningTopology,
@@ -96,14 +47,17 @@ async fn start_vector() -> (
 
 #[tokio::test]
 async fn test_logs() {
+    trace_init();
+
     println!("foo test");
 
     // panics if vector errors during startup
     let (_topology, _shutdown) = start_vector().await;
 
-    let _agent_payloads = get_payloads_agent().await;
+    let logs_endpoint = "/api/v2/logs";
+    let _agent_payloads = get_payloads_agent(&logs_endpoint).await;
 
-    let _vector_payloads = get_payloads_vector().await;
+    let _vector_payloads = get_payloads_vector(&logs_endpoint).await;
 
     assert!(true);
 }
--- tests/e2e/datadog/mod.rs
+++ tests/e2e/datadog/mod.rs
@@ -1,1 +1,59 @@
 pub mod logs;
+
+
+use reqwest::{Client, Method};
+use serde::Deserialize;
+
+
+fn vector_receive_port() -> u16 {
+    std::env::var("VECTOR_RECEIVE_PORT")
+        .unwrap_or_else(|_| "8081".to_string())
+        .parse::<u16>()
+        .unwrap()
+}
+
+fn fake_intake_vector_endpoint() -> String {
+    std::env::var("FAKE_INTAKE_VECTOR_ENDPOINT")
+        .unwrap_or_else(|_| "http://127.0.0.1:8082".to_string())
+}
+
+fn fake_intake_agent_endpoint() -> String {
+    std::env::var("FAKE_INTAKE_AGENT_ENDPOINT")
+        .unwrap_or_else(|_| "http://127.0.0.1:8083".to_string())
+}
+
+// Fakeintake response
+#[derive(Deserialize, Debug)]
+struct Payloads {
+    payloads: Vec<Payload>,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+struct Payload {
+    // base64 encoded
+    data: String,
+    encoding: String,
+    timestamp: String,
+}
+
+async fn get_fakeintake_payloads(base: &str, endpoint: &str) -> Payloads {
+    let url = format!("{}/fakeintake/payloads?endpoint={}", base, endpoint,);
+
+    Client::new()
+        .request(Method::GET, &url)
+        .send()
+        .await
+        .unwrap_or_else(|_| panic!("Sending GET request to {} failed", &url))
+        .json::<Payloads>()
+        .await
+        .expect("Parsing fakeintake payloads failed")
+}
+
+async fn get_payloads_agent(endpoint: &str) -> Payloads {
+    get_fakeintake_payloads(&fake_intake_agent_endpoint(), endpoint).await
+}
+
+async fn get_payloads_vector(endpoint: &str) -> Payloads {
+    get_fakeintake_payloads(&fake_intake_vector_endpoint(), endpoint).await
+}
