6cfccf659f854a98c2188343bc600af9233304a0
Stephen Wakely <fungus.humungus@gmail.com>
2023-05-26 21:32:51 +0100

    Fix merge errors
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/statsd/service.rs
+++ src/sinks/statsd/service.rs
@@ -49,7 +49,7 @@ impl DriverResponse for StatsdResponse {
     fn events_sent(&self) -> CountByteSize {
         CountByteSize(
             self.metadata.event_count(),
-            self.metadata.events_byte_size(),
+            self.metadata.events_estimated_json_encoded_byte_size(),
         )
     }
 
