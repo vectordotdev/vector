257ff068325cc8b85679830748725a3689963077
Stephen Wakely <fungus.humungus@gmail.com>
2023-05-30 17:44:58 +0100

    Try inlining JsonSize functions
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- lib/vector-common/src/json_size.rs
+++ lib/vector-common/src/json_size.rs
@@ -19,32 +19,39 @@ impl fmt::Display for JsonSize {
 impl Sub for JsonSize {
     type Output = JsonSize;
 
-    fn sub(self, rhs: Self) -> Self::Output {
-        JsonSize(self.0 - rhs.0)
+    #[inline]
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        self.0 = self.0 - rhs.0;
+        self
     }
 }
 
 impl Add for JsonSize {
     type Output = JsonSize;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        JsonSize(self.0 + rhs.0)
+    #[inline]
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self.0 = self.0 + rhs.0;
+        self
     }
 }
 
 impl AddAssign for JsonSize {
+    #[inline]
     fn add_assign(&mut self, rhs: Self) {
-        *self = JsonSize::new(self.0 + rhs.0);
+        self.0 += rhs.0;
     }
 }
 
 impl Sum for JsonSize {
+    #[inline]
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(JsonSize(0), |a, b| a + b)
+        iter.fold(JsonSize::zero(), |a, b| a + b)
     }
 }
 
 impl From<usize> for JsonSize {
+    #[inline]
     fn from(value: usize) -> Self {
         Self(value)
     }
@@ -53,18 +60,21 @@ impl From<usize> for JsonSize {
 impl JsonSize {
     /// Create a new instance with the specified size.
     #[must_use]
+    #[inline]
     pub const fn new(size: usize) -> Self {
         Self(size)
     }
 
     /// Create a new instance with size 0.
     #[must_use]
+    #[inline]
     pub const fn zero() -> Self {
         Self(0)
     }
 
     /// Returns the contained size.
     #[must_use]
+    #[inline]
     pub fn get(&self) -> usize {
         self.0
     }
@@ -76,12 +86,14 @@ pub struct NonZeroJsonSize(JsonSize);
 
 impl NonZeroJsonSize {
     #[must_use]
+    #[inline]
     pub fn new(size: JsonSize) -> Option<Self> {
         (size.0 > 0).then_some(NonZeroJsonSize(size))
     }
 }
 
 impl From<NonZeroJsonSize> for JsonSize {
+    #[inline]
     fn from(value: NonZeroJsonSize) -> Self {
         value.0
     }
