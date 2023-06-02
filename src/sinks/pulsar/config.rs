3395cfdb90b165653dda7e9014057aac1dba2d28
neuronull <neuronull@pm.me>
2023-06-02 15:19:13 -0600

    chore(deps): bump pulsar from 5.1.1 to 6.0.0 (#17587)
    - Bumps [pulsar](https://github.com/streamnative/pulsar-rs) from 5.1.1
    to 6.0.0.
    - As part of this, a new config option for batch `max_bytes` is exposed
    for the sink.

--- src/sinks/pulsar/config.rs
+++ src/sinks/pulsar/config.rs
@@ -87,13 +87,17 @@ pub struct PulsarSinkConfig {
 #[configurable_component]
 #[derive(Clone, Copy, Debug, Default)]
 pub(crate) struct PulsarBatchConfig {
-    /// The maximum size of a batch before it is flushed.
+    /// The maximum amount of events in a batch before it is flushed.
     ///
     /// Note this is an unsigned 32 bit integer which is a smaller capacity than
     /// many of the other sink batch settings.
     #[configurable(metadata(docs::type_unit = "events"))]
     #[configurable(metadata(docs::examples = 1000))]
     pub max_events: Option<u32>,
+
+    /// The maximum size of a batch before it is flushed.
+    #[configurable(metadata(docs::type_unit = "bytes"))]
+    pub max_bytes: Option<usize>,
 }
 
 /// Authentication configuration.
@@ -235,6 +239,7 @@ impl PulsarSinkConfig {
             metadata: Default::default(),
             schema: None,
             batch_size: self.batch.max_events,
+            batch_byte_size: self.batch.max_bytes,
             compression: None,
         };
 
