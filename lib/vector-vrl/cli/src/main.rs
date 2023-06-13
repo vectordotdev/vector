2c354edf8e5cc9e5da8c4dbfc3721425d24d4e9d
Dominic Burkart <dominic.burkart@datadoghq.com>
2023-06-13 15:33:21 +0200

    revert cli changes as the exit code is already propagated

--- lib/vector-vrl/cli/src/main.rs
+++ lib/vector-vrl/cli/src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
 use vrl::cli::{cmd::cmd, Opts};
-use std::process::ExitCode;
 
-fn main() -> ExitCode {
+fn main() {
     let mut functions = vrl::stdlib::all();
     functions.extend(vector_vrl_functions::all());
 
-    cmd(&Opts::parse(), functions)
+    std::process::exit(cmd(&Opts::parse(), functions));
 }
