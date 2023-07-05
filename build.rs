95167bb37fb44c61460b78fcd0832ea2848d45d0
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-07-05 16:15:28 -0600

    Fix use of `PathBuf` in `build.rs`

--- build.rs
+++ build.rs
@@ -130,7 +130,7 @@ fn main() {
         // in a type-safe way, which is necessary for incrementally building certain payloads, like
         // the ones generated in the `datadog_metrics` sink.
         let protobuf_fds_path =
-            PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
+            Path::new(&std::env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
                 .join("protobuf-fds.bin");
 
         let mut prost_build = prost_build::Config::new();
