e43afe687fb502a5efbc5fad22d14e6cb0db22cc
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2023-07-05 08:29:37 -0700

    chore(ci): Set HOMEBREW_NO_INSTALL_FROM_API in CI
    We seem to be hitting https://github.com/Homebrew/homebrew-cask/issues/150323 which recommends this
    workaround.
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/environment/bootstrap-macos-10.sh
+++ scripts/environment/bootstrap-macos-10.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 set -e -o verbose
 
+# https://github.com/Homebrew/homebrew-cask/issues/150323
+unset HOMEBREW_NO_INSTALL_FROM_API
+
 brew update
 
 brew install ruby@2.7 coreutils cue-lang/tap/cue protobuf
