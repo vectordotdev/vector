bc1b83ad51a5118aa6a7c3cab62dfb5eb3ce2c91
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-07-06 11:48:37 -0600

    bug(lua transform): Emit events with the `source_id` set (#17870)
    By default, the `lua` transform creates output events with no source id.
    This is, however, the only component that does that. This change causes
    all output events to have a source ID:
    
    1. All events resulting from processing an event (either the default v1
    path or the v2 `process` hook) will copy the source ID from the incoming
    event.
    2. All other events will set the source ID to the fixed string `lua`.

--- build.rs
+++ build.rs
@@ -1,11 +1,4 @@
-use std::{
-    collections::HashSet,
-    env,
-    fs::File,
-    io::Write,
-    path::{Path, PathBuf},
-    process::Command,
-};
+use std::{collections::HashSet, env, fs::File, io::Write, path::Path, process::Command};
 
 struct TrackedEnv {
     tracked: HashSet<String>,
@@ -137,7 +130,7 @@ fn main() {
         // in a type-safe way, which is necessary for incrementally building certain payloads, like
         // the ones generated in the `datadog_metrics` sink.
         let protobuf_fds_path =
-            PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
+            Path::new(&std::env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
                 .join("protobuf-fds.bin");
 
         let mut prost_build = prost_build::Config::new();
