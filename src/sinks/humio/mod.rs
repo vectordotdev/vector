b8b81f3b13ffc746d75b2a5cb632afb5ba95e894
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2023-07-17 13:19:29 -0400

    Update src/sinks/humio/mod.rs
    Co-authored-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/humio/mod.rs
+++ src/sinks/humio/mod.rs
@@ -2,5 +2,5 @@ pub mod logs;
 pub mod metrics;
 
 fn host_key() -> String {
-    crate::config::log_schema().host_key().unwrap().to_string()
+    crate::config::log_schema().host_key().unwrap_or_default().to_string()
 }
