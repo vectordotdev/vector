3d2e206cd531280fbd4b8a07143a264d41cf4098
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2023-07-24 07:37:18 -0700

    Try updating llvm
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -9,19 +9,8 @@ apt-get install -y \
   gnupg \
   wget
 
-# we need LLVM >= 3.9 for onig_sys/bindgen
-
-cat <<-EOF > /etc/apt/sources.list.d/llvm.list
-deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
-deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
-EOF
-
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key| apt-key add -
-
-apt-get update
-
 # needed by onig_sys
 apt-get install -y \
-      libclang1-9 \
-      llvm-9 \
+      libclang1-14 \
+      llvm-14 \
       unzip
