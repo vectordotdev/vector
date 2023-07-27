2f3cf337d2aead383656d5077fcc046fc6bc189c
Matt <sjoeboo@users.noreply.github.com>
2023-07-26 22:28:09 -0400

    Update src/sinks/gcp/pubsub.rs
    Co-authored-by: Stephen Wakely <stephen@lisp.space>

--- src/sinks/gcp/pubsub.rs
+++ src/sinks/gcp/pubsub.rs
@@ -329,7 +329,6 @@ mod integration_tests {
         for i in 0..input.len() {
             let data = messages[i].message.decode_data();
             let data = serde_json::to_value(data).unwrap();
-            //let expected = serde_json::to_value(input[i].as_metric());
             let expected = serde_json::to_value(input[i].as_metric()).unwrap();
             assert_eq!(data, expected);
         }
