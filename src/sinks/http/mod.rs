3fc23a1cf9f86e1466a00148ca21993a5e990859
neuronull <neuronull@pm.me>
2023-08-09 15:14:28 -0600

    docs touchup

--- src/sinks/http/mod.rs
+++ src/sinks/http/mod.rs
@@ -1,4 +1,4 @@
-//! The HTTP [`vector_core::sink::VectorSink`]
+//! The HTTP [`vector_core::sink::VectorSink`].
 //!
 //! This module contains the [`vector_core::sink::VectorSink`] instance that is responsible for
 //! taking a stream of [`vector_core::event::Event`]s and forwarding them to an HTTP server.
