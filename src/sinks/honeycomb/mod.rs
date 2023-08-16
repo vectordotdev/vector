17198f2d575f6b5ca7fb7da134b8b5dbec03afd4
neuronull <neuronull@pm.me>
2023-08-16 17:19:47 -0600

    doc

--- src/sinks/honeycomb/mod.rs
+++ src/sinks/honeycomb/mod.rs
@@ -1,4 +1,7 @@
-//! TODO
+//! The Honeycomb [`vector_core::sink::VectorSink`].
+//!
+//! This module contains the [`vector_core::sink::VectorSink`] instance that is responsible for
+//! taking a stream of [`vector_core::event::Event`]s and forwarding them to the Honeycomb service.
 
 mod config;
 mod encoder;
