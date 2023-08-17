80f01df7076698bf92c99e9ef949e3c91099e72d
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2023-08-17 11:24:45 -0700

    Fix input type determination
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- src/sinks/websocket/config.rs
+++ src/sinks/websocket/config.rs
@@ -94,7 +94,7 @@ impl SinkConfig for WebSocketSinkConfig {
     }
 
     fn input(&self) -> Input {
-        Input::new(self.encoding.config().1.input_type())
+        Input::new(self.encoding.config().input_type())
     }
 
     fn acknowledgements(&self) -> &AcknowledgementsConfig {
