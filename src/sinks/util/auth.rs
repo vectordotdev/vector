e2672a94c91ea7210cf8a22ce621def0ff6de819
suikammd <suikalala@gmail.com>
2023-08-25 23:53:14 +0800

    modify auth aws feature

--- src/sinks/util/auth.rs
+++ src/sinks/util/auth.rs
@@ -1,14 +1,9 @@
-#[cfg(feature = "aws-core")]
-use aws_credential_types::provider::SharedCredentialsProvider;
-#[cfg(feature = "aws-core")]
-use aws_types::region::Region;
-
 #[derive(Debug, Clone)]
 pub enum Auth {
     Basic(crate::http::Auth),
     #[cfg(feature = "aws-core")]
     Aws {
-        credentials_provider: SharedCredentialsProvider,
-        region: Region,
+        credentials_provider: aws_credential_types::provider::SharedCredentialsProvider,
+        region: aws_types::region::Region,
     },
 }
