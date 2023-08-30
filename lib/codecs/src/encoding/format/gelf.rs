7d5f2b3015e2f004185b622bab54f4bf8e7ad91c
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-08-30 13:15:17 -0600

    Bump `chrono` to 0.4.27

--- lib/codecs/src/encoding/format/gelf.rs
+++ lib/codecs/src/encoding/format/gelf.rs
@@ -243,7 +243,7 @@ mod tests {
     use crate::encoding::SerializerConfig;
 
     use super::*;
-    use chrono::{DateTime, NaiveDateTime, Utc};
+    use chrono::NaiveDateTime;
     use vector_core::event::{Event, EventMetadata};
     use vrl::btreemap;
     use vrl::value::Value;
@@ -347,7 +347,7 @@ mod tests {
         {
             let naive_dt =
                 NaiveDateTime::parse_from_str("1970-01-01 00:00:00.1", "%Y-%m-%d %H:%M:%S%.f");
-            let dt = DateTime::<Utc>::from_utc(naive_dt.unwrap(), Utc);
+            let dt = naive_dt.unwrap().and_utc();
 
             let event_fields = btreemap! {
                 VERSION => "1.1",
@@ -365,7 +365,7 @@ mod tests {
         {
             let naive_dt =
                 NaiveDateTime::parse_from_str("1970-01-01 00:00:00.0", "%Y-%m-%d %H:%M:%S%.f");
-            let dt = DateTime::<Utc>::from_utc(naive_dt.unwrap(), Utc);
+            let dt = naive_dt.unwrap().and_utc();
 
             let event_fields = btreemap! {
                 VERSION => "1.1",
