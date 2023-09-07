d3bc82452393a031bbbc9ab2f568dcbd0ff53f89
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2023-09-07 12:42:00 -0400

    add hyperlinks for versions and address nits

--- lib/vector-vrl/web-playground/build.rs
+++ lib/vector-vrl/web-playground/build.rs
@@ -21,9 +21,9 @@ fn get_vector_toml_path() -> PathBuf {
 
 fn get_git_hash() -> String {
     let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
-        .unwrap();
+        .expect("Failed to get git HEAD sha");
     let mut git_hash = String::from_utf8(output.stdout).unwrap();
     git_hash.truncate(8);
     git_hash
