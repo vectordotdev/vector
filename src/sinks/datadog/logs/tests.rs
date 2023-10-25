dea6fc82b28592ab7aac55e56fb185761e9aee1e
Stephen Wakely <fungus.humungus@gmail.com>
2023-10-25 16:11:10 +0100

    Test local setting can override the global one
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/sinks/datadog/logs/tests.rs
+++ src/sinks/datadog/logs/tests.rs
@@ -571,3 +571,44 @@ async fn global_options() {
         .iter()
         .all(|value| value.to_str().unwrap() == "globalkey"));
 }
+
+#[tokio::test]
+async fn override_global_options() {
+    let config = indoc! {r#"
+            default_api_key = "localkey"
+            compression = "none"
+        "#};
+
+    // Set a global key option, which should be overridden by the option in the component configuration.
+    let mut cx = SinkContext::default();
+    cx.datadog.api_key = Some("globalkey".to_string().into());
+    let (mut config, cx) = load_sink_with_context::<DatadogLogsConfig>(config, cx).unwrap();
+
+    let addr = next_addr();
+    // Swap out the endpoint so we can force send it
+    // to our local server
+    let endpoint = format!("http://{}", addr);
+    config.dd_common.endpoint = Some(endpoint.clone());
+
+    let (sink, _) = config.build(cx).await.unwrap();
+
+    let (rx, _trigger, server) = test_server(addr, ApiStatus::OKv1);
+    tokio::spawn(server);
+
+    let (batch, receiver) = BatchNotifier::new_with_receiver();
+    let (_expected, events) = random_lines_with_stream(100, 10, Some(batch));
+
+    run_and_assert_sink_compliance(sink, events, &SINK_TAGS).await;
+
+    assert_eq!(receiver.await, BatchStatus::Delivered);
+
+    let keys = rx
+        .take(1)
+        .map(|r| r.0.headers.get("DD-API-KEY").unwrap().clone())
+        .collect::<Vec<_>>()
+        .await;
+
+    assert!(keys
+        .iter()
+        .all(|value| value.to_str().unwrap() == "localkey"));
+}
