459d2594c34eccfaf1d3590d5e834e60769a98e1
Bruce Guenter <bruce.guenter@datadoghq.com>
2023-10-27 09:38:02 -0600

    chore(dev): Add wrapper for `vector-api-client` in `vector-lib`

--- lib/vector-lib/src/lib.rs
+++ lib/vector-lib/src/lib.rs
@@ -2,6 +2,8 @@ pub use codecs;
 pub use enrichment;
 #[cfg(feature = "file-source")]
 pub use file_source;
+#[cfg(feature = "api-client")]
+pub use vector_api_client as api_client;
 pub use vector_buffers as buffers;
 pub use vector_common::{
     assert_event_data_eq, btreemap, byte_size_of, byte_size_of::ByteSizeOf, conversion,
