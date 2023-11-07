536a9e23b763c2b9e6e322a60af214e08efa0f2a
Doug Smith <doug.smith3197@gmail.com>
2023-11-07 17:36:38 -0500

    fix(throttle transform): replace metric with unbounded cardinality with a debug log

--- src/internal_events/throttle.rs
+++ src/internal_events/throttle.rs
@@ -1,4 +1,3 @@
-use metrics::counter;
 use vector_lib::internal_event::{ComponentEventsDropped, InternalEvent, INTENTIONAL};
 
 #[derive(Debug)]
@@ -8,23 +7,12 @@ pub(crate) struct ThrottleEventDiscarded {
 
 impl InternalEvent for ThrottleEventDiscarded {
     fn emit(self) {
-        // TODO: Technically, the Component Specification states that the discarded events metric
-        // must _only_ have the `intentional` tag, in addition to the core tags like
-        // `component_kind`, etc, and nothing else.
-        //
-        // That doesn't give us the leeway to specify which throttle bucket the events are being
-        // discarded for... but including the key/bucket as a tag does seem useful and so I wonder
-        // if we should change the specification wording? Sort of a similar situation to the
-        // `error_code` tag for the component errors metric, where it's meant to be optional and
-        // only specified when relevant.
-        counter!(
-            "events_discarded_total", 1,
-            "key" => self.key,
-        ); // Deprecated.
+        let message = "Rate limit exceeded.";
+        debug!(message, key = self.key, internal_log_rate_limit = true);
 
         emit!(ComponentEventsDropped::<INTENTIONAL> {
             count: 1,
-            reason: "Rate limit exceeded."
+            reason: message
         })
     }
 }
