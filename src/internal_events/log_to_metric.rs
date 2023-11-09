6aded12c524ed48a9fd6e3ba25bf71b77ffd8669
Stephan Steward <stephan.steward@riotgames.com>
2023-11-09 17:25:50 -0600

    Convert type errors to single error with type.

--- src/internal_events/log_to_metric.rs
+++ src/internal_events/log_to_metric.rs
@@ -90,24 +90,25 @@ impl<'a> InternalEvent for MetricMetadataInvalidFieldValueError<'a> {
     }
 }
 
-pub struct MetricMetadataParseFloatError<'a> {
+pub struct MetricMetadataParseError<'a> {
     pub field: &'a str,
+    pub kind: &'a str,
 }
 
-impl<'a> InternalEvent for MetricMetadataParseFloatError<'a> {
+impl<'a> InternalEvent for MetricMetadataParseError<'a> {
     fn emit(self) {
         let reason = "Failed to parse field as float.";
         error!(
             message = reason,
             field = %self.field,
-            error_code = "failed_parsing_float",
+            error_code = format!("failed_parsing_{}", self.kind),
             error_type = error_type::PARSER_FAILED,
             stage = error_stage::PROCESSING,
             internal_log_rate_limit = true
         );
         counter!(
             "component_errors_total", 1,
-            "error_code" => "failed_parsing_float",
+            "error_code" => format!("failed_parsing_{}", self.kind),
             "error_type" => error_type::PARSER_FAILED,
             "stage" => error_stage::PROCESSING,
             "field" => self.field.to_string(),
@@ -117,59 +118,6 @@ impl<'a> InternalEvent for MetricMetadataParseFloatError<'a> {
     }
 }
 
-pub struct MetricMetadataParseIntError<'a> {
-    pub field: &'a str,
-}
-
-impl<'a> InternalEvent for MetricMetadataParseIntError<'a> {
-    fn emit(self) {
-        let reason = "Failed to parse field as int.";
-        error!(
-            message = reason,
-            field = %self.field,
-            error_code = "failed_parsing_int",
-            error_type = error_type::PARSER_FAILED,
-            stage = error_stage::PROCESSING,
-            internal_log_rate_limit = true
-        );
-        counter!(
-            "component_errors_total", 1,
-            "error_code" => "failed_parsing_int",
-            "error_type" => error_type::PARSER_FAILED,
-            "stage" => error_stage::PROCESSING,
-            "field" => self.field.to_string(),
-        );
-
-        emit!(ComponentEventsDropped::<UNINTENTIONAL> { count: 1, reason })
-    }
-}
-
-pub struct MetricMetadataParseArrayError<'a> {
-    pub field: &'a str,
-}
-
-impl<'a> InternalEvent for MetricMetadataParseArrayError<'a> {
-    fn emit(self) {
-        let reason = "Failed to parse field as array.";
-        error!(
-            message = reason,
-            field = %self.field,
-            error_code = "failed_parsing_array",
-            error_type = error_type::PARSER_FAILED,
-            stage = error_stage::PROCESSING,
-            internal_log_rate_limit = true
-        );
-        counter!(
-            "component_errors_total", 1,
-            "error_code" => "failed_parsing_array",
-            "error_type" => error_type::PARSER_FAILED,
-            "stage" => error_stage::PROCESSING,
-            "field" => self.field.to_string(),
-        );
-
-        emit!(ComponentEventsDropped::<UNINTENTIONAL> { count: 1, reason })
-    }
-}
 pub struct MetricMetadataMetricDetailsNotFoundError {}
 
 impl InternalEvent for MetricMetadataMetricDetailsNotFoundError {
