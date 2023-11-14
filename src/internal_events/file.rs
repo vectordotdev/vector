be744f41bf43143bb94eabb0437fc586e870dc9d
Doug Smith <dsmith3197@users.noreply.github.com>
2023-11-14 14:08:14 -0500

    fix(file source, kubernetes_logs source, file sink): make file internal metric tag opt-in (#19145)
    * fix(file source, kubernetes_logs source, file sink): make file internal metric tag opt-in
    
    * update cue
    
    * fix tests

--- src/internal_events/file.rs
+++ src/internal_events/file.rs
@@ -12,16 +12,14 @@ use vector_lib::internal_event::{error_stage, error_type};
 
 /// Configuration of internal metrics for file-based components.
 #[configurable_component]
-#[derive(Clone, Debug, PartialEq, Eq, Derivative)]
-#[derivative(Default)]
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
 #[serde(deny_unknown_fields)]
 pub struct FileInternalMetricsConfig {
     /// Whether or not to include the "file" tag on the component's corresponding internal metrics.
     ///
     /// This is useful for distinguishing between different files while monitoring. However, the tag's
-    /// cardinality is unbounded. This defaults to true for now but will default to false in a future version.
-    #[serde(default = "crate::serde::default_true")]
-    #[derivative(Default(value = "true"))]
+    /// cardinality is unbounded.
+    #[serde(default = "crate::serde::default_false")]
     pub include_file_tag: bool,
 }
 
