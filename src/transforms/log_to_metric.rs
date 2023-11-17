a17c646ae3cf8982f6ce9f9fb50d2fc126bf95ae
Steve Steward <steve@dygfloyd.com>
2023-11-17 13:36:21 -0600

    Update src/transforms/log_to_metric.rs
    Co-authored-by: Nathan Fox <fuchsnj@gmail.com>

--- src/transforms/log_to_metric.rs
+++ src/transforms/log_to_metric.rs
@@ -722,7 +722,7 @@ fn to_metrics(event: &Event) -> Result<Metric, TransformError> {
     if let Some(els) = log.get(event_path!("tags")) {
         if let Some(el) = els.as_object() {
             for (key, value) in el {
-                tags.insert(String::from(key.as_ref()).to_string(), bytes_to_str(value));
+                tags.insert(key.to_string(), bytes_to_str(value));
             }
         }
     }
