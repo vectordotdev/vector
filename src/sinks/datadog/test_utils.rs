62b6ea0670258ca10090e211237cf42d221df406
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2023-12-06 12:24:32 +0200

    fix: unused enum variants

---
+++ src/sinks/datadog/test_utils.rs
@@ -0,0 +1,39 @@
+#![cfg(test)]
+
+use bytes::Bytes;
+use http::status::StatusCode;
+
+use crate::sinks::util::test::build_test_server_status;
+
+// The sink must support v1 and v2 API endpoints which have different codes for
+// signaling status. This enum allows us to signal which API endpoint and what
+// kind of response we want our test to model without getting into the details
+// of exactly what that code is.
+#[cfg(any(feature = "sinks-datadog_log", feature = "sinks-datadog_metrics"))]
+pub(super) enum ApiStatus {
+    OKv1,
+    OKv2,
+    BadRequestv1,
+    BadRequestv2,
+}
+
+#[cfg(any(feature = "sinks-datadog_log", feature = "sinks-datadog_metrics"))]
+pub(super) fn test_server(
+    addr: std::net::SocketAddr,
+    api_status: ApiStatus,
+) -> (
+    futures::channel::mpsc::Receiver<(http::request::Parts, Bytes)>,
+    stream_cancel::Trigger,
+    impl std::future::Future<Output = Result<(), ()>>,
+) {
+    let status = match api_status {
+        ApiStatus::OKv1 => StatusCode::OK,
+        ApiStatus::OKv2 => StatusCode::ACCEPTED,
+        ApiStatus::BadRequestv1 | ApiStatus::BadRequestv2 => StatusCode::BAD_REQUEST,
+    };
+
+    // NOTE: we pass `Trigger` out to the caller even though this suite never
+    // uses it as it being dropped cancels the stream machinery here,
+    // indicating that failures that might not be valid.
+    build_test_server_status(addr, status)
+}
