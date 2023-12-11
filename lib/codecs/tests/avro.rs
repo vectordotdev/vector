62c1747c54134a670192ca2c39bc3455473ec4b7
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2023-12-11 18:57:23 +0100

    fmt

--- lib/codecs/tests/avro.rs
+++ lib/codecs/tests/avro.rs
@@ -19,9 +19,7 @@ use vector_core::{config::LogNamespace, event::Event};
 #[case(false)]
 fn roundtrip_avro_fixtures(
     #[files("tests/data/avro/generated/*.avro")]
-    #[exclude(
-        ".*(date|decimal_var|fixed|time_millis|timestamp_micros|timestamp_millis).avro"
-    )]
+    #[exclude(".*(date|decimal_var|fixed|time_millis|timestamp_micros|timestamp_millis).avro")]
     path: PathBuf,
     #[case] reserialize: bool,
 ) {
