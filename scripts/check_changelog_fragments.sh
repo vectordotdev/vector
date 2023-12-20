9696d1ff62ce8126b16912f9773922689c1c26c8
neuronull <neuronull@pm.me>
2023-12-20 09:04:57 -0700

    check scripts

--- scripts/check_changelog_fragments.sh
+++ scripts/check_changelog_fragments.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # This script is intended to run during CI, however it can be run locally by
 # committing changelog fragments before executing the script. If the script
@@ -16,7 +16,7 @@ fi
 # diff-filter=A lists only added files
 ADDED=$(git diff --name-only --diff-filter=A origin/master ${CHANGELOG_DIR})
 
-if [ $(echo "$ADDED" | grep -c .) -lt 1 ]; then
+if [ "$(echo "$ADDED" | grep -c .)" -lt 1 ]; then
   echo "No changelog fragments detected"
   echo "If no changes  necessitate user-facing explanations, add the GH label 'no-changelog'"
   echo "Otherwise, add changelog fragments to changelog.d/"
@@ -24,7 +24,7 @@ if [ $(echo "$ADDED" | grep -c .) -lt 1 ]; then
 fi
 
 # extract the basename from the file path
-ADDED=$(echo ${ADDED} | xargs -n1 basename)
+ADDED=$(echo "${ADDED}" | xargs -n1 basename)
 
 # validate the fragments
 while IFS= read -r fname; do
@@ -35,7 +35,7 @@ while IFS= read -r fname; do
 
   echo "validating '${fname}'"
 
-  arr=(${fname//./ })
+  IFS="." read -r -a arr <<< "${fname}"
 
   if [ "${#arr[@]}" -ne 3 ]; then
     echo "invalid fragment filename: wrong number of period delimiters. expected '<pr_number>.<fragment_type>.md'. (${fname})"
