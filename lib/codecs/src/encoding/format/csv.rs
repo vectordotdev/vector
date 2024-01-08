8439b55294ed777eaed55d1ba224c9810fd6f60e
neuronull <neuronull@pm.me>
2024-01-08 16:53:06 -0700

    fix issues with lint on ConfigTargetPath

--- lib/codecs/src/encoding/format/csv.rs
+++ lib/codecs/src/encoding/format/csv.rs
@@ -318,7 +318,7 @@ mod tests {
         let mut tree = ObjectMap::new();
 
         for (field_name, field_value) in field_data.into_iter() {
-            let field = ConfigTargetPath::try_from(field_name.to_string()).unwrap();
+            let field = field_name.into();
             fields.push(field);
 
             let field_value = Value::from(field_value.to_string());
@@ -351,15 +351,15 @@ mod tests {
             "other" => Value::from("data"),
         }));
         let fields = vec![
-            ConfigTargetPath::try_from("foo".to_string()).unwrap(),
-            ConfigTargetPath::try_from("int".to_string()).unwrap(),
-            ConfigTargetPath::try_from("comma".to_string()).unwrap(),
-            ConfigTargetPath::try_from("float".to_string()).unwrap(),
-            ConfigTargetPath::try_from("missing".to_string()).unwrap(),
-            ConfigTargetPath::try_from("space".to_string()).unwrap(),
-            ConfigTargetPath::try_from("time".to_string()).unwrap(),
-            ConfigTargetPath::try_from("quote".to_string()).unwrap(),
-            ConfigTargetPath::try_from("bool".to_string()).unwrap(),
+            "foo".into(),
+            "int".into(),
+            "comma".into(),
+            "float".into(),
+            "missing".into(),
+            "space".into(),
+            "time".into(),
+            "quote".into(),
+            "bool".into(),
         ];
 
         let opts = CsvSerializerOptions {
@@ -388,11 +388,11 @@ mod tests {
             "field5" => Value::from("value5"),
         }));
         let fields = vec![
-            ConfigTargetPath::try_from("field1".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field5".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field5".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field3".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field2".to_string()).unwrap(),
+            "field1".into(),
+            "field5".into(),
+            "field5".into(),
+            "field3".into(),
+            "field2".into(),
         ];
         let opts = CsvSerializerOptions {
             fields,
@@ -419,10 +419,10 @@ mod tests {
             "field4" => Value::from("baz,bas"),
         }));
         let fields = vec![
-            ConfigTargetPath::try_from("field1".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field2".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field3".to_string()).unwrap(),
-            ConfigTargetPath::try_from("field4".to_string()).unwrap(),
+            "field1".into(),
+            "field2".into(),
+            "field3".into(),
+            "field4".into(),
         ];
 
         let mut default_bytes = BytesMut::new();
