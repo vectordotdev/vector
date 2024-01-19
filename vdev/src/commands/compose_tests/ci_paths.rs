25bae6a3b042ef0df64e2dc796535f4d8806a19a
neuronull <neuronull@pm.me>
2024-01-19 16:33:05 -0700

    ci paths

--- vdev/src/commands/compose_tests/ci_paths.rs
+++ vdev/src/commands/compose_tests/ci_paths.rs
@@ -4,12 +4,12 @@ use crate::testing::config::ComposeTestConfig;
 
 pub(crate) fn exec(path: &str) -> Result<()> {
     // placeholder for changes that should run all integration tests
-    println!("all-int: []");
+    println!("all-tests: []");
 
     // paths for each integration are defined in their respective config files.
-    for (integration, config) in ComposeTestConfig::collect_all(path)? {
+    for (test_name, config) in ComposeTestConfig::collect_all(path)? {
         if let Some(paths) = config.paths {
-            println!("{integration}:");
+            println!("{test_name}:");
             for path in paths {
                 println!("- \"{path}\"");
             }
