15a6d664a4197890f28bcc11f8c93d438b2b6569
neuronull <neuronull@pm.me>
2024-02-05 16:06:36 -0700

    wip

--- src/transforms/sample/mod.rs
+++ src/transforms/sample/mod.rs
@@ -1,19 +1,20 @@
 use vector_lib::config::{LegacyKey, LogNamespace};
 use vector_lib::configurable::configurable_component;
+use vrl::owned_value_path;
 use vrl::value::Kind;
-use vrl::{event_path, owned_value_path};
 
 use crate::{
-    conditions::{AnyCondition, Condition},
+    conditions::AnyCondition,
     config::{
         DataType, GenerateConfig, Input, OutputId, TransformConfig, TransformContext,
         TransformOutput,
     },
-    event::Event,
-    internal_events::SampleEventDiscarded,
     schema,
-    transforms::{FunctionTransform, OutputBuffer, Transform},
+    transforms::Transform,
 };
+
+mod sample;
+use sample::Sample;
 
 /// Configuration for the `sample` transform.
 #[configurable_component(transform(
@@ -63,6 +64,7 @@ impl GenerateConfig for SampleConfig {
 impl TransformConfig for SampleConfig {
     async fn build(&self, context: &TransformContext) -> crate::Result<Transform> {
         Ok(Transform::function(Sample::new(
+            Self::NAME,
             self.rate,
             self.key_field.clone(),
             self.exclude
@@ -103,99 +105,15 @@ impl TransformConfig for SampleConfig {
     }
 }
 
-#[derive(Clone)]
-pub struct Sample {
-    rate: u64,
-    key_field: Option<String>,
-    exclude: Option<Condition>,
-    count: u64,
-}
-
-impl Sample {
-    pub const fn new(rate: u64, key_field: Option<String>, exclude: Option<Condition>) -> Self {
-        Self {
-            rate,
-            key_field,
-            exclude,
-            count: 0,
-        }
-    }
-}
-
-impl FunctionTransform for Sample {
-    fn transform(&mut self, output: &mut OutputBuffer, event: Event) {
-        let mut event = {
-            if let Some(condition) = self.exclude.as_ref() {
-                let (result, event) = condition.check(event);
-                if result {
-                    output.push(event);
-                    return;
-                } else {
-                    event
-                }
-            } else {
-                event
-            }
-        };
-
-        let value = self
-            .key_field
-            .as_ref()
-            .and_then(|key_field| match &event {
-                Event::Log(event) => event
-                    .parse_path_and_get_value(key_field.as_str())
-                    .ok()
-                    .flatten(),
-                Event::Trace(event) => event
-                    .parse_path_and_get_value(key_field.as_str())
-                    .ok()
-                    .flatten(),
-                Event::Metric(_) => panic!("component can never receive metric events"),
-            })
-            .map(|v| v.to_string_lossy());
-
-        let num = if let Some(value) = value {
-            seahash::hash(value.as_bytes())
-        } else {
-            self.count
-        };
-
-        self.count = (self.count + 1) % self.rate;
-
-        if num % self.rate == 0 {
-            match event {
-                Event::Log(ref mut event) => {
-                    event.namespace().insert_source_metadata(
-                        SampleConfig::NAME,
-                        event,
-                        Some(LegacyKey::Overwrite(vrl::path!("sample_rate"))),
-                        vrl::path!("sample_rate"),
-                        self.rate.to_string(),
-                    );
-                }
-                Event::Trace(ref mut event) => {
-                    event.insert(event_path!("sample_rate"), self.rate.to_string());
-                }
-                Event::Metric(_) => panic!("component can never receive metric events"),
-            };
-            output.push(event);
-        } else {
-            emit!(SampleEventDiscarded);
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use approx::assert_relative_eq;
 
     use super::*;
     use crate::{
         conditions::{Condition, ConditionalConfig, VrlConfig},
-        config::log_schema,
-        event::{Event, LogEvent, TraceEvent},
-        test_util::{components::assert_transform_compliance, random_lines},
-        transforms::test::{create_topology, transform_one},
+        event::LogEvent,
+        test_util::components::assert_transform_compliance,
+        transforms::test::create_topology,
     };
     use tokio::sync::mpsc;
     use tokio_stream::wrappers::ReceiverStream;
@@ -214,185 +132,6 @@ mod tests {
     #[test]
     fn generate_config() {
         crate::test_util::test_generate_config::<SampleConfig>();
-    }
-
-    #[test]
-    fn hash_samples_at_roughly_the_configured_rate() {
-        let num_events = 10000;
-
-        let events = random_events(num_events);
-        let mut sampler = Sample::new(
-            2,
-            log_schema().message_key().map(ToString::to_string),
-            Some(condition_contains(
-                log_schema().message_key().unwrap().to_string().as_str(),
-                "na",
-            )),
-        );
-        let total_passed = events
-            .into_iter()
-            .filter_map(|event| {
-                let mut buf = OutputBuffer::with_capacity(1);
-                sampler.transform(&mut buf, event);
-                buf.into_events().next()
-            })
-            .count();
-        let ideal = 1.0f64 / 2.0f64;
-        let actual = total_passed as f64 / num_events as f64;
-        assert_relative_eq!(ideal, actual, epsilon = ideal * 0.5);
-
-        let events = random_events(num_events);
-        let mut sampler = Sample::new(
-            25,
-            log_schema().message_key().map(ToString::to_string),
-            Some(condition_contains(
-                log_schema().message_key().unwrap().to_string().as_str(),
-                "na",
-            )),
-        );
-        let total_passed = events
-            .into_iter()
-            .filter_map(|event| {
-                let mut buf = OutputBuffer::with_capacity(1);
-                sampler.transform(&mut buf, event);
-                buf.into_events().next()
-            })
-            .count();
-        let ideal = 1.0f64 / 25.0f64;
-        let actual = total_passed as f64 / num_events as f64;
-        assert_relative_eq!(ideal, actual, epsilon = ideal * 0.5);
-    }
-
-    #[test]
-    fn hash_consistently_samples_the_same_events() {
-        let events = random_events(1000);
-        let mut sampler = Sample::new(
-            2,
-            log_schema().message_key().map(ToString::to_string),
-            Some(condition_contains(
-                log_schema().message_key().unwrap().to_string().as_str(),
-                "na",
-            )),
-        );
-
-        let first_run = events
-            .clone()
-            .into_iter()
-            .filter_map(|event| {
-                let mut buf = OutputBuffer::with_capacity(1);
-                sampler.transform(&mut buf, event);
-                buf.into_events().next()
-            })
-            .collect::<Vec<_>>();
-        let second_run = events
-            .into_iter()
-            .filter_map(|event| {
-                let mut buf = OutputBuffer::with_capacity(1);
-                sampler.transform(&mut buf, event);
-                buf.into_events().next()
-            })
-            .collect::<Vec<_>>();
-
-        assert_eq!(first_run, second_run);
-    }
-
-    #[test]
-    fn always_passes_events_matching_pass_list() {
-        for key_field in &[None, log_schema().message_key().map(ToString::to_string)] {
-            let event = Event::Log(LogEvent::from("i am important"));
-            let mut sampler = Sample::new(
-                0,
-                key_field.clone(),
-                Some(condition_contains(
-                    log_schema().message_key().unwrap().to_string().as_str(),
-                    "important",
-                )),
-            );
-            let iterations = 0..1000;
-            let total_passed = iterations
-                .filter_map(|_| {
-                    transform_one(&mut sampler, event.clone())
-                        .map(|result| assert_eq!(result, event))
-                })
-                .count();
-            assert_eq!(total_passed, 1000);
-        }
-    }
-
-    #[test]
-    fn handles_key_field() {
-        for key_field in &[None, Some("other_field".into())] {
-            let mut event = Event::Log(LogEvent::from("nananana"));
-            let log = event.as_mut_log();
-            log.insert("other_field", "foo");
-            let mut sampler = Sample::new(
-                0,
-                key_field.clone(),
-                Some(condition_contains("other_field", "foo")),
-            );
-            let iterations = 0..1000;
-            let total_passed = iterations
-                .filter_map(|_| {
-                    transform_one(&mut sampler, event.clone())
-                        .map(|result| assert_eq!(result, event))
-                })
-                .count();
-            assert_eq!(total_passed, 1000);
-        }
-    }
-
-    #[test]
-    fn sampler_adds_sampling_rate_to_event() {
-        for key_field in &[None, log_schema().message_key().map(ToString::to_string)] {
-            let events = random_events(10000);
-            let message_key = log_schema().message_key().unwrap().to_string();
-            let mut sampler = Sample::new(
-                10,
-                key_field.clone(),
-                Some(condition_contains(&message_key, "na")),
-            );
-            let passing = events
-                .into_iter()
-                .filter(|s| !s.as_log()[&message_key].to_string_lossy().contains("na"))
-                .find_map(|event| transform_one(&mut sampler, event))
-                .unwrap();
-            assert_eq!(passing.as_log()["sample_rate"], "10".into());
-
-            let events = random_events(10000);
-            let mut sampler = Sample::new(
-                25,
-                key_field.clone(),
-                Some(condition_contains(&message_key, "na")),
-            );
-            let passing = events
-                .into_iter()
-                .filter(|s| !s.as_log()[&message_key].to_string_lossy().contains("na"))
-                .find_map(|event| transform_one(&mut sampler, event))
-                .unwrap();
-            assert_eq!(passing.as_log()["sample_rate"], "25".into());
-
-            // If the event passed the regex check, don't include the sampling rate
-            let mut sampler = Sample::new(
-                25,
-                key_field.clone(),
-                Some(condition_contains(&message_key, "na")),
-            );
-            let event = Event::Log(LogEvent::from("nananana"));
-            let passing = transform_one(&mut sampler, event).unwrap();
-            assert!(passing.as_log().get("sample_rate").is_none());
-        }
-    }
-
-    #[test]
-    fn handles_trace_event() {
-        let event: TraceEvent = LogEvent::from("trace").into();
-        let trace = Event::Trace(event);
-        let mut sampler = Sample::new(2, None, None);
-        let iterations = 0..2;
-        let total_passed = iterations
-            .filter_map(|_| transform_one(&mut sampler, trace.clone()))
-            .count();
-        assert_eq!(total_passed, 1);
     }
 
     #[tokio::test]
@@ -416,12 +155,5 @@ mod tests {
             assert_eq!(out.recv().await, None);
         })
         .await
-    }
-
-    fn random_events(n: usize) -> Vec<Event> {
-        random_lines(10)
-            .take(n)
-            .map(|e| Event::Log(LogEvent::from(e)))
-            .collect()
     }
 }
