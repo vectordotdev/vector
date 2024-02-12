ba8638825bd60eaad8abf188d060f083423cf08e
Sebastian Tia <sebastian.tia@datadoghq.com>
2024-02-12 16:40:10 -0500

    rebase

--- src/sinks/util/mod.rs
+++ src/sinks/util/mod.rs
@@ -20,7 +20,7 @@ pub mod snappy;
 pub mod socket_bytes_sink;
 pub mod statistic;
 pub mod tcp;
-#[cfg(any(test, feature = "test-util"))]
+#[cfg(any(test, feature = "test-utils"))]
 pub mod test;
 pub mod udp;
 #[cfg(all(any(feature = "sinks-socket", feature = "sinks-statsd"), unix))]
