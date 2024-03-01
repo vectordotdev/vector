82f557bef43acfaa9025bf6fa504667f79274397
Tommy Schmidt <tommy.schmidt@idealo.de>
2024-03-01 17:23:41 +0100

    feat(gh-19990): Support loading secrets from AWS Secrets Manager

--- src/secrets/mod.rs
+++ src/secrets/mod.rs
@@ -6,6 +6,7 @@ use vector_lib::configurable::{configurable_component, NamedComponent};
 
 use crate::{config::SecretBackend, signal};
 
+mod aws_secrets_manager;
 mod exec;
 mod test;
 
@@ -18,6 +19,9 @@ pub enum SecretBackends {
     /// Exec.
     Exec(exec::ExecBackend),
 
+    /// AWS Secrets Manager.
+    AwsSecretsManager(aws_secrets_manager::AwsSecretsManagerBackend),
+
     /// Test.
     #[configurable(metadata(docs::hidden))]
     Test(test::TestBackend),
@@ -28,6 +32,7 @@ impl NamedComponent for SecretBackends {
     fn get_component_name(&self) -> &'static str {
         match self {
             Self::Exec(config) => config.get_component_name(),
+            Self::AwsSecretsManager(config) => config.get_component_name(),
             Self::Test(config) => config.get_component_name(),
         }
     }
