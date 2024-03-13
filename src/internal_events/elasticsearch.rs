b55ce977b1df867bc90513f888011fa68de87357
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-03-13 08:57:41 -0700

    Reapply "Use different name for event since not part of the component event framework"
    This reverts commit 9d0b2612a5988e0b97f174fc9e511edab299b82b.

--- src/internal_events/elasticsearch.rs
+++ src/internal_events/elasticsearch.rs
@@ -2,14 +2,14 @@ use http::Response;
 use vector_lib::internal_event::InternalEvent;
 
 #[derive(Debug)]
-pub struct ElasticsearchResponseError<'a> {
+pub struct BadElasticsearchResponse<'a> {
     response: &'a Response<bytes::Bytes>,
     message: &'static str,
     error_code: String,
 }
 
 #[cfg(feature = "sinks-elasticsearch")]
-impl<'a> ElasticsearchResponseError<'a> {
+impl<'a> BadElasticsearchResponse<'a> {
     pub fn new(message: &'static str, response: &'a Response<bytes::Bytes>) -> Self {
         let error_code = super::prelude::http_error_code(response.status().as_u16());
         Self {
@@ -20,7 +20,7 @@ impl<'a> ElasticsearchResponseError<'a> {
     }
 }
 
-impl<'a> InternalEvent for ElasticsearchResponseError<'a> {
+impl<'a> InternalEvent for BadElasticsearchResponse<'a> {
     fn emit(self) {
         // Emission of Error internal event and metrics is handled upstream by the caller.
         error!(
