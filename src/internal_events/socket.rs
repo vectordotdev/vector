3dcd406181d14ff0ae5afcd30fb7e517cb858a5d
Pablo Estrada <pabloem@apache.org>
2024-04-09 10:56:20 -0400

    cargo fmt

--- src/internal_events/socket.rs
+++ src/internal_events/socket.rs
@@ -40,9 +40,7 @@ impl InternalEvent for SocketBytesReceived {
             "component_received_bytes_total", self.byte_size as u64,
             "protocol" => protocol,
         );
-        histogram!(
-            "component_received_bytes", self.byte_size as f64
-        );
+        histogram!("component_received_bytes", self.byte_size as f64);
     }
 }
 
