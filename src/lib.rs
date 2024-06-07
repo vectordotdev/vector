9c00f93bdeabf6cbf17ccfea96a0dc9599e1b596
Bruce Guenter <bruce.guenter@datadoghq.com>
2024-06-07 16:27:27 -0600

    Fix unused trait

--- src/lib.rs
+++ src/lib.rs
@@ -97,6 +97,7 @@ pub mod serde;
 #[cfg(windows)]
 pub mod service;
 pub mod signal;
+#[cfg(all(any(feature = "sinks-socket", feature = "sinks-statsd"), unix))]
 pub(crate) mod sink_ext;
 #[allow(unreachable_pub)]
 pub mod sinks;
