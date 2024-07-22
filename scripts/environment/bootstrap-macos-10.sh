a770807a0e663909e953e277483dfade6d146cf6
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-07-22 14:00:59 -0700

    Reinstall rustup
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/environment/bootstrap-macos-10.sh
+++ scripts/environment/bootstrap-macos-10.sh
@@ -12,11 +12,16 @@ brew list -1 | grep python | while read -r formula; do brew unlink "$formula"; b
 
 brew install ruby@3 coreutils cue-lang/tap/cue protobuf
 
+# rustup-init (renamed to rustup) is preinstalled on CI, but seems to be lacking the rustup binary
+# Reinstalling seems to fix it
+# TODO(jszwedko): It's possible GHA just needs to update its images and this won't be needed in the
+# future
+brew install --reinstall rustup
+
 gem install bundler
 
 echo "export PATH=\"/usr/local/opt/ruby/bin:\$PATH\"" >> "$HOME/.bash_profile"
 
 if [ -n "${CI-}" ] ; then
   echo "/usr/local/opt/ruby/bin" >> "$GITHUB_PATH"
-  echo "/opt/homebrew/opt/rustup/bin" >> "$GITHUB_PATH"
 fi
