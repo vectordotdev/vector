cd5934f2eca0610d6369d60c7841ae8ca042c74b
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-07-22 21:18:08 -0400

    chore(ci): Reinstall rustup in MacOS bootstrap (#20911)
    * chore(ci): Install rustup in MacOS bootstrap
    
    The package was renamed from rustup-init to rustup which seems to be causing some issues in CI.
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    * Ensure rustup in PATH
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    * debug
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    * Reinstall rustup
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    * Actually reinstall
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    * Remove pr trigger
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>
    
    ---------
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/environment/bootstrap-macos-10.sh
+++ scripts/environment/bootstrap-macos-10.sh
@@ -12,6 +12,12 @@ brew list -1 | grep python | while read -r formula; do brew unlink "$formula"; b
 
 brew install ruby@3 coreutils cue-lang/tap/cue protobuf
 
+# rustup-init (renamed to rustup) is already installed in GHA, but seems to be lacking the rustup binary
+# Reinstalling seems to fix it
+# TODO(jszwedko): It's possible GHA just needs to update its images and this won't be needed in the
+# future
+brew reinstall rustup
+
 gem install bundler
 
 echo "export PATH=\"/usr/local/opt/ruby/bin:\$PATH\"" >> "$HOME/.bash_profile"
