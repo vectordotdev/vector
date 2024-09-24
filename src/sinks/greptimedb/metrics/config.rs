7f5e21b1a7788f2087fc40473de42f8c10d7f772
Ning Sun <sunng@protonmail.com>
2024-09-24 15:53:48 -0700

    chore: update comments for the new option

--- src/sinks/greptimedb/metrics/config.rs
+++ src/sinks/greptimedb/metrics/config.rs
@@ -111,10 +111,22 @@ pub struct GreptimeDBMetricsConfig {
     #[configurable(derived)]
     pub tls: Option<TlsConfig>,
 
-    /// Use greptime prefixed naming for time index and value columns
+    /// Use Greptime's prefixed naming for time index and value columns.
     ///
-    /// This is to keep consistency with greptimedb's naming pattern
-    /// Default to `false` for compatibility
+    /// This is to keep consistency with GreptimeDB's naming pattern. By
+    /// default, this sink will use `val` for value column name, and `ts` for
+    /// time index name. When turned on, `greptime_value` and
+    /// `greptime_timestamp` will be used for these names.
+    ///
+    /// If you are using this Vector sink together with other data ingestion
+    /// sources of GreptimeDB, like Prometheus Remote Write and Influxdb Line
+    /// Protocol, it is highly recommended to turn on this.
+    ///
+    /// Also if there is a tag name conflict from your data source, for
+    /// example, you have a tag named as `val` or `ts`, you need to turn on
+    /// this option to avoid the conflict.
+    ///
+    /// Default to `false` for compatibility.
     #[configurable(metadata(docs::examples = false))]
     pub new_naming: Option<bool>,
 }
