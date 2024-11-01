3654eff69d7beb158600179439d338531ea690ab
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-11-01 13:32:47 -0700

    leave node_modules
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- scripts/ci-free-disk-space.sh
+++ scripts/ci-free-disk-space.sh
@@ -53,5 +53,5 @@ sudo rm -rf /usr/local/.ghcup/
 sudo rm -rf /usr/local/share/powershell
 sudo rm -rf /usr/local/share/chromium
 sudo rm -rf /usr/local/lib/android
-sudo rm -rf /usr/local/lib/node_modules
+# sudo rm -rf /usr/local/lib/node_modules # we use node
 df -h
