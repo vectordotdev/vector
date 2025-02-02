d29dda4889016970efed8a435260228472e2b3ac
chenyee.cy.huang <eric.cy.huang@linecorp.com>
2025-02-03 02:10:48 +0800

    Generate schema for secrets

--- src/secrets/mod.rs
+++ src/secrets/mod.rs
@@ -2,8 +2,9 @@
 use std::collections::{HashMap, HashSet};
 
 use enum_dispatch::enum_dispatch;
-use vector_lib::configurable::{configurable_component, NamedComponent};
+use vector_lib::configurable::configurable_component;
 
+use crate::config::GenerateConfig;
 use crate::{config::SecretBackend, signal};
 
 #[cfg(feature = "secrets-aws-secrets-manager")]
@@ -15,10 +16,11 @@ mod test;
 
 /// Configurable secret backends in Vector.
 #[allow(clippy::large_enum_variant)]
-#[configurable_component]
+#[configurable_component(global_option("secrets", "secret type"))]
 #[derive(Clone, Debug)]
 #[enum_dispatch(SecretBackend)]
 #[serde(tag = "type", rename_all = "snake_case")]
+#[configurable(metadata(docs::enum_tag_description = "secret type"))]
 pub enum SecretBackends {
     /// File.
     File(file::FileBackend),
@@ -38,16 +40,11 @@ pub enum SecretBackends {
     Test(test::TestBackend),
 }
 
-// TODO: Use `enum_dispatch` here.
-impl NamedComponent for SecretBackends {
-    fn get_component_name(&self) -> &'static str {
-        match self {
-            Self::File(config) => config.get_component_name(),
-            Self::Directory(config) => config.get_component_name(),
-            Self::Exec(config) => config.get_component_name(),
-            #[cfg(feature = "secrets-aws-secrets-manager")]
-            Self::AwsSecretsManager(config) => config.get_component_name(),
-            Self::Test(config) => config.get_component_name(),
-        }
+impl GenerateConfig for SecretBackends {
+    fn generate_config() -> toml::Value {
+        toml::Value::try_from(Self::File(file::FileBackend {
+            path: "path/to/file".into(),
+        }))
+        .unwrap()
     }
 }
