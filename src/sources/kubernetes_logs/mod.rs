16e5175d4120efee09a57e5c1ae4ddb221a87f5d
Orri Ganel <oganel@palantir.com>
2025-02-04 13:54:23 -0500

    Copy over code from file.rs to set up acknowledgements for kubernetes_logs

--- src/sources/kubernetes_logs/mod.rs
+++ src/sources/kubernetes_logs/mod.rs
@@ -21,17 +21,20 @@ use kube::{
 };
 use lifecycle::Lifecycle;
 use serde_with::serde_as;
+use tokio::sync::oneshot;
 use vector_lib::{
     EstimatedJsonEncodedSizeOf, TimeZone,
     codecs::{BytesDeserializer, BytesDeserializerConfig},
     config::{LegacyKey, LogNamespace},
     configurable::configurable_component,
+    event::{BatchNotifier, BatchStatus},
     file_source::file_server::{
         FileServer, Line, Shutdown as FileServerShutdown, calculate_ignore_before,
     },
     file_source_common::{
         Checkpointer, FingerprintStrategy, Fingerprinter, ReadFrom, ReadFromConfig,
     },
+    finalizer::OrderedFinalizer,
     internal_event::{ByteSize, BytesReceived, InternalEventHandle as _, Protocol},
     lookup::{OwnedTargetPath, lookup_v2::OptionalTargetPath, owned_value_path, path},
 };
@@ -41,8 +44,8 @@ use crate::{
     SourceSender,
     built_info::{PKG_NAME, PKG_VERSION},
     config::{
-        ComponentKey, DataType, GenerateConfig, GlobalOptions, SourceConfig, SourceContext,
-        SourceOutput, log_schema,
+        ComponentKey, DataType, GenerateConfig, GlobalOptions, SourceAcknowledgementsConfig,
+        SourceConfig, SourceContext, SourceOutput, log_schema,
     },
     event::Event,
     internal_events::{
@@ -52,9 +55,10 @@ use crate::{
         KubernetesLogsPodInfo, StreamClosedError,
     },
     kubernetes::{custom_reflector, meta_cache::MetaCache},
+    serde::bool_or_struct,
     shutdown::ShutdownSignal,
     sources,
-    sources::kubernetes_logs::partial_events_merger::merge_partial_events,
+    sources::{file::FinalizerEntry, kubernetes_logs::partial_events_merger::merge_partial_events},
     transforms::{FunctionTransform, OutputBuffer},
 };
 
@@ -278,6 +282,10 @@ pub struct Config {
     #[configurable(metadata(docs::type_unit = "seconds"))]
     #[serde(default = "default_rotate_wait", rename = "rotate_wait_secs")]
     rotate_wait: Duration,
+
+    #[configurable(derived)]
+    #[serde(default, deserialize_with = "bool_or_struct")]
+    acknowledgements: SourceAcknowledgementsConfig,
 }
 
 const fn default_read_from() -> ReadFromConfig {
@@ -326,6 +334,7 @@ impl Default for Config {
             log_namespace: None,
             internal_metrics: Default::default(),
             rotate_wait: default_rotate_wait(),
+            acknowledgements: Default::default(),
         }
     }
 }
@@ -335,7 +344,8 @@ impl Default for Config {
 impl SourceConfig for Config {
     async fn build(&self, cx: SourceContext) -> crate::Result<sources::Source> {
         let log_namespace = cx.log_namespace(self.log_namespace);
-        let source = Source::new(self, &cx.globals, &cx.key).await?;
+        let acknowledgements = cx.do_acknowledgements(self.acknowledgements);
+        let source = Source::new(self, &cx.globals, &cx.key, acknowledgements).await?;
 
         Ok(Box::pin(
             source
@@ -584,6 +594,7 @@ struct Source {
     delay_deletion: Duration,
     include_file_metric_tag: bool,
     rotate_wait: Duration,
+    acknowledgements: bool,
 }
 
 impl Source {
@@ -591,6 +602,7 @@ impl Source {
         config: &Config,
         globals: &GlobalOptions,
         key: &ComponentKey,
+        acknowledgements: bool,
     ) -> crate::Result<Self> {
         let self_node_name = if config.self_node_name.is_empty()
             || config.self_node_name == default_self_node_name_env_template()
@@ -673,6 +685,7 @@ impl Source {
             delay_deletion,
             include_file_metric_tag: config.internal_metrics.include_file_tag,
             rotate_wait: config.rotate_wait,
+            acknowledgements: acknowledgements,
         })
     }
 
@@ -710,6 +723,7 @@ impl Source {
             delay_deletion,
             include_file_metric_tag,
             rotate_wait,
+            acknowledgements,
         } = self;
 
         let mut reflectors = Vec::new();
@@ -874,6 +888,33 @@ impl Source {
 
         let (file_source_tx, file_source_rx) = futures::channel::mpsc::channel::<Vec<Line>>(2);
 
+        let (finalizer, shutdown_checkpointer) = if acknowledgements {
+            // The shutdown sent in to the finalizer is the global
+            // shutdown handle used to tell it to stop accepting new batch
+            // statuses and just wait for the remaining acks to come in.
+            let (finalizer, mut ack_stream) = OrderedFinalizer::<FinalizerEntry>::new(None);
+
+            // We set up a separate shutdown signal to tie together the
+            // finalizer and the checkpoint writer task in the file
+            // server, to make it continue to write out updated
+            // checkpoints until all the acks have come in.
+            let (send_shutdown, shutdown2) = oneshot::channel::<()>();
+            let checkpoints = checkpointer.view();
+            tokio::spawn(async move {
+                while let Some((status, entry)) = ack_stream.next().await {
+                    if status == BatchStatus::Delivered {
+                        checkpoints.update(entry.file_id, entry.offset);
+                    }
+                }
+                send_shutdown.send(())
+            });
+            (Some(finalizer), shutdown2.map(|_| ()).boxed())
+        } else {
+            // When not dealing with end-to-end acknowledgements, just
+            // clone the global shutdown to stop the checkpoint writer.
+            (None, global_shutdown.clone().map(|_| ()).boxed())
+        };
+
         let checkpoints = checkpointer.view();
         let events = file_source_rx.flat_map(futures::stream::iter);
         let bytes_received = register!(BytesReceived::from(Protocol::HTTP));
@@ -918,7 +959,18 @@ impl Source {
                 }
             }
 
-            checkpoints.update(line.file_id, line.end_offset);
+            if let Some(finalizer) = &finalizer {
+                let (batch, receiver) = BatchNotifier::new_with_receiver();
+                event = event.with_batch_notifier(&batch);
+                let entry = FinalizerEntry {
+                    file_id: line.file_id,
+                    offset: line.end_offset,
+                };
+                finalizer.add(entry, receiver);
+            } else {
+                checkpoints.update(line.file_id, line.end_offset);
+            }
+
             event
         });
 
@@ -942,7 +994,7 @@ impl Source {
         let mut lifecycle = Lifecycle::new();
         {
             let (slot, shutdown) = lifecycle.add();
-            let fut = util::run_file_server(file_server, file_source_tx, shutdown, checkpointer)
+            let fut = util::run_file_server(file_server, file_source_tx, shutdown, shutdown_checkpointer, checkpointer)
                 .map(|result| match result {
                     Ok(FileServerShutdown) => info!(message = "File server completed gracefully."),
                     Err(error) => emit!(KubernetesLifecycleError {
