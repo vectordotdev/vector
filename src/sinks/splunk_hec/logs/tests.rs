f9e6c520edfd6336eecd98488fa5ce5724d2607e
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-02-06 18:10:06 -0500

    fix(ci): splunk integration tests timeout

--- src/sinks/splunk_hec/logs/tests.rs
+++ src/sinks/splunk_hec/logs/tests.rs
@@ -71,6 +71,7 @@ fn get_processed_event_timestamp(
     timestamp_key: Option<OptionalTargetPath>,
     auto_extract_timestamp: bool,
 ) -> HecProcessedEvent {
+    // TODO: this was added to trigger tests, remove me, do not merge
     let mut event = Event::Log(LogEvent::from("hello world"));
     event
         .as_mut_log()
