64c56ed302502bae1e0f5fa29608a3616d7e90e5
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-02-12 16:25:19 +0100

    feat(sources): add custom auth strategy for components with HTTP server (#22236)
    * feat(sources)!: add custom auth strategy for components with HTTP server
    
    This adds `custom` auth strategy for components with HTTP server (`http_server`, `datadog_agent`,
    `opentelemetry`, `prometheus`) besides the default basic auth. This is a breaking change because
    `strategy` is now required for auth - for existing configurations `strategy: "basic"` needs to be
    added.
    
    Related: #22213
    
    * Generate components docs
    
    * Add changelog entry
    
    * Rename `user` to `username` to match old config
    
    * Apply docs suggestions from code review
    
    Co-authored-by: Esther Kim <esthercodes2019@gmail.com>
    
    * Update all docs based on suggestions
    
    * Prevent breaking change by defaulting to `basic` strategy for `HttpServerAuthConfig`
    
    * Remove breaking change flag on changelog entry
    
    * Add missing docs to `common/http/error`
    
    * Make tests clearer by using `panic` instead of matching enum variant
    
    * Move out `VRL` auth handling into a separate function for readability
    
    * Clean up changelog text
    
    Co-authored-by: Pavlos Rontidis <pavlos.rontidis@gmail.com>
    
    * Move test only functions in `server_auth` to `tests` module
    
    * Return serde tag for correct docs generation
    
    ---------
    
    Co-authored-by: Esther Kim <esthercodes2019@gmail.com>
    Co-authored-by: Pavlos Rontidis <pavlos.rontidis@gmail.com>

--- src/config/sink.rs
+++ src/config/sink.rs
@@ -247,6 +247,7 @@ dyn_clone::clone_trait_object!(SinkConfig);
 pub struct SinkContext {
     pub healthcheck: SinkHealthcheckOptions,
     pub globals: GlobalOptions,
+    pub enrichment_tables: vector_lib::enrichment::TableRegistry,
     pub proxy: ProxyConfig,
     pub schema: schema::Options,
     pub app_name: String,
@@ -262,6 +263,7 @@ impl Default for SinkContext {
         Self {
             healthcheck: Default::default(),
             globals: Default::default(),
+            enrichment_tables: Default::default(),
             proxy: Default::default(),
             schema: Default::default(),
             app_name: crate::get_app_name().to_string(),
