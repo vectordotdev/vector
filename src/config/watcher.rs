62fda2a4302cd7c500d18bb502ca3111a5e07b96
Guillaume Le Blanc <gleblanc@proton.me>
2025-03-13 10:30:37 +0100

    Fix testing

--- src/config/watcher.rs
+++ src/config/watcher.rs
@@ -200,12 +200,10 @@ mod tests {
         )
     }
 
-    async fn test_component_reload(
-        file: &mut File,
-        expected_component: &ComponentKey,
-        timeout: Duration,
-        mut receiver: SignalRx,
-    ) -> bool {
+    async fn test_component_reload(file: &mut File,
+                                   expected_component: &ComponentKey,
+                                   timeout: Duration,
+                                   mut receiver: SignalRx) -> bool {
         file.write_all(&[0]).unwrap();
         file.sync_all().unwrap();
 
@@ -216,60 +214,6 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn component_update() {
-        trace_init();
-
-        let delay = Duration::from_secs(3);
-        let dir = temp_dir().to_path_buf();
-        let watcher_conf = WatcherConfig::RecommendedWatcher;
-        let component_file_path = vec![dir.join("tls.cert"), dir.join("tls.key")];
-        let http_component = ComponentKey::from("http");
-
-        std::fs::create_dir(&dir).unwrap();
-
-        let mut component_files: Vec<std::fs::File> = component_file_path
-            .iter()
-            .map(|file| File::create(file).unwrap())
-            .collect();
-        let component_config =
-            ComponentConfig::new(component_file_path.clone(), http_component.clone());
-
-        let (signal_tx, signal_rx) = broadcast::channel(128);
-        spawn_thread(
-            watcher_conf,
-            signal_tx,
-            &[dir],
-            vec![component_config],
-            delay,
-        )
-        .unwrap();
-
-        let signal_rx = signal_rx.resubscribe();
-        let signal_rx2 = signal_rx.resubscribe();
-
-        if !test_component_reload(
-            &mut component_files[0],
-            &http_component,
-            delay * 5,
-            signal_rx,
-        )
-        .await
-        {
-            panic!("Test timed out");
-        }
-
-        if !test_component_reload(
-            &mut component_files[1],
-            &http_component,
-            delay * 5,
-            signal_rx2,
-        )
-        .await
-        {
-            panic!("Test timed out");
-        }
-    }
-    #[tokio::test]
     async fn file_directory_update() {
         trace_init();
 
@@ -278,10 +222,15 @@ mod tests {
         let file_path = dir.join("vector.toml");
         let watcher_conf = WatcherConfig::RecommendedWatcher;
         let component_file_path = vec![dir.join("tls.cert"), dir.join("tls.key")];
+        let http_component = ComponentKey::from("http");
+
         let component_config =
-            ComponentConfig::new(component_file_path, ComponentKey::from("http"));
+            ComponentConfig::new(component_file_path.clone(), http_component.clone());
         std::fs::create_dir(&dir).unwrap();
         let mut file = File::create(&file_path).unwrap();
+        let mut component_files: Vec<std::fs::File> = component_file_path.iter().map(|file| {
+            File::create(&file).unwrap()
+        }).collect();
 
         let (signal_tx, signal_rx) = broadcast::channel(128);
         spawn_thread(
@@ -293,7 +242,18 @@ mod tests {
         )
         .unwrap();
 
-        if !test(&mut file, delay * 5, signal_rx).await {
+        let signal_rx2 = signal_rx.resubscribe();
+        let signal_rx3 = signal_rx.resubscribe();
+
+        if !test_component_reload(&mut component_files[0], &http_component, delay * 5, signal_rx).await {
+            panic!("Test timed out");
+        }
+
+        if !test_component_reload(&mut component_files[1], &http_component, delay * 5, signal_rx2).await {
+            panic!("Test timed out");
+        }
+
+        if !test(&mut file, delay * 5, signal_rx3).await {
             panic!("Test timed out");
         }
     }
