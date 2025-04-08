cf9185f1e47bad13ae6ffbe14f2f2cbcb0c051f4
Jakub Onderka <ahoj@jakubonderka.cz>
2025-04-08 20:15:11 +0200

    enhancement(deps): Use zlib-rs for much faster zlib decoding/encoding (#22533)
    * chore: Use zlib-rs for much faster zlib decoding/encoding
    
    * fix spellchecking
    
    * rename
    
    * dd-rust-license-tool write
    
    * chore: Bump flate2 to 1.1.1 to fix linking vector on windows
    
    * test: Change vrl to zlib-rs branch to check building on windows
    
    * Revert "test: Change vrl to zlib-rs branch to check building on windows"
    
    This reverts commit 446dbc369d696a6c7e3c6ab14f45ac0f6fc85749.
    
    * cargo update -p vrl
    
    * updated licenses
    
    * fix bad rebase
    
    ---------
    
    Co-authored-by: Pavlos Rontidis <pavlos.rontidis@gmail.com>

--- lib/codecs/src/decoding/framing/chunked_gelf.rs
+++ lib/codecs/src/decoding/framing/chunked_gelf.rs
@@ -1064,7 +1064,7 @@ mod tests {
     async fn decode_malformed_zlib_message() {
         let mut compressed_payload = BytesMut::new();
         compressed_payload.extend(ZLIB_MAGIC);
-        compressed_payload.extend(&[0x9c, 0x12, 0x34, 0x56]);
+        compressed_payload.extend(&[0x9c, 0x12, 0x00, 0xFF]);
         let mut decoder = ChunkedGelfDecoder::default();
 
         let error = decoder
