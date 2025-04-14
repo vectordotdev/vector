5d974c5fad6f9274d989ea64f66dc6197259d0ec
bas smit <bas@baslab.org>
2025-04-14 16:40:36 +0200

    otel-proto: support json serialization
    Opentelemtry allows both binary protobuf as well as JSON protobuf when
    using the HTTP transport. This commit adds support.
    
    The implementation is basically stolen from opentelemetry-rust[1], so all
    go credits to them.
    
    [1]: https://github.com/open-telemetry/opentelemetry-rust

--- lib/opentelemetry-proto/build.rs
+++ lib/opentelemetry-proto/build.rs
@@ -1,9 +1,108 @@
 use std::io::Error;
 
+// NOTE: Serde related attributes are copied opentelemetry-rust
+// Source:  https://github.com/open-telemetry/opentelemetry-rust/
+// File: opentelemetry-proto/tests/grpc_build.rs
+// License: Apache-2.0
+fn set_serde_attributes(mut builder: tonic_build::Builder) -> tonic_build::Builder {
+    // Optional numeric, string and array fields need to default to their default value otherwise
+    // JSON files without those field cannot deserialize
+    // we cannot add serde(default) to all generated types because enums cannot be annotated with serde(default)
+    for path in [
+        "trace.v1.Span",
+        "trace.v1.Span.Link",
+        "trace.v1.ScopeSpans",
+        "trace.v1.ResourceSpans",
+        "common.v1.InstrumentationScope",
+        "resource.v1.Resource",
+        "trace.v1.Span.Event",
+        "trace.v1.Status",
+        "logs.v1.LogRecord",
+        "logs.v1.ScopeLogs",
+        "logs.v1.ResourceLogs",
+        "metrics.v1.Metric",
+        "metrics.v1.ResourceMetrics",
+        "metrics.v1.ScopeMetrics",
+        "metrics.v1.Gauge",
+        "metrics.v1.Sum",
+        "metrics.v1.Histogram",
+        "metrics.v1.ExponentialHistogram",
+        "metrics.v1.Summary",
+        "metrics.v1.NumberDataPoint",
+        "metrics.v1.HistogramDataPoint",
+    ] {
+        builder = builder.type_attribute(
+            path,
+            "#[cfg_attr(feature = \"with-serde\", serde(default))]",
+        )
+    }
+
+    // special serializer and deserializer for traceId and spanId
+    // OTLP/JSON format uses hex string for traceId and spanId
+    // the proto file uses bytes for traceId and spanId
+    // Thus, special serializer and deserializer are needed
+    for path in [
+        "trace.v1.Span.trace_id",
+        "trace.v1.Span.span_id",
+        "trace.v1.Span.parent_span_id",
+        "trace.v1.Span.Link.trace_id",
+        "trace.v1.Span.Link.span_id",
+        "logs.v1.LogRecord.span_id",
+        "logs.v1.LogRecord.trace_id",
+        "metrics.v1.Exemplar.span_id",
+        "metrics.v1.Exemplar.trace_id",
+    ] {
+        builder = builder
+            .field_attribute(path, "#[cfg_attr(feature = \"with-serde\", serde(serialize_with = \"crate::proto::serializers::serialize_to_hex_string\", deserialize_with = \"crate::proto::serializers::deserialize_from_hex_string\"))]")
+    }
+
+    // special serializer and deserializer for timestamp
+    // OTLP/JSON format may uses string for timestamp
+    // the proto file uses u64 for timestamp
+    // Thus, special serializer and deserializer are needed
+    for path in [
+        "trace.v1.Span.start_time_unix_nano",
+        "trace.v1.Span.end_time_unix_nano",
+        "trace.v1.Span.Event.time_unix_nano",
+        "logs.v1.LogRecord.time_unix_nano",
+        "logs.v1.LogRecord.observed_time_unix_nano",
+        "metrics.v1.HistogramDataPoint.start_time_unix_nano",
+        "metrics.v1.HistogramDataPoint.time_unix_nano",
+        "metrics.v1.NumberDataPoint.start_time_unix_nano",
+        "metrics.v1.NumberDataPoint.time_unix_nano",
+    ] {
+        builder = builder
+            .field_attribute(path, "#[cfg_attr(feature = \"with-serde\", serde(serialize_with = \"crate::proto::serializers::serialize_u64_to_string\", deserialize_with = \"crate::proto::serializers::deserialize_string_to_u64\"))]")
+    }
+
+    // flatten
+    for path in [
+        "metrics.v1.Metric.data",
+        "metrics.v1.NumberDataPoint.value",
+        "common.v1.AnyValue.value",
+    ] {
+        builder =
+            builder.field_attribute(path, "#[cfg_attr(feature =\"with-serde\", serde(flatten))]");
+    }
+    builder
+}
+
 fn main() -> Result<(), Error> {
-    tonic_build::configure()
+    let mut builder = tonic_build::configure()
         .build_client(true)
         .build_server(true)
+        .type_attribute(
+            ".",
+            "#[cfg_attr(feature = \"with-serde\", derive(serde::Serialize, serde::Deserialize))]",
+        )
+        .type_attribute(
+            ".",
+            "#[cfg_attr(feature = \"with-serde\", serde(rename_all = \"camelCase\"))]",
+        );
+
+    builder = set_serde_attributes(builder);
+
+    builder
         .compile(
             &[
                 "src/proto/opentelemetry-proto/opentelemetry/proto/common/v1/common.proto",
