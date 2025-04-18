b85fecd08a14feae3864cc8cead41b88448da088
Orri Ganel <oganel@palantir.com>
2025-04-18 14:33:33 -0400

    Fix broken merge

--- src/sources/kubernetes_logs/mod.rs
+++ src/sources/kubernetes_logs/mod.rs
@@ -2285,6 +2285,7 @@ mod tests {
                         schema_definitions: Default::default(),
                         schema: Default::default(),
                         extra_context: Default::default(),
+                        enrichment_tables: Default::default(),
                     },
                     client,
                 )
