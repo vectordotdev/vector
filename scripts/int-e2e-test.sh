bb1a44728bdd7c9c60d7bbab329f58b20151815d
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-06-09 15:30:29 -0400

    chore(e2e): output docker compose logs when in debug mode

--- scripts/int-e2e-test.sh
+++ scripts/int-e2e-test.sh
@@ -21,6 +21,14 @@ cargo vdev -v "${TEST_TYPE}" start -a "${TEST_NAME}"
 sleep 30
 cargo vdev -v "${TEST_TYPE}" test --retries 2 -a "${TEST_NAME}"
 RET=$?
+
+# Output docker compose logs when debug logging is enabled
+SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+COMPOSE_DIR="${SCRIPT_DIR}/${TEST_TYPE}/${TEST_NAME}"
+if [[ "${RUNNER_DEBUG:-}" == "true" ]]; then
+  (cd "${COMPOSE_DIR}" && docker compose logs)
+fi
+
 cargo vdev -v "${TEST_TYPE}" stop -a "${TEST_NAME}"
 
 # Only upload test results if CI is defined
