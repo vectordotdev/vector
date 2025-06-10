8219fbddf407ea1df8e676b13fe42ff1c4fb59c3
Chapman, Joe B <joe.chapman@pnnl.gov>
2025-06-10 07:57:44 -0700

    Fixed formatting issue in build_processor function. (#10870)

--- src/sinks/aws_s3/config.rs
+++ src/sinks/aws_s3/config.rs
@@ -220,10 +220,7 @@ impl S3SinkConfig {
         let request_limits = self.request.into_settings();
         let retry_logic = self.retry_logic.clone();
         let service = ServiceBuilder::new()
-            .settings(
-                request_limits,
-                retry_logic.unwrap_or_default()
-            )
+            .settings(request_limits, retry_logic.unwrap_or_default())
             .service(service);
 
         let offset = self
