96cbcb15a2615614a7a128265a0367020a488f55
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-07-24 11:57:43 -0400

    chore(dev): reduce arbitarty sleep for IT script

--- scripts/int-e2e-test.sh
+++ scripts/int-e2e-test.sh
@@ -18,7 +18,8 @@ TEST_TYPE=$1 # either "int" or "e2e"
 TEST_NAME=$2
 
 cargo vdev -v "${TEST_TYPE}" start -a "${TEST_NAME}"
-sleep 30
+# TODO this is arbitrary sleep. Investigate if it can be safely removed.
+sleep 15
 cargo vdev -v "${TEST_TYPE}" test --retries 2 -a "${TEST_NAME}"
 RET=$?
 
