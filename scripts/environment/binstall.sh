3d7d2a5db20e1945f524c24657fde28033d07112
Thomas <thomas.schneider@datadoghq.com>
2025-07-25 14:31:23 -0400

    chore(ci): use cargo binstall in prepare.sh

---
+++ scripts/environment/binstall.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+BINSTALL_VERSION="v1.14.1"
+BINSTALL_SHA256SUM_X86_64_LINUX="6102f94b57813dd89d79e562c70ce67eb20b4fb0abb6114428f55e489e641bd9"
+BINSTALL_SHA256SUM_AARCH64_LINUX="17d69bcc07a0e38c912e7f596ed71b1f5f59dc8980da59890c5bc86c07e8506a"
+BINSTALL_SHA256SUM_ARMV7_LINUX="e4ba720023e02b071aa805ae62412e94741c1bb0e0a2bb2b35896fec3d140128"
+BINSTALL_SHA256SUM_AARCH64_DARWIN="07d46d31fb68ac10b906c5d39d611ded7787966f4ed15c598cb6175b45a2b069"
+BINSTALL_SHA256SUM_X86_64_DARWIN="3de381bdcca08c418dc790d2a283711894a0577c6e55bba0d4e6cb8b0378b36"
+
+pushd "$(mktemp -d)"
+
+base_url="https://github.com/cargo-bins/cargo-binstall/releases/download/${BINSTALL_VERSION}/cargo-binstall"
+
+download() {
+  curl --retry 3 --proto '=https' --tlsv1.2 -fsSL "$@"
+}
+
+os="$(uname -s)"
+machine="$(uname -m)"
+
+if [ "$os" = "Darwin" ]; then
+  if [ "$machine" = "arm64" ]; then
+    url="${base_url}-aarch64-apple-darwin.zip"
+    download_sha256sum="${BINSTALL_SHA256SUM_AARCH64_DARWIN}"
+  elif [ "$machine" = "x86_64" ]; then
+    url="${base_url}-x86_64-apple-darwin.zip"
+    download_sha256sum="${BINSTALL_SHA256SUM_X86_64_DARWIN}"
+  else
+    echo "Unsupported OS ${os} machine ${machine}"
+    popd
+    exit 1
+  fi
+
+  download -o output.zip "$url"
+elif [ "$os" = "Linux" ]; then
+  if [ "$machine" = "armv7l" ]; then
+    target="armv7-unknown-linux-musleabihf"
+    download_sha256sum="${BINSTALL_SHA256SUM_ARMV7_LINUX}"
+  elif [ "$machine" = "aarch64" ]; then
+    target="${machine}-unknown-linux-musl"
+    download_sha256sum="${BINSTALL_SHA256SUM_AARCH64_LINUX}"
+  elif [ "$machine" = "x86_64" ]; then
+    target="${machine}-unknown-linux-musl"
+    download_sha256sum="${BINSTALL_SHA256SUM_X86_64_LINUX}"
+  else
+    echo "Unsupported OS ${os} machine ${machine}"
+    popd
+    exit 1
+  fi
+
+  url="${base_url}-${target}.tgz"
+
+  download -o output.tgz "$url"
+# elif [ "${OS-}" = "Windows_NT" ]; then
+#   target="${machine}-pc-windows-msvc"
+#   url="${base_url}-${target}.zip"
+#   download -o output.zip "$url"
+else
+    echo "Unsupported OS ${os}"
+    popd
+    exit 1
+fi
+
+echo "${download_sha256sum} $(echo output.*)" | sha256sum --check
+
+case "$(echo output.*)" in
+    *.zip) unzip output.* ;; # Cargo home is already in path
+    *.tgz) tar -xvzf output.* ;;
+    *) >&2 echo "output.* not found"; exit 1 ;;
+esac
+
+./cargo-binstall --self-install || ./cargo-binstall -y --force cargo-binstall
+
+CARGO_HOME="${CARGO_HOME:-$HOME/.cargo}"
+
+case ":$PATH:" in
+    *":$CARGO_HOME/bin:"*) ;;
+    *) needs_cargo_home=1 ;;
+esac
+
+if [ -n "${needs_cargo_home:-}" ]; then
+    if [ -n "${CI:-}" ] && [ -n "${GITHUB_PATH:-}" ]; then
+        echo "$CARGO_HOME/bin" >> "$GITHUB_PATH"
+    else
+        echo
+        printf "\033[0;31mYour path is missing %s, you might want to add it.\033[0m\n" "$CARGO_HOME/bin"
+        echo
+    fi
+fi
--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -77,8 +77,21 @@ contains_module() {
 # Always ensure git safe.directory is set
 git config --global --add safe.directory "$(pwd)"
 
-if ! contains_module rustup; then
-  REQUIRES_RUSTUP=(cargo-deb cross cargo-nextest cargo-deny cargo-msrv dd-rust-license-tool wasm-pack)
+REQUIRES_RUSTUP=(dd-rust-license-tool cargo-deb cross cargo-nextest cargo-deny cargo-msrv wasm-pack)
+
+REQUIRES_BINSTALL=("${REQUIRES_RUSTUP[@]}")
+unset -v 'REQUIRES_BINSTALL[0]' # remove dd-rust-license-tool
+require_binstall=false
+
+for tool in "${REQUIRES_BINSTALL[@]}"; do
+  if contains_module "$tool"; then
+    require_binstall=true
+    MODULES=(rustup "${MODULES[@]}")
+    break
+  fi
+done
+
+if [ "${require_binstall}" = "false" ] && ! contains_module rustup; then
   for tool in "${REQUIRES_RUSTUP[@]}"; do
     if contains_module "$tool"; then
       MODULES=(rustup "${MODULES[@]}")
@@ -87,40 +100,49 @@ if ! contains_module rustup; then
   done
 fi
 
+install="install"
 if contains_module rustup; then
   . scripts/environment/release-flags.sh
 
   rustup show active-toolchain || rustup toolchain install stable
   rustup show
-fi
 
+  if [ "${require_binstall}" = "true" ]; then
+    if cargo binstall -V &>/dev/null || ./scripts/environment/binstall.sh; then
+      install="binstall -y"
+    else
+      echo "Failed to install cargo binstall, defaulting to cargo install"
+    fi
+  fi
+fi
+set -e -o verbose
 if contains_module cargo-deb; then
   if [[ "$(cargo-deb --version 2>/dev/null)" != "2.0.2" ]]; then
-    rustup run stable cargo install cargo-deb --version 2.0.2 --force --locked
+    rustup run stable cargo $install cargo-deb --version 2.0.2 --force --locked
   fi
 fi
 
 if contains_module cross; then
   if ! cross --version 2>/dev/null | grep -q '^cross 0.2.5'; then
-    rustup run stable cargo install cross --version 0.2.5 --force --locked
+    rustup run stable cargo $install cross --version 0.2.5 --force --locked
   fi
 fi
 
 if contains_module cargo-nextest; then
   if ! cargo-nextest --version 2>/dev/null | grep -q '^cargo-nextest 0.9.95'; then
-    rustup run stable cargo install cargo-nextest --version 0.9.95 --force --locked
+    rustup run stable cargo $install cargo-nextest --version 0.9.95 --force --locked
   fi
 fi
 
 if contains_module cargo-deny; then
   if ! cargo-deny --version 2>/dev/null | grep -q '^cargo-deny 0.16.2'; then
-    rustup run stable cargo install cargo-deny --version 0.16.2 --force --locked
+    rustup run stable cargo $install cargo-deny --version 0.16.2 --force --locked
   fi
 fi
 
 if contains_module cargo-msrv; then
   if ! cargo-msrv --version 2>/dev/null | grep -q '^cargo-msrv 0.18.4'; then
-    rustup run stable cargo install cargo-msrv --version 0.18.4 --force --locked
+    rustup run stable cargo $install cargo-msrv --version 0.18.4 --force --locked
   fi
 fi
 
@@ -132,7 +154,7 @@ fi
 
 if contains_module wasm-pack; then
   if ! wasm-pack --version | grep -q '^wasm-pack 0.13.1'; then
-    rustup run stable cargo install --locked --version 0.13.1 wasm-pack
+    rustup run stable cargo $install --locked --version 0.13.1 wasm-pack
   fi
 fi
 
