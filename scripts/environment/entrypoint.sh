14e1e4700ff3e1b6f87d669a74ceca7146207b10
Thomas <thomas.schneider@datadoghq.com>
2025-07-28 12:49:32 -0400

    Fix shellcheck

--- scripts/environment/entrypoint.sh
+++ scripts/environment/entrypoint.sh
@@ -2,13 +2,15 @@
 
 # set HOSTNAME to container id for `cross`
 if [ -f /.docker-container-id ]; then
-  export HOSTNAME="$(cat /.docker-container-id)"
+  HOSTNAME="$(cat /.docker-container-id)"
+  export HOSTNAME
 fi
 
 if [ -z "$HOSTNAME" ]; then
   echo "Failed to properly set HOSTNAME, cross may not work"
   # Fallback if everything else fails
-  export HOSTNAME="$(hostname || echo init)"
+  HOSTNAME="$(hostname || echo init)"
+  export HOSTNAME
 fi
 
 exec "$@"
