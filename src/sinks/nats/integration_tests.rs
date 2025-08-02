916eb5c228dfd84e8aba3395072799343ef813e2
benjamin-awd <benjamindornel@gmail.com>
2025-08-02 16:25:32 +0800

    refactor(tests): dry nats sink config

--- src/sinks/nats/integration_tests.rs
+++ src/sinks/nats/integration_tests.rs
@@ -15,6 +15,21 @@ use crate::{
 };
 use snafu::ResultExt;
 
+fn generate_sink_config(url: &str, subject: &str) -> NatsSinkConfig {
+    NatsSinkConfig {
+        acknowledgements: Default::default(),
+        encoding: TextSerializerConfig::default().into(),
+        connection_name: "".to_owned(),
+        subject: Template::try_from(subject).unwrap(),
+        url: url.to_string(),
+        tls: None,
+        auth: None,
+        request: Default::default(),
+        jetstream: false,
+        headers: None,
+    }
+}
+
 async fn publish_and_check(conf: NatsSinkConfig) -> Result<(), NatsError> {
     // Publish `N` messages to NATS.
     //
@@ -71,17 +86,7 @@ async fn nats_no_auth() {
     let url =
         std::env::var("NATS_ADDRESS").unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: None,
-        request: Default::default(),
-        jetstream: false,
-    };
+    let conf = generate_sink_config(&url, &subject);
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -98,22 +103,13 @@ async fn nats_userpass_auth_valid() {
     let url = std::env::var("NATS_USERPASS_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::UserPassword {
-            user_password: NatsAuthUserPassword {
-                user: "natsuser".to_string(),
-                password: "natspass".to_string().into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::UserPassword {
+        user_password: NatsAuthUserPassword {
+            user: "natsuser".to_string(),
+            password: "natspass".to_string().into(),
+        },
+    });
 
     publish_and_check(conf)
         .await
@@ -128,22 +124,13 @@ async fn nats_userpass_auth_invalid() {
     let url = std::env::var("NATS_USERPASS_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::UserPassword {
-            user_password: NatsAuthUserPassword {
-                user: "natsuser".to_string(),
-                password: "wrongpass".to_string().into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::UserPassword {
+        user_password: NatsAuthUserPassword {
+            user: "natsuser".to_string(),
+            password: "wrongpass".to_string().into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -160,21 +147,12 @@ async fn nats_token_auth_valid() {
     let url = std::env::var("NATS_TOKEN_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::Token {
-            token: NatsAuthToken {
-                value: "secret".to_string().into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::Token {
+        token: NatsAuthToken {
+            value: "secret".to_string().into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -191,21 +169,12 @@ async fn nats_token_auth_invalid() {
     let url = std::env::var("NATS_TOKEN_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::Token {
-            token: NatsAuthToken {
-                value: "wrongsecret".to_string().into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::Token {
+        token: NatsAuthToken {
+            value: "wrongsecret".to_string().into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -222,22 +191,13 @@ async fn nats_nkey_auth_valid() {
     let url = std::env::var("NATS_NKEY_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::Nkey {
-            nkey: NatsAuthNKey {
-                nkey: "UD345ZYSUJQD7PNCTWQPINYSO3VH4JBSADBSYUZOBT666DRASFRAWAWT".into(),
-                seed: "SUANIRXEZUROTXNFN3TJYMT27K7ZZVMD46FRIHF6KXKS4KGNVBS57YAFGY".into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::Nkey {
+        nkey: NatsAuthNKey {
+            nkey: "UD345ZYSUJQD7PNCTWQPINYSO3VH4JBSADBSYUZOBT666DRASFRAWAWT".into(),
+            seed: "SUANIRXEZUROTXNFN3TJYMT27K7ZZVMD46FRIHF6KXKS4KGNVBS57YAFGY".into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -254,22 +214,13 @@ async fn nats_nkey_auth_invalid() {
     let url = std::env::var("NATS_NKEY_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: Some(NatsAuthConfig::Nkey {
-            nkey: NatsAuthNKey {
-                nkey: "UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".into(),
-                seed: "SBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB".into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.auth = Some(NatsAuthConfig::Nkey {
+        nkey: NatsAuthNKey {
+            nkey: "UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".into(),
+            seed: "SBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB".into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -286,23 +237,14 @@ async fn nats_tls_valid() {
     let url =
         std::env::var("NATS_TLS_ADDRESS").unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: Some(TlsEnableableConfig {
-            enabled: Some(true),
-            options: TlsConfig {
-                ca_file: Some("tests/data/nats/rootCA.pem".into()),
-                ..Default::default()
-            },
-        }),
-        auth: None,
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.tls = Some(TlsEnableableConfig {
+        enabled: Some(true),
+        options: TlsConfig {
+            ca_file: Some("tests/data/nats/rootCA.pem".into()),
+            ..Default::default()
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -319,17 +261,7 @@ async fn nats_tls_invalid() {
     let url =
         std::env::var("NATS_TLS_ADDRESS").unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: None,
-        auth: None,
-        request: Default::default(),
-        jetstream: false,
-    };
+    let conf = generate_sink_config(&url, &subject);
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -346,25 +278,16 @@ async fn nats_tls_client_cert_valid() {
     let url = std::env::var("NATS_TLS_CLIENT_CERT_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: Some(TlsEnableableConfig {
-            enabled: Some(true),
-            options: TlsConfig {
-                ca_file: Some("tests/data/nats/rootCA.pem".into()),
-                crt_file: Some("tests/data/nats/nats-client.pem".into()),
-                key_file: Some("tests/data/nats/nats-client.key".into()),
-                ..Default::default()
-            },
-        }),
-        auth: None,
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.tls = Some(TlsEnableableConfig {
+        enabled: Some(true),
+        options: TlsConfig {
+            ca_file: Some("tests/data/nats/rootCA.pem".into()),
+            crt_file: Some("tests/data/nats/nats-client.pem".into()),
+            key_file: Some("tests/data/nats/nats-client.key".into()),
+            ..Default::default()
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -381,23 +304,14 @@ async fn nats_tls_client_cert_invalid() {
     let url = std::env::var("NATS_TLS_CLIENT_CERT_ADDRESS")
         .unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: Some(TlsEnableableConfig {
-            enabled: Some(true),
-            options: TlsConfig {
-                ca_file: Some("tests/data/nats/rootCA.pem".into()),
-                ..Default::default()
-            },
-        }),
-        auth: None,
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.tls = Some(TlsEnableableConfig {
+        enabled: Some(true),
+        options: TlsConfig {
+            ca_file: Some("tests/data/nats/rootCA.pem".into()),
+            ..Default::default()
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -414,27 +328,19 @@ async fn nats_tls_jwt_auth_valid() {
     let url =
         std::env::var("NATS_JWT_ADDRESS").unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: Some(TlsEnableableConfig {
-            enabled: Some(true),
-            options: TlsConfig {
-                ca_file: Some("tests/data/nats/rootCA.pem".into()),
-                ..Default::default()
-            },
-        }),
-        auth: Some(NatsAuthConfig::CredentialsFile {
-            credentials_file: NatsAuthCredentialsFile {
-                path: "tests/data/nats/nats.creds".into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.tls = Some(TlsEnableableConfig {
+        enabled: Some(true),
+        options: TlsConfig {
+            ca_file: Some("tests/data/nats/rootCA.pem".into()),
+            ..Default::default()
+        },
+    });
+    conf.auth = Some(NatsAuthConfig::CredentialsFile {
+        credentials_file: NatsAuthCredentialsFile {
+            path: "tests/data/nats/nats.creds".into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
@@ -451,27 +357,19 @@ async fn nats_tls_jwt_auth_invalid() {
     let url =
         std::env::var("NATS_JWT_ADDRESS").unwrap_or_else(|_| String::from("nats://localhost:4222"));
 
-    let conf = NatsSinkConfig {
-        acknowledgements: Default::default(),
-        encoding: TextSerializerConfig::default().into(),
-        connection_name: "".to_owned(),
-        subject: Template::try_from(subject.as_str()).unwrap(),
-        url,
-        tls: Some(TlsEnableableConfig {
-            enabled: Some(true),
-            options: TlsConfig {
-                ca_file: Some("tests/data/nats/rootCA.pem".into()),
-                ..Default::default()
-            },
-        }),
-        auth: Some(NatsAuthConfig::CredentialsFile {
-            credentials_file: NatsAuthCredentialsFile {
-                path: "tests/data/nats/nats-bad.creds".into(),
-            },
-        }),
-        request: Default::default(),
-        jetstream: false,
-    };
+    let mut conf = generate_sink_config(&url, &subject);
+    conf.tls = Some(TlsEnableableConfig {
+        enabled: Some(true),
+        options: TlsConfig {
+            ca_file: Some("tests/data/nats/rootCA.pem".into()),
+            ..Default::default()
+        },
+    });
+    conf.auth = Some(NatsAuthConfig::CredentialsFile {
+        credentials_file: NatsAuthCredentialsFile {
+            path: "tests/data/nats/nats-bad.creds".into(),
+        },
+    });
 
     let r = publish_and_check(conf).await;
     assert!(
