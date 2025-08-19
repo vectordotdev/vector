8a253c4db28a907aea3423e5819f25e40ab9011a
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-08-19 12:13:06 -0400

    fix one windows failure

--- src/signal.rs
+++ src/signal.rs
@@ -177,7 +177,7 @@ impl SignalHandler {
 /// Signals from OS/user.
 #[cfg(unix)]
 fn os_signals(runtime: &Runtime) -> impl Stream<Item = SignalTo> + use<> {
-    use tokio::signal::unix::{SignalKind, signal};
+    use tokio::signal::unix::{signal, SignalKind};
 
     // The `signal` function must be run within the context of a Tokio runtime.
     runtime.block_on(async {
