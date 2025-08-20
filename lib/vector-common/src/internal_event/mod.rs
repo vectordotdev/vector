8373eea6c9dbee6ad37b6148d2db88feec0a6038
benjamin-awd <benjamindornel@gmail.com>
2025-08-21 01:10:36 +0800

    fix: formatting

--- lib/vector-common/src/internal_event/mod.rs
+++ lib/vector-common/src/internal_event/mod.rs
@@ -18,7 +18,7 @@ pub use bytes_sent::BytesSent;
 pub use cached_event::{RegisterTaggedInternalEvent, RegisteredEventCache};
 pub use component_events_dropped::{ComponentEventsDropped, INTENTIONAL, UNINTENTIONAL};
 pub use events_received::{EventsReceived, EventsReceivedHandle};
-pub use events_sent::{EventsSent, TaggedEventsSent, DEFAULT_OUTPUT};
+pub use events_sent::{DEFAULT_OUTPUT, EventsSent, TaggedEventsSent};
 pub use optional_tag::OptionalTag;
 pub use prelude::{error_stage, error_type};
 pub use service::{CallError, PollReadyError};
