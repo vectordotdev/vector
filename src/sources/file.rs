00c5ebc3f9e6ec5400e624222555f83fa484a3af
Thomas <thomas.schneider@datadoghq.com>
2025-08-26 12:52:04 -0400

    Revert "Revert fix"
    This reverts commit 0fcf880e644edbd5674ff00eb91bc50422c40662.

--- src/sources/file.rs
+++ src/sources/file.rs
@@ -702,11 +702,11 @@ pub fn file_source(
         });
 
         let span = info_span!("file_server");
-        tokio::spawn(async move {
+        tokio::task::spawn_blocking(move || {
             let _enter = span.enter();
-            let result = file_server
-                .run(tx, shutdown, shutdown_checkpointer, checkpointer)
-                .await;
+            let rt = tokio::runtime::Handle::current();
+            let result = rt.block_on(file_server
+                .run(tx, shutdown, shutdown_checkpointer, checkpointer));
             emit!(FileOpen { count: 0 });
             // Panic if we encounter any error originating from the file server.
             // We're at the `spawn_blocking` call, the panic will be caught and
