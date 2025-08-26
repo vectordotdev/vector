bed474c63c78df2aa0ecaa1149f3f10366d85dfd
Thomas <thomas.schneider@datadoghq.com>
2025-08-26 15:34:05 -0400

    Fix OwnedIdentity type change

--- src/amqp.rs
+++ src/amqp.rs
@@ -72,7 +72,7 @@ impl AmqpConfig {
                 };
                 let identity = if let Some(identity) = &tls.key_file {
                     let der = tokio::fs::read(identity.to_owned()).await?;
-                    Some(OwnedIdentity {
+                    Some(OwnedIdentity::PKCS12 {
                         der,
                         password: tls
                             .key_pass
