b537cd3597a3685a56b5009863fb31b08b6e5dd9
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-08-28 14:25:28 -0400

    vrl update fixes

--- lib/codecs/src/decoding/format/protobuf.rs
+++ lib/codecs/src/decoding/format/protobuf.rs
@@ -4,16 +4,16 @@ use bytes::Bytes;
 use chrono::Utc;
 use derivative::Derivative;
 use prost_reflect::{DynamicMessage, MessageDescriptor};
-use smallvec::{SmallVec, smallvec};
+use smallvec::{smallvec, SmallVec};
 use vector_config::configurable_component;
 use vector_core::event::{LogEvent, TraceEvent};
 use vector_core::{
-    config::{DataType, LogNamespace, log_schema},
+    config::{log_schema, DataType, LogNamespace},
     event::Event,
     schema,
 };
 use vrl::protobuf::descriptor::{get_message_descriptor, get_message_descriptor_from_bytes};
-use vrl::protobuf::parse::{Options, proto_to_value};
+use vrl::protobuf::parse::{proto_to_value, Options};
 use vrl::value::{Kind, Value};
 
 use super::Deserializer;
@@ -189,7 +189,7 @@ mod tests {
     ) {
         let input = Bytes::from(protobuf_bin_message);
         let message_descriptor =
-            vrl::protobuf::get_message_descriptor(&protobuf_desc_path, message_type).unwrap();
+            get_message_descriptor(&protobuf_desc_path, message_type).unwrap();
         let deserializer = ProtobufDeserializer::new(message_descriptor);
 
         for namespace in [LogNamespace::Legacy, LogNamespace::Vector] {
@@ -285,7 +285,7 @@ mod tests {
     #[test]
     fn deserialize_error_invalid_protobuf() {
         let input = Bytes::from("{ foo");
-        let message_descriptor = vrl::protobuf::get_message_descriptor(
+        let message_descriptor = get_message_descriptor(
             &test_data_dir().join("protos/test_protobuf.desc"),
             "test_protobuf.Person",
         )
