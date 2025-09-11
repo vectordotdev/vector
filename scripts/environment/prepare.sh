fdf02b543e2e0d529a1f4312555ae0d8ef88851e
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-09-11 16:26:08 -0400

    install rustup

--- scripts/environment/prepare.sh
+++ scripts/environment/prepare.sh
@@ -108,6 +108,11 @@ fi
 
 install=(install)
 if contains_module rustup; then
+  if ! command -v rustup >/dev/null 2>&1; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
+    source "$HOME/.cargo/env"
+  fi
+
   . "${SCRIPT_DIR}"/release-flags.sh
 
   rustup show active-toolchain || rustup toolchain install stable
