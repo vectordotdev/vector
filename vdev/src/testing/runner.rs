a84bb9e6f2270f2753033a5e5f5caedc1480c729
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-09-19 10:12:26 -0400

    ran cargo fmt

--- vdev/src/testing/runner.rs
+++ vdev/src/testing/runner.rs
@@ -27,8 +27,7 @@ const TEST_COMMAND: &[&str] = &[
     "--no-default-features",
 ];
 // The upstream container we publish artifacts to on a successful master build.
-const UPSTREAM_IMAGE: &str =
-    "docker.io/timberio/vector-dev:latest";
+const UPSTREAM_IMAGE: &str = "docker.io/timberio/vector-dev:latest";
 
 pub enum RunnerState {
     Running,
