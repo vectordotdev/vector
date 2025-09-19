e1cd39c78e439a8cb054aef69782cc00524ddb11
Thomas <thomas.schneider@datadoghq.com>
2025-09-19 16:21:33 -0400

    feat(dev): enable colors when running in nextest (#23819)
    * feat(dev): enable colors when running in nextest
    
    * Fix and simplify colors
    
    * Remove CI check
    
    * Make test fail
    
    * Set CARGO_TERM_COLOR always in test.yml
    
    * Revert "Make test fail"
    
    This reverts commit bf6acf2b1f1241e1ed76be03da3881329b20084b.

--- src/test_util/mod.rs
+++ src/test_util/mod.rs
@@ -139,6 +139,10 @@ pub fn trace_init() {
     let color = {
         use std::io::IsTerminal;
         std::io::stdout().is_terminal()
+            || std::env::var("NEXTEST")
+                .ok()
+                .and(Some(true))
+                .unwrap_or(false)
     };
     // Windows: ANSI colors are not supported by cmd.exe
     // Color is false for everything except unix.
