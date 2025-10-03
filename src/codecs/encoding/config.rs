5fa6aee2198d692a8ef03adf4284fc1e80b57f53
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-10-03 09:24:15 -0400

    reverting most changes, will go with a codec approach

--- src/codecs/encoding/config.rs
+++ src/codecs/encoding/config.rs
@@ -10,7 +10,7 @@ use crate::codecs::Transformer;
 
 /// Encoding configuration.
 #[configurable_component]
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 /// Configures how events are encoded into raw bytes.
 /// The selected encoding also determines which input types (logs, metrics, traces) are supported.
 pub struct EncodingConfig {
@@ -60,7 +60,7 @@ where
 
 /// Encoding configuration.
 #[configurable_component]
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct EncodingConfigWithFraming {
     #[configurable(derived)]
