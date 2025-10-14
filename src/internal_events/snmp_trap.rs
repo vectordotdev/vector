a93d78b11ce9b67c6dfbb9358067d986e6e8aa34
bachgarash <bakhtiyar.garashov@zendesk.com>
2025-10-15 00:18:36 +0300

    feat: initial implementation for snmp v1 and v2 traps #4567
    This PR contains initial implementation for SNMP source to log v1 and v2 version traps.

---
+++ src/internal_events/snmp_trap.rs
@@ -0,0 +1,27 @@
+use metrics::counter;
+use vector_lib::internal_event::InternalEvent;
+use vector_lib::internal_event::{error_stage, error_type};
+
+#[derive(Debug)]
+pub struct SnmpTrapParseError {
+    pub error: String,
+}
+
+impl InternalEvent for SnmpTrapParseError {
+    fn emit(self) {
+        error!(
+            message = "Error parsing SNMP trap.",
+            error = %self.error,
+            error_type = error_type::PARSER_FAILED,
+            stage = error_stage::PROCESSING,
+            internal_log_rate_limit = true,
+        );
+        counter!(
+            "component_errors_total",
+            "error_type" => error_type::PARSER_FAILED,
+            "stage" => error_stage::PROCESSING,
+        )
+        .increment(1);
+    }
+}
+
