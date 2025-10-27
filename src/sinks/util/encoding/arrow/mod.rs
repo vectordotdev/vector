56c4b298edcb2f5d86a17c528678eb96999f34a7
benjamin-awd <benjamindornel@gmail.com>
2025-10-27 21:31:08 +0800

    chore: remove redundant docstring

--- src/sinks/util/encoding/arrow/mod.rs
+++ src/sinks/util/encoding/arrow/mod.rs
@@ -1,8 +1,3 @@
-//! Arrow encoding utilities for Vector sinks.
-//!
-//! This module provides generic Arrow encoding functionality that can be used
-//! by any sink that supports Apache Arrow format (ClickHouse, Snowflake, Databricks, etc.).
-
 mod encoder;
 
 pub use encoder::{ArrowEncodingError, encode_events_to_arrow_stream};
