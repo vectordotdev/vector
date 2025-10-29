f2315cc64e8d1060254610600ec723f3dcb989b5
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-10-29 12:57:00 -0400

    generate Cargo.lock

--- vdev/src/commands/fmt.rs
+++ vdev/src/commands/fmt.rs
@@ -11,9 +11,6 @@ impl Cli {
     pub fn exec(self) -> Result<()> {
         app::exec("scripts/check-style.sh", ["--fix"], true)?;
         app::exec("cargo", ["fmt", "--all"], true)?;
-
-        // Format vdev (standalone crate)
-        info!("Formatting vdev...");
         app::exec("cargo", ["fmt", "--manifest-path=vdev/Cargo.toml", "--all"], true)
     }
 }
