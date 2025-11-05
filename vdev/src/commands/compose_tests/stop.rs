b456dbd62b9e5f16b9fb5a2c0a6992ed0212e2c1
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-11-05 16:27:44 -0500

    chore(vdev): rework build-all arg

--- vdev/src/commands/compose_tests/stop.rs
+++ vdev/src/commands/compose_tests/stop.rs
@@ -7,17 +7,14 @@ use crate::testing::{
 
 use super::active_projects::find_active_environment_for_integration;
 
-pub(crate) fn exec(
-    local_config: ComposeTestLocalConfig,
-    test_name: &str,
-    all_features: bool,
-) -> Result<()> {
+pub(crate) fn exec(local_config: ComposeTestLocalConfig, test_name: &str) -> Result<()> {
     let (_test_dir, config) = ComposeTestConfig::load(local_config.directory, test_name)?;
     let active_environment =
         find_active_environment_for_integration(local_config.directory, test_name, &config)?;
 
     if let Some(environment) = active_environment {
-        ComposeTest::generate(local_config, test_name, environment, all_features, 0)?.stop()
+        // all_features doesn't matter for stop since we always use the same container name now
+        ComposeTest::generate(local_config, test_name, environment, false, 0)?.stop()
     } else {
         println!("No environment for {test_name} is active.");
         Ok(())
