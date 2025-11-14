47efca249944c05953b8ba4c8eff6d2a9d1f599f
Thomas <thomas.schneider@datadoghq.com>
2025-11-14 14:25:37 -0500

    Run file_start_position_server_restart_acknowledged and file_start_position_server_restart_no_acknowledge on non-linux

--- src/sources/file.rs
+++ src/sources/file.rs
@@ -1552,19 +1552,16 @@ mod tests {
         }
     }
 
-    #[cfg(target_os = "linux")] // see #7988
     #[tokio::test]
     async fn file_start_position_server_restart_acknowledged() {
         file_start_position_server_restart(Acks).await
     }
 
-    #[cfg(target_os = "linux")] // see #7988
     #[tokio::test]
     async fn file_start_position_server_restart_no_acknowledge() {
         file_start_position_server_restart(NoAcks).await
     }
 
-    #[cfg(target_os = "linux")] // see #7988
     async fn file_start_position_server_restart(acking: AckingMode) {
         let dir = tempdir().unwrap();
         let config = file::FileConfig {
