aaece39a101e02eb5a44246e11cac1d5be4eca87
Karol Chrapek <kchrapek@zendesk.com>
2025-12-29 14:06:41 +0100

    Add event's timestamp aggregation option

--- src/internal_events/aggregate.rs
+++ src/internal_events/aggregate.rs
@@ -1,6 +1,6 @@
 use metrics::counter;
 use vector_lib::NamedInternalEvent;
-use vector_lib::internal_event::InternalEvent;
+use vector_lib::internal_event::{ComponentEventsDropped, INTENTIONAL, InternalEvent};
 
 #[derive(Debug, NamedInternalEvent)]
 pub struct AggregateEventRecorded;
@@ -26,5 +26,19 @@ pub struct AggregateUpdateFailed;
 impl InternalEvent for AggregateUpdateFailed {
     fn emit(self) {
         counter!("aggregate_failed_updates").increment(1);
+    }
+}
+
+#[derive(Debug, NamedInternalEvent)]
+pub struct AggregateEventDropped {
+    pub reason: &'static str,
+}
+
+impl InternalEvent for AggregateEventDropped {
+    fn emit(self) {
+        emit!(ComponentEventsDropped::<INTENTIONAL> {
+            count: 1,
+            reason: self.reason,
+        });
     }
 }
