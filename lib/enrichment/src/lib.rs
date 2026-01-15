b022918ace224ccb861b252f84e262f0f3221f15
Thomas <thomas.schneider@datadoghq.com>
2026-01-15 17:18:49 -0500

    Add usage method to VRL functions with descriptions from cue files

--- lib/enrichment/src/lib.rs
+++ lib/enrichment/src/lib.rs
@@ -9,6 +9,7 @@ mod test_util;
 mod vrl_util;
 
 use dyn_clone::DynClone;
+use indoc::indoc;
 pub use tables::{TableRegistry, TableSearch};
 use vrl::{
     compiler::Function,
@@ -97,3 +98,59 @@ pub fn vrl_functions() -> Vec<Box<dyn Function>> {
         Box::new(find_enrichment_table_records::FindEnrichmentTableRecords) as _,
     ]
 }
+
+pub(crate) const ENRICHMENT_TABLE_EXPLAINER: &str = indoc! {r#"
+    For `file` enrichment tables, this condition needs to be a VRL object in which
+    the key-value pairs indicate a field to search mapped to a value to search in that field.
+    This function returns the rows that match the provided condition(s). _All_ fields need to
+    match for rows to be returned; if any fields do not match, then no rows are returned.
+
+    There are currently three forms of search criteria:
+
+    1. **Exact match search**. The given field must match the value exactly. Case sensitivity
+       can be specified using the `case_sensitive` argument. An exact match search can use an
+       index directly into the dataset, which should make this search fairly "cheap" from a
+       performance perspective.
+
+    2. **Wildcard match search**. The given fields specified by the exact match search may also
+        be matched exactly to the value provided to the `wildcard` parameter.
+        A wildcard match search can also use an index directly into the dataset.
+
+    3. **Date range search**. The given field must be greater than or equal to the `from` date
+       and/or less than or equal to the `to` date. A date range search involves
+       sequentially scanning through the rows that have been located using any exact match
+       criteria. This can be an expensive operation if there are many rows returned by any exact
+       match criteria. Therefore, use date ranges as the _only_ criteria when the enrichment
+       data set is very small.
+
+    For `geoip` and `mmdb` enrichment tables, this condition needs to be a VRL object with a single key-value pair
+    whose value needs to be a valid IP address. Example: `{"ip": .ip }`. If a return field is expected
+    and without a value, `null` is used. This table can return the following fields:
+
+    * ISP databases:
+        * `autonomous_system_number`
+        * `autonomous_system_organization`
+        * `isp`
+        * `organization`
+
+    * City databases:
+        * `city_name`
+        * `continent_code`
+        * `country_code`
+        * `country_name`
+        * `region_code`
+        * `region_name`
+        * `metro_code`
+        * `latitude`
+        * `longitude`
+        * `postal_code`
+        * `timezone`
+
+    * Connection-Type databases:
+        * `connection_type`
+
+    To use this function, you need to update your configuration to
+    include an
+    [`enrichment_tables`](/docs/reference/configuration/global-options/#enrichment_tables)
+    parameter.
+"#};
