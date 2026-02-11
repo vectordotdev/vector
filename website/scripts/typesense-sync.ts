de8437f3da512b545e3e47e4935e9bd9dd1e1e2f
Reda El Issati <reda.elissati@datadoghq.com>
2026-02-11 15:05:24 +0100

    feat: upgrade typesense-sync to be v30 compatible

--- website/scripts/typesense-sync.ts
+++ website/scripts/typesense-sync.ts
@@ -1,19 +1,18 @@
-const { typesenseSync } = require('typesense-sync');
-const { saveSettings } = require('typesense-sync/settings');
-const tsConfig = require('../typesense.config.json');
+// Will be running in the CI/CD pipeline
+import process from 'node:process';
+import { typesenseSync } from 'typesense-sync';
+const configFilePath = './typesense.config.json';
 
-const syncCollection = async () => {
-  const promises: Promise<any>[] = []
+const config = {
+  configFilePath,
+  shouldSaveSettings: true,
+};
 
-  for (const collection of tsConfig.collections) {
-    console.log(`Updating collection ${collection.name}`)
-    promises.push(typesenseSync(collection.name, collection.file_path))
-  }
-
-  return await Promise.all(promises)
-}
-
-saveSettings()
-  .then(() => syncCollection())
-  .then(() => console.log('Typesense sync completed'))
-  .catch(error => console.log('An error occurred', error))
+typesenseSync(config)
+  .then(() => {
+    console.log('Typesense sync completed');
+  })
+  .catch((err) => {
+    console.error('An error occurred', err);
+    process.exit(1);
+  });
