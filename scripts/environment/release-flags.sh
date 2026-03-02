d2c8669342d45314514b52f46b2bda596b8e9663
Thomas <thomas.schneider@datadoghq.com>
2026-03-02 15:18:30 -0500

    Revert "chore(dev): add codegen-units = 1 in Cargo.toml"
    This reverts commit dfdcf2589f69f5c270af1f9e4b10d2d2b46507f0.

--- scripts/environment/release-flags.sh
+++ scripts/environment/release-flags.sh
@@ -2,13 +2,13 @@
 set -e -o verbose
 
 # We want to ensure we're building using "full" release capabilities when possible, which
-# means full LTO.  This maximizes performance of the resulting code, but increases
-# compilation time.  We only set this if we're in CI _and_ we haven't been instructed to
-# use the debug profile (via PROFILE environment variable).
-# Note: codegen-units=1 is set directly in Cargo.toml's [profile.release].
+# means full LTO and a single codegen unit.  This maximizes performance of the resulting
+# code, but increases compilation time.  We only set this if we're in CI _and_ we haven't
+# been instructed to use the debug profile (via PROFILE environment variable).
 if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
   {
     echo "CARGO_PROFILE_RELEASE_LTO=fat";
+    echo "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1";
     echo "CARGO_PROFILE_RELEASE_DEBUG=false";
   } >> "${GITHUB_ENV}"
 fi
