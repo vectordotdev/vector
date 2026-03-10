748f9173340544e7302edad60fcf96de9f645ebf
Thomas <thomas.schneider@datadoghq.com>
2026-03-10 12:23:22 -0400

    Fix macro order

--- src/config/api.rs
+++ src/config/api.rs
@@ -4,10 +4,10 @@ use url::Url;
 use vector_lib::configurable::configurable_component;
 
 /// API options.
+#[configurable_component]
 #[configurable(metadata(
     docs::warnings = "The API has no authentication and exposes all event data flowing through Vector. It must not be exposed to untrusted clients."
 ))]
-#[configurable_component]
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[serde(default, deny_unknown_fields)]
 pub struct Options {
