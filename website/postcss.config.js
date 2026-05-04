1a96f3b5749df570dc2e19ba0205dc92910938a2
Thomas <thomas.schneider@datadoghq.com>
2026-05-04 16:18:47 -0400

    chore(website): upgrade Tailwind CSS from v2 to v3

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,64 +1,11 @@
 import postcssImport from "postcss-import";
 import tailwindCss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import purgecssPlugin from "@fullhuman/postcss-purgecss";
-
-// These are classes for things that are applied by JS, and thus missed by Hugo.
-// See assets/js/*.js for places where this happens.
-const safeClasses = {
-  standard: [
-    "search-input",
-    "search-results-list",
-    "search-result",
-    "code-sample-filename",
-    "type",
-    // All Algolia-specific classes
-    /^ais-/,
-    /^aa-/,
-    /^DocSearch/,
-    // Search widgets (TODO: improve this by consolidating these into higher classes)
-    "block",
-    "border-gray-200",
-    "border-gray-300",
-    "border-r",
-    "border-t",
-    "font-medium",
-    "h-2",
-    "h-3",
-    "h-full",
-    "inline",
-    "leading-relaxed",
-    "mb-1",
-    "ml-1",
-    "mr-1",
-    "p-2",
-    "p-4",
-    "pl-2",
-    "py-4",
-    "text-gray-600",
-    "text-gray-800",
-    "text-md",
-    "text-sm",
-    "w-2",
-    "w-3"
-  ]
-};
-
-const purgecss = purgecssPlugin({
-  content: ["./hugo_stats.json"],
-  safelist: safeClasses,
-  defaultExtractor: (content) => {
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-    return broadMatches.concat(innerMatches);
-  }
-});
 
 export default {
   plugins: [
     postcssImport,
     tailwindCss,
     autoprefixer,
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : [])
   ]
 };
