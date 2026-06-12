0799fec3eccab50daa32acb32204e26f969d5290
Thomas <thomas.schneider@datadoghq.com>
2026-06-12 16:04:51 -0400

    fix(clippy): allow GzipDecoder::new inside gzip_multiple_decoder itself

--- lib/vector-common/src/compression.rs
+++ lib/vector-common/src/compression.rs
@@ -1,6 +1,7 @@
 use async_compression::tokio::bufread::GzipDecoder;
 use tokio::io::AsyncBufRead;
 
+#[allow(clippy::disallowed_methods)]
 pub fn gzip_multiple_decoder<R: AsyncBufRead>(reader: R) -> GzipDecoder<R> {
     let mut decoder = GzipDecoder::new(reader);
     decoder.multiple_members(true);
