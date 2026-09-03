813b07ed17315a673260c5b4cbadc4a44c1c7b66
Jansen <jansen.wenberg@datadoghq.com>
2026-09-03 17:11:28 -0400

    refactor(buffers): isolate disk sender retries

--- lib/vector-buffers/src/topology/channel/mod.rs
+++ lib/vector-buffers/src/topology/channel/mod.rs
@@ -1,7 +1,12 @@
+mod disk_v2_sender;
 mod limited_queue;
 mod receiver;
 mod sender;
 
+#[cfg(test)]
+pub(crate) use disk_v2_sender::CapacityBlockedHook;
+
+pub use disk_v2_sender::DiskV2Sender;
 pub use limited_queue::{
     BufferChannelKind, ChannelMetricMetadata, DEFAULT_EWMA_HALF_LIFE_SECONDS, LimitedReceiver,
     LimitedSender, SendError, limited,
@@ -9,5 +14,7 @@ pub use limited_queue::{
 pub use receiver::*;
 pub use sender::*;
 
+#[cfg(test)]
+mod disk_v2_sender_tests;
 #[cfg(test)]
 mod tests;
